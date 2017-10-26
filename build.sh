291c75a3c9530e67e813f5cf779c587019334874
Ondřej Čertík <ondrej@certik.us>
2017-10-26 15:23:16 -0600

    Add a build script for Python

--- build.sh
+++ build.sh
@@ -4,17 +4,5 @@ set -e
 set -x
 
 antlr4="java org.antlr.v4.Tool"
-grun="java org.antlr.v4.gui.TestRig"
 
-$antlr4 fortran.g4
-javac fortran*.java
-
-# Test:
-$grun fortran module -tree examples/m1.f90
-echo
-$grun fortran module -tree examples/subroutine1.f90
-echo
-$grun fortran module -tree examples/expr1.f90
-
-# Visualize
-#grun fortran module -gui examples/m1.f90
+$antlr4 -Dlanguage=Python3 fortran.g4
---
+++ build_java.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+antlr4="java org.antlr.v4.Tool"
+grun="java org.antlr.v4.gui.TestRig"
+
+$antlr4 fortran.g4
+javac fortran*.java
+
+# Test:
+$grun fortran module -tree examples/m1.f90
+echo
+$grun fortran module -tree examples/subroutine1.f90
+echo
+$grun fortran module -tree examples/expr1.f90
+
+# Visualize
+#grun fortran module -gui examples/m1.f90
