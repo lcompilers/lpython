81b217287256af01abe4945a61c4dc818a571156
Ondřej Čertík <ondrej@certik.us>
2017-10-26 16:22:58 -0600

    Test fortran sources in files

---
+++ tests/test_files.py
@@ -0,0 +1,10 @@
+from glob import glob
+from fortran_parser import parse_file
+
+def test_files():
+    filenames = glob("examples/*.f90")
+    assert len(filenames) == 3
+    print("Testing filenames:")
+    for filename in filenames:
+        print(filename)
+        assert parse_file(filename)
