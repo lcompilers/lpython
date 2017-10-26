77aac067847e1488ff8fdee2d34390f2c8a49fbb
Ondřej Čertík <ondrej@certik.us>
2017-10-26 16:03:24 -0600

    Refactor add error handling and tests

--- fortran_parser.py
+++ fortran_parser.py
@@ -1,15 +1,36 @@
 import antlr4
+from antlr4.error.ErrorListener import ErrorListener
 from fortranParser import fortranParser
 from fortranLexer import fortranLexer
 from fortranListener import fortranListener
 
-def test_fortran_parser():
-    stream = antlr4.FileStream("examples/m1.f90")
+class SyntaxErrorException(Exception):
+    pass
+
+class VerboseListener(ErrorListener) :
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        stack = recognizer.getRuleInvocationStack()
+        stack.reverse()
+        print("rule stack: ", str(stack))
+        print("line", line, ":", column, "at", offendingSymbol, ":", msg)
+        raise SyntaxErrorException("Syntax error.")
+
+def parse(source, rule="module"):
+    stream = antlr4.InputStream(source)
     lexer = fortranLexer(stream)
     tokens = antlr4.CommonTokenStream(lexer)
     parser = fortranParser(tokens)
-    tree = parser.module()
-    print(tree.toStringTree(recog=parser))
+    parser.removeErrorListeners()
+    parser.addErrorListener(VerboseListener())
+    try:
+        tree = getattr(parser, rule)()
+    except SyntaxErrorException:
+        return None
+    return tree.toStringTree(recog=parser)
+
+def parse_file(filename):
+    source = open(filename).read()
+    return parse(source)
 
 if __name__ == "__main__":
-    test_fortran_parser()
+    print(parse_file("examples/m1.f90"))
---
+++ tests/test_expr.py
@@ -0,0 +1,16 @@
+from fortran_parser import parse
+
+def test_var():
+    r = "var_sym_decl"
+    assert(parse("x", r))
+    assert(parse("yx", r))
+    assert(not parse("1x", r))
+    assert(not parse("2", r))
+
+def test_expr1():
+    r = "expr"
+    assert(parse("1", r))
+    assert(parse("x+y", r))
+    assert(parse("(1+3)*4", r))
+    assert(parse("1+3*4", r))
+    assert(not parse("(1+", r))
