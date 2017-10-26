539fa5ff06100e558a003d0424a3839baf7d92e9
Ondřej Čertík <ondrej@certik.us>
2017-10-26 16:28:53 -0600

    Add more thorough tests for expressions

--- tests/test_expr.py
+++ tests/test_expr.py
@@ -14,3 +14,56 @@ def test_expr1():
     assert parse("(1+3)*4", r)
     assert parse("1+3*4", r)
     assert not parse("(1+", r)
+
+def test_expr2():
+    r = "subroutine"
+    assert parse("""\
+subroutine a
+integer :: a, b
+a = 1+2*3
+b = (1+2+a)*3
+end subroutine
+""", r)
+    assert not parse("""\
+subroutine a
+integer :: a, b
+a = 1+2*3
+b = (1x+2+a)*3
+end subroutine
+""", r)
+    assert parse("""\
+subroutine a
+integer :: b
+b = 1+2*3
+end subroutine
+""", r)
+    assert not parse("""\
+subroutine a
+integer :: b
+b = 1+2*
+end subroutine
+""", r)
+    assert not parse("""\
+subroutine a
+integer :: b
+b = 1+
+end subroutine
+""", r)
+    assert parse("""\
+subroutine a
+integer :: b
+b = f(3)+6
+end subroutine
+""", r)
+    assert not parse("""\
+subroutine a
+integer :: b
+b = f(3+6
+end subroutine
+""", r)
+    assert parse("""\
+subroutine a
+integer :: b
+b = f(3+6)
+end subroutine
+""", r)
