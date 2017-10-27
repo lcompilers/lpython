a3d4d8dedee53b5869395a5bdae42ea15c80a02b
Ondřej Čertík <ondrej@certik.us>
2017-10-26 20:47:47 -0600

    Get the random.f90 working

--- tests/test_files.py
+++ tests/test_files.py
@@ -3,7 +3,7 @@ from fortran_parser import parse_file
 
 def test_files():
     filenames = glob("examples/*.f90")
-    assert len(filenames) == 4
+    assert len(filenames) == 5
     print("Testing filenames:")
     for filename in filenames:
         print(filename)
