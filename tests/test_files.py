068270c5fdfa2fc4069c9cc44287a534104a4c7d
Ondřej Čertík <ondrej@certik.us>
2017-10-30 23:00:28 -0600

    Add a Fortran compiler
    Use the grammar from ANTLR4 and use llvmlite to emit LLVM instructions and to
    compile to object files. Link object files with ld.

--- tests/test_files.py
+++ tests/test_files.py
@@ -3,7 +3,7 @@ from fortran_parser import parse_file
 
 def test_files():
     filenames = glob("examples/*.f90")
-    assert len(filenames) == 8
+    assert len(filenames) == 9
     print("Testing filenames:")
     for filename in filenames:
         print(filename)
