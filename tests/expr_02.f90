f6493625c2ed891557383c037aa58c8fca887758
Ondřej Čertík <ondrej@certik.us>
2017-11-01 11:37:33 -0600

    Add more expression tests

--- tests/expr_02.f90
+++ tests/expr_02.f90
@@ -3,4 +3,13 @@ implicit none
 integer :: x
 x = (2+3)*5
 if (x /= 25) stop 1
+
+x = (2+3)*4
+if (x /= 20) stop 1
+
+x = (2+3)*(2+3)
+if (x /= 25) stop 1
+
+x = (2+3)*(2+3)*4*2*(1+2)
+if (x /= 600) stop 1
 end program
