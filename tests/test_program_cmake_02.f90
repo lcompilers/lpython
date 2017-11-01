62a66b4d13968650f359404b2b4b3dff66926dba
Ondřej Čertík <ondrej@certik.us>
2017-10-31 22:07:04 -0600

    Add another cmake test program

---
+++ tests/test_program_cmake_02.f90
@@ -0,0 +1,5 @@
+! This is a test program that CMake uses to check that the Fortran compiler can
+! compile Fortran 90 code.
+      PROGRAM TESTFortran90
+      integer stop ; stop = 1 ; do while ( stop .eq. 0 ) ; end do
+      END PROGRAM TESTFortran90
