367b67599cdc4d7146d729baeda68064c8e385ff
Ondřej Čertík <ondrej@certik.us>
2017-11-01 12:49:05 -0600

    Add more tests

--- tests/variables_01.f90
+++ tests/variables_01.f90
@@ -14,4 +14,21 @@ y = 1
 if (y == 1) x = 1
 if (y /= 1) x = 2
 if (x /= 1) stop 1
+
+y = 2
+if (y == 1) x = 1
+if (y /= 1) x = 2
+if (x /= 2) stop 1
+
+x = 2
+y = 1
+if (y == 1) x = x + 1
+if (y /= 1) x = x - 1
+if (x /= 3) stop 1
+
+x = 2
+y = 2
+if (y == 1) x = x + 1
+if (y /= 1) x = x - 1
+if (x /= 1) stop 1
 end program
