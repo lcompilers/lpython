8f4872722ac735e12e10c232f5a668057658d317
Ondřej Čertík <ondrej@certik.us>
2017-11-03 12:32:58 -0600

    Add to_tuple from Python

--- liblfort/ast/tests/test_parser.py
+++ liblfort/ast/tests/test_parser.py
@@ -1,5 +1,20 @@
 from liblfort.ast import parse, dump
 
+def to_tuple(t):
+    if t is None or isinstance(t, (str, int, complex)):
+        return t
+    elif isinstance(t, list):
+        return [to_tuple(e) for e in t]
+    result = [t.__class__.__name__]
+    if hasattr(t, 'lineno') and hasattr(t, 'col_offset'):
+        result.append((t.lineno, t.col_offset))
+    if t._fields is None:
+        return tuple(result)
+    for f in t._fields:
+        result.append(to_tuple(getattr(t, f)))
+    return tuple(result)
+
+
 def test_dump_expr():
     assert dump(parse("1+1")) == \
             "BinOp(left=Num(n='1'), op=Add(), right=Num(n='1'))"
@@ -8,6 +23,25 @@ def test_dump_expr():
     assert dump(parse("(x+y)**2")) == \
             "BinOp(left=BinOp(left=Name(id='x'), op=Add(), " \
             "right=Name(id='y')), op=Pow(), right=Num(n='2'))"
+
+def test_to_tuple():
+    ast_tree = parse("2+3")
+    t = to_tuple(ast_tree)
+    t_ref = ('BinOp', (1, 1), ('Num', (1, 1), '2'), ('Add',), ('Num', (1, 1),
+        '3'))
+    assert t == t_ref
+
+    ast_tree = parse("2+x")
+    t = to_tuple(ast_tree)
+    t_ref = ('BinOp', (1, 1), ('Num', (1, 1), '2'), ('Add',), ('Name', (1, 1),
+        'x'))
+    assert t == t_ref
+
+    ast_tree = parse("(x+y)**2")
+    t = to_tuple(ast_tree)
+    t_ref = ('BinOp', (1, 1), ('BinOp', (1, 1), ('Name', (1, 1), 'x'),
+        ('Add',), ('Name', (1, 1), 'y')), ('Pow',), ('Num', (1, 1), '2'))
+    assert t == t_ref
 
 def test_dump_statements():
     assert dump(parse("if (x == 1) stop\n")) == \
