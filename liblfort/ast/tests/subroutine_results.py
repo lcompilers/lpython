bb7ae0941dacddb782b996e53c8f917ccf6ee7ee
Ondřej Čertík <ondrej@certik.us>
2017-11-03 15:20:45 -0600

    Add subroutine tests

---
+++ liblfort/ast/tests/subroutine_results.py
@@ -0,0 +1,5 @@
+results = [
+    ('Assignment', (1, 1), 'r', ('Num', (1, 1), '1')),
+    ('Assignment', (1, 1), 'r', ('Num', (1, 1), '1')),
+    ('Assignment', (1, 1), 'f', ('Num', (1, 1), '1')),
+]
