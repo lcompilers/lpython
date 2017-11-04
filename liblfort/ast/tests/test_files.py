65b0cf9b5ea4245e28f8ccf5e23cbef6111ddb06
Ondřej Čertík <ondrej@certik.us>
2017-11-04 09:26:27 -0600

    Blacklist filenames that do not work yet

--- liblfort/ast/tests/test_files.py
+++ liblfort/ast/tests/test_files.py
@@ -3,11 +3,13 @@ import os
 
 from liblfort.ast import parse_file
 
-def _test_files():
+def test_files():
     here = os.path.dirname(__file__)
     filenames = glob(os.path.join(here, "..", "..", "..", "examples", "*.f90"))
     assert len(filenames) == 9
     print("Testing filenames:")
+    skip = ["bsplines.f90", "main_rand.f90", "random.f90"]
     for filename in filenames:
+        if os.path.basename(filename) in skip: continue
         print(filename)
         parse_file(filename)
