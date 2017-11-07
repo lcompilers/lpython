75452119cf0711bbcfffa2b183a704a5614dddd3
Ondřej Čertík <ondrej@certik.us>
2017-11-07 16:54:56 -0700

    Fix the AST imports

--- liblfort/ast/utils.py
+++ liblfort/ast/utils.py
@@ -90,9 +90,9 @@ class NodeVisitor(object):
         for field, value in iter_fields(node):
             if isinstance(value, list):
                 for item in value:
-                    if isinstance(item, AST):
+                    if isinstance(item, ast.AST):
                         self.visit(item)
-            elif isinstance(value, AST):
+            elif isinstance(value, ast.AST):
                 self.visit(value)
 
 
@@ -137,16 +137,16 @@ class NodeTransformer(NodeVisitor):
             if isinstance(old_value, list):
                 new_values = []
                 for value in old_value:
-                    if isinstance(value, AST):
+                    if isinstance(value, ast.AST):
                         value = self.visit(value)
                         if value is None:
                             continue
-                        elif not isinstance(value, AST):
+                        elif not isinstance(value, ast.AST):
                             new_values.extend(value)
                             continue
                     new_values.append(value)
                 old_value[:] = new_values
-            elif isinstance(old_value, AST):
+            elif isinstance(old_value, ast.AST):
                 new_node = self.visit(old_value)
                 if new_node is None:
                     delattr(node, field)
