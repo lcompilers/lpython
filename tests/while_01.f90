a823115e7ffb2bc8226c477ceb4b9ed7f3108907
Ondřej Čertík <ondrej@certik.us>
2017-11-07 10:18:15 -0700

    Add few more tests

--- tests/while_01.f90
+++ tests/while_01.f90
@@ -8,4 +8,23 @@ do while (i < 11)
     i = i + 1
 end do
 if (j /= 55) error stop
+if (i /= 11) error stop
+
+i = 1
+j = 0
+do while (i <= 10)
+    j = j + i
+    i = i + 1
+end do
+if (j /= 55) error stop
+if (i /= 11) error stop
+
+i = 1
+j = 0
+do while (i < 1)
+    j = j + i
+    i = i + 1
+end do
+if (j /= 0) error stop
+if (i /= 1) error stop
 end
