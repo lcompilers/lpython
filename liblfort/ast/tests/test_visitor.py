6afaab82edc624f788a20d788d5314da9f2b6787
Ondřej Čertík <ondrej@certik.us>
2017-11-07 16:39:11 -0700

    Add a test for NodeVisitor

--- liblfort/ast/tests/test_visitor.py
+++ liblfort/ast/tests/test_visitor.py
@@ -40,3 +40,44 @@ end module
     assert v.get_subroutine_list() == """\
 subroutine sub1(a, b)
 subroutine sub2(c)"""
+
+class SubroutinesVisitor2(ast.utils.NodeVisitor):
+
+    def __init__(self):
+        self.subroutine_list = []
+
+    def visit_Subroutine(self, node):
+        self.subroutine_list.append("subroutine %s(%s)" % (node.name,
+            ", ".join([x.arg for x in node.args])))
+        self.visit_sequence(node.body)
+
+    def get_subroutine_list(self):
+        return "\n".join(self.subroutine_list)
+
+
+def test_list_subroutines2():
+    source = """\
+module subroutine1
+implicit none
+
+contains
+
+subroutine sub1(a, b)
+integer, intent(in) :: a
+integer, intent(out) :: b
+b = a + 1
+end subroutine
+
+subroutine sub2(c)
+integer, intent(out) :: c
+call sub1(8, c)
+end subroutine
+
+end module
+"""
+    tree = ast.parse(source)
+    v = SubroutinesVisitor()
+    v.visit(tree)
+    assert v.get_subroutine_list() == """\
+subroutine sub1(a, b)
+subroutine sub2(c)"""
