58e46b006a7c1c8a120a004c2da479de2047a93e
Ondřej Čertík <ondrej@certik.us>
2017-11-07 21:42:10 -0700

    More tests

--- tests/doloop_01.f90
+++ tests/doloop_01.f90
@@ -38,13 +38,31 @@ end do
 if (j /= 22) error stop
 
 j = 0
+do i = 10, 1, -3
+    j = j + i
+end do
+if (j /= 22) error stop
+
+j = 0
 do i = 1, 1
     j = j + i
 end do
 if (j /= 1) error stop
 
 j = 0
+do i = 1, 1, -1
+    j = j + i
+end do
+if (j /= 1) error stop
+
+j = 0
 do i = 1, 0
+    j = j + i
+end do
+if (j /= 0) error stop
+
+j = 0
+do i = 0, 1, -1
     j = j + i
 end do
 if (j /= 0) error stop
