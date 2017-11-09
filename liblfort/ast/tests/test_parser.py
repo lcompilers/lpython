ccce8d0809dd3e8a2f20222aec3b2ebec923d130
Ondřej Čertík <ondrej@certik.us>
2017-11-09 09:07:32 -0700

    Use different dimensions in the test

--- liblfort/ast/tests/test_parser.py
+++ liblfort/ast/tests/test_parser.py
@@ -384,15 +384,15 @@ def test_decl():
     tests = [
         "integer x",
         "integer :: x",
-        "integer :: a(10,10), b(10)",
-        "integer, dimension(10,10) :: c",
+        "integer :: a(9,10), b(10)",
+        "integer, dimension(9,10) :: c",
         "integer, dimension(:,:), intent(in) :: e",
         "integer(c_int) :: i",
 
         "real a",
         "real :: a",
         "real(dp) :: a, b",
-        "real(dp) :: a(10,10), b(10)",
+        "real(dp) :: a(9,10), b(10)",
         "real(dp) :: a(m,n)",
         "real(dp), allocatable :: a(:,:)",
         "real(dp) y = 5",
