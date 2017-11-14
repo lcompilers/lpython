68dc275d3bf63b5fbd64dd5ebf2727d11efdc0a3
Ondřej Čertík <ondrej@certik.us>
2017-11-13 20:42:34 -0700

    Add more tests

--- tests/subroutines_01.f90
+++ tests/subroutines_01.f90
@@ -5,7 +5,23 @@ i = 1
 j = 1
 if (j /= 1) error stop
 call f(i, j)
+if (i /= 1) error stop
 if (j /= 2) error stop
+
+j = 1
+if (j /= 1) error stop
+call f(3, j)
+if (j /= 4) error stop
+
+j = 1
+if (j /= 1) error stop
+call f(1+2, j)
+if (j /= 4) error stop
+
+j = 1
+if (j /= 1) error stop
+call f(i+2, j)
+if (j /= 4) error stop
 
 contains
 
