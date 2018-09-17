49f6d803d02f6913b1df19e40ac99e7d7e3ab477
Ondřej Čertík <ondrej@certik.us>
2018-09-16 22:20:13 -0600

    Update the prompt.py to only use the evaluator

--- prompt.py
+++ prompt.py
@@ -32,48 +32,20 @@ def handle_input(engine, evaluator, source):
     print_bold("Input:")
     print(source)
 
-    print()
-    try:
-        ast_tree = parse(source, translation_unit=False)
-    except SyntaxErrorException as err:
-        print(str(err))
-        return
-    print_bold("AST:")
-    print(dump(ast_tree))
-
-    print()
-    symbol_table = create_symbol_table(ast_tree)
-    print_bold("Symbol table:")
-    print(symbol_table)
-
-    #print()
-    annotate_tree(ast_tree, symbol_table)
-    # TODO: implement pretty printing of the annotated tree
-    #print("Annotated tree:")
-    #print(ast_tree)
-
-    print()
-    # TODO: use the generated source_ll
-    #module = codegen(ast_tree, symbol_table)
-    #source_ll = str(module)
     result = evaluator.evaluate(source)
+
+    print()
+    print_bold("AST:")
+    print(dump(evaluator.ast_tree))
+
+    print()
+    print_bold("Symbol table:")
+    print(evaluator.symbol_table)
+
+    print()
     print_bold("LLVM IR:")
     print(evaluator._source_ll)
 
-    # NOTE: This shows how to execute the function and how to pass data in and
-    # out. The idea is that if "ast_tree" is a:
-    # * Fortran expression (`3+3`): wrapped into a function with no input, but
-    #   the output will be the correct type of the expression, gets executed
-    #   and the result printed out
-    # * Fortran statement (`x=3+3` or `call fn()`): then it gets wrapped into a
-    #   subroutine with no input and no output, and executed. This works for
-    #   the function call `call fn()`, but when local variables are modified as
-    #   in `x=3+3`, we need to save the variable into "global" variables. We
-    #   should pretend, that "Fortran statements" are simply part of a
-    #   "Program", that gets executed line by line.
-    # * Subroutine/Module: gets loaded into the engine, but not executed
-    # * Program: gets compiled and executed.
-    # For anything else, we get an error.
     print_bold("Result:")
     print(result)
 
