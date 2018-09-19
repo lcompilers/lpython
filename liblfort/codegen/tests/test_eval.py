e499eb66c4e53d7ab8ace4958791fc2d6d37a4da
Ondřej Čertík <ondrej@certik.us>
2018-09-19 13:52:45 -0600

    Add more tests

--- liblfort/codegen/tests/test_eval.py
+++ liblfort/codegen/tests/test_eval.py
@@ -101,6 +101,60 @@ define void @inc()
     e.voidfn("inc")
     assert e.intfn("f1") == 7
 
+    e.add_module("""\
+@count = external global i64
+
+define void @inc2()
+{
+  %1 = load i64, i64* @count
+  %2 = add i64 %1, 2
+  store i64 %2, i64* @count
+  ret void
+}
+""")
+    assert e.intfn("f1") == 7
+    e.voidfn("inc2")
+    assert e.intfn("f1") == 9
+    e.voidfn("inc")
+    assert e.intfn("f1") == 10
+    e.voidfn("inc2")
+    assert e.intfn("f1") == 12
+
+    # Test that we can have another independent LLVMEvaluator and use both at
+    # the same time:
+    e2 = LLVMEvaluator()
+    e2.add_module("""\
+@count = global i64 5
+
+define i64 @f1()
+{
+  %1 = load i64, i64* @count
+  ret i64 %1
+}
+
+define void @inc()
+{
+  %1 = load i64, i64* @count
+  %2 = add i64 %1, 1
+  store i64 %2, i64* @count
+  ret void
+}
+""")
+    assert e2.intfn("f1") == 5
+    e2.voidfn("inc")
+    assert e2.intfn("f1") == 6
+    e2.voidfn("inc")
+    assert e2.intfn("f1") == 7
+
+    assert e.intfn("f1") == 12
+    e2.voidfn("inc")
+    assert e2.intfn("f1") == 8
+    assert e.intfn("f1") == 12
+    e.voidfn("inc")
+    assert e2.intfn("f1") == 8
+    assert e.intfn("f1") == 13
+
+
 
 
 # Fortran
