19f579a18963bf6f8596a7e7effe08bd621832ec
Ondřej Čertík <ondrej@certik.us>
2018-09-30 09:48:18 -0600

    Install scripts

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://gitlab.com/lfortran/lfortran",
     packages=setuptools.find_packages(),
+    scripts=['lfort', 'prompt.py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
