eb757a80e1df1d089fdd1a2bf237c4ed8f8353e4
Ondřej Čertík <ondrej@certik.us>
2018-10-08 12:18:55 -0600

    Make the version.sh work without CI as well

--- ci/version.sh
+++ ci/version.sh
@@ -3,12 +3,15 @@
 set -e
 set -x
 
-if [[ $CI_COMMIT_TAG == "" ]]; then
+COMMIT_TAG=$(git tag -l --points-at HEAD)
+COMMIT_SHA=$(git rev-parse HEAD)
+
+if [[ $COMMIT_TAG == "" ]]; then
     # The commit is not tagged, use the first 7 chars of git commit hash
-    version=${CI_COMMIT_SHA:0:7}
+    version=${COMMIT_SHA:0:7}
 else
     # The commit is tagged, convert tag version properly:v1.0.1 -> "1.0.1"
-    version=${CI_COMMIT_TAG:1}
+    version=${COMMIT_TAG:1}
 fi
 
-echo ${version} > ../version
+echo ${version} > version
