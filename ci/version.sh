0d302b10737d9c3b12e3a48c57edac43aec6f7c7
Ondřej Čertík <ondrej@certik.us>
2018-11-04 12:00:29 -0700

    Use PEP 440 compatible versions

--- ci/version.sh
+++ ci/version.sh
@@ -21,7 +21,7 @@ fi
 
 if [[ $COMMIT_TAG == "" ]]; then
     # The commit is not tagged, use the first 7 chars of git commit hash
-    version=${COMMIT_SHA:0:7}
+    version="0.0+git.${COMMIT_SHA:0:7}"
 else
     # The commit is tagged, convert tag version properly:v1.0.1 -> "1.0.1"
     version=${COMMIT_TAG:1}
