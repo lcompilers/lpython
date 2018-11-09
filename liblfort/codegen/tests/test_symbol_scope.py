9590439c1e6943c0c1be6b3b5429a0ec5e8a43bf
Ondřej Čertík <ondrej@certik.us>
2018-11-09 16:43:03 -0700

    Test global variable

--- liblfort/codegen/tests/test_symbol_scope.py
+++ liblfort/codegen/tests/test_symbol_scope.py
@@ -1,6 +1,6 @@
 from liblfort.codegen.evaluator import FortranEvaluator
 
-def test_vars1():
+def test_fn_dummy():
     e = FortranEvaluator()
     e.evaluate("""\
 function f(a)
@@ -9,3 +9,17 @@ f = a + 1
 end function
 """)
     assert e.evaluate("f(2)") == 3
+
+def test_fn_global():
+    e = FortranEvaluator()
+    e.evaluate("integer :: b")
+    e.evaluate("b = 5")
+    e.evaluate("""\
+function f2(a)
+integer :: a
+f2 = a + b
+end function
+""")
+    assert e.evaluate("f2(2)") == 7
+    e.evaluate("b = 6")
+    assert e.evaluate("f2(2)") == 8
