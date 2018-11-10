d9603b37859b252c6e4fd8da2690963a4039abea
Ondřej Čertík <ondrej@certik.us>
2018-11-10 16:14:40 -0700

    Build shared library

--- build1.sh
+++ build1.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 # Compile LFort runtime library
-CFLAGS="-Wall -g"
+CFLAGS="-Wall -g -fPIC"
 gcc $CFLAGS -o lfort_intrinsics.o -c lfort_intrinsics.c
 ar rcs liblfort.a lfort_intrinsics.o
+gcc -shared -o liblfort.so lfort_intrinsics.o
