2c93d6cba0b3dff51b399799d5d358649193323b
Ondřej Čertík <ondrej@certik.us>
2018-11-17 11:23:53 -0700

    Add a test for LLD

---
+++ liblfort/tests/test_lld.py
@@ -0,0 +1,40 @@
+from subprocess import call
+
+import llvmlite.binding as llvm
+
+def test_linux64_program():
+    """
+    This is the simplest assembly program that uses Linux 64bit syscall to
+    return an exit value.
+    """
+    source_ll = r"""
+define void @syscall1(i64, i64) {
+    %3 = alloca i64, align 8
+    %4 = alloca i64, align 8
+    store i64 %0, i64* %3, align 8
+    store i64 %1, i64* %4, align 8
+    call void asm sideeffect "movq $0, %rax\0Amovq $1, %rdi\0Asyscall", "*m,*m,~{rax},~{rdi},~{dirflag},~{fpsr},~{flags}"(i64* %3, i64* %4)
+    ret void
+}
+
+define void @_start() {
+    call void @syscall1(i64 60, i64 42)
+    ret void
+}
+    """
+    objfile = "test1.o"
+    binfile = "test1"
+
+    llvm.initialize()
+    llvm.initialize_native_asmprinter()
+    llvm.initialize_native_asmparser()
+    llvm.initialize_native_target()
+    target = llvm.Target.from_triple(llvm.get_default_triple())
+    target_machine = target.create_target_machine()
+    mod = llvm.parse_assembly(source_ll)
+    mod.verify()
+    with open(objfile, "wb") as o:
+        o.write(target_machine.emit_object(mod))
+    llvm.lld_main(["ld.lld", "-o", binfile, objfile])
+    r = call("./%s" % binfile)
+    assert r == 42
