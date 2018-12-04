a6bff04644529f99c95d5f9cfadbc31ad8246b1b
Ondřej Čertík <ondrej@certik.us>
2018-12-03 23:35:19 -0700

    Add a test for case sensitivity

---
+++ liblfort/ast/tests/case_sensitivity_results.py
@@ -0,0 +1,13 @@
+results = [
+    ('Declaration', (1, 1), [('decl', 'x', 'Integer', [])]),
+    ('Declaration', (1, 1), [('decl', 'x', 'INTEGER', [])]),
+    ('Declaration', (1, 1), [('decl', 'x', 'iNteger', [])]),
+    ('Declaration', (1, 1), [('decl', 'c', 'integeR', [])]),
+    ('Declaration', (1, 1), [('decl', 'e', 'integer', [])]),
+    ('Declaration', (1, 1), [('decl', 'i', 'integer', [])]),
+    ('Declaration', (1, 1), [('decl', 'a', 'Real', [])]),
+    ('Declaration', (1, 1), [('decl', 'a', 'REAL', [])]),
+    ('Declaration', (1, 1), [('decl', 'a', 'real', [('dimension', None, None), ('dimension', None, None)])]),
+    ('Declaration', (1, 1), [('decl', 'c', 'cHAracter', [])]),
+    ('Declaration', (1, 1), [('decl', 'x', 'tYPe', []), ('decl', 'y', 'tYPe', [])]),
+]
