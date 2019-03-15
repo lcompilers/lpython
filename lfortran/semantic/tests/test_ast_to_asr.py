c76ab7e3998a4a64ce823620ade2bbdc86505439
Ondřej Čertík <ondrej@certik.us>
2019-03-14 21:17:34 -0600

    Add another test

--- lfortran/semantic/tests/test_ast_to_asr.py
+++ lfortran/semantic/tests/test_ast_to_asr.py
@@ -36,4 +36,32 @@ end module
     assert fn1.body[0].target == fn1.return_var
     assert fn1.body[0].value.left == fn1.args[0]
     assert fn1.body[0].value.right == fn1.args[1]
+
+def test_function2():
+    source = """\
+module test
+implicit none
+contains
+
+    integer function fn1(a, b) result(r)
+    integer, intent(in) :: a, b
+    r = a + b
+    end function
+
+end module
+"""
+    ast = parse(source)
+    asrepr = ast_to_asr(ast)
+    verify_asr(asrepr)
+    assert 'test' in asrepr.global_scope.symbols
+    m = asrepr.global_scope.symbols['test']
+    assert isinstance(m, asr.Module)
+    fn1 = m.symtab.symbols["fn1"]
+    assert isinstance(fn1, asr.Function)
+    assert fn1.args[0].name == "a"
+    assert fn1.args[1].name == "b"
+    assert fn1.return_var.name == "r"
+    assert fn1.body[0].target == fn1.return_var
+    assert fn1.body[0].value.left == fn1.args[0]
+    assert fn1.body[0].value.right == fn1.args[1]
     print(print_fortran(asr_to_ast(m)))
