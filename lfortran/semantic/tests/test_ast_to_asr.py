790b563463502fb191fc08eaa93e3f2353e3518f
Ondřej Čertík <ondrej@certik.us>
2019-03-14 22:00:06 -0600

    Parse individual function

--- lfortran/semantic/tests/test_ast_to_asr.py
+++ lfortran/semantic/tests/test_ast_to_asr.py
@@ -40,24 +40,17 @@ end module
 
 def test_function2():
     source = """\
-module test
-implicit none
-contains
-
-    integer function fn1(a, b) result(r)
-    integer, intent(in) :: a, b
-    r = a + b
-    end function
-
-end module
+integer function fn1(a, b) result(r)
+integer, intent(in) :: a, b
+r = a + b
+end function
 """
-    ast = parse(source)
+    ast = parse(source, translation_unit=False)
     asrepr = ast_to_asr(ast)
     verify_asr(asrepr)
-    assert 'test' in asrepr.global_scope.symbols
-    m = asrepr.global_scope.symbols['test']
-    assert isinstance(m, asr.Module)
-    fn1 = m.symtab.symbols["fn1"]
+    assert isinstance(asrepr, asr.TranslationUnit)
+    assert 'fn1' in asrepr.global_scope.symbols
+    fn1 = asrepr.global_scope.symbols['fn1']
     assert isinstance(fn1, asr.Function)
     assert fn1.args[0].name == "a"
     assert fn1.args[1].name == "b"
@@ -65,4 +58,4 @@ end module
     assert fn1.body[0].target == fn1.return_var
     assert fn1.body[0].value.left == fn1.args[0]
     assert fn1.body[0].value.right == fn1.args[1]
-    print(print_fortran(asr_to_ast(m)))
+    print(print_fortran(asr_to_ast(fn1)))
