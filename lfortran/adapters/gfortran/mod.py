9a50280c6a5ef39df9f3dc33623a4720f1806442
Ondřej Čertík <ondrej@certik.us>
2019-03-17 21:27:05 -0600

    Make gfortran.mod produce ASR directly

--- lfortran/adapters/gfortran/mod.py
+++ lfortran/adapters/gfortran/mod.py
@@ -58,10 +58,114 @@ expression (represented by Expr).
 The `public_symbols` variable contains the list of public module symbols,
 represented by the PublicSymbol namedtuple.
 
+
+Update: Use the `mod_to_asr()` function that returns LFortran's ASR directly.
+That way all the semantics is already in the (verified) ASR, and one can use
+all the tooling around ASR right away without needing to learn about some
+internal representation in this module that is specific to GFortran.
+
 """
 
 from collections import namedtuple
 import gzip
+
+from lfortran.asr import asr
+from lfortran.asr.asr_check import verify_asr
+from lfortran.asr.builder import (make_translation_unit,
+    translation_unit_make_module, scope_add_function, make_type_integer,
+    make_type_real, type_eq, make_binop, scope_add_symbol)
+
+
+def string_to_type(stype, kind=None):
+    """
+    Converts a string `stype` and a numerical `kind` to an ASR type.
+    """
+    if stype == "integer":
+        return make_type_integer(kind)
+    elif stype == "real":
+        return make_type_real(kind)
+    elif stype == "complex":
+        raise NotImplementedError("Complex not implemented")
+    elif stype == "character":
+        raise NotImplementedError("Complex not implemented")
+    elif stype == "logical":
+        raise NotImplementedError("Complex not implemented")
+    raise Exception("Unknown type")
+
+def tofortran_bound(symtab, lscope, b):
+    if isinstance(b, Integer):
+        return asr.Num(n=b.i, type=make_type_integer())
+    elif isinstance(b, VarIdx):
+        v = symtab[b.idx]
+        w = lscope.resolve(v.name)
+        assert type_eq(w.type, string_to_type(v.type))
+        return w
+    raise NotImplementedError("Unsupported bound type")
+
+def convert_arg(table, lscope, idx):
+    arg = table[idx]
+    assert isinstance(arg.name, str)
+    assert isinstance(arg.type, str)
+    a = asr.Variable(name=arg.name, type=string_to_type(arg.type), dummy=True)
+    assert isinstance(arg.intent, str)
+    a.intent = arg.intent
+
+    if arg.bounds:
+        dims = []
+        for bound in arg.bounds:
+            lb, ub = bound
+            if lb:
+                lb = tofortran_bound(table, lscope, lb)
+            if ub:
+                ub = tofortran_bound(table, lscope, ub)
+            dims.append(asr.dimension(lb, ub))
+        a.type.dims = dims
+    return a
+
+def convert_function(symtab, table, f):
+    assert isinstance(f, Procedure)
+    assert isinstance(f.name, str)
+    assert isinstance(f.type, str)
+    return_var = asr.Variable(name=f.name, type=string_to_type(f.type))
+    lf = scope_add_function(symtab, f.name, return_var=return_var)
+    args = []
+    for arg in f.args:
+        assert isinstance(arg, VarIdx)
+        larg = convert_arg(table, lf.symtab, arg.idx)
+        scope_add_symbol(lf.symtab, larg)
+        args.append(larg)
+    lf.args = args
+
+def convert_module(table, public_symbols):
+    # Determine the module name
+    module_name = None
+    for sym in public_symbols:
+        s = table[sym.idx.idx]
+        if isinstance(s, Module):
+            # Skip modules if they are listed in public symbols
+            continue
+        assert isinstance(s, Procedure)
+        if module_name:
+            assert module_name == s.mod
+        else:
+            module_name = s.mod
+
+
+    u = make_translation_unit()
+    m = translation_unit_make_module(u, module_name)
+
+    # Convert functions
+    for sym in public_symbols:
+        s = table[sym.idx.idx]
+        if isinstance(s, Module):
+            continue
+        assert isinstance(s, Procedure)
+        convert_function(m.symtab, table, s)
+
+    return u
+
+
+
 
 class GFortranModuleParseError(Exception):
     pass
@@ -302,3 +406,9 @@ def load_module(filename):
         return version, orig_file, symtab_dict, symtree_list
     else:
         raise GFortranModuleParseError("Unsupported version")
+
+def mod_to_asr(filename):
+    version, orig_file, table, public_symbols = load_module(filename)
+    u = convert_module(table, public_symbols)
+    verify_asr(u)
+    return u
--- tests/interop/wrap2.py
+++ tests/interop/wrap2.py
@@ -11,104 +11,14 @@ sys.path.append("../..")
 
 from lfortran.asr import asr
 from lfortran.asr.asr_check import verify_asr
+from lfortran.asr.asr_to_ast import asr_to_ast
+from lfortran.ast.ast_to_src import ast_to_src
+from lfortran.adapters.gfortran.mod import mod_to_asr
 from lfortran.asr.builder import (make_translation_unit,
     translation_unit_make_module, scope_add_function, make_type_integer,
     make_type_real, type_eq, make_binop, scope_add_symbol)
-from lfortran.asr.asr_to_ast import asr_to_ast
-from lfortran.ast.ast_to_src import ast_to_src
-import lfortran.adapters.gfortran.mod as gp
 
-def string_to_type(stype, kind=None):
-    """
-    Converts a string `stype` and a numerical `kind` to an ASR type.
-    """
-    if stype == "integer":
-        return make_type_integer(kind)
-    elif stype == "real":
-        return make_type_real(kind)
-    elif stype == "complex":
-        raise NotImplementedError("Complex not implemented")
-    elif stype == "character":
-        raise NotImplementedError("Complex not implemented")
-    elif stype == "logical":
-        raise NotImplementedError("Complex not implemented")
-    raise Exception("Unknown type")
-
-def tofortran_bound(symtab, lscope, b):
-    if isinstance(b, gp.Integer):
-        return asr.Num(n=b.i, type=make_type_integer())
-    elif isinstance(b, gp.VarIdx):
-        v = symtab[b.idx]
-        w = lscope.resolve(v.name)
-        assert type_eq(w.type, string_to_type(v.type))
-        return w
-    raise NotImplementedError("Unsupported bound type")
-
-def convert_arg(table, lscope, idx):
-    arg = table[idx]
-    assert isinstance(arg.name, str)
-    assert isinstance(arg.type, str)
-    a = asr.Variable(name=arg.name, type=string_to_type(arg.type), dummy=True)
-    assert isinstance(arg.intent, str)
-    a.intent = arg.intent
-
-    if arg.bounds:
-        dims = []
-        for bound in arg.bounds:
-            lb, ub = bound
-            if lb:
-                lb = tofortran_bound(table, lscope, lb)
-            if ub:
-                ub = tofortran_bound(table, lscope, ub)
-            dims.append(asr.dimension(lb, ub))
-        a.type.dims = dims
-    return a
-
-def convert_function(symtab, table, f):
-    assert isinstance(f, gp.Procedure)
-    assert isinstance(f.name, str)
-    assert isinstance(f.type, str)
-    return_var = asr.Variable(name=f.name, type=string_to_type(f.type))
-    lf = scope_add_function(symtab, f.name, return_var=return_var)
-    args = []
-    for arg in f.args:
-        assert isinstance(arg, gp.VarIdx)
-        larg = convert_arg(table, lf.symtab, arg.idx)
-        scope_add_symbol(lf.symtab, larg)
-        args.append(larg)
-    lf.args = args
-
-def convert_module(table, public_symbols):
-    # Determine the module name
-    module_name = None
-    for sym in public_symbols:
-        s = table[sym.idx.idx]
-        if isinstance(s, gp.Module):
-            # Skip modules if they are listed in public symbols
-            continue
-        assert isinstance(s, gp.Procedure)
-        if module_name:
-            assert module_name == s.mod
-        else:
-            module_name = s.mod
-
-
-    u = make_translation_unit()
-    m = translation_unit_make_module(u, module_name)
-
-    # Convert functions
-    for sym in public_symbols:
-        s = table[sym.idx.idx]
-        if isinstance(s, gp.Module):
-            continue
-        assert isinstance(s, gp.Procedure)
-        convert_function(m.symtab, table, s)
-
-    return u
-
-version, orig_file, table, public_symbols = gp.load_module("mod1.mod")
-u = convert_module(table, public_symbols)
-verify_asr(u)
+u = mod_to_asr("mod1.mod")
 a = asr_to_ast(u)
 s = ast_to_src(a)
 print(s)
