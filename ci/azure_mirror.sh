b9286dc2f496ce4550ca05a997a57d64540c88d2
Ondřej Čertík <ondrej@certik.us>
2019-03-26 20:41:22 +0000

    Set up CI with Azure Pipelines
    We only run tests for macOS, since Linux and Windows are already tested on our
    other CI.
    
    The code is mirrored to Azure only if the GitLab CI environment variable
    SSH_PRIVATE_KEY_AZURE is set. Otherwise the Azure build are skipped.

---
+++ ci/azure_mirror.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan ssh.dev.azure.com >> ~/.ssh/known_hosts
+
+eval "$(ssh-agent -s)"
+
+set +x
+if [[ "${SSH_PRIVATE_KEY_AZURE}" == "" ]]; then
+    echo "Error: SSH_PRIVATE_KEY_AZURE is empty."
+    exit 1
+fi
+# Generate the private/public key pair using:
+#
+#     ssh-keygen -f deploy_key -N ""
+#
+# then set the $SSH_PRIVATE_KEY_AZURE environment variable in the GitLab-CI to
+# the base64 encoded private key:
+#
+#     cat deploy_key | base64 -w0
+#
+# and add the public key `deploy_key.pub` into the target git repository (with
+# write permissions).
+
+ssh-add <(echo "$SSH_PRIVATE_KEY_AZURE" | base64 -d)
+set -x
+
+pwd
+git branch -D $CI_COMMIT_REF_NAME || echo ok
+git checkout -t origin/$CI_COMMIT_REF_NAME
+git show-ref
+git remote -v
+git push git@ssh.dev.azure.com:v3/lfortran/lfortran/lfortran +$CI_COMMIT_REF_NAME:$CI_COMMIT_REF_NAME
