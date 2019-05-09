09019e7c401206d50cede445cecb33511d683dce
Ondřej Čertík <ondrej@certik.us>
2019-05-09 17:20:12 -0600

    Update convert_nb.py to convert all notebooks

--- doc/convert_nb.py
+++ doc/convert_nb.py
@@ -1,12 +1,11 @@
 """
 Converts all Jupyter notebooks into mkdocs compatible Markdown pages.
 """
+import os
+from glob import glob
 from jinja2 import DictLoader
 import nbformat
 import nbconvert
-
-with open("nb/AST and ASR.ipynb") as f:
-    nb = nbformat.read(f, 4)
 
 dl = DictLoader({'markdown2.tpl':
 """\
@@ -19,12 +18,19 @@ dl = DictLoader({'markdown2.tpl':
 {% endblock stream %}
 """})
 
-ep = nbconvert.preprocessors.ExecutePreprocessor(timeout=300)
-ep.preprocess(nb, {'metadata': {'path': 'nb/'}})
+for infile in glob("nb/*.ipynb"):
+    basename, _ = os.path.splitext(os.path.basename(infile))
+    outfile = "src/%s.md" % basename
+    print("%s -> %s" % (infile, outfile))
+    with open(infile) as f:
+        nb = nbformat.read(f, 4)
 
-m = nbconvert.MarkdownExporter(extra_loaders=[dl])
-m.template_file = "markdown2.tpl"
-body, resources = m.from_notebook_node(nb)
+    ep = nbconvert.preprocessors.ExecutePreprocessor(timeout=300)
+    ep.preprocess(nb, {'metadata': {'path': 'nb/'}})
 
-with open("src/AST and ASR.md", "w") as f:
-    f.write(body)
+    m = nbconvert.MarkdownExporter(extra_loaders=[dl])
+    m.template_file = "markdown2.tpl"
+    body, resources = m.from_notebook_node(nb)
+
+    with open(outfile, "w") as f:
+        f.write(body)
