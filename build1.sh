e42071b43187043c85204f72b6e076f54d8d50f2
Ondřej Čertík <ondrej@certik.us>
2019-05-14 16:57:21 -0600

    Build both static and dynamic libraries in one run

--- build1.sh
+++ build1.sh
@@ -3,7 +3,5 @@
 set -e
 set -x
 
-cmake -DCMAKE_INSTALL_PREFIX=`pwd` -DBUILD_SHARED_LIBS=OFF .
-make install
-cmake -DCMAKE_INSTALL_PREFIX=`pwd` -DBUILD_SHARED_LIBS=ON .
-make install
+cmake -DCMAKE_INSTALL_PREFIX=`pwd` .
+cmake --build . --target install
