faa9bfa53729b4c20b2c1f92105d14eeb6172771
Ondřej Čertík <ondrej@certik.us>
2019-05-14 14:46:39 -0600

    Switch to CMake build system for lfortran runtime

--- build1.sh
+++ build1.sh
@@ -3,11 +3,7 @@
 set -e
 set -x
 
-# Compile LFort runtime library
-CFLAGS="-Wall -g -fPIC"
-gcc $CFLAGS -o lfort_intrinsics.o -c lfort_intrinsics.c
-ar rcs liblfortran.a lfort_intrinsics.o
-gcc -shared -o liblfortran.so lfort_intrinsics.o
-mkdir -p share/lfortran/lib
-mv liblfortran.a share/lfortran/lib/
-mv liblfortran.so share/lfortran/lib/
+cmake -DCMAKE_INSTALL_PREFIX=`pwd` -DBUILD_SHARED_LIBS=OFF .
+make install
+cmake -DCMAKE_INSTALL_PREFIX=`pwd` -DBUILD_SHARED_LIBS=ON .
+make install
