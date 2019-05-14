dea7d9c36a0021be0c1add3e08304f2434e473cb
Ondřej Čertík <ondrej@certik.us>
2019-05-14 15:31:32 -0600

    CI: Use cmake on macOS

--- ci/azure_install_macos.sh
+++ ci/azure_install_macos.sh
@@ -5,7 +5,7 @@ set -ex
 conda config --set always_yes yes --set changeps1 no
 conda info -a
 conda update -q conda
-conda install python=3.7 pytest llvmlite prompt_toolkit
+conda install python=3.7 pytest llvmlite prompt_toolkit cmake make
 pip install antlr4-python3-runtime
 
 python grammar/asdl_py.py
@@ -16,12 +16,9 @@ curl -O https://www.antlr.org/download/antlr-4.7-complete.jar
 java -cp antlr-4.7-complete.jar org.antlr.v4.Tool -Dlanguage=Python3 -no-listener -visitor fortran.g4 -o ../lfortran/parser
 cd ..
 
-CFLAGS="-Wall -g -fPIC"
-gcc $CFLAGS -o lfort_intrinsics.o -c lfort_intrinsics.c
-ar rcs liblfortran.a lfort_intrinsics.o
-gcc -shared -o liblfortran.so lfort_intrinsics.o
-mkdir -p share/lfortran/lib
-mv liblfortran.a share/lfortran/lib/
-mv liblfortran.so share/lfortran/lib/
+cmake -DCMAKE_INSTALL_PREFIX=`pwd` -DBUILD_SHARED_LIBS=OFF .
+make install
+cmake -DCMAKE_INSTALL_PREFIX=`pwd` -DBUILD_SHARED_LIBS=ON .
+make install
 
 pytest
