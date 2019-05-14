a680aa58b19bf364ee004849f901d4028ce5ce02
Ondřej Čertík <ondrej@certik.us>
2019-05-14 14:42:24 -0600

    Use scikit-build

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import skbuild
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -6,7 +7,7 @@ with open("README.md", "r") as fh:
 with open("version", "r") as fh:
     version = fh.read().strip()
 
-setuptools.setup(
+skbuild.setup(
     name="lfortran",
     version=version,
     author="Ondřej Čertík",
@@ -19,9 +20,6 @@ setuptools.setup(
     include_package_data=True,
     data_files=[
         ('share/lfortran/nb', ['share/lfortran/nb/Demo.ipynb']),
-        ('share/lfortran/lib', [
-            'share/lfortran/lib/liblfortran.a',
-            'share/lfortran/lib/liblfortran.so']),
         ],
     scripts=['lfort'],
     classifiers=[
