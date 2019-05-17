39d13c12da2b06d8bd1e6032f33a5e444ff491a5
Ondřej Čertík <ondrej@certik.us>
2019-05-16 20:06:33 -0600

    Development version can fail to upload
    It fails due to having a "local version", which is ok by PEP 440, but not by
    PyPI.

--- ci/upload_pypi.sh
+++ ci/upload_pypi.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+lfortran_version=$(<version)
+
 if [[ $CI_COMMIT_TAG == "" ]]; then
     # Development version
     export TWINE_REPOSITORY_URL="https://test.pypi.org/legacy/"
@@ -10,6 +12,7 @@ if [[ $CI_COMMIT_TAG == "" ]]; then
     export TWINE_USERNAME=${TEST_PYPI_USERNAME}
     export TWINE_PASSWORD=${TEST_PYPI_PASSWORD}
     set -x
+    twine upload dist/lfortran-${lfortran_version}.tar.gz || echo "OK to fail."
 else
     # Release version
     export TWINE_REPOSITORY_URL="https://upload.pypi.org/legacy/"
@@ -17,8 +20,5 @@ else
     export TWINE_USERNAME=${PYPI_USERNAME}
     export TWINE_PASSWORD=${PYPI_PASSWORD}
     set -x
+    twine upload dist/lfortran-${lfortran_version}.tar.gz
 fi
-
-lfortran_version=$(<version)
-
-twine upload dist/lfortran-${lfortran_version}.tar.gz
