78d8da7c9aec85a724234008a497bbd6943c703a
Ondřej Čertík <ondrej@certik.us>
2019-05-16 19:59:08 -0600

    Do both test and release pypi

--- ci/upload_pypi.sh
+++ ci/upload_pypi.sh
@@ -6,24 +6,19 @@ set -x
 if [[ $CI_COMMIT_TAG == "" ]]; then
     # Development version
     export TWINE_REPOSITORY_URL="https://test.pypi.org/legacy/"
+    set +x
+    export TWINE_USERNAME=${TEST_PYPI_USERNAME}
+    export TWINE_PASSWORD=${TEST_PYPI_PASSWORD}
+    set -x
 else
     # Release version
     export TWINE_REPOSITORY_URL="https://upload.pypi.org/legacy/"
+    set +x
+    export TWINE_USERNAME=${PYPI_USERNAME}
+    export TWINE_PASSWORD=${PYPI_PASSWORD}
+    set -x
 fi
 
 lfortran_version=$(<version)
-
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-
-eval "$(ssh-agent -s)"
-
-set +x
-export TWINE_USERNAME=${PYPI_USERNAME}
-export TWINE_PASSWORD=${PYPI_PASSWORD}
-set -x
-
 
 twine upload dist/lfortran-${lfortran_version}.tar.gz
