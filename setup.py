1b297a646bc359ec257e10514eb6ba0bc5c2ebcb
Ondřej Čertík <ondrej@certik.us>
2019-07-26 10:16:37 -0600

    Pass correct MACOSX_DEPLOYMENT_TARGET into CMake

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import setuptools
 import skbuild
 
@@ -6,6 +7,13 @@ with open("README.md", "r") as fh:
 
 with open("version", "r") as fh:
     version = fh.read().strip()
+
+# scikit-build sometimes passes the incorrect MACOSX_DEPLOYMENT_TARGET value to
+# CMake. This ensures that the correct value is always passed in:
+cmake_args = []
+mdt = os.getenv("MACOSX_DEPLOYMENT_TARGET")
+if mdt:
+    cmake_args.append("-DCMAKE_OSX_DEPLOYMENT_TARGET={}".format(mdt))
 
 skbuild.setup(
     name="lfortran",
@@ -22,6 +30,7 @@ skbuild.setup(
         ('share/lfortran/nb', ['share/lfortran/nb/Demo.ipynb']),
         ],
     scripts=['lfort'],
+    cmake_args = cmake_args,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
