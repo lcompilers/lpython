d3716680b2155bcbc118cab380a46b1a22d0f8ca
Ondřej Čertík <ondrej@certik.us>
2019-08-15 00:55:25 -0600

    Simplify more code
    Things got faster.

--- src/lfortran/parser/sem4b.h
+++ src/lfortran/parser/sem4b.h
@@ -18,17 +18,10 @@ using LFortran::AST::expr_t;
 using LFortran::AST::Name_t;
 using LFortran::AST::BaseWalkVisitor;
 using LFortran::AST::make_BinOp_t;
+using LFortran::AST::make_Name_t;
+using LFortran::AST::make_Num_t;
 
 typedef struct LFortran::AST::expr_t *PExpr;
-
-
-static inline PExpr make_symbol(std::string s) {
-    return LFortran::AST::make_Name_t(al, &s[0]);
-}
-
-static inline PExpr make_integer(std::string s) {
-    return LFortran::AST::make_Num_t(al, s[0]);
-}
 
 
 class CountVisitor : public BaseWalkVisitor<CountVisitor>
@@ -54,8 +47,8 @@ static inline int count(const expr_t &b) {
 #define MUL(x, y) make_BinOp_t(al, x, operatorType::Mul, y)
 #define DIV(x, y) make_BinOp_t(al, x, operatorType::Div, y)
 #define POW(x, y) make_BinOp_t(al, x, operatorType::Pow, y)
-#define SYMBOL(x) make_symbol(x)
-#define INTEGER(x) make_integer(x)
+#define SYMBOL(x) make_Name_t(al, &x[0])
+#define INTEGER(x) make_Num_t(al, x[0])
 //#define PRINT(x) std::cout << x->type << std::endl
 #define PRINT(x) std::cout << count(*x) << std::endl;
 
