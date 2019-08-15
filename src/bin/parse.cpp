79634f462f2476da69ebd0776d76e482272f8caf
Ondřej Čertík <ondrej@certik.us>
2019-08-15 10:46:50 -0600

    Count in the benchmark

--- src/bin/parse.cpp
+++ src/bin/parse.cpp
@@ -3,6 +3,26 @@
 #include <lfortran/parser/parser.h>
 
 using LFortran::parse;
+using LFortran::AST::expr_t;
+using LFortran::AST::Name_t;
+using LFortran::AST::BaseWalkVisitor;
+
+class CountVisitor : public BaseWalkVisitor<CountVisitor>
+{
+    int c_;
+public:
+    CountVisitor() : c_{0} {}
+    void visit_Name(const Name_t &x) { c_ += 1; }
+    int get_count() {
+        return c_;
+    }
+};
+
+int count(const expr_t &b) {
+    CountVisitor v;
+    v.visit_expr(b);
+    return v.get_count();
+}
 
 int main(int argc, char *argv[])
 {
@@ -18,10 +38,12 @@ int main(int argc, char *argv[])
     }
     std::cout << "Parse" << std::endl;
     auto t1 = std::chrono::high_resolution_clock::now();
-    parse(text);
+    auto result = parse(text);
+    int c = count(*result);
     auto t2 = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1)
                      .count()
               << "ms" << std::endl;
+    std::cout << "Count: " << c << std::endl;
     return 0;
 }
