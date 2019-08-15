6a36e1ffbe1d1e7850987f2f1c8336706b3fed64
Ondřej Čertík <ondrej@certik.us>
2019-08-15 16:02:08 -0600

    Convert to a test

--- src/lfortran/tests/test_ast_to_json.cpp
+++ src/lfortran/tests/test_ast_to_json.cpp
@@ -1,11 +1,47 @@
+#include <tests/doctest.h>
 #include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 #include <lfortran/ast_to_json.h>
 
-int main(int argc, char *argv[])
-{
+TEST_CASE("Check ast_to_json()") {
     std::string s = LFortran::ast_to_json(nullptr);
-    std::cout << "Result:" << std::endl;
-    std::cout << s << std::endl;
-    return 0;
+    auto r = R"(
+        {
+            "answer": {
+                "everything": 42
+            },
+            "happy": true,
+            "list": [
+                1,
+                0,
+                2
+            ],
+            "name": "Niels",
+            "nothing": null,
+            "object": {
+                "currency": "USD",
+                "value": 42.99
+            },
+            "objects": [
+                {
+                    "currency": "USD",
+                    "value": 42.99
+                },
+                {
+                    "currency": "USD",
+                    "value": 42.99
+                },
+                {
+                    "currency": "USD",
+                    "value": 42.99
+                }
+            ],
+            "pi": 3.141
+        }
+    )"_json;
+
+    CHECK(json::parse(s) == r);
 }
