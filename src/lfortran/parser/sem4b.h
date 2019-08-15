bc359114299a0ccb41472c9eefb580ce053deea4
Ondřej Čertík <ondrej@certik.us>
2019-08-15 00:53:01 -0600

    Remove code

--- src/lfortran/parser/sem4b.h
+++ src/lfortran/parser/sem4b.h
@@ -17,13 +17,10 @@ using LFortran::AST::operatorType;
 using LFortran::AST::expr_t;
 using LFortran::AST::Name_t;
 using LFortran::AST::BaseWalkVisitor;
+using LFortran::AST::make_BinOp_t;
+
 typedef struct LFortran::AST::expr_t *PExpr;
 
-
-
-static inline PExpr make_binop(operatorType type, PExpr x, PExpr y) {
-    return LFortran::AST::make_BinOp_t(al, x, type, y);
-}
 
 static inline PExpr make_symbol(std::string s) {
     return LFortran::AST::make_Name_t(al, &s[0]);
@@ -52,11 +49,11 @@ static inline int count(const expr_t &b) {
 }
 
 #define TYPE PExpr
-#define ADD(x, y) make_binop(operatorType::Add, x, y)
-#define SUB(x, y) make_binop(operatorType::Sub, x, y)
-#define MUL(x, y) make_binop(operatorType::Mul, x, y)
-#define DIV(x, y) make_binop(operatorType::Div, x, y)
-#define POW(x, y) make_binop(operatorType::Pow, x, y)
+#define ADD(x, y) make_BinOp_t(al, x, operatorType::Add, y)
+#define SUB(x, y) make_BinOp_t(al, x, operatorType::Sub, y)
+#define MUL(x, y) make_BinOp_t(al, x, operatorType::Mul, y)
+#define DIV(x, y) make_BinOp_t(al, x, operatorType::Div, y)
+#define POW(x, y) make_BinOp_t(al, x, operatorType::Pow, y)
 #define SYMBOL(x) make_symbol(x)
 #define INTEGER(x) make_integer(x)
 //#define PRINT(x) std::cout << x->type << std::endl
