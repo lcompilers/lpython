41a57bd4b0ebfbbbc2b55c3b277e711f65a8fb4a
Ondřej Čertík <ondrej@certik.us>
2019-08-15 14:45:01 -0600

    Use json

--- src/lfortran/ast_to_json.cpp
+++ src/lfortran/ast_to_json.cpp
@@ -2,10 +2,40 @@
 
 #include <lfortran/ast_to_json.h>
 
+using json = nlohmann::json;
+
 namespace LFortran {
 
 std::string ast_to_json(LFortran::AST::expr_t *ast) {
-    return "OK!";
+    json j = {
+        {"pi", 3.141},
+        {"happy", true},
+        {"name", "Niels"},
+        {"nothing", nullptr},
+        {"answer", {
+            {"everything", 42}
+        }},
+        {"list", {1, 0, 2}},
+        {"object", {
+            {"currency", "USD"},
+            {"value", 42.99}
+        }},
+        {"objects", {
+            {
+                {"currency", "USD"},
+                {"value", 42.99}
+            },
+            {
+                {"currency", "USD"},
+                {"value", 42.99}
+            },
+            {
+                {"currency", "USD"},
+                {"value", 42.99}
+            },
+        }}
+    };
+    return j.dump(4);
 }
 
 }
