595a998ef0d47a9db14e79d7f698996c4720cd8b
Ondřej Čertík <ondrej@certik.us>
2019-08-16 13:56:09 -0600

    Switch to RapidJSON

---
+++ cmake/FindRapidJSON.cmake
@@ -0,0 +1,8 @@
+find_path(RAPIDJSON_HEADER rapidjson/rapidjson.h)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RAPIDJSON DEFAULT_MSG RAPIDJSON_HEADER)
+
+add_library(p::rapidjson INTERFACE IMPORTED)
+set_property(TARGET p::rapidjson PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+    ${RAPIDJSON_HEADER})
