904cb6301e86f95e6ad86d571ea74b23cd2b0e70
Ondřej Čertík <ondrej@certik.us>
2019-08-16 14:35:28 -0600

    Move to RapidJSON
    Spedup the compilation of ast_to_json.cpp from 4.8s to 1.8s.

--- src/lfortran/ast_to_json.cpp
+++ src/lfortran/ast_to_json.cpp
@@ -1,8 +1,10 @@
-#include <nlohmann/json.hpp>
+#define RAPIDJSON_HAS_STDSTRING 1
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
 
 #include <lfortran/ast_to_json.h>
 
-using json = nlohmann::json;
 using LFortran::AST::expr_t;
 using LFortran::AST::Name_t;
 using LFortran::AST::Num_t;
@@ -28,34 +30,38 @@ std::string op2str(const operatorType type)
 
 class JSONVisitor : public BaseWalkVisitor<JSONVisitor>
 {
-    json j;
+    rapidjson::Document d;
+    rapidjson::Value j;
+    rapidjson::Document::AllocatorType& al;
 public:
+    JSONVisitor () : al{d.GetAllocator()} {}
+
     void visit_BinOp(const BinOp_t &x) {
         this->visit_expr(*x.m_left);
-        json left = j;
+        rapidjson::Value left = std::move(j);
         this->visit_expr(*x.m_right);
-        json right = j;
-        j = {
-            {"type", "BinOp"},
-            {"op", op2str(x.m_op)},
-            {"left", left },
-            {"right", right }
-        };
+        rapidjson::Value right = std::move(j);
+        j.SetObject();
+        j.AddMember("type", "BinOp", al);
+        j.AddMember("op", op2str(x.m_op), al);
+        j.AddMember("left", left, al);
+        j.AddMember("right", right, al);
     }
     void visit_Name(const Name_t &x) {
-        j = {
-            {"type", "Name"},
-            {"id", x.m_id }
-        };
+        j.SetObject();
+        j.AddMember("type", "Name", al);
+        j.AddMember("id", std::string(x.m_id, 1), al);
     }
     void visit_Num(const Num_t &x) {
-        j = {
-            {"type", "Num"},
-            {"n", x.m_n }
-        };
+        j.SetObject();
+        j.AddMember("type", "Num", al);
+        j.AddMember("n", x.m_n, al);
     }
     std::string get_str() {
-        return j.dump(4);
+        rapidjson::StringBuffer strbuf;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
+        j.Accept(writer);
+	    return strbuf.GetString();
     }
 };
 
