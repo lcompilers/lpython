492885ac76f0728c71961ec36a375edb0fca6c45
Ondřej Čertík <ondrej@certik.us>
2019-08-17 12:34:05 -0600

    Add tests for arithmetics

--- src/lfortran/tests/test_pickle.cpp
+++ src/lfortran/tests/test_pickle.cpp
@@ -11,12 +11,23 @@ std::string p(Allocator &al, const std::string &s)
     return LFortran::pickle(*result);
 }
 
+#define P(x) p(al, x)
+
 
 TEST_CASE("Check pickle()") {
     Allocator al(4*1024);
 
-    CHECK(p(al, "2*x")   == "(BinOp Mul 2 x)");
-    CHECK(p(al, "(2*x)") == "(BinOp Mul 2 x)");
-    CHECK(p(al, "3*x**y") == "(BinOp Mul 3 (BinOp Pow x y))");
-    CHECK(p(al, "x = 2*y") == "(Assignment x (BinOp Mul 2 y))");
+    CHECK(P("2*x")   == "(BinOp Mul 2 x)");
+    CHECK(P("(2*x)") == "(BinOp Mul 2 x)");
+    CHECK(P("3*x**y") == "(BinOp Mul 3 (BinOp Pow x y))");
+    CHECK(P("x = 2*y") == "(Assignment x (BinOp Mul 2 y))");
+}
+
+TEST_CASE("Arithmetics") {
+    Allocator al(4*1024);
+
+    CHECK(P("1+2*3")   == "(BinOp Add 1 (BinOp Mul 2 3))");
+    CHECK(P("1*2+3")   == "(BinOp Add (BinOp Mul 1 2) 3)");
+    CHECK(P("(1+2)*3") == "(BinOp Mul (BinOp Add 1 2) 3)");
+    CHECK(P("1+(2*3)") == "(BinOp Add 1 (BinOp Mul 2 3))");
 }
