84333e3364ccb0cf29cb430b5f8b0b121cd84090
Ondřej Čertík <ondrej@certik.us>
2019-08-17 12:38:31 -0600

    Add tests for Pow

--- src/lfortran/tests/test_pickle.cpp
+++ src/lfortran/tests/test_pickle.cpp
@@ -27,7 +27,13 @@ TEST_CASE("Arithmetics") {
     Allocator al(4*1024);
 
     CHECK(P("1+2*3")   == "(BinOp Add 1 (BinOp Mul 2 3))");
+    CHECK(P("1+(2*3)") == "(BinOp Add 1 (BinOp Mul 2 3))");
     CHECK(P("1*2+3")   == "(BinOp Add (BinOp Mul 1 2) 3)");
     CHECK(P("(1+2)*3") == "(BinOp Mul (BinOp Add 1 2) 3)");
-    CHECK(P("1+(2*3)") == "(BinOp Add 1 (BinOp Mul 2 3))");
+    CHECK(P("1+2*3**4")   == "(BinOp Add 1 (BinOp Mul 2 (BinOp Pow 3 4)))");
+    CHECK(P("1+2*(3**4)") == "(BinOp Add 1 (BinOp Mul 2 (BinOp Pow 3 4)))");
+    CHECK(P("1+(2*3)**4") == "(BinOp Add 1 (BinOp Pow (BinOp Mul 2 3) 4))");
+    CHECK(P("(1+2*3)**4") == "(BinOp Pow (BinOp Add 1 (BinOp Mul 2 3)) 4)");
+    CHECK(P("1+2**3*4")   == "(BinOp Add 1 (BinOp Mul (BinOp Pow 2 3) 4))");
+    CHECK(P("1+2**(3*4)") == "(BinOp Add 1 (BinOp Pow 2 (BinOp Mul 3 4)))");
 }
