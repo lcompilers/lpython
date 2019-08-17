3973494c30da08b8124861ce38551d4bfa379513
Ondřej Čertík <ondrej@certik.us>
2019-08-17 12:24:32 -0600

    Simplify tests

--- src/lfortran/tests/test_pickle.cpp
+++ src/lfortran/tests/test_pickle.cpp
@@ -7,31 +7,24 @@
 
 TEST_CASE("Check pickle()") {
     Allocator al(4*1024);
-    std::string s, r;
+    std::string s;
     LFortran::AST::ast_t* result;
 
     result = LFortran::parse(al, "2*x");
     s = LFortran::pickle(*result);
-    std::cout << s << std::endl;
-    r = "(BinOp Mul 2 x)";
-    CHECK(s == r);
+    CHECK(s == "(BinOp Mul 2 x)");
 
 
     result = LFortran::parse(al, "(2*x)");
     s = LFortran::pickle(*result);
-    std::cout << s << std::endl;
-    CHECK(s == r);
+    CHECK(s == "(BinOp Mul 2 x)");
 
 
     result = LFortran::parse(al, "3*x**y");
     s = LFortran::pickle(*result);
-    std::cout << s << std::endl;
-    r = "(BinOp Mul 3 (BinOp Pow x y))";
-    CHECK(s == r);
+    CHECK(s == "(BinOp Mul 3 (BinOp Pow x y))");
 
     result = LFortran::parse(al, "x = 2*y");
     s = LFortran::pickle(*result);
-    std::cout << s << std::endl;
-    r = "(Assignment x (BinOp Mul 2 y))";
-    CHECK(s == r);
+    CHECK(s == "(Assignment x (BinOp Mul 2 y))");
 }
