8078597aeaec1d2608fec084d32755b07090272b
Ondřej Čertík <ondrej@certik.us>
2019-08-18 22:57:46 -0600

    Test the tokenizer

--- src/lfortran/tests/test_parse.cpp
+++ src/lfortran/tests/test_parse.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 
 #include <lfortran/parser/parser.h>
+#include <lfortran/parser/parser.tab.hh>
 
 using LFortran::parse;
 using LFortran::AST::ast_t;
@@ -53,4 +54,45 @@ TEST_CASE("Test longer parser (N = 500)") {
     std::cout << "String size (bytes):      " << text.size() << std::endl;
     std::cout << "Allocator usage (bytes): " << al.size_current() << std::endl;
     CHECK(c == 4509);
+}
+
+TEST_CASE("Tokenizer 1") {
+    std::string input = R"(subroutine
+    x = y
+    x = 2*y
+    subroutine)";
+    /*
+    END_OF_FILE = 0,
+    IDENTIFIER = 258,
+    NUMERIC = 259,
+    KW_EXIT = 260,
+    KW_NEWLINE = 261,
+    KW_SUBROUTINE = 262,
+    POW = 263
+    */
+    std::vector<int> ref = {
+        yytokentype::KW_SUBROUTINE,
+        yytokentype::KW_NEWLINE,
+        yytokentype::IDENTIFIER,
+        '=',
+        yytokentype::IDENTIFIER,
+        yytokentype::KW_NEWLINE,
+        yytokentype::IDENTIFIER,
+        '=',
+        yytokentype::NUMERIC,
+        '*',
+        yytokentype::IDENTIFIER,
+        yytokentype::KW_NEWLINE,
+        yytokentype::KW_SUBROUTINE,
+        yytokentype::END_OF_FILE,
+    };
+    LFortran::Tokenizer t;
+    t.set_string(input);
+    for (size_t i = 0; i < ref.size(); i++) {
+        int token;
+        LFortran::YYSTYPE y;
+        token = t.lex(y);
+        CAPTURE(i);
+        CHECK(token == ref[i]);
+    }
 }
