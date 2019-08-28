dbac790ebcc136f2970b73a68ed8c9f3ea6fa824
Ondřej Čertík <ondrej@certik.us>
2019-08-28 10:19:28 -0600

    Convert a string to bytes

--- lfortran/parser/cparser.pyx
+++ lfortran/parser/cparser.pyx
@@ -4,6 +4,7 @@ def parse(s):
     cdef cwrapper.LFortranCParser *h
     h = cwrapper.lfortran_parser_new()
     cdef cwrapper.lfortran_exceptions_t e;
-    cdef const char *str = s;
+    sb = s.encode()
+    cdef char *str = sb;
     e = cwrapper.lfortran_parser_parse(h, str)
     cwrapper.lfortran_parser_free(h)
