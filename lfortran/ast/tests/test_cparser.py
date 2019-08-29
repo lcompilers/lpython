cac6b26fecdae50ec501e547f7d22b7b46a0683a
Ondřej Čertík <ondrej@certik.us>
2019-08-29 11:06:08 -0600

    Add another test

--- lfortran/ast/tests/test_cparser.py
+++ lfortran/ast/tests/test_cparser.py
@@ -2,3 +2,9 @@ from lfortran.parser.cparser import parse
 
 def test_parse1():
     assert parse("1+1") == "(+ 1 1)"
+    assert parse("""\
+if (x > 0) then
+    a = 1
+else
+    a = 2
+end if""") == "(if (> x 0) [(= a 1)] [(= a 2)])"
