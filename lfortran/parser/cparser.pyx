8f6d4e3b10ebe6831b104fcac9865e8cc956f32f
Ondřej Čertík <ondrej@certik.us>
2019-08-29 21:30:54 -0600

    Create Parser and AST

--- lfortran/parser/cparser.pyx
+++ lfortran/parser/cparser.pyx
@@ -2,7 +2,6 @@ cimport cwrapper
 
 cdef class AST(object):
     cdef cwrapper.lfortran_ast_t *thisptr
-    cdef cwrapper.LFortranCParser *h;
 
     def pickle(self):
         cdef char *out
@@ -15,21 +14,28 @@ cdef class AST(object):
             raise Exception("str_free failed")
         return sout
 
+cdef class Parser(object):
+    cdef cwrapper.LFortranCParser *h;
+
     def __cinit__(self):
         self.h = cwrapper.lfortran_parser_new()
-#        self.thisptr = 0;
 
     def __dealloc__(self):
         cwrapper.lfortran_parser_free(self.h)
 
+    def parse(self, s):
+        sb = s.encode()
+        cdef char *str = sb
+        cdef cwrapper.lfortran_ast_t *ast;
+        e = cwrapper.lfortran_parser_parse(self.h, str, &ast)
+        if (e != cwrapper.LFORTRAN_NO_EXCEPTION):
+            raise Exception("parser_parse failed")
+        a = AST()
+        a.thisptr = ast
+        return a
+
 def parse(s):
-    a = AST()
-    sb = s.encode()
-    cdef char *str = sb
-    cdef cwrapper.lfortran_ast_t *ast;
-    e = cwrapper.lfortran_parser_parse(a.h, str, &ast)
-    if (e != cwrapper.LFORTRAN_NO_EXCEPTION):
-        raise Exception("parser_parse failed")
-    a.thisptr = ast
+    p = Parser()
+    a = p.parse(s)
     s = a.pickle()
     return s
