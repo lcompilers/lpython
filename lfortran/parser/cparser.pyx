0e36109fb23d071c5bb04767fc194ca34454b54a
Ondřej Čertík <ondrej@certik.us>
2019-08-29 21:00:07 -0600

    Wrap new C interface into Python

--- lfortran/parser/cparser.pyx
+++ lfortran/parser/cparser.pyx
@@ -6,8 +6,12 @@ def parse(s):
     cdef cwrapper.lfortran_exceptions_t e
     sb = s.encode()
     cdef char *str = sb
+    cdef cwrapper.lfortran_ast_t *ast;
+    e = cwrapper.lfortran_parser_parse(h, str, &ast)
+    if (e != cwrapper.LFORTRAN_NO_EXCEPTION):
+        raise Exception("parser_parse failed")
     cdef char *out
-    e = cwrapper.lfortran_parser_parse(h, str, &out)
+    e = cwrapper.lfortran_parser_pickle(ast, &out)
     if (e != cwrapper.LFORTRAN_NO_EXCEPTION):
         raise Exception("parser_parse failed")
     sout = out.decode()
