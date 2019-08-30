edc916475e6d56920ccab1cb41cc27691b8002b5
Ondřej Čertík <ondrej@certik.us>
2019-08-29 22:14:47 -0600

    Python: add a test for Parser and AST classes

--- lfortran/ast/tests/test_cparser.py
+++ lfortran/ast/tests/test_cparser.py
@@ -1,4 +1,4 @@
-from lfortran.parser.cparser import parse
+from lfortran.parser.cparser import parse, Parser, AST
 
 def test_parse1():
     assert parse("1+1") == "(+ 1 1)"
@@ -8,3 +8,15 @@ if (x > 0) then
 else
     a = 2
 end if""") == "(if (> x 0) [(= a 1)] [(= a 2)])"
+
+def test_parse2():
+    p = Parser()
+    a = p.parse("1+1")
+    assert a.pickle() == "(+ 1 1)"
+    a = p.parse("""\
+if (x > 0) then
+    a = 1
+else
+    a = 2
+end if""")
+    assert a.pickle() == "(if (> x 0) [(= a 1)] [(= a 2)])"
