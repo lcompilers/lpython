ffbd358e7e265205e43294dab765d88b1f5d9b4e
Ondřej Čertík <ondrej@certik.us>
2019-09-05 02:15:45 -0600

    Call CMake properly on Windows

--- ci/azure_install_macos.sh
+++ ci/azure_install_macos.sh
@@ -7,4 +7,7 @@ conda info -a
 conda update -q conda
 conda install -c conda-forge python=3.7 re2c bison m4 cython xonsh scikit-build
 export MACOSX_DEPLOYMENT_TARGET="10.9"
+export CONDA_PREFIX=/usr/local/miniconda
+export LFORTRAN_CMAKE_GENERATOR="Unix Makefiles"
+export WIN=0
 xonsh ci/build.xsh
