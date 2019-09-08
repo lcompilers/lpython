a77ac815b4a19096659c50690da9d0b4a3ceae4c
Ondřej Čertík <ondrej@certik.us>
2019-09-08 10:00:52 -0600

    Initial implementation: Build ASR from AST

--- src/lfortran/semantics/ast_to_asr.cpp
+++ src/lfortran/semantics/ast_to_asr.cpp
@@ -2,27 +2,37 @@
 #include <memory>
 
 #include <lfortran/ast.h>
+#include <lfortran/asr.h>
 #include <lfortran/semantics/ast_to_asr.h>
 
-using LFortran::AST::BaseVisitor;
-using LFortran::AST::TranslationUnit_t;
-using LFortran::AST::Function_t;
 
 namespace LFortran {
 
-class SymbolTableVisitor : public BaseVisitor<SymbolTableVisitor>
+class SymbolTableVisitor : public LFortran::AST::BaseVisitor<SymbolTableVisitor>
 {
 public:
-    SymbolTableVisitor() {  }
-    void visit_Function(const Function_t &x) {
+    LFortran::ASR::asr_t *asr;
+    Allocator &al;
+    SymbolTableVisitor(Allocator &al) : al{al} {}
+    void visit_Function(const LFortran::AST::Function_t &x) {
         std::cout << "Function: " << x.m_name << std::endl;
+        asr = LFortran::ASR::make_Function_t(al, x.base.base.loc,
+            /*char* a_name*/ x.m_name,
+            /*expr_t** a_args*/ nullptr, /*size_t n_args*/ 0,
+            /*stmt_t** a_body*/ nullptr, /*size_t n_body*/ 0,
+            /*tbind_t* a_bind*/ nullptr,
+            /*expr_t* a_return_var*/ nullptr,
+            /*char* a_module*/ nullptr,
+            /*int *object* a_symtab*/ 0);
     }
 };
 
-void ast_to_asr(LFortran::AST::ast_t &ast)
+void ast_to_asr(LFortran::AST::ast_t &ast, Allocator &al,
+        LFortran::ASR::asr_t **asr)
 {
-    SymbolTableVisitor v;
+    SymbolTableVisitor v(al);
     v.visit_ast(ast);
+    *asr = v.asr;
 }
 
 } // namespace LFortran
--- src/lfortran/tests/test_ast_to_asr.cpp
+++ src/lfortran/tests/test_ast_to_asr.cpp
@@ -5,12 +5,13 @@
 #include <lfortran/parser/parser.h>
 #include <lfortran/semantics/ast_to_asr.h>
 
-bool p(Allocator &al, const std::string &s)
+std::string p(Allocator &al, const std::string &s)
 {
-    LFortran::AST::ast_t* result;
-    result = LFortran::parse2(al, s);
-    LFortran::ast_to_asr(*result);
-    return true;
+    LFortran::AST::ast_t* ast;
+    ast = LFortran::parse2(al, s);
+    LFortran::ASR::asr_t* asr;
+    LFortran::ast_to_asr(*ast, al, &asr);
+    return LFortran::pickle(*ast);
 }
 
 #define P(x) p(al, x)
@@ -21,5 +22,5 @@ TEST_CASE("Functions") {
     CHECK(P(R"(function f()
 integer :: f
 f = 5
-end function)"));
+end function)") == "xx");
 }
