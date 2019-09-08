20ecae088d64bd5e6cb6a328d701c4a3dfe4fde3
Ondřej Čertík <ondrej@certik.us>
2019-09-08 10:31:21 -0600

    Add a return Variable

--- src/lfortran/semantics/ast_to_asr.cpp
+++ src/lfortran/semantics/ast_to_asr.cpp
@@ -8,6 +8,12 @@
 
 namespace LFortran {
 
+static inline ASR::expr_t* EXPR(const ASR::asr_t *f)
+{
+    LFORTRAN_ASSERT(f->type == ASR::asrType::expr);
+    return (ASR::expr_t*)f;
+}
+
 class SymbolTableVisitor : public AST::BaseVisitor<SymbolTableVisitor>
 {
 public:
@@ -16,7 +22,8 @@ public:
     SymbolTableVisitor(Allocator &al) : al{al} {}
     void visit_Function(const AST::Function_t &x) {
         std::cout << "Function: " << x.m_name << std::endl;
-        ASR::expr_t *return_var=nullptr;
+        ASR::expr_t *return_var = EXPR(ASR::make_Variable_t(al, x.base.base.loc,
+                x.m_name, nullptr, 1, nullptr));
         asr = ASR::make_Function_t(al, x.base.base.loc,
             /*char* a_name*/ x.m_name,
             /*expr_t** a_args*/ nullptr, /*size_t n_args*/ 0,
