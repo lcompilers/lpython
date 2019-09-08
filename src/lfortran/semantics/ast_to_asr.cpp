52464534ad9e9cc39d631f2830816d712b9cda9e
Ondřej Čertík <ondrej@certik.us>
2019-09-08 17:47:12 -0600

    Add BodyVisitor

--- src/lfortran/semantics/ast_to_asr.cpp
+++ src/lfortran/semantics/ast_to_asr.cpp
@@ -42,11 +42,37 @@ public:
     }
 };
 
+class BodyVisitor : public AST::BaseVisitor<BodyVisitor>
+{
+public:
+    Allocator &al;
+    ASR::asr_t *asr;
+    BodyVisitor(Allocator &al, ASR::asr_t *unit) : al{al}, asr{unit} {}
+    void visit_Function(const AST::Function_t &x) {
+        ASR::ttype_t *type = TYPE(ASR::make_Integer_t(al, x.base.base.loc,
+                8, nullptr, 0));
+        ASR::expr_t *return_var = EXPR(ASR::make_Variable_t(al, x.base.base.loc,
+                x.m_name, nullptr, 1, type));
+        asr = ASR::make_Function_t(al, x.base.base.loc,
+            /*char* a_name*/ x.m_name,
+            /*expr_t** a_args*/ nullptr, /*size_t n_args*/ 0,
+            /*stmt_t** a_body*/ nullptr, /*size_t n_body*/ 0,
+            /*tbind_t* a_bind*/ nullptr,
+            /*expr_t* a_return_var*/ return_var,
+            /*char* a_module*/ nullptr,
+            /*int *object* a_symtab*/ 0);
+    }
+};
+
 ASR::asr_t *ast_to_asr(Allocator &al, AST::ast_t &ast)
 {
     SymbolTableVisitor v(al);
     v.visit_ast(ast);
-    return v.asr;
+    ASR::asr_t *unit = v.asr;
+
+    BodyVisitor b(al, unit);
+    b.visit_ast(ast);
+    return unit;
 }
 
 } // namespace LFortran
