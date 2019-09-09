51832cbec59e1e6231e3d537ed36dd8105ece4e1
Ondřej Čertík <ondrej@certik.us>
2019-09-09 16:17:41 -0600

    Read the string from input

--- src/bin/lfortran.cpp
+++ src/bin/lfortran.cpp
@@ -7,11 +7,14 @@
 int main(int argc, char *argv[])
 {
     std::cout << "Interactive Fortran" << std::endl;
-    std::cout << "Text" << std::endl;
-    std::string t0 = "(a*z+3+2*x + 3*y - x/(z**2-4) - x**(y**z))";
-    std::cout << t0 << std::endl;
+    std::cout << ">>> ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << "Input:" << std::endl;
+    //std::string t0 = "(a*z+3+2*x + 3*y - x/(z**2-4) - x**(y**z))";
+    std::cout << input << std::endl;
     Allocator al(16*1024);
-    LFortran::AST::ast_t* result = LFortran::parse2(al, t0);
+    LFortran::AST::ast_t* result = LFortran::parse2(al, input);
     std::string ast = LFortran::pickle(*result);
     std::cout << "AST" << std::endl;
     std::cout << ast << std::endl;
