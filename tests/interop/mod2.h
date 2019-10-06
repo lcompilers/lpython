630ea8b213d40b19f4f3b0f9c7e2730c216690d8
Ondřej Čertík <ondrej@certik.us>
2019-10-06 15:34:49 -0600

    Extract mod2.h

---
+++ tests/interop/mod2.h
@@ -0,0 +1,16 @@
+#ifndef MOD2_H
+#define MOD2_H
+
+extern "C" {
+
+int32_t __mod1_MOD_f1(int32_t *a, int32_t *b);
+int32_t __mod1_MOD_f2(int32_t *n, int32_t *a);
+int32_t __mod1_MOD_f3(int32_t *n, int32_t *m, int32_t *a);
+int32_t __mod1_MOD_f2b(descriptor<1, int32_t> *descr);
+int32_t __mod1_MOD_f3b(descriptor<2, int32_t> *descr);
+int32_t __mod1_MOD_f4(int32_t *a, int32_t *b);
+int32_t __mod1_MOD_f5b(descriptor<2, int32_t> *descr);
+
+}
+
+#endif // MOD2_H
--- tests/interop/test_cpp.cpp
+++ tests/interop/test_cpp.cpp
@@ -52,17 +52,7 @@ descriptor<Rank, Type> c_desc(Type *array_ptr,
     return a;
 }
 
-extern "C" {
-
-int32_t __mod1_MOD_f1(int32_t *a, int32_t *b);
-int32_t __mod1_MOD_f2(int32_t *n, int32_t *a);
-int32_t __mod1_MOD_f3(int32_t *n, int32_t *m, int32_t *a);
-int32_t __mod1_MOD_f2b(descriptor<1, int32_t> *descr);
-int32_t __mod1_MOD_f3b(descriptor<2, int32_t> *descr);
-int32_t __mod1_MOD_f4(int32_t *a, int32_t *b);
-int32_t __mod1_MOD_f5b(descriptor<2, int32_t> *descr);
-
-}
+#include "mod2.h"
 
 TEST_CASE("f1")
 {
