f13348440972b26b8d5600b7101d1daa48391c28
Ondřej Čertík <ondrej@certik.us>
2020-05-31 12:49:48 -0600

    Add a test for subroutines

---
+++ tests/subroutine2.f90
@@ -0,0 +1,39 @@
+subroutine g
+x = y
+x = 2*y
+end subroutine
+
+subroutine g
+x = y
+
+x = 2*y
+end subroutine
+
+subroutine g
+
+    x = y
+
+
+    x = 2*y
+
+
+
+end subroutine
+
+subroutine g
+x = y
+;;;;;; ; ; ;
+x = 2*y
+end subroutine
+
+subroutine g
+x = y;
+x = 2*y;
+end subroutine
+
+subroutine g
+x = y; ;
+x = 2*y;; ;
+end subroutine
+
+subroutine g; x = y; x = 2*y; end subroutine
