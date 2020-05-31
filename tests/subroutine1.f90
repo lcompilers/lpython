44b382159cf02fe9f1e9408881d1b75f59785e9e
Ondřej Čertík <ondrej@certik.us>
2020-05-31 13:14:16 -0600

    Add another subroutine to subroutine1.f90

--- tests/subroutine1.f90
+++ tests/subroutine1.f90
@@ -1,7 +1,15 @@
-subroutine g
+subroutine g()
 integer :: x, i
 x = 1
 do i = 1, 10
     x = x*i
+end do
+end subroutine
+
+subroutine h()
+integer :: x, i
+x = 1
+do i = 1, 10
+    x = i*x
 end do
 end subroutine
