4180c36b5d88486b26ff830d9b104ebf60928c07
Ondřej Čertík <ondrej@certik.us>
2020-05-31 13:20:05 -0600

    Port the rest of subroutine tests from test_pickle

--- tests/subroutine2.f90
+++ tests/subroutine2.f90
@@ -37,3 +37,40 @@ x = 2*y;; ;
 end subroutine
 
 subroutine g; x = y; x = 2*y; end subroutine
+
+subroutine f
+subroutine = y
+x = 2*subroutine
+end subroutine
+
+subroutine f
+integer :: x
+end subroutine
+
+subroutine f()
+integer :: x
+end subroutine
+
+subroutine f(a, b, c, d)
+integer, intent(in) :: a, b
+integer, intent ( in ) :: c, d
+integer :: z
+integer::y
+end subroutine
+
+subroutine f(a, b, c, d)
+integer, intent(out) :: a, b
+integer, intent(inout) :: c, d
+integer :: z
+integer::y
+end subroutine
+
+subroutine saxpy(n, a, x, y)
+real(dp), intent(in) :: x(:), a
+real(dp), intent(inout) :: y(:)
+integer, intent(in) :: n
+integer :: i
+do i = 1, n
+    y(i) = a*x(i)+y(i)
+enddo
+end subroutine saxpy
