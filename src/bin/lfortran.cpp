4c2ba7458b6d4fcce80a04b384a1495eac1683d4
Ondřej Čertík <ondrej@certik.us>
2020-05-31 13:12:12 -0600

    Print all AST nodes

--- src/bin/lfortran.cpp
+++ src/bin/lfortran.cpp
@@ -126,9 +126,9 @@ int emit_ast(const std::string &infile, const std::string &outfile)
     std::string input = read_file(infile);
     // Src -> AST
     Allocator al(64*1024*1024);
-    LFortran::AST::ast_t* ast;
+    std::vector<LFortran::AST::ast_t*> ast;
     try {
-        ast = LFortran::parse2(al, input);
+        ast = LFortran::parsen2(al, input);
     } catch (const LFortran::TokenizerError &e) {
         std::cout << "Tokenizing error: " << e.msg() << std::endl;
         return 1;
@@ -142,7 +142,9 @@ int emit_ast(const std::string &infile, const std::string &outfile)
     {
         std::ofstream file;
         file.open(outfile);
-        file << LFortran::pickle(*ast) << std::endl;
+        for (auto a: ast) {
+            file << LFortran::pickle(*a) << std::endl;
+        }
     }
     return 0;
 }
