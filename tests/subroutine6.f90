5c452b0ee87d82ffcd24c9e46df34e56a60cbff0
Jacob Marks <bughunterstudios@gmail.com>
2020-07-29 09:17:26 -0600

    Added test with two do concurrent loops.

---
+++ tests/subroutine6.f90
@@ -0,0 +1,14 @@
+subroutine triad(a, b, scalar, c)
+real, intent(in) :: a(:), b(:), scalar
+real, intent(out) :: c(:)
+integer :: N, N2, i
+N = size(a)
+N2 = size(b)
+do concurrent (i = 1:N)
+    c(i) = a(i) + scalar * b(i)
+end do
+
+do concurrent (j = 1:N2)
+    c(j) = b(j) + scalar
+end do
+end subroutine
