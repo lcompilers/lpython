b746ed85a51054117529be66125a70f6bc7a7118
Ondřej Čertík <ondrej@certik.us>
2020-08-02 00:23:50 -0600

    Add a test for the Bison grammar
    This test checks that all the conflicts can be removed by removing all
    keywords as an option for an identifier. That way it ensures that we do
    not accidentally introduce any other kind of conflict.

---
+++ ci/grammar_conflicts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+patch -p1 < ci/parser.yy.patch
+sed -i '/^%expect-rr/d' src/lfortran/parser/parser.yy
+sed -i 's/^%expect .*/%expect 0/' src/lfortran/parser/parser.yy
+sed -i '/^%glr-parser/d' src/lfortran/parser/parser.yy
+(cd src/lfortran/parser && bison -Wall -d -r all parser.yy)
+
+echo "Grammar is LALR(1), no conflicts"
