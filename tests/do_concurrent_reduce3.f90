f6464c97af94a25a3a9725ba29bde5de5ef7f475
Ondřej Čertík <ondrej@certik.us>
2020-08-03 13:10:13 -0600

    Add a test for * and MIN

---
+++ tests/do_concurrent_reduce3.f90
@@ -0,0 +1,13 @@
+subroutine sum_reduce(a, s)
+real, intent(in) :: a(:)
+real, intent(out) :: s
+integer :: N, i
+N = size(a)
+s = 0
+do concurrent (i = 1:N) reduce(*: s)
+    s = s + a(i)
+end do
+do concurrent (i = 1:N) reduce(MIN: s)
+    s = s + a(i)
+end do
+end subroutine
