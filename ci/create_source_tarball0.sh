5fecb5119b2958dbd26e0a65ca9378e410a673bf
Ondřej Čertík <ondrej@certik.us>
2020-08-07 15:38:01 -0600

    Use Bash and CMake to create the tarball

--- ci/create_source_tarball0.sh
+++ ci/create_source_tarball0.sh
@@ -1,5 +1,9 @@
-mkdir $dest
-cp -r src cmake examples CMakeLists.txt README.md LICENSE version $dest
-mkdir dist
-tar czf dist/$dest.tar.gz $dest
-rm -r $dest
+#!/bin/bash
+
+set -ex
+
+cmake -E make_directory $dest
+cmake -E copy src cmake examples CMakeLists.txt README.md LICENSE version $dest
+cmake -E make_directory dist
+cmake -E tar cfz dist/$dest.tar.gz $dest
+cmake -E remove_directory $dest
