15a8c14bd67c40bce61a7e8ecf67ecde3e6d2c91
Ondřej Čertík <ondrej@certik.us>
2020-08-07 14:48:34 -0600

    Include the version file in the tarball

--- ci/create_source_tarball.sh
+++ ci/create_source_tarball.sh
@@ -7,6 +7,6 @@ lfortran_version=$1
 dest=lfortran-$lfortran_version
 
 mkdir $dest
-cp -r src cmake examples CMakeLists.txt README.md LICENSE $dest
+cp -r src cmake examples CMakeLists.txt README.md LICENSE version $dest
 mkdir dist
 tar czf dist/$dest.tar.gz $dest
