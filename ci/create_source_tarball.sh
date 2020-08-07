f2ffed99a69492f8d94ee695a4b3ad28f5d590c4
Ondřej Čertík <ondrej@certik.us>
2020-08-07 14:46:02 -0600

    Create a gz tarball

--- ci/create_source_tarball.sh
+++ ci/create_source_tarball.sh
@@ -9,4 +9,4 @@ dest=lfortran-$lfortran_version
 mkdir $dest
 cp -r src cmake examples CMakeLists.txt README.md LICENSE $dest
 mkdir dist
-tar cjf dist/$dest.tar.bz2 $dest
+tar czf dist/$dest.tar.gz $dest
