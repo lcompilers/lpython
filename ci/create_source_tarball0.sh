926cff50c526033918104ee6344a09d1311339b2
Ondřej Čertík <ondrej@certik.us>
2020-08-07 15:48:16 -0600

    Use the cmake -E correctly

--- ci/create_source_tarball0.sh
+++ ci/create_source_tarball0.sh
@@ -3,7 +3,14 @@
 set -ex
 
 cmake -E make_directory $dest
-cmake -E copy src cmake examples CMakeLists.txt README.md LICENSE version $dest
+
+# Directories:
+cmake -E copy_directory src $dest/src
+cmake -E copy_directory cmake $dest/cmake
+cmake -E copy_directory examples $dest/examples
+# Files:
+cmake -E copy CMakeLists.txt README.md LICENSE version $dest
+
 cmake -E make_directory dist
 cmake -E tar cfz dist/$dest.tar.gz $dest
 cmake -E remove_directory $dest
