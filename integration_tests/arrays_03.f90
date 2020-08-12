411cbe5849244d4ccd849a09d44b703944c9b7cf
Ondřej Čertík <ondrej@certik.us>
2020-08-12 15:16:59 -0600

    Add an arrays_03 test

---
+++ integration_tests/arrays_03.f90
@@ -0,0 +1,21 @@
+program arrays_03
+implicit none
+integer :: x(10), s, i
+do i = 1, size(x)
+    x(i) = i
+end do
+s = mysum(x)
+print *, s
+if (s /= 55) error stop
+
+contains
+
+    integer function mysum(a) result(r)
+    integer, intent(in) :: a(:)
+    integer :: i
+    r = 0
+    do i = 1, size(a)
+        r = r + a(i)
+    end do
+    end function
+end
