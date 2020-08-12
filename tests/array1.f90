b4207c88b6408126c1127f23f4d1ed1084a9bad8
Ondřej Čertík <ondrej@certik.us>
2020-08-11 21:32:06 -0600

    Add multidimensional arrays, update tests

--- tests/array1.f90
+++ tests/array1.f90
@@ -1,6 +1,6 @@
 program array1
 implicit none
-real :: d, e(5)
-integer :: f, g(3)
-logical :: a, b(2)
+real :: d, e(5), e2(2, 3), e3(1, 2, 3)
+integer :: f, g(3), g2(6, 3), g3(4, 3, 2)
+logical :: a, b(2), b2(3, 2), b3(2, 1, 2)
 end program
