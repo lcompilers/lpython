3c248a290afdcdab7c93c5541125601801168c54
Ondřej Čertík <ondrej@certik.us>
2020-08-11 21:34:58 -0600

    Test more dimensions in array2

--- tests/array2.f90
+++ tests/array2.f90
@@ -1,5 +1,16 @@
 program array2
 implicit none
+
 real, dimension(5) :: a, b
 integer, dimension(3) :: c
+logical, dimension(2) :: d
+
+real, dimension(2,3) :: e
+integer, dimension(3,4) :: f
+logical, dimension(5,2) :: g
+
+real, dimension(2,3,4) :: h
+integer, dimension(3,4,3) :: i
+logical, dimension(5,2,2) :: j
+
 end program
