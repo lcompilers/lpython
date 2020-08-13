e4f1b29f00c4c61bb45e45e799b333f03218c753
Ondřej Čertík <ondrej@certik.us>
2020-08-13 12:51:03 -0600

    Add more comparison tests

--- integration_tests/types_06.f90
+++ integration_tests/types_06.f90
@@ -10,4 +10,29 @@ if (r < r) error stop
 if (r < i) error stop
 if (i < r) error stop
 
+if (i > i) error stop
+if (r > r) error stop
+if (r > i) error stop
+if (i > r) error stop
+
+if (i /= i) error stop
+if (r /= r) error stop
+if (r /= i) error stop
+if (i /= r) error stop
+
+if (i+1 <= i) error stop
+if (r+1 <= r) error stop
+if (r+1 <= i) error stop
+if (i+1 <= r) error stop
+
+if (i >= i+1) error stop
+if (r >= r+1) error stop
+if (r >= i+1) error stop
+if (i >= r+1) error stop
+
+if (i == i+1) error stop
+if (r == r+1) error stop
+if (r == i+1) error stop
+if (i == r+1) error stop
+
 end program
