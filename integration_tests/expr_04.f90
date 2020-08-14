099874252c0a87bf278aede8a4eb48341130fdc0
Ondřej Čertík <ondrej@certik.us>
2020-08-13 21:11:22 -0600

    Add an integration test for a power

---
+++ integration_tests/expr_04.f90
@@ -0,0 +1,12 @@
+program expr_04
+implicit none
+integer :: x
+!x = 2**4;
+!if (x /= 16) error stop
+x = 2.**4;
+if (x /= 16) error stop
+x = 2**4.;
+if (x /= 16) error stop
+x = 2.**4.;
+if (x /= 16) error stop
+end program
