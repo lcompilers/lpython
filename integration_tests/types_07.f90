646a647d6f85a6c73dc378fb356c074181b2c902
Ondřej Čertík <ondrej@certik.us>
2020-08-16 12:33:16 -0600

    Test len=* also

--- integration_tests/types_07.f90
+++ integration_tests/types_07.f90
@@ -3,12 +3,14 @@ implicit none
 
 contains
 
-    subroutine f(s)
+    subroutine f(s, s2)
     character(len=:), allocatable, intent(in) :: s
+    character(:), allocatable, intent(in) :: s2
     end subroutine
 
-    subroutine g(s)
+    subroutine g(s, s2)
     character(len=*), intent(in) :: s
+    character(*), intent(in) :: s2
     end subroutine
 
 end program
