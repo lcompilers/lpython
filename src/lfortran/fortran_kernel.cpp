1b27d63b3562234c121b86329f4db77a47dbd7b2
Ondřej Čertík <ondrej@certik.us>
2020-08-21 16:25:46 -0600

    Add an initial Jupyter kernel

---
+++ src/lfortran/fortran_kernel.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+
+#include <xeus/xinterpreter.hpp>
+#include <xeus/xkernel.hpp>
+#include <xeus/xkernel_configuration.hpp>
+
+#include <nlohmann/json.hpp>
+
+namespace nl = nlohmann;
+
+namespace LFortran
+{
+    class custom_interpreter : public xeus::xinterpreter
+    {
+    public:
+
+        custom_interpreter() = default;
+        virtual ~custom_interpreter() = default;
+
+    private:
+
+        void configure_impl() override;
+
+        nl::json execute_request_impl(int execution_counter,
+                                      const std::string& code,
+                                      bool silent,
+                                      bool store_history,
+                                      nl::json user_expressions,
+                                      bool allow_stdin) override;
+
+        nl::json complete_request_impl(const std::string& code,
+                                       int cursor_pos) override;
+
+        nl::json inspect_request_impl(const std::string& code,
+                                      int cursor_pos,
+                                      int detail_level) override;
+
+        nl::json is_complete_request_impl(const std::string& code) override;
+
+        nl::json kernel_info_request_impl() override;
+
+        void shutdown_request_impl() override;
+    };
+
+
+    nl::json custom_interpreter::execute_request_impl(int execution_counter, // Typically the cell number
+                                                      const std::string& /*code*/, // Code to execute
+                                                      bool /*silent*/,
+                                                      bool /*store_history*/,
+                                                      nl::json /*user_expressions*/,
+                                                      bool /*allow_stdin*/)
+    {
+        // You can use the C-API of your target language for executing the code,
+        // e.g. `PyRun_String` for the Python C-API
+        //      `luaL_dostring` for the Lua C-API
+
+        // Use this method for publishing the execution result to the client,
+        // this method takes the ``execution_counter`` as first argument,
+        // the data to publish (mime type data) as second argument and metadata
+        // as third argument.
+        // Replace "Hello World !!" by what you want to be displayed under the execution cell
+        nl::json pub_data;
+        pub_data["text/plain"] = "Hello World !!";
+        publish_execution_result(execution_counter, std::move(pub_data), nl::json::object());
+
+        // You can also use this method for publishing errors to the client, if the code
+        // failed to execute
+        // publish_execution_error(error_name, error_value, error_traceback);
+        publish_execution_error("TypeError", "123", {"!@#$", "*(*"});
+
+        nl::json result;
+        result["status"] = "ok";
+        return result;
+    }
+
+    void custom_interpreter::configure_impl()
+    {
+        // Perform some operations
+    }
+
+    nl::json custom_interpreter::complete_request_impl(const std::string& code,
+                                                       int cursor_pos)
+    {
+        nl::json result;
+
+        // Code starts with 'H', it could be the following completion
+        if (code[0] == 'H')
+        {
+            result["status"] = "ok";
+            result["matches"] = {"Hello", "Hey", "Howdy"};
+            result["cursor_start"] = 5;
+            result["cursor_end"] = cursor_pos;
+        }
+        // No completion result
+        else
+        {
+            result["status"] = "ok";
+            result["matches"] = nl::json::array();
+            result["cursor_start"] = cursor_pos;
+            result["cursor_end"] = cursor_pos;
+        }
+
+        return result;
+    }
+
+    nl::json custom_interpreter::inspect_request_impl(const std::string& code,
+                                                      int /*cursor_pos*/,
+                                                      int /*detail_level*/)
+    {
+        nl::json result;
+
+        if (code.compare("print") == 0)
+        {
+            result["found"] = true;
+            result["text/plain"] = "Print objects to the text stream file, [...]";
+        }
+        else
+        {
+            result["found"] = false;
+        }
+
+        result["status"] = "ok";
+        return result;
+    }
+
+    nl::json custom_interpreter::is_complete_request_impl(const std::string& /*code*/)
+    {
+        nl::json result;
+
+        // if (is_complete(code))
+        // {
+            result["status"] = "complete";
+        // }
+        // else
+        // {
+        //    result["status"] = "incomplete";
+        //    result["indent"] = 4;
+        //}
+
+        return result;
+    }
+
+    nl::json custom_interpreter::kernel_info_request_impl()
+    {
+        nl::json result;
+        result["implementation"] = "my_kernel";
+        result["implementation_version"] = "0.1.0";
+        result["language_info"]["name"] = "python";
+        result["language_info"]["version"] = "3.7";
+        result["language_info"]["mimetype"] = "text/x-python";
+        result["language_info"]["file_extension"] = ".py";
+        return result;
+    }
+
+    void custom_interpreter::shutdown_request_impl() {
+        std::cout << "Bye!!" << std::endl;
+    }
+
+    int run_kernel(const std::string &connection_filename)
+    {
+        // Load configuration file
+        xeus::xconfiguration config = xeus::load_configuration(connection_filename);
+
+        // Create interpreter instance
+        using interpreter_ptr = std::unique_ptr<custom_interpreter>;
+        interpreter_ptr interpreter = interpreter_ptr(new custom_interpreter());
+
+        // Create kernel instance and start it
+        xeus::xkernel kernel(config, xeus::get_user_name(), std::move(interpreter));
+        kernel.start();
+
+        return 0;
+    }
+
+}
