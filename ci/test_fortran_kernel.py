c5231596ce5c09073a1f7685e1636f1dfd79b83e
Ondřej Čertík <ondrej@certik.us>
2020-08-21 20:59:10 -0600

    Add a test for the Fortran kernel

---
+++ ci/test_fortran_kernel.py
@@ -0,0 +1,25 @@
+import unittest
+import jupyter_kernel_test as jkt
+
+class IRKernelTests(jkt.KernelTests):
+    kernel_name = "fortran"
+
+    language_name = "fortran"
+
+    file_extension = ".f90"
+
+    #code_hello_world = 'print *, "hello, world"'
+
+    #complete_code_samples = ['1', 'print *, "hello, world"',
+    #        "integer :: i"]
+    #incomplete_code_samples = ["subroutine f("]
+
+    code_generate_error = "1x"
+
+    code_execute_result = [
+        {'code': "1+2+3", 'result': "6"},
+        {'code': "1+2", 'result': "3"},
+    ]
+
+if __name__ == '__main__':
+    unittest.main()
