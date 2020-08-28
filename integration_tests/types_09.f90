3a6431856754ceed081dc05b11101426e823327b
Ondřej Čertík <ondrej@certik.us>
2020-08-28 09:28:13 -0600

    Add AST test for types_09
    We comment out the hex constant, that is not supported yet.

--- integration_tests/types_09.f90
+++ integration_tests/types_09.f90
@@ -2,7 +2,7 @@ program types_09
 use iso_c_binding, only: c_char
 implicit none
 integer, parameter :: ucs4=selected_char_kind("ISO_10646")
-character(1,ucs4), parameter :: nen=char(int(Z'5e74'),ucs4)
+!character(1,ucs4), parameter :: nen=char(int(Z'5e74'),ucs4)
 
 contains
 
