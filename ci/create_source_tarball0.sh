85fa4898409de22cab1bd83da3eb951dc38d0a8b
Ondřej Čertík <ondrej@certik.us>
2020-08-27 18:28:58 -0600

    Improve documentation in create_source_tarball0

--- ci/create_source_tarball0.sh
+++ ci/create_source_tarball0.sh
@@ -4,14 +4,16 @@ set -ex
 
 cmake -E make_directory $dest
 
-# Directories:
+# Copy Directories:
 cmake -E copy_directory src $dest/src
 cmake -E copy_directory share $dest/share
 cmake -E copy_directory cmake $dest/cmake
 cmake -E copy_directory examples $dest/examples
-# Files:
+
+# Copy Files:
 cmake -E copy CMakeLists.txt README.md LICENSE version $dest
 
+# Create the tarball
 cmake -E make_directory dist
 cmake -E tar cfz dist/$dest.tar.gz $dest
 cmake -E remove_directory $dest
