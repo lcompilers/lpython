d6a1beac1ba5e0964db8b44976ab89527bb22fd6
Ondřej Čertík <ondrej@certik.us>
2020-08-28 16:30:33 -0600

    Add a test

---
+++ integration_tests/program_02.f90
@@ -0,0 +1,18 @@
+module program_02_mod
+implicit none
+end
+
+program program_02
+use program_02_mod
+implicit none
+
+contains
+
+    subroutine f1()
+    end
+
+    integer function f2()
+    f2 = 3
+    end
+
+end
