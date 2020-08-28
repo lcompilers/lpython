e6b05dd5168ababa7444eee8bfa9f50e9faea250
Ondřej Čertík <ondrej@certik.us>
2020-08-28 08:49:31 -0600

    Add more tests

--- tests/write1.f90
+++ tests/write1.f90
@@ -1,4 +1,5 @@
 write(*,*) "a", 2
+write(*,*)
 write(*) a
 write(out_str,'(" Found index ", i9," but item is not in list.")') index
 write(out_string,*) 'Scalar_in = ', scalar_in
@@ -9,3 +10,8 @@ write(unit=*, fmt='(a)') trim(prog) // ': ' // message
 write(unit=0,fmt='(a,i4.4)') "Assertion failed at " // file // ":", line
 write(*,fmt='(a,l1)') 'exactly_conservative= ',exactly_conservative
 write(unit=lun) vec
+write(*,'(a,a)',advance='no') trim(input_string),": "
+
+print *, "2"
+print *
+print "(a, es22.14)", "Ekin:     ", Ekin
