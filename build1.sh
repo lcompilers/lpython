e3dcc4aada8aa7c6ca34395ea42544eb2bd9e833
Ondřej Čertík <ondrej@certik.us>
2020-08-30 08:19:05 -0600

    Update the build line

--- build1.sh
+++ build1.sh
@@ -7,7 +7,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
     -DWITH_STACKTRACE=yes \
-    -DCMAKE_PREFIX_PATH=$CONDA_PREFIX \
+    -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH;$CONDA_PREFIX" \
     -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
     .
 cmake --build . --target install
