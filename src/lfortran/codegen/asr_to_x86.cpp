866f4be8408bfe0b272368fabd319461c203e1c7
Ondřej Čertík <ondrej@certik.us>
2020-09-02 13:51:20 -0600

    Implement an initial print function

--- src/lfortran/codegen/asr_to_x86.cpp
+++ src/lfortran/codegen/asr_to_x86.cpp
@@ -13,11 +13,18 @@
 
 namespace LFortran {
 
+// Platform dependent fast unique hash:
+uint64_t static get_hash(ASR::asr_t *node)
+{
+    return (uint64_t)node;
+}
+
 class ASRToX86Visitor : public ASR::BaseVisitor<ASRToX86Visitor>
 {
 public:
     Allocator &m_al;
     X86Assembler m_a;
+    std::map<std::string,std::string> m_global_strings;
 public:
 
     ASRToX86Visitor(Allocator &al) : m_al{al}, m_a{al} {}
@@ -35,20 +42,35 @@ public:
     }
 
     void visit_Program(const ASR::Program_t &x) {
-        /*
+
+        emit_elf32_header(m_a);
+
+        emit_exit(m_a, "exit");
+
+        m_a.add_label("_start");
+
         for (size_t i=0; i<x.n_body; i++) {
             this->visit_stmt(*x.m_body[i]);
         }
-        */
-        std::string msg = "Hello World!\n";
 
-        emit_elf32_header(m_a);
-        m_a.add_label("_start");
-        emit_print(m_a, "msg", msg.size());
         m_a.asm_call_label("exit");
-        emit_exit(m_a, "exit");
-        emit_data_string(m_a, "msg", msg);
+
+        for (auto &s : m_global_strings) {
+            emit_data_string(m_a, s.first, s.second);
+        }
+
         emit_elf32_footer(m_a);
+    }
+
+    void visit_Print(const ASR::Print_t &x) {
+        LFORTRAN_ASSERT(x.n_values == 1);
+        ASR::expr_t *e = x.m_values[0];
+        ASR::Str_t *s = EXPR_STR((ASR::asr_t*)e);
+        std::string msg = s->m_s;
+        msg += "\n";
+        std::string id = "string" + std::to_string(get_hash((ASR::asr_t*)e));
+        emit_print(m_a, id, msg.size());
+        m_global_strings[id] = msg;
     }
 
 
@@ -62,7 +84,6 @@ void asr_to_x86(ASR::TranslationUnit_t &asr, Allocator &al,
     pass_wrap_global_stmts_into_function(al, asr, "f");
     pass_replace_do_loops(al, asr);
     v.visit_asr((ASR::asr_t&)asr);
-
 
     v.m_a.verify();
 
