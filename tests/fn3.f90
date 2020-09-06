fcf2519518209effc3216735554de6103cef40be
Ondřej Čertík <ondrej@certik.us>
2020-09-06 11:35:27 -0600

    Add a test

---
+++ tests/fn3.f90
@@ -0,0 +1,15 @@
+recursive pure integer function a()
+end function
+
+pure integer elemental function b()
+end function
+
+elemental pure integer function c()
+end function
+
+elemental pure function cb()
+integer cb
+end function
+
+integer pure recursive function d()
+end function
