f55bf7f97dee5d28b966fa2d4110e638b2a277b3
Ondřej Čertík <ondrej@certik.us>
2020-09-06 15:42:46 -0600

    Add a test for subroutione prefixes

---
+++ tests/subroutine8.f90
@@ -0,0 +1,14 @@
+recursive pure subroutine a()
+end subroutine
+
+pure elemental subroutine b()
+end subroutine
+
+elemental pure subroutine c()
+end subroutine
+
+pure recursive subroutine d()
+end subroutine
+
+impure elemental module subroutine e()
+end subroutine
