7d16ecca10daf52bf6eef61f4e55ce08a382007c
Ondřej Čertík <ondrej@certik.us>
2020-09-11 15:17:36 -0600

    Implement down_cast()

--- src/lfortran/tests/test_ast.cpp
+++ src/lfortran/tests/test_ast.cpp
@@ -14,11 +14,28 @@ TEST_CASE("Operator types") {
     std::cout << "OK: " << ASR::operatorType::Pow  << std::endl;
 }
 
+template <class T, class U>
+inline bool is_a2(const U &x)
+{
+    return T::class_type == x.type;
+}
+
+template <class T>
+static inline T* down_cast2(const AST::ast_t *f)
+{
+    LFORTRAN_ASSERT(f->type == AST::astType::expr);
+    //LFORTRAN_ASSERT(is_a2<AST::expr_t>(*f));
+    AST::expr_t *t = (AST::expr_t *)f;
+    LFORTRAN_ASSERT(is_a2<T>(*t));
+    return (T*)t;
+}
+
+
 TEST_CASE("Test types") {
     Allocator al(1024*1024);
     Location loc;
     AST::ast_t &a = *AST::make_Num_t(al, loc, 5);
-    AST::Num_t &x = (AST::Num_t &)a;
+    AST::Num_t &x = *down_cast2<AST::Num_t>(&a);
 
     CHECK(AST::is_a<AST::Num_t>(x));
     CHECK(! AST::is_a<AST::BinOp_t>(x));
