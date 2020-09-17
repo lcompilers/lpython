0fa3b7c2e52da7010a98eadc9d2cd4c0b555d06e
Ondřej Čertík <ondrej@certik.us>
2020-09-16 22:31:53 -0600

    Implement %%showasm

--- src/lfortran/codegen/evaluator.h
+++ src/lfortran/codegen/evaluator.h
@@ -50,6 +50,7 @@ public:
     int64_t intfn(const std::string &name);
     float floatfn(const std::string &name);
     void voidfn(const std::string &name);
+    std::string get_asm(llvm::Module &m);
     void save_asm_file(llvm::Module &m, const std::string &filename);
     void save_object_file(llvm::Module &m, const std::string &filename);
     static std::string module_to_string(llvm::Module &m);
@@ -84,6 +85,7 @@ public:
     std::string get_ast(const std::string &code);
     std::string get_asr(const std::string &code);
     std::string get_llvm(const std::string &code);
+    std::string get_asm(const std::string &code);
 
 private:
     Allocator al;
