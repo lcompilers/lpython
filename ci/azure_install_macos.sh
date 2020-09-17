cfe860d8ddb89c53eb2c231292c8de6693bfa393
Ondřej Čertík <ondrej@certik.us>
2020-09-17 14:27:30 -0600

    Install with XEUS on all platforms
    Add the necessary dependencies.

--- ci/azure_install_macos.sh
+++ ci/azure_install_macos.sh
@@ -5,7 +5,7 @@ set -ex
 conda config --set always_yes yes --set changeps1 no
 conda info -a
 conda update -q conda
-conda install -c conda-forge python=3.7 re2c bison m4 xonsh scikit-build llvmdev=9.0.1 toml cmake=3.17.0
+conda install -c conda-forge python=3.7 re2c bison m4 xonsh scikit-build llvmdev=9.0.1 toml cmake=3.17.0 jupyter pytest xeus xtl nlohmann_json cppzmq jupyter_kernel_test
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 export CONDA_PREFIX=/usr/local/miniconda
 export LFORTRAN_CMAKE_GENERATOR="Unix Makefiles"
