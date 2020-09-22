fb21023b0a6afd30747e6e11460c91f8d6e77fc6
Ondřej Čertík <ondrej@certik.us>
2020-09-22 13:24:42 -0600

    Add tests for GFortran module files

---
+++ tests/interop/generate_mod_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+gfortran-7 -c mod1.f90 -o /tmp/a.o
+mv mod1.mod mod1-14.mod
+cp mod1-14.mod mod1-14-unpacked.mod.gz
+gzip -df mod1-14-unpacked.mod.gz
+
+gfortran-10 -c mod1.f90 -o /tmp/a.o
+mv mod1.mod mod1-15.mod
+cp mod1-15.mod mod1-15-unpacked.mod.gz
+gzip -df mod1-15-unpacked.mod.gz
