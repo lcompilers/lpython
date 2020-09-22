ccd866785a33d235c8400c1da06c2d259a7f6db5
Ondřej Čertík <ondrej@certik.us>
2020-09-22 01:59:40 -0600

    Port get_llvm()

--- src/lfortran/codegen/evaluator.h
+++ src/lfortran/codegen/evaluator.h
@@ -129,7 +129,7 @@ public:
     std::string get_ast(const std::string &code);
     Result<std::string> get_asr(const std::string &code);
     Result<ASR::TranslationUnit_t*> get_asr2(const std::string &code);
-    std::string get_llvm(const std::string &code);
+    Result<std::string> get_llvm(const std::string &code);
     Result<std::unique_ptr<LLVMModule>> get_llvm2(const std::string &code);
     std::string get_asm(const std::string &code);
     std::string get_cpp(const std::string &code);
