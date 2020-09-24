56a0bfd71f92a00eeed604cf973bb9988a9d381c
Ondřej Čertík <ondrej@certik.us>
2020-09-24 14:18:12 -0600

    Link with zlib

---
+++ cmake/FindZLIB.cmake
@@ -0,0 +1,11 @@
+find_path(ZLIB_INCLUDE_DIR zlib.h)
+find_library(ZLIB_LIBRARY z)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ZLIB DEFAULT_MSG ZLIB_INCLUDE_DIR ZLIB_LIBRARY)
+
+add_library(p::zlib INTERFACE IMPORTED)
+set_property(TARGET p::zlib PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+    ${ZLIB_INCLUDE_DIR})
+set_property(TARGET p::zlib PROPERTY INTERFACE_LINK_LIBRARIES
+    ${ZLIB_LIBRARY})
