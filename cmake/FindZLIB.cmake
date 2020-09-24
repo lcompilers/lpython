251919d923db4432ab6cb58932b1d39311f33d09
Ondřej Čertík <ondrej@certik.us>
2020-09-24 15:19:37 -0600

    Prefer static library if available

--- cmake/FindZLIB.cmake
+++ cmake/FindZLIB.cmake
@@ -1,5 +1,5 @@
 find_path(ZLIB_INCLUDE_DIR zlib.h)
-find_library(ZLIB_LIBRARY z)
+find_library(ZLIB_LIBRARY NAMES libz.a z)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ZLIB DEFAULT_MSG ZLIB_INCLUDE_DIR ZLIB_LIBRARY)
@@ -9,3 +9,6 @@ set_property(TARGET p::zlib PROPERTY INTERFACE_INCLUDE_DIRECTORIES
     ${ZLIB_INCLUDE_DIR})
 set_property(TARGET p::zlib PROPERTY INTERFACE_LINK_LIBRARIES
     ${ZLIB_LIBRARY})
+
+message("ZLIB_INCLUDE_DIR: ${ZLIB_INCLUDE_DIR}")
+message("ZLIB_LIBRARY: ${ZLIB_LIBRARY}")
