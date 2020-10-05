cf77b9b5ce10a8d0cf7e9f29c4970b58d1510736
Ondřej Čertík <ondrej@certik.us>
2020-10-05 15:27:47 -0600

    Add more tests for character*N

--- integration_tests/types_13.f90
+++ integration_tests/types_13.f90
@@ -1,11 +1,35 @@
 program types_13
-use iso_c_binding, only: c_int, c_double
+use iso_c_binding, only: c_int, c_double, c_char
 implicit none
 
 interface
     subroutine g(d)
     character*(*) :: d
     end subroutine
+
+    subroutine g2(d)
+    character(len=*) :: d
+    end subroutine
+
+    subroutine g3(d)
+    import :: c_char
+    character(len=*, kind=c_char) :: d
+    end subroutine
+
+    subroutine g4(d)
+    import :: c_char
+    character(*, kind=c_char) :: d
+    end subroutine
+
+    subroutine g5(d)
+    import :: c_char
+    character(kind=c_char, len=*) :: d
+    end subroutine
+
+    subroutine g6(d)
+    character*5 :: d
+    end subroutine
+
 
     integer(c_int) function f1(a, b, c, d) result(r) bind(c)
     import :: c_int, c_double
