bb61536c5055c28c0fc1a7b1852c4274aa2a7de0
Ondřej Čertík <ondrej@certik.us>
2020-10-05 16:46:39 -0600

    Add tests for `:` and an expression for `len`

--- tests/fmt1.f90
+++ tests/fmt1.f90
@@ -6,3 +6,13 @@ character(len=*) c
 character(len=*, kind=c_char) c
 character(*, kind=c_char) c
 character(kind=c_char, len=*) c
+character(:) c
+character(len=:) c
+character(len=:, kind=c_char) c
+character(:, kind=c_char) c
+character(kind=c_char, len=:) c
+character(n) c
+character(len=n) c
+character(len=n, kind=c_char) c
+character(n, kind=c_char) c
+character(kind=c_char, len=n) c
