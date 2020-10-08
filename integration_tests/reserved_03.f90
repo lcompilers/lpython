bb876beb2994ed81bb6b7df4c0fb17bd0fc473f2
Ondřej Čertík <ondrej@certik.us>
2020-10-08 14:01:18 -0600

    Add another test

---
+++ integration_tests/reserved_03.f90
@@ -0,0 +1,14 @@
+program reserved_03
+implicit none
+integer endsubroutine
+call integer
+if (endsubroutine /= 1) error stop
+
+contains
+
+subroutine integer
+integer subroutine
+endsubroutine = subroutine/subroutine
+endsubroutine
+
+end program
