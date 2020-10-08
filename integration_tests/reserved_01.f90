50818a8bad00564f8a89b28fe7e102064387dfd6
Ondřej Čertík <ondrej@certik.us>
2020-10-08 13:35:41 -0600

    Add tests for keyword identifiers

---
+++ integration_tests/reserved_01.f90
@@ -0,0 +1,15 @@
+program reserved_01
+implicit none
+if (end(1, 2) /= 2) error stop
+if (end(2, 1) /= 2) error stop
+
+contains
+
+integer function end(return,&
+integer)result(function);;;;&
+integer integer,return;;;;;;&
+;;function=return;if(return>&
+integer)return;;;;;function=&
+integer;;end function end;;;;
+
+end program
---
+++ integration_tests/reserved_02.f90
@@ -0,0 +1,13 @@
+program reserved_02
+implicit none
+if (integer() /= 1) error stop
+
+contains
+
+integer function integer()
+integer function
+function = function/function
+integer = function
+end function
+
+end program
