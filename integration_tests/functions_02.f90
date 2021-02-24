d587a2d2fb277420a20d3efc17399b0848eede18
Ondřej Čertík <ondrej@certik.us>
2021-02-24 11:39:49 -0700

    Use myabs() for testing

--- integration_tests/functions_02.f90
+++ integration_tests/functions_02.f90
@@ -7,7 +7,7 @@ f = 3.5
 
 g = mysum(i, f)
 
-print *, g
+if (myabs(g-4.5) > 1e-5) error stop
 
 contains
 
@@ -15,6 +15,15 @@ contains
     integer, intent(in) :: a
     real, intent(in) :: b
     c = a + b
+    end function
+
+    real function myabs(a) result(c)
+    real, intent(in) :: a
+    if (a >= 0) then
+        c = a
+    else
+        c = -a
+    end if
     end function
 
 end program
