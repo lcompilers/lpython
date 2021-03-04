f4bb5de222a8b10cf2fa656d4d3bd879ca60efb8
Ondřej Čertík <ondrej@certik.us>
2021-03-03 22:05:27 -0700

    Add a comment how to disable exceptions in doctest

--- src/tests/doctest.cpp
+++ src/tests/doctest.cpp
@@ -1,2 +1,7 @@
+// Uncomment the following line to stop catching exceptions and segfaults, thus
+// allowing to use gdb to figure out where the code fails (also put this line
+// into every test at the top):
+//#define DOCTEST_CONFIG_NO_EXCEPTIONS
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
