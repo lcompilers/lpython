1583c3010efaf0d89f1d678814cc2774c322cf32
czgdp1807 <gdp.1807@gmail.com>
2021-03-08 19:38:52 +0530

    removed 128 bit float

--- integration_tests/real_dp_param.f90
+++ integration_tests/real_dp_param.f90
@@ -1,11 +1,9 @@
 program real_dp_param
 
     integer, parameter :: prec1 = 4, prec2 = 8
-    integer, parameter :: prec3 = 16
     real(prec1) :: u = 1.05
     real(prec2) :: v = 1.05
-    real(prec3) :: w = 1.05
-    real(prec2) :: zero = 0.0
-    print *, u, v, w, zero
+    real(prec2) :: zero = 0.0_4
+    print *, u, v, zero
 
 end program
