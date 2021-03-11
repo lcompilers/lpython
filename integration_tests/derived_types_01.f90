078775d193ff2d755fa88a3177ddd4632eab9f08
Ondřej Čertík <ondrej@certik.us>
2021-03-11 11:25:13 -0700

    Improve the derived type test
    Update tests.

--- integration_tests/derived_types_01.f90
+++ integration_tests/derived_types_01.f90
@@ -3,12 +3,26 @@ implicit none
 
 type :: X
     integer :: i
+    real :: r
 end type
+
+contains
+
+subroutine set(a)
+type(X), intent(out) :: a
+a%i = 1
+a%r = 1.5
+end subroutine
 
 end module
 
 program derived_types_01
-use a_01, only: X
+use a_01, only: X, set
 implicit none
 type(X) :: b
+b%i = 5
+b%r = 3.5
+print *, b%i, b%r
+call set(b)
+print *, b%i, b%r
 end
