97705030ef69d445ba88b1939848db46264387b5
Ondřej Čertík <ondrej@certik.us>
2021-03-16 23:40:52 -0600

    Implement bool printing

--- grammar/asdl_cpp.py
+++ grammar/asdl_cpp.py
@@ -585,6 +585,12 @@ class PickleVisitorVisitor(ASDLVisitor):
                     self.emit("}", 2)
                 else:
                     self.emit('s.append(std::to_string(x.m_%s));' % field.name, 2)
+            elif field.type == "bool" and not field.seq and not field.opt:
+                self.emit("if (x.m_%s) {" % field.name, 2)
+                self.emit(    's.append(".true.");', 3)
+                self.emit("} else {", 2)
+                self.emit(    's.append(".false.");', 3)
+                self.emit("}", 2)
             elif field.type in self.data.simple_types:
                 if field.opt:
                     self.emit('s.append("Unimplementedopt");', 2)
