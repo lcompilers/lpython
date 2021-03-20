2c9d3c93f05287f37575632cdb82e76f173458be
Ondřej Čertík <ondrej@certik.us>
2021-03-20 11:57:45 -0600

    Fix the bisection to work in all cases

--- src/lfortran/stacktrace.cpp
+++ src/lfortran/stacktrace.cpp
@@ -562,32 +562,28 @@ void address_to_line_number(const std::vector<std::string> &filenames,
           std::string &filename,
           int &line_number) {
     int n = addresses.size() / 3;
-    // Bisection-Search 
-    int start_ind =0, end_ind = n-1;
-
-    int mid = (start_ind + end_ind)/2;
-
-
-    if ( addresses[0] > (address-8) ) {
-        filename = filenames[addresses[2]];
-        line_number = addresses[1];
-        return;
+    // Bisection-Search
+    int start_ind = 0, end_ind = n-1;
+    if (addresses[3*start_ind] > (address-8)) {
+      line_number = addresses[3*start_ind+1];
+      filename = filenames[addresses[3*start_ind+2]];
+      return;
     }
-
-    while (start_ind <= end_ind) {
-
-      if ( addresses[3*mid] > (address-8) ) {
-        filename = filenames[addresses[3*mid+2]];
-        line_number = addresses[3*mid + 1];
-        return;
-      }
-      else{
-        start_ind = mid+1;
-      }
-      mid = (start_ind + end_ind)/2;
+    if (addresses[3*end_ind] < (address-8)) {
+      line_number = -1;
+      filename = "";
+      return;
     }
-    filename = "";
-    line_number = -1;
+    while (start_ind+1 < end_ind) {
+      int mid = (start_ind + end_ind)/2;
+      if (addresses[3*mid] > (address-8)) {
+        end_ind = mid;
+      } else {
+        start_ind = mid;
+      }
+    }
+    line_number = addresses[3*end_ind+1];
+    filename = filenames[addresses[3*end_ind+2]];
 }
 // address_to_line_number_l this is linear search
 void address_to_line_number_l(const std::vector<std::string> &filenames,
