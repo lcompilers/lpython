384f16a876c5088c74982ddaa55979936373bf4e
czgdp1807 <gdp.1807@gmail.com>
2021-03-22 12:51:14 +0530

    modified tests

--- integration_tests/int_dp.f90
+++ integration_tests/int_dp.f90
@@ -1,11 +1,7 @@
 program int_dp
 
-    integer(4) :: zero
-    integer(8) :: v
-    integer :: x
-    zero = 0.0
-    v = 1.05
-    x = 1.05
-    print *, x, v, zero
+    integer(4) :: u = 2147483647
+    integer(8) :: v = 2147483658
+    print *, x, v
 
 end program
--- integration_tests/int_dp_param.f90
+++ integration_tests/int_dp_param.f90
@@ -1,9 +1,8 @@
 program int_dp_param
 
     integer, parameter :: prec1 = 4, prec2 = 8
-    integer(prec1) :: u = 1.05
-    integer(prec2) :: v = 1.05
-    integer(prec2) :: zero = 0.0_4
-    print *, u, v, zero
+    integer(prec1) :: u = 2147483647
+    integer(prec2) :: v = 2147483658
+    print *, u, v
 
 end program
