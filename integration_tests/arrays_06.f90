63c03d9a1c69f3bdcea75b25a7fca565b2f6ef3b
Ondřej Čertík <ondrej@certik.us>
2021-04-01 10:32:03 -0600

    Add more tests for generators

--- integration_tests/arrays_06.f90
+++ integration_tests/arrays_06.f90
@@ -1,7 +1,8 @@
 program arrays_06
 implicit none
 real, dimension(6) :: x
-integer :: i
+real, dimension(10,10) :: A
+integer :: i, j
 x = [(i*2, i = 1, 6)]
 print *, x
 x = [(i+1, i*2, i = 1, 3)]
@@ -10,4 +11,6 @@ x = [(i+1, i**2, i*2, i = 1, 2)]
 print *, x
 x = [(2*i, 3*i, 4*i, i+1, i**2, i*2, i = 2, 2)]
 print *, x
+A = 3
+print '("Matrix A"/(10F8.2))', ((A(i,j), i = 1, 10), j = 1, 10)
 end program
