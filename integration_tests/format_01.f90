590f96ef46a49e101a5f46e4f1e9c85642ac73f5
Ondřej Čertík <ondrej@certik.us>
2021-04-01 09:12:29 -0600

    Add more format tests

--- integration_tests/format_01.f90
+++ integration_tests/format_01.f90
@@ -10,4 +10,16 @@ implicit none
     8 format(i6, /)
     9 format(i6,/)
     10 format(/ , /)
+    422 FORMAT( /, 4X, 'Manufactured/Computed Solutions Max Diff=',    &
+                 ES13.6 )
+    158 FORMAT( 4X, 'epsi = ', ES11.4, /, 4X, 'iitm = ', I3, /,        &
+                4X, 'oitm = ', I4, /, 4X, 'timedep = ', I1, /,         &
+                4X, 'swp_typ = ', I1, /, 4X, 'multiswp = ', I1, /,     &
+                4X, 'angcpy = ', I1, /,  4X, 'it_det = ', I1, /,       &
+                4X, 'soloutp = ', I1, /, 4X, 'kplane = ', I4, /,       &
+                4X, 'popout = ', I1, /, 4X, 'fluxp = ', I1, /,         &
+                4X, 'fixup = ', I1, / )
+    161 FORMAT( 'slgg(nmat,nmom,ng,ng) echo', /, 'Column-order loops:',&
+                ' Mats (fastest ), Moments, Groups, Groups (slowest)' )
+    162 FORMAT( 2X, ES15.8, 2X, ES15.8, 2X, ES15.8, 2X, ES15.8 )
 end program
