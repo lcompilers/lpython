ca178729301163b013b67610550693b13db4ec74
Ondřej Čertík <ondrej@certik.us>
2021-04-02 14:38:18 -0600

    Add more tests for read()

--- tests/read1.f90
+++ tests/read1.f90
@@ -1,5 +1,13 @@
+program read1
+! AST only tests
 read(*,*) a, b
+read(unit=*,fmt=*) a, b
+read(*,fmt=*) a, b
 read(*) b
 read(zone(1:3), '(i3)') hour
 read(u) b
 read(u, fmt=x) b, c, e
+read(*, fmt=x) b, c, e
+READ( iunit, NML=invar, IOSTAT=ierr )
+READ( UNIT=iunit, NML=invar, IOSTAT=ierr )
+end program
