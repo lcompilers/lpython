3cbb3e46b9afc74231a32e68974070190822b810
Ondřej Čertík <ondrej@certik.us>
2021-04-08 16:44:36 -0600

    Add another test

--- tests/array6.f90
+++ tests/array6.f90
@@ -1,9 +1,11 @@
 program array6
 implicit none
 contains
-    subroutine t(a, b, c, d)
+    subroutine t(n, a, b, c, d, e)
+    integer, intent(in) :: n
     real :: a(*), b(3:*)
     real, dimension(*) :: c
     real, dimension(4:*) :: d
+    real :: e(n:*)
     end subroutine
 end program
