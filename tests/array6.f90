4736b5828ea6df404c7fa05a26eef378ce97b59b
Ondřej Čertík <ondrej@certik.us>
2021-04-08 16:52:56 -0600

    Add a test with more dimensions

--- tests/array6.f90
+++ tests/array6.f90
@@ -4,8 +4,8 @@ contains
     subroutine t(n, a, b, c, d, e)
     integer, intent(in) :: n
     real :: a(*), b(3:*)
-    real, dimension(*) :: c
-    real, dimension(4:*) :: d
+    real, dimension(3,n,*) :: c
+    real, dimension(3,n,4:*) :: d
     real :: e(n:*)
     end subroutine
 end program
