afa9e3ce1cca08cf5fe689aef530ed5b6d80367d
Ondřej Čertík <ondrej@certik.us>
2021-04-09 21:51:40 -0600

    Add a test for labels
    Both single and multiple line statement labels are tested.

--- integration_tests/doloop_05.f90
+++ integration_tests/doloop_05.f90
@@ -1,10 +1,10 @@
 program doloop_05
 implicit none
 integer :: i, j
-j = 0
-i_loop: do i = 1, 10
-    if (i == 2) cycle i_loop
-    j = j + i
-end do i_loop
-if (j /= 53) error stop
+1 j = 0
+2 i_loop: do i = 1, 10
+3    if (i == 2) cycle i_loop
+4    j = j + i
+5 end do i_loop
+6 if (j /= 53) error stop
 end
