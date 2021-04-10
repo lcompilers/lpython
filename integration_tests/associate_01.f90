f4c063435d2592135f4084a7a92d0fd78d1d19c3
Thirumalai Shaktivel <thirumalaiShaktivel@gmail.com>
2021-04-10 22:50:52 +0530

    refactor and update tests

--- integration_tests/associate_01.f90
+++ integration_tests/associate_01.f90
@@ -4,15 +4,16 @@ real, allocatable :: a(:), b(:,:), c(:,:,:)
 real, pointer :: x(:), y(:,:,:)
 real :: a_1, c_234
 integer :: n
+a_1 = 0
 n = 10
 allocate(a(5))
 allocate(b(n,n), c(n, 5, n))
 1 loop: associate (x => a, y => c)
-    x(1) = x(1) + 5
+    x(1) = a_1 + 5
     y(2,3,4) = 3
 end associate loop
-if(a(1).EQ.5) GO TO 1
 a_1 = a(1)
+if(a(1).EQ.5) GO TO 1
 c_234 = c(2,3,4)
 print *, a_1
 print *, c_234
