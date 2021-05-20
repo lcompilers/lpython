273aa347102944a9c37e9d7f8b1dc6131a1a11f0
czgdp1807 <gdp.1807@gmail.com>
2021-05-20 10:53:04 +0530

    array2.f90 generalised

--- tests/array2.f90
+++ tests/array2.f90
@@ -2,15 +2,18 @@ program array2
 implicit none
 
 real, dimension(5) :: a, b
+complex, dimension(6) :: x
 integer, dimension(3) :: c
 logical, dimension(2) :: d
 
 real, dimension(2,3) :: e
+complex, dimension(6,8) :: y
 integer, dimension(3,4) :: f
 logical, dimension(5,2) :: g
 
-real, dimension(2,3,4) :: h
-integer, dimension(3,4,3) :: i
-logical, dimension(5,2,2) :: j
+real, dimension(2:3,3:4,4:5) :: h
+complex, dimension(6:7,8:10,11:13) :: z
+integer, dimension(3:4,4:5,3:4) :: i
+logical, dimension(5:6,2:3,2:4) :: j
 
 end program
