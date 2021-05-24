2fd727145627d53c3bb09ec6581c5c39d5bfed4c
czgdp1807 <gdp.1807@gmail.com>
2021-05-24 11:50:16 +0530

    refactored implied do loop pass

--- integration_tests/arrays_op_1.f90
+++ integration_tests/arrays_op_1.f90
@@ -1,22 +1,25 @@
 program array_op
 implicit none
 
-integer :: a(3), b(3), c(3)
+integer :: a(4), b(4), c(4)
+integer :: i, j
 
-a = [1, 2, 3]
+i = 0
 
-b = [4, 10, 18]
+a = [i + 1, (i + j, j = 1, 2), i + 4]
 
-c = a + b
-print *, c
+b = [4*a(i + 1), 5*a(i + 2), 6*a(i + 3), 7*a(i + 4)]
 
-c = a - b
-print *, c
+! c = a + b
+! print *, c
 
-c = a*b
-print *, c
+! c = a - b
+! print *, c
 
-c = b/a
-print *, c
+! c = a*b
+! print *, c
+
+! c = b/a
+! print *, c
 
 end program
--- src/lfortran/pass/implied_do_loops.cpp
+++ src/lfortran/pass/implied_do_loops.cpp
@@ -94,52 +94,67 @@ public:
         transform_stmts(xx.m_body, xx.n_body);
     }
 
+    void create_implied_do_loop(ASR::ImpliedDoLoop_t* idoloop, ASR::Var_t* arr_var, int arr_idx_offset) {
+        ASR::do_loop_head_t head;
+        head.m_v = idoloop->m_var;
+        head.m_start = idoloop->m_start;
+        head.m_end = idoloop->m_end;
+        head.m_increment = idoloop->m_increment;
+        head.loc = head.m_v->base.loc;
+        Vec<ASR::stmt_t*> doloop_body;
+        doloop_body.reserve(al, 1);
+        ASR::symbol_t* arr = arr_var->m_v;
+        ASR::ttype_t *_type = expr_type(idoloop->m_start);
+        ASR::expr_t* const_1 = EXPR(ASR::make_ConstantInteger_t(al, arr_var->base.base.loc, 1, _type));
+        ASR::expr_t* idx_offset = nullptr;
+        if( arr_idx_offset > 0 ) {
+            idx_offset = EXPR(ASR::make_ConstantInteger_t(al, arr_var->base.base.loc, arr_idx_offset, _type));
+        }
+        ASR::expr_t* const_n = EXPR(ASR::make_ConstantInteger_t(al, arr_var->base.base.loc, idoloop->n_values, _type));
+        ASR::expr_t* offset = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, idoloop->m_var, ASR::binopType::Sub, idoloop->m_start, _type));
+        ASR::expr_t* num_grps = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, offset, ASR::binopType::Mul, const_n, _type));
+        ASR::expr_t* grp_start = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, num_grps, ASR::binopType::Add, const_1, _type));
+        for( size_t i = 0; i < idoloop->n_values; i++ ) {
+            Vec<ASR::array_index_t> args;
+            ASR::array_index_t ai;
+            ai.loc = arr_var->base.base.loc;
+            ai.m_left = nullptr;
+            ASR::expr_t* const_i = EXPR(ASR::make_ConstantInteger_t(al, arr_var->base.base.loc, i, _type));
+            ASR::expr_t* idx = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, 
+                                                        grp_start, ASR::binopType::Add, const_i, 
+                                                        _type));
+            if( arr_idx_offset != 0 ) {
+                idx = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, 
+                                            idx, ASR::binopType::Add, idx_offset, 
+                                            _type));
+            }
+            ai.m_right = idx;
+            ai.m_step = nullptr;
+            args.reserve(al, 1);
+            args.push_back(al, ai);
+            ASR::expr_t* array_ref = EXPR(ASR::make_ArrayRef_t(al, arr_var->base.base.loc, arr, 
+                                                                args.p, args.size(), 
+                                                                expr_type(EXPR((ASR::asr_t*)arr_var))));
+            if( idoloop->m_values[i]->type == ASR::exprType::ImpliedDoLoop ) {
+                throw SemanticError("Pass for nested ImpliedDoLoop nodes isn't implemented yet.", idoloop->m_values[i]->base.loc);
+            }
+            ASR::stmt_t* doloop_stmt = STMT(ASR::make_Assignment_t(al, arr_var->base.base.loc, array_ref, idoloop->m_values[i]));
+            doloop_body.push_back(al, doloop_stmt);
+        }
+        ASR::stmt_t* doloop = STMT(ASR::make_DoLoop_t(al, arr_var->base.base.loc, head, doloop_body.p, doloop_body.size()));
+        implied_do_loop_result.push_back(al, doloop);
+    }
+
     void visit_Assignment(const ASR::Assignment_t &x) {
         if( x.m_value->type == ASR::exprType::ArrayInitializer ) {
             ASR::ArrayInitializer_t* arr_init = ((ASR::ArrayInitializer_t*)(&(x.m_value->base)));
             if( arr_init->n_args == 1 && arr_init->m_args[0] != nullptr && 
                 arr_init->m_args[0]->type == ASR::exprType::ImpliedDoLoop ) {
                 ASR::ImpliedDoLoop_t* idoloop = ((ASR::ImpliedDoLoop_t*)(&(arr_init->m_args[0]->base)));
-                ASR::do_loop_head_t head;
-                head.m_v = idoloop->m_var;
-                head.m_start = idoloop->m_start;
-                head.m_end = idoloop->m_end;
-                head.m_increment = idoloop->m_increment;
-                head.loc = head.m_v->base.loc;
-                Vec<ASR::stmt_t*> doloop_body;
-                doloop_body.reserve(al, 1);
                 ASR::Var_t* arr_var = (ASR::Var_t*)(&(x.m_target->base));
-                ASR::symbol_t* arr = arr_var->m_v;
-                ASR::ttype_t *_type = expr_type(idoloop->m_start);
-                ASR::expr_t* const_1 = EXPR(ASR::make_ConstantInteger_t(al, arr_var->base.base.loc, 1, _type));
-                ASR::expr_t* const_n = EXPR(ASR::make_ConstantInteger_t(al, arr_var->base.base.loc, idoloop->n_values, _type));
-                ASR::expr_t* offset = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, idoloop->m_var, ASR::binopType::Sub, idoloop->m_start, _type));
-                ASR::expr_t* num_grps = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, offset, ASR::binopType::Mul, const_n, _type));
-                ASR::expr_t* grp_start = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, num_grps, ASR::binopType::Add, const_1, _type));
-                for( size_t i = 0; i < idoloop->n_values; i++ ) {
-                    Vec<ASR::array_index_t> args;
-                    ASR::array_index_t ai;
-                    ai.loc = arr_var->base.base.loc;
-                    ai.m_left = nullptr;
-                    ASR::expr_t* const_i = EXPR(ASR::make_ConstantInteger_t(al, arr_var->base.base.loc, i, _type));
-                    ASR::expr_t* idx = EXPR(ASR::make_BinOp_t(al, arr_var->base.base.loc, 
-                                                                grp_start, ASR::binopType::Add, const_i, 
-                                                                _type));
-                    ai.m_right = idx;
-                    ai.m_step = nullptr;
-                    args.reserve(al, 1);
-                    args.push_back(al, ai);
-                    ASR::expr_t* array_ref = EXPR(ASR::make_ArrayRef_t(al, arr_var->base.base.loc, arr, 
-                                                                        args.p, args.size(), 
-                                                                        expr_type(EXPR((ASR::asr_t*)arr_var))));
-                    if( idoloop->m_values[i]->type == ASR::exprType::ImpliedDoLoop ) {
-                        throw SemanticError("Pass for nested ImpliedDoLoop nodes isn't implemented yet.", idoloop->m_values[i]->base.loc);
-                    }
-                    ASR::stmt_t* doloop_stmt = STMT(ASR::make_Assignment_t(al, arr_var->base.base.loc, array_ref, idoloop->m_values[i]));
-                    doloop_body.push_back(al, doloop_stmt);
-                }
-                ASR::stmt_t* doloop = STMT(ASR::make_DoLoop_t(al, x.base.base.loc, head, doloop_body.p, doloop_body.size()));
-                implied_do_loop_result.push_back(al, doloop);
+                create_implied_do_loop(idoloop, arr_var, 0);
+            } else if( arr_init->n_args > 1 && arr_init->m_args[0] != nullptr ) {
+                
             }
         }
     }
