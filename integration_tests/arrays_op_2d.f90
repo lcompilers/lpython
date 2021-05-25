68d65d987450dc8921a47f85d5d8aca6a49cee5c
czgdp1807 <gdp.1807@gmail.com>
2021-05-25 14:37:51 +0530

    fixed do_loops.cpp for handling do loops of depth more than 2

--- integration_tests/arrays_op_2d.f90
+++ integration_tests/arrays_op_2d.f90
@@ -1,32 +1,34 @@
 program array_op_2d
 implicit none
 
-integer :: a(2, 2), b(2, 2), c(2, 2)
-integer :: i, j
+integer :: a(2, 2, 1), b(2, 2, 1), c(2, 2, 1)
+integer :: i, j, k
 
 do i = 1, 2
     do j = 1, 2
-        a(i, j) = i*i
-        b(i, j) = j*j
+        do k = 1, 1
+            a(i, j, k) = i*i
+            b(i, j, k) = j*j
+        end do
     end do
 end do
 
 i = 0
 
-print *, a(1, 1), a(1, 2), a(2, 1), a(2, 2)
+print *, a(1, 1, 1), a(1, 2, 1), a(2, 1, 1), a(2, 2, 1)
 
-print *, b(1, 1), b(1, 2), b(2, 1), b(2, 2)
+print *, b(1, 1, 1), b(1, 2, 1), b(2, 1, 1), b(2, 2, 1)
 
 c = a + b
-print *, c(1, 1), c(1, 2), c(2, 1), c(2, 2)
+print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
 
 c = a - b
-print *, c(1, 1), c(1, 2), c(2, 1), c(2, 2)
+print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
 
 c = a*b
-print *, c(1, 1), c(1, 2), c(2, 1), c(2, 2)
+print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
 
 c = b/a
-print *, c(1, 1), c(1, 2), c(2, 1), c(2, 2)
+print *, c(1, 1, 1), c(1, 2, 1), c(2, 1, 1), c(2, 2, 1)
 
 end program
