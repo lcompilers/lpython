1b9aaf65be7162a4446159fc60be051b6fded8a2
czgdp1807 <gdp.1807@gmail.com>
2021-05-27 10:21:54 +0530

    final update done

--- integration_tests/arrays_06.f90
+++ integration_tests/arrays_06.f90
@@ -3,6 +3,7 @@ implicit none
 integer, dimension(2, 2) :: A
 integer :: i, j
 integer, dimension(6) :: x
+j = 4
 x = [(i*2, i = 1, 6)]
 print *, x(1), x(2), x(3), x(4), x(5), x(6)
 x = [(i+1, i*2, i = 1, 3)]
@@ -11,7 +12,7 @@ x = [(i+1, i**2, i*2, i = 1, 2)]
 print *, x(1), x(2), x(3), x(4), x(5), x(6)
 x = [(2*i, 3*i, 4*i, i+1, i**2, i*2, i = 2, 2)]
 print *, x(1), x(2), x(3), x(4), x(5), x(6)
-A = 3.0
+A = 3.0 + 4
 print *, A(1, 1), A(1, 2), A(2, 1), A(2, 2)
 ! print '("Matrix A"/(10F8.2))',  ((A(i, j), i = 1, 2), j = 1, 2)
 end program
