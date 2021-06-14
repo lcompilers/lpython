c7e4b8aab5fb612abf2ffbc7e0400e7b36e09e56
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2021-06-14 08:24:47 +0530

    Update tests

--- tests/critical1.f90
+++ tests/critical1.f90
@@ -2,6 +2,8 @@ program critical1
 implicit none
 integer :: joblist[*], job
 integer :: a, b
+integer :: IntegerSample
+character(50) :: Sample
 if (this_image() == 1) read(*,*) joblist
 sync all
 do
@@ -9,6 +11,18 @@ do
       job = joblist[1]
       joblist[1] = job - 1
     end critical
+
+! >>>>>>>> Syntax check(AST) >>>>>>>>
+    critical()
+        IntegerSample = 0
+    end critical
+    critical(stat = IntegerSample)
+        Sample = "Error message"
+    end critical
+    critical(errmsg = Sample)
+        Sample = "Error message"
+    end critical
+! <<<<<<<< Syntax check(AST) <<<<<<<<
 
     if (job > 0) then
         b = a**2 + b**2
