9072fbcfcec9d15e1678e14f9725bc270d6ffb9a
czgdp1807 <gdp.1807@gmail.com>
2021-06-18 17:38:20 +0530

    updated tests

--- integration_tests/allocate_01.f90
+++ integration_tests/allocate_01.f90
@@ -4,35 +4,38 @@ integer, allocatable :: a(:), b(:,:), c(:,:,:)
 integer :: n, ierr
 integer :: i, j, k
 n = 10
-allocate(a(5))
-allocate(b(n + 1,n), c(n, 5, n), stat=ierr)
-do i = 1, 5
+allocate(a(5:n + 5))
+allocate(b(n:2*n, n:3*n), c(n, 2:n, 3:10), stat=ierr)
+if( size(a) /= n + 1 ) error stop
+if( size(b) /= (n + 1)*(2*n + 1) ) error stop
+if( size(c) /= n*(n - 1)*8 ) error stop
+do i = lbound(a, 1), ubound(a, 1)
     a(i) = i
 end do
-do i = 1, n
-    do j = 1, n
+do i = lbound(b, 1), ubound(b, 1)
+    do j = lbound(b, 2), ubound(b, 2)
         b(i, j) = i + j
     end do
 end do
-do i = 1, n
-    do j = 1, 5
-        do k = 1, n
+do i = lbound(c, 1), ubound(c, 1)
+    do j = lbound(c, 2), ubound(c, 2)
+        do k = lbound(c, 3), ubound(c, 3)
             c(i, j, k) = i + j + k
         end do
     end do
 end do
 
-do i = 1, 5
+do i = lbound(a, 1), ubound(a, 1)
     if (a(i) /= i) error stop
 end do
-do i = 1, n
-    do j = 1, n
+do i = lbound(b, 1), ubound(b, 1)
+    do j = lbound(b, 2), ubound(b, 2)
         if (b(i, j) /= i + j) error stop
     end do
 end do
-do i = 1, n
-    do j = 1, 5
-        do k = 1, n
+do i = lbound(c, 1), ubound(c, 1)
+    do j = lbound(c, 2), ubound(c, 2)
+        do k = lbound(c, 3), ubound(c, 3)
             if (c(i, j, k) /= i + j + k) error stop
         end do
     end do
