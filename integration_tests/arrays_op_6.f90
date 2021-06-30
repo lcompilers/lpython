e0dccbcceb42f031e0a787b06d74c722a6dd6966
czgdp1807 <gdp.1807@gmail.com>
2021-06-30 17:20:45 +0530

    bug location found

--- integration_tests/arrays_op_6.f90
+++ integration_tests/arrays_op_6.f90
@@ -3,19 +3,20 @@ implicit none
 
 real :: a(2, 2, 2), b(2, 2, 2), c(2, 2, 2)
 real :: d(2, 2, 2)
-! integer :: i, j, k
+integer :: i, j, k
 
-! do i = 1, 2
-!     do j = 1, 2
-!         do k = 1, 2
-!             a(i, j, k) = i*i + j*j + k*k
-!             b(i, j, k) = 2*(i*j + j*k + k*j)
-!             c(i, j, k) = i*j*k
-!         end do
-!     end do
-! end do
+do i = 1, 2
+    do j = 1, 2
+        do k = 1, 2
+            a(i, j, k) = i*i + j*j + k*k
+            b(i, j, k) = 2*(i*j + j*k + i*k)
+            c(i, j, k) = 0
+        end do
+    end do
+end do
 
 call asquare(a, b, c, d)
+call check(d)
 
 contains
 
@@ -25,25 +26,25 @@ contains
         real :: a(:, :, :), b(:, :, :), c(:, :, :)
         real :: d(:, :, :)
 
-        d = a + b
+        d = a + b + c
 
     end subroutine asquare
 
-    ! subroutine check(c)
-    ! implicit none
+    subroutine check(c)
+    implicit none
 
-    ! complex, intent(in) :: c(:, :, :)
+    real, intent(in) :: c(:, :, :)
 
-    ! integer :: i, j, k
+    integer :: i, j, k
 
-    ! do i = lbound(c, 1), ubound(c, 1)
-    !     do j = lbound(c, 2), ubound(c, 2)
-    !         do k = lbound(c, 3), ubound(c, 3)
-    !             if(c(i, j, k) /= (i + j + k)**2) error stop
-    !         end do
-    !     end do
-    ! end do
+    do i = lbound(c, 1), ubound(c, 1)
+        do j = lbound(c, 2), ubound(c, 2)
+            do k = lbound(c, 3), ubound(c, 3)
+                if(c(i, j, k) /= (i + j + k)**2) error stop
+            end do
+        end do
+    end do
 
-    ! end subroutine check
+    end subroutine check
 
 end program
