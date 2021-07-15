0f414810eeedf06c1a4395a6e6c2b88f8525f94b
Ondřej Čertík <ondrej@certik.us>
2021-07-14 22:51:46 -0600

    Add a test for "public operator(*)"
    This is currently ambiguous:
    
    * public :: OPERATOR(*) <- operator
    * public :: operator(*) <- array in a function (the public attribute is not
      allowed in a function)

--- tests/interface3.f90
+++ tests/interface3.f90
@@ -2,6 +2,7 @@ module interface3
 implicit none
 !checks the syntax
 public :: x, y, z, assignment(=), operator(+), operator(.and.), operator(.in.)
+public :: operator(*)
 
 interface
     module procedure sample
