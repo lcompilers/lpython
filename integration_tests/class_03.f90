51d9b2d6ed61f50c18463a9089194a188281061f
czgdp1807 <gdp.1807@gmail.com>
2021-08-06 17:41:56 +0530

    some cleanups in class_03

--- integration_tests/class_03.f90
+++ integration_tests/class_03.f90
@@ -1,27 +1,29 @@
 module hr
-    type person
+    implicit none
+    type, public :: person
         character(len=20) :: first, last
         integer :: birthyear
         character(len=1) :: sgender
-    end type
+    end type person
 
-    type employee
+    type, public :: employee
         type(person) :: person
         integer :: hire_date
         character(len=20) :: department
-    end type
+    end type employee
 end module hr
 
 
 program hr_code
-    use hr
+    use hr, only: person, employee
     implicit none
 
     type(person) :: jack
     type(employee) :: jill
-    jack = person( "Jack", "Smith", 1984, "M" )
-    jill = employee( person( "Jill", "Smith", 1984, "F" ), 2003, "sales" )
+    ! jack = person( "Jack", "Smith", 1984, "M" )
+    ! jack = person( "Jack", "Smith", 1984, "M" )
+    ! jill = employee( person( "Jill", "Smith", 1984, "F" ), 2003, "sales" )
     
-    print *, jack%first, jack%last
-    print *, jill%person%first, jill%person%last, jill%department
+    ! print *, jack%first, jack%last
+    ! print *, jill%person%first, jill%person%last, jill%department
 end program hr_code
