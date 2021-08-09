ab47474450630e1aca635e978fb53f41ef1959d7
Ondřej Čertík <ondrej@certik.us>
2021-08-09 14:22:20 -0600

    Implement initial iso_c_binding module

---
+++ src/runtime/lfortran_intrinsic_iso_c_binding.f90
@@ -0,0 +1,9 @@
+module lfortran_intrinsic_iso_c_binding
+implicit none
+
+integer, parameter :: c_long = 4
+integer, parameter :: c_long_long = 8
+integer, parameter :: c_float = 4
+integer, parameter :: c_double = 8
+
+end module
