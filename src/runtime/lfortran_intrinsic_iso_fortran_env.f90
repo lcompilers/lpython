9d65b9af3b7faa7c35f6a8246edb91bfbb293d62
Ondřej Čertík <ondrej@certik.us>
2021-08-09 14:09:32 -0600

    Implement initial iso_fortran_env

---
+++ src/runtime/lfortran_intrinsic_iso_fortran_env.f90
@@ -0,0 +1,12 @@
+module lfortran_intrinsic_iso_fortran_env
+implicit none
+
+integer, parameter :: int8 = -1
+integer, parameter :: int16 = -1
+integer, parameter :: int32 = 4
+integer, parameter :: int64 = 8
+integer, parameter :: real32 = 4
+integer, parameter :: real64 = 8
+integer, parameter :: real128 = -1
+
+end module
