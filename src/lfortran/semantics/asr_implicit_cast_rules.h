2d37ba5fb59a17b0682d6c9c78411059b10d5220
Ondřej Čertík <ondrej@certik.us>
2021-08-08 21:13:39 -0600

    Pass value through Integer/RealToReal ImplicitCast
    That way the value gets properly evaluated at compile time even if implicit
    cast is used.

--- src/lfortran/semantics/asr_implicit_cast_rules.h
+++ src/lfortran/semantics/asr_implicit_cast_rules.h
@@ -130,6 +130,30 @@ public:
                 value = (ASR::expr_t *)ASR::make_ConstantInteger_t(al, a_loc,
                     i, dest_type);
             }
+        } else if ((ASR::cast_kindType)cast_kind == ASR::cast_kindType::RealToReal) {
+            if (ASRUtils::expr_value(*convert_can)) {
+                LFORTRAN_ASSERT(ASR::is_a<ASR::Real_t>(*dest_type)
+                 || ASR::is_a<ASR::RealPointer_t>(*dest_type))
+                LFORTRAN_ASSERT(ASR::is_a<ASR::Real_t>(*ASRUtils::expr_type(*convert_can)))
+                value = ASRUtils::expr_value(*convert_can);
+                LFORTRAN_ASSERT(ASR::is_a<ASR::ConstantReal_t>(*value))
+                ASR::ConstantReal_t *r = ASR::down_cast<ASR::ConstantReal_t>(value);
+                double rval = r->m_r;
+                value = (ASR::expr_t *)ASR::make_ConstantReal_t(al, a_loc,
+                    rval, dest_type);
+            }
+
+        } else if ((ASR::cast_kindType)cast_kind == ASR::cast_kindType::IntegerToInteger) {
+            if (ASRUtils::expr_value(*convert_can)) {
+                LFORTRAN_ASSERT(ASR::is_a<ASR::Integer_t>(*dest_type))
+                LFORTRAN_ASSERT(ASR::is_a<ASR::Integer_t>(*ASRUtils::expr_type(*convert_can)))
+                value = ASRUtils::expr_value(*convert_can);
+                LFORTRAN_ASSERT(ASR::is_a<ASR::ConstantInteger_t>(*value))
+                ASR::ConstantInteger_t *i = ASR::down_cast<ASR::ConstantInteger_t>(value);
+                int64_t ival = i->m_n;
+                value = (ASR::expr_t *)ASR::make_ConstantInteger_t(al, a_loc,
+                    ival, dest_type);
+            }
 
         }
 
