a8684bf0e74fe1348938e361d2d261da9e58bb1c
Rohit Goswami <rog32@hi.is>
2021-08-10 22:58:17 +0000

    tests: Use another cast

--- integration_tests/parameter_05.f90
+++ integration_tests/parameter_05.f90
@@ -1,7 +1,7 @@
 program paramter_05
   implicit none
 
-  integer, parameter :: x = 5
+  integer, parameter :: x = 5.0
   real, parameter :: y = 5
   real(kind=8), parameter :: z = 5
   print*, x,y,z
