9372710e5416ef7f84f9091b630905769d8ee15a
Ondřej Čertík <ondrej@certik.us>
2021-08-11 16:16:29 -0600

    Add a test for sin(20.0)

--- src/runtime/tests/test_sin2.f90
+++ src/runtime/tests/test_sin2.f90
@@ -5,4 +5,6 @@ implicit none
 real(dp) :: x
 x = sin(1.5_dp)
 if (abs(x - 0.997494996_dp) > 1e-5_dp) error stop
+x = sin(20.0_dp)
+if (abs(x - 0.9129452507276277_dp) > 1e-5_dp) error stop
 end program
