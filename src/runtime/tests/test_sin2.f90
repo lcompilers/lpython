d632e008cd27a74012699201f4c0798fca3051e5
Rohit Goswami <rog32@hi.is>
2021-08-11 18:05:56 +0000

    runtime: Inititial sin(x) implementation

---
+++ src/runtime/tests/test_sin2.f90
@@ -0,0 +1,8 @@
+program test_sin2
+use iso_fortran_env, only: dp => real64
+use lfortran_intrinsic_math2, only: sin
+implicit none
+real :: x
+x = sin(1.5_dp)
+if (abs(x - 0.997494996_dp) > 1e-5_dp) error stop
+end program
