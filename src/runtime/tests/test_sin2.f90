1e7b8c1f76c1c33421058870206ec0673f336dcd
Ondřej Čertík <ondrej@certik.us>
2021-08-12 11:15:57 -0600

    Test large numbers also

--- src/runtime/tests/test_sin2.f90
+++ src/runtime/tests/test_sin2.f90
@@ -21,4 +21,13 @@ end do
 print *
 print *, "Maximum error:", err_max
 if (err_max > 1e-9_dp) error stop "Maximum error too large"
+
+! Large number
+x = 2e10_dp
+s1 = sin2(x); s2 = sin(x); err = abs(s1-s2)
+print *, x, err
+
+x = 2e40_dp
+s1 = sin2(x); s2 = sin(x); err = abs(s1-s2)
+print *, x, err
 end program
