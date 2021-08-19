b5f3f250bb184a049db95f696489460f935b5e6e
Ondřej Čertík <ondrej@certik.us>
2021-08-19 13:58:30 -0600

    bind(c): add tests for subroutines

--- integration_tests/modules_15c.c
+++ integration_tests/modules_15c.c
@@ -11,7 +11,7 @@ int f_int_double(int *a, double *b) {
 }
 
 int f_int_float_complex(int *a, float_complex_t *b) {
-    return *a + creal(*b) + cimag(*b);
+    return *a + crealf(*b) + cimagf(*b);
 }
 
 int f_int_double_complex(int *a, double_complex_t *b) {
@@ -19,7 +19,7 @@ int f_int_double_complex(int *a, double_complex_t *b) {
 }
 
 int f_int_float_complex_value(int a, float_complex_t b) {
-    return a + creal(b) + cimag(b);
+    return a + crealf(b) + cimagf(b);
 }
 
 int f_int_double_complex_value(int a, double_complex_t b) {
@@ -62,4 +62,68 @@ double f_int_doublearray(int n, double *b) {
         s += b[i];
     }
     return s;
+}
+
+// --------------------------------------------------------------------
+
+void sub_int_float(int *a, float *b, int *r) {
+    *r = *a + *b;
+}
+
+void sub_int_double(int *a, double *b, int *r) {
+    *r = *a + *b;
+}
+
+void sub_int_float_complex(int *a, float_complex_t *b, int *r) {
+    *r = *a + crealf(*b) + cimagf(*b);
+}
+
+void sub_int_double_complex(int *a, double_complex_t *b, int *r) {
+    *r = *a + creal(*b) + cimag(*b);
+}
+
+void sub_int_float_value(int a, float b, int *r) {
+    *r = a + b;
+}
+
+void sub_int_double_value(int a, double b, int *r) {
+    *r = a + b;
+}
+
+void sub_int_float_complex_value(int a, float_complex_t b, int *r) {
+    *r = a + crealf(b) + cimagf(b);
+}
+
+void sub_int_double_complex_value(int a, double_complex_t b, int *r) {
+    *r = a + creal(b) + cimag(b);
+}
+
+void sub_int_intarray(int n, int *b, int *r) {
+    int i;
+    int s;
+    s = 0;
+    for (i=0; i < n; i++) {
+        s += b[i];
+    }
+    *r = s;
+}
+
+void sub_int_floatarray(int n, float *b, float *r) {
+    int i;
+    float s;
+    s = 0;
+    for (i=0; i < n; i++) {
+        s += b[i];
+    }
+    *r = s;
+}
+
+void sub_int_doublearray(int n, double *b, double *r) {
+    int i;
+    double s;
+    s = 0;
+    for (i=0; i < n; i++) {
+        s += b[i];
+    }
+    *r = s;
 }
--- integration_tests/modules_15c.h
+++ integration_tests/modules_15c.h
@@ -30,4 +30,18 @@ int f_int_intarray(int n, int *b);
 float f_int_floatarray(int n, float *b);
 double f_int_doublearray(int n, double *b);
 
+
+void sub_int_float(int *a, float *b, int *r);
+void sub_int_double(int *a, double *b, int *r);
+void sub_int_float_complex(int *a, float_complex_t *b, int *r);
+void sub_int_double_complex(int *a, double_complex_t *b, int *r);
+void sub_int_float_value(int a, float b, int *r);
+void sub_int_double_value(int a, double b, int *r);
+void sub_int_float_complex_value(int a, float_complex_t b, int *r);
+void sub_int_double_complex_value(int a, double_complex_t b, int *r);
+void sub_int_intarray(int n, int *b, int *r);
+void sub_int_floatarray(int n, float *b, float *r);
+void sub_int_doublearray(int n, double *b, double *r);
+
+
 #endif // MODULES_15C
