edbf75592b180a9d0dd99dac723eb6a8d810e63c
Ondřej Čertík <ondrej@certik.us>
2021-08-19 16:53:28 -0600

    Add the last missing conversion test

--- integration_tests/intrinsics_15.f90
+++ integration_tests/intrinsics_15.f90
@@ -4,15 +4,18 @@ integer :: x
 real(sp) :: r32
 real(dp) :: r64
 x = 5; r32 = 5; r64 = 5
+! No kind argument
 r32 = real(x)
 r32 = real(r32)
 r32 = real(r64)
+
+! single precision kind
 r32 = real(x, sp)
 r32 = real(r32, sp)
-
-r64 = real(x, dp)
-r64 = real(r64, dp)
-
 r32 = real(r64, sp)
 
+! double precision kind
+r64 = real(x, dp)
+r64 = real(r32, dp)
+r64 = real(r64, dp)
 end program
