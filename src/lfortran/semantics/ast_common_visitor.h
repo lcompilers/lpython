7b7fd05589e9fb745bb84f34e738e41ca0f367e6
Ondřej Čertík <ondrej@certik.us>
2021-08-19 16:51:40 -0600

    Return ImplicitCast from real()

--- src/lfortran/semantics/ast_common_visitor.h
+++ src/lfortran/semantics/ast_common_visitor.h
@@ -464,9 +464,10 @@ public:
   }
 
 static ASR::asr_t* comptime_intrinsic_real(ASR::expr_t *A,
-        ASR::expr_t * /*kind*/,
-        Allocator &al, const Location &loc, Vec<ASR::expr_t*> &args,
-        ASR::symbol_t *v, ASR::ttype_t *return_type) {
+        ASR::expr_t * kind,
+        Allocator &al, const Location &loc, Vec<ASR::expr_t*> &/*args*/,
+        ASR::symbol_t */*v*/, ASR::ttype_t */*return_type*/) {
+    /*
     ASR::expr_t* real_expr = A;
     ASR::expr_t* real_expr_value = LFortran::ASRUtils::expr_value(real_expr);
     ASR::ttype_t *func_type = LFortran::ASRUtils::expr_type(real_expr);
@@ -494,9 +495,29 @@ static ASR::asr_t* comptime_intrinsic_real(ASR::expr_t *A,
             }
         }
     }
-    return ASR::make_FunctionCall_t(al, loc,
-        v, nullptr, args.p, args.size(), nullptr, 0, return_type,
-        value, nullptr);
+    */
+    int kind_int = 4;
+    if (kind) {
+        ASR::expr_t* kind_value = LFortran::ASRUtils::expr_value(kind);
+        if (kind_value) {
+            if (ASR::is_a<ASR::ConstantInteger_t>(*kind_value)) {
+                kind_int = ASR::down_cast<ASR::ConstantInteger_t>(kind_value)->m_n;
+            } else {
+                throw SemanticError("kind argument to real(a, kind) is not a constant integer", loc);
+            }
+        } else {
+            throw SemanticError("kind argument to real(a, kind) is not constant", loc);
+        }
+    }
+    ASR::expr_t *result = A;
+    ASR::ttype_t *dest_type = LFortran::ASRUtils::TYPE(ASR::make_Real_t(al, loc,
+                kind_int, nullptr, 0));
+    ASR::ttype_t *source_type = LFortran::ASRUtils::expr_type(A);
+
+    // TODO: this is explicit cast, use ExplicitCast
+    ImplicitCastRules::set_converted_value(al, loc, &result,
+                                           source_type, dest_type);
+    return (ASR::asr_t*)result;
 }
 
 }; // class CommonVisitorMethods
