4fc5f366d26704c4470d50304fee4e3a7faef6fd
Ondřej Čertík <ondrej@certik.us>
2021-08-21 16:21:55 -0600

    rtlib: exp, log, erf

---
+++ integration_tests/intrinsics_17.f90
@@ -0,0 +1,21 @@
+program intrinsics_17
+use iso_fortran_env, only: sp=>real32, dp=>real64
+real(sp) :: x
+real(dp) :: y
+
+x = exp(1.5_sp)
+print *, x
+y = exp(1.5_dp)
+print *, y
+
+x = log(1.5_sp)
+print *, x
+y = log(1.5_dp)
+print *, y
+
+x = erf(1.5_sp)
+print *, x
+y = erf(1.5_dp)
+print *, y
+
+end
