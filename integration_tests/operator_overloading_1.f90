7803ed11bbe676d567fbbb31fbe0d54b4bed98b5
czgdp1807 <gdp.1807@gmail.com>
2021-09-09 17:13:12 +0530

    added example for operator overloading

---
+++ integration_tests/operator_overloading_1.f90
@@ -0,0 +1,26 @@
+module overload_asterisk_m
+    implicit none
+    private
+    public operator (*)
+
+    interface operator (*)
+        module procedure logical_and
+    end interface
+contains
+    pure logical function logical_and(log1, log2)
+        logical, intent (in) :: log1, log2
+
+        logical_and = (log1 .and. log2)
+    end function
+end module
+
+program main
+  use overload_asterisk_m
+  implicit none
+  logical, parameter :: T = .true., F = .false.
+
+  print *, "T*T:", T*T  ! Yields: T
+  print *, "T*F:", T*F  ! Yields: F
+  print *, "F*T:", F*T  ! Yields: F
+  print *, "F*F:", F*F  ! Yields: F
+end program
