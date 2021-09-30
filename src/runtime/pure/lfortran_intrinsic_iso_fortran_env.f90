b4187244db1eecce31e16ed323b1b0eaec42c145
Ondřej Čertík <ondrej@certik.us>
2021-09-30 09:14:33 -0600

    Add stdin, stdout and stderr to iso_fortran_env

--- src/runtime/pure/lfortran_intrinsic_iso_fortran_env.f90
+++ src/runtime/pure/lfortran_intrinsic_iso_fortran_env.f90
@@ -9,4 +9,8 @@ integer, parameter :: real32 = 4
 integer, parameter :: real64 = 8
 integer, parameter :: real128 = -1
 
+integer, parameter :: input_unit = 5
+integer, parameter :: output_unit = 6
+integer, parameter :: error_unit = 0
+
 end module
