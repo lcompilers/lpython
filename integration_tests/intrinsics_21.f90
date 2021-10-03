80b65172abfdfbef7c4b5e0955bcae125e2848eb
Ondřej Čertík <ondrej@certik.us>
2021-10-03 15:53:52 -0600

    Add a test for -5, -3

--- integration_tests/intrinsics_21.f90
+++ integration_tests/intrinsics_21.f90
@@ -23,6 +23,14 @@ print *, mod(5., -3.), mod(5._dp, -3._dp), mod(x1, x2), mod(y1, y2)
 print *, modulo(5, -3), modulo(i1, i2)
 print *, mod(5, -3), mod(i1, i2)
 
+i1 = -5; i2 = -3
+x1 = i1; y1 = i1;
+x2 = i2; y2 = i2;
+print *, modulo(-5., -3.), modulo(-5._dp, -3._dp), modulo(x1, x2),modulo(y1, y2)
+print *, mod(-5., -3.), mod(-5._dp, -3._dp), mod(x1, x2), mod(y1, y2)
+print *, modulo(-5, -3), modulo(i1, i2)
+print *, mod(-5, -3), mod(i1, i2)
+
 i1 = 5; i2 = 3
 x1 = i1; y1 = i1;
 x2 = i2; y2 = i2;
