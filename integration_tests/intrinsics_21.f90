0a3885ff20f446be271ebed279b4f8b3ddf820bc
Ondřej Čertík <ondrej@certik.us>
2021-10-03 15:49:36 -0600

    Add a test for 5, -3

--- integration_tests/intrinsics_21.f90
+++ integration_tests/intrinsics_21.f90
@@ -7,25 +7,27 @@ real :: x1, x2
 real(dp) :: y1, y2
 integer :: i1, i2
 
-x1 = -5
-x2 = 3
-y1 = -5
-y2 = 3
-i1 = -5
-i2 = 3
-
-! Real arguments
+i1 = -5; i2 = 3
+x1 = i1; y1 = i1;
+x2 = i2; y2 = i2;
 print *, modulo(-5., 3.), modulo(-5._dp, 3._dp), modulo(x1, x2), modulo(y1, y2)
 print *, mod(-5., 3.), mod(-5._dp, 3._dp), mod(x1, x2), mod(y1, y2)
-x1 = -x1
-y1 = -y1
-print *, modulo(5., 3.), modulo(5._dp, 3._dp), modulo(x1, x2), modulo(y1, y2)
-print *, mod(5., 3.), mod(5._dp, 3._dp), mod(x1, x2), mod(y1, y2)
-
-! Integer arguments
 print *, modulo(-5, 3), modulo(i1, i2)
 print *, mod(-5, 3), mod(i1, i2)
-i1 = -i1
+
+i1 = 5; i2 = -3
+x1 = i1; y1 = i1;
+x2 = i2; y2 = i2;
+print *, modulo(5., -3.), modulo(5._dp, -3._dp), modulo(x1, x2), modulo(y1, y2)
+print *, mod(5., -3.), mod(5._dp, -3._dp), mod(x1, x2), mod(y1, y2)
+print *, modulo(5, -3), modulo(i1, i2)
+print *, mod(5, -3), mod(i1, i2)
+
+i1 = 5; i2 = 3
+x1 = i1; y1 = i1;
+x2 = i2; y2 = i2;
+print *, modulo(5., 3.), modulo(5._dp, 3._dp), modulo(x1, x2), modulo(y1, y2)
+print *, mod(5., 3.), mod(5._dp, 3._dp), mod(x1, x2), mod(y1, y2)
 print *, modulo(5, 3), modulo(i1, i2)
 print *, mod(5, 3), mod(i1, i2)
 
