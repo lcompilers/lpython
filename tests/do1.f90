514a7e4f5f24a34e40b81f9cc07eaf2f679f195b
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2021-10-05 18:05:14 +0530

    Add tests

--- tests/do1.f90
+++ tests/do1.f90
@@ -19,4 +19,18 @@ do
     a = a + i
     b = 3
 enddo
+
+do, i = 1, 5
+    a = a + i
+end do
+
+n = 0
+do 50, i = 1, 10
+  j = i
+  do k = 1, 5
+    l = k
+    n = n + 1  ! this statement executes 50 times
+  end do       ! nonlabeled do inside a labeled do
+50 continue
+
 end subroutine
