73249eb7e4d745c4ca07d43dd8a83311c85b684a
Ondřej Čertík <ondrej@certik.us>
2021-10-07 21:39:43 -0600

    Document the interval design

--- src/lfortran/parser/parser.h
+++ src/lfortran/parser/parser.h
@@ -50,6 +50,71 @@ static inline uint32_t bisection(const std::vector<uint32_t> &vec, uint32_t i) {
 
 struct LocationManager {
     // The index into these vectors is the interval ID, starting from 0
+    //
+    // The output code is partitioned into consecutive (non-overlapping)
+    // interval by a single array `out_start` of the starting points: interval N
+    // is (out_start[N], out_start[N+1]). Each interval has an ID (starting from
+    // 0), and each interval corresponds to some interval in the original code.
+    //
+    // There are two types of intervals, stored in `interval_type`:
+    //
+    // 0 ... 1:1 interval. One to one intervals have the same lengths in both in
+    //       and out codes and they correspond to just copying text from in to
+    //       out. The length of the interval N is out_start[N+1]-out_start[N],
+    //       and it starts at in_start[N] in input code and out_start[N] in
+    //       output code. Every character in output code can be mapped to a
+    //       character in input code and vice versa.
+    // 1 ... Many to many interval: This type happens for example in a macro
+    //       expansion, e.g.:
+    //           #define X(a,b) -> sin(a*x)+cos(b*y)
+    //           X(1.5, 3.5)
+    //       gets expanded to:
+    //           sin(1.5*x)+cos(3.5*y)
+    //       So the single input code interval `X(1.5, 3.5)` corresponds to an interval
+    //       `sin(1.5*x)+cos(3.5*y)`, but we do not specify any more detailed
+    //       correspondence on a character basis. If for example `x` is defined
+    //       but `y` is undefined, then the compiler error message can for
+    //       example look like this:
+    //           Semantic error: `y` is undefined:
+    //           sin(1.5*x)+cos(3.5*y)
+    //                              ^
+    //           Note: In a macro expansion:
+    //           X(1.5, 3.5)
+    //           ^~~~~~~~~~~
+    //           Note: the macro was defined at:
+    //           #define X(a,b) -> sin(a*x)+cos(b*y)
+    //                   ^~~~~~
+    //       The last part can be inferred from a Preprocessor symbol table: The
+    //       error message of undefined `y` leads to line with the macro `X`, so
+    //       we can lookup `X` in a macro symbol table and find its definition.
+    //
+    // # Handling of include files
+    //
+    // Example of input file `A.cpp`:
+    //     #include "B.h"
+    //     void f(const MyType &x);
+    // Expands to the output file:
+    //     struct MyType {
+    //         int i;
+    //     }
+    //     void f(const MyType &x);
+    // Here `f` from the output file maps using interval type 0 to `f` in A.cpp.
+    // The `struct MyType` maps using interval type 0 to `struct MyType` in B.h.
+    // If we wanted to also store that it comes via the `#include` line, we
+    // would need another mapping using interval type 1 from `struct MyType` to
+    // `#include "B.h"`. Including files is recursive. Then we need one interval
+    // type 0 to point to the original code and several type 1 intervals to
+    // specify all the include lines. Macro expansion is also recursive and so
+    // the same idea applies.
+    //
+    // Note: even for macro expansion we can use intervals type 0 to point to
+    // the copied text, such as `cos(` can point directly into the macro
+    // definition, `3.5` can point to `b` using interval 1 (but can also point
+    // to `3.5` in `X(1.5, 3.5)` using interval 0), `*y)` can again use interval
+    // 0.
+    //
+    //
+    //
     std::vector<uint32_t> out_start; // consecutive intervals in the output code
     std::vector<uint32_t> in_start; // start + size in the original code
     std::vector<uint32_t> in_newlines; // position of all \n in the original code
@@ -60,12 +125,9 @@ struct LocationManager {
     bool preprocessor = false;
     std::vector<uint32_t> out_start0; // consecutive intervals in the output code
     std::vector<uint32_t> in_start0; // start + size in the original code
+    std::vector<uint32_t> in_size0; // Size of the `in` interval
+    std::vector<uint8_t> interval_type0; // 0 .... 1:1; 1 ... many to many;
     std::vector<uint32_t> in_newlines0; // position of all \n in the original code
-// `in_size` is current commented out, because it is equivalent to
-// out_start[n+1]-out_start[n], due to each interval being 1:1 for now. Once we
-// implement multiple interval types, we will need it:
-//    std::vector<uint32_t> in_size;
-//    std::vector<uint8_t> interval_type; // 0 .... 1:1; 1 ... any to one;
 //    std::vector<uint32_t> filename_id; // file name for each interval, ID
 //    std::vector<std::string> filenames; // filenames lookup for an ID
 
