49f738acc00e3be45cabd187203dbf05adfcccb9
Ondřej Čertík <ondrej@certik.us>
2021-10-15 13:13:36 -0600

    Add a test

--- tests/preprocessor11.f90
+++ tests/preprocessor11.f90
@@ -4,10 +4,17 @@ implicit none
 
 #define X 1
 #define Y 5
+
 #if X == 1
 print *, "X is 1"
 #else
 print *, "X is not 1"
+#endif
+
+#if X == 2
+print *, "X is 2"
+#else
+print *, "X is not 2"
 #endif
 
 end program
