c9c77972183bfa577bf3b8a7f8bf2f11e96661bb
Ondřej Čertík <ondrej@certik.us>
2021-10-15 13:18:28 -0600

    Add more tests

--- tests/preprocessor11.f90
+++ tests/preprocessor11.f90
@@ -17,4 +17,58 @@ print *, "X is 2"
 print *, "X is not 2"
 #endif
 
+#if X != 1
+print *, "X is not 1"
+#else
+print *, "Not X is not 1"
+#endif
+
+#if X == 1 && Y == 5
+print *, "X is 1 and Y is 5"
+#else
+print *, "Not (X is 1 and Y is 5)"
+#endif
+
+#if X == 2 && Y == 5
+print *, "X is 2 and Y is 5"
+#else
+print *, "Not (X is 2 and Y is 5)"
+#endif
+
+#if X < 3 && Y > 3
+print *, "X<3 and Y>3"
+#else
+print *, "Not (X<3 and Y>3)"
+#endif
+
+#if X > 3 && Y < 3
+print *, "X>3 and Y<3"
+#else
+print *, "Not (X>3 and Y<3)"
+#endif
+
+#if X == 3-2
+print *, "X is 3-2"
+#else
+print *, "X is not 3-2"
+#endif
+
+#if X == 3-1
+print *, "X is 3-1"
+#else
+print *, "X is not 3-1"
+#endif
+
+#if Y == (6*5-5)/5
+print *, "Y is (6*5-5)/5"
+#else
+print *, "Y is not (6*5-5)/5"
+#endif
+
+#if Y == (6*5-6)/5
+print *, "Y is (6*5-6)/5"
+#else
+print *, "Y is not (6*5-6)/5"
+#endif
+
 end program
