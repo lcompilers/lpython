03d8ed2c37ebfb006d02335833df66757a973aa5
Ondřej Čertík <ondrej@certik.us>
2021-10-15 15:11:11 -0600

    Add a test for a typical ASSERT macro

---
+++ tests/preprocessor12.f90
@@ -0,0 +1,26 @@
+program preprocessor12
+! Test that a typical ASSERT macro works
+implicit none
+
+#ifdef NDEBUG
+# define ASSERT(x) !! assert( x )
+#else
+# define ASSERT(x) if(.not.(x)) call f90_assert(__FILE__,__LINE__)
+#endif
+
+ASSERT(.true.)
+ASSERT(5 > 3)
+ASSERT(5+3*8 > 3)
+
+ASSERT(5 < 3)
+
+contains
+
+    subroutine f90_assert(file, line)
+    character(len=*), intent(in) :: file
+    integer,          intent(in) :: line
+    print *, "Assertion failed at " // file // ":", line
+    error stop
+    end subroutine f90_assert
+
+end program
