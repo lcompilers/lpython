8957b826334a53e8332208fcf003a540acd12075
Gagandeep Singh <gdp.1807@gmail.com>
2021-10-19 17:47:07 +0530

    add tests for boz

--- integration_tests/bits_01.f90
+++ integration_tests/bits_01.f90
@@ -1,7 +1,11 @@
-program bits_01
-use iso_fortran_env, only: int32, int64
+program boz_01
 implicit none
 
-if (ibclr(3_int32, 0) /= 2_int32) error stop
-if (ibclr(4_int64, 1) /= 4_int64) error stop
+integer :: boz_1, boz_2, boz_3
+
+boz_1 = int(b'01011101')
+boz_2 = int(o'2347')
+boz_3 = int(z'ABC')
+
+print *, boz_1, boz_2, boz_3
 end program
