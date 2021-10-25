ba4831fe675edb3f4ff81037eef2c9f73bd28973
Ondřej Čertík <ondrej@certik.us>
2021-10-25 15:33:44 -0600

    Add all combinations for primary labels

--- src/lfortran/tests/test_error_rendering.cpp
+++ src/lfortran/tests/test_error_rendering.cpp
@@ -22,7 +22,7 @@ using LFortran::diag::Label;
 using LFortran::Location;
 using LFortran::LocationManager;
 
-TEST_CASE("Error Render I") {
+TEST_CASE("Error Render: no labels") {
     std::string out, ref;
 
     out = render_diagnostic(Diagnostic(
@@ -57,14 +57,12 @@ warning: Error no label
     CHECK(out == ref);
 }
 
-TEST_CASE("Error Render II") {
+TEST_CASE("Error Render: only primary labels") {
+    // All combinations of primary error labels
     std::string out, ref, input;
-    Location loc;
-    loc.first = 4;
-    loc.last = 7;
-
-    input = "One line text\n";
+    Location loc1, loc2, loc3;
     LocationManager lm;
+    input = "One line text\n";
     lm.in_filename = "input";
     lm.get_newlines(input, lm.in_newlines);
     lm.out_start.push_back(0);
@@ -72,10 +70,18 @@ TEST_CASE("Error Render II") {
     lm.in_start.push_back(input.size());
     lm.out_start.push_back(input.size());
 
+    loc1.first = 4;
+    loc1.last = 7;
+    loc2.first = 9;
+    loc2.last = 12;
+    loc3.first = 0;
+    loc3.last = 2;
+
+    // 1 Label 1 Span
     auto d = Diagnostic(
             "Error with label no message",
             Level::Error, Stage::Semantic, {
-                Label("", {loc})
+                Label("", {loc1})
             }
         );
     out = render_diagnostic(d, input, lm, false, false);
@@ -89,14 +95,11 @@ semantic error: Error with label no message
     CHECK(out == ref);
 
 
-
-    Location loc2;
-    loc2.first = 9;
-    loc2.last = 12;
+    // 1 Label 2 Spans
     d = Diagnostic(
             "Error with label and message",
             Level::Error, Stage::Semantic, {
-                Label("label message", {loc, loc2})
+                Label("label message", {loc1, loc2})
             }
         );
     out = render_diagnostic(d, input, lm, false, false);
@@ -109,12 +112,28 @@ semantic error: Error with label and message
 )""");
     CHECK(out == ref);
 
-    loc2.first = 9;
-    loc2.last = 12;
+    // 1 Label 3 Spans
+    d = Diagnostic(
+            "Error with label and message",
+            Level::Error, Stage::Semantic, {
+                Label("label message", {loc1, loc2, loc3})
+            }
+        );
+    out = render_diagnostic(d, input, lm, false, false);
+    ref = S(R"""(
+semantic error: Error with label and message
+ --> input:1:5
+  |
+1 | One line text
+  | ^^^ ^^^^ ^^^^ label message
+)""");
+//    CHECK(out == ref);
+
+    // 2 Label 1 Span
     d = Diagnostic(
             "Error with two labels and message",
             Level::Error, Stage::Semantic, {
-                Label("label1 message", {loc}),
+                Label("label1 message", {loc1}),
                 Label("label2 message", {loc2})
             }
         );
@@ -127,7 +146,32 @@ semantic error: Error with two labels and message
   |     ^^^^ label1 message
   |
 1 | One line text
-  |          ~~~~ label2 message
+  |          ^^^^ label2 message
 )""");
-    CHECK(out == ref);
+//    CHECK(out == ref);
+
+    // 3 Label 1 Span
+    d = Diagnostic(
+            "Error with two labels and message",
+            Level::Error, Stage::Semantic, {
+                Label("label1 message", {loc1}),
+                Label("label2 message", {loc2}),
+                Label("label3 message", {loc3})
+            }
+        );
+    out = render_diagnostic(d, input, lm, false, false);
+    ref = S(R"""(
+semantic error: Error with two labels and message
+ --> input:1:5
+  |
+1 | One line text
+  |     ^^^^ label1 message
+  |
+1 | One line text
+  |          ^^^^ label2 message
+  |
+1 | One line text
+  | ^^^ label3 message
+)""");
+//    CHECK(out == ref);
 }
