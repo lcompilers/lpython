7a29b5d6b90eaaca723e8faf8f87d92dafb04637
Ondřej Čertík <ondrej@certik.us>
2021-11-02 21:44:31 -0600

    Type comments

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 import ast
 
 input = open("doconcurrentloop_01.py").read()
-a = ast.parse(input)
+a = ast.parse(input, type_comments=True)
 print(ast.unparse(a))
 print()
 print(ast.dump(a))
--- doconcurrentloop_01.py
+++ doconcurrentloop_01.py
@@ -1,9 +1,8 @@
-def triad(a, b: f32[:], scalar: f32, c: f32[:]):
+def triad(a: f32[:], b: f32[:], scalar: f32, c: f32[:]):
     N: i32
     i: i32
     N = size(a)
-    # parallel
-    for i in range(N):
+    for i in range(N): # type: parallel
         c[i] = a[i] + scalar * b[i]
 
 def main():
@@ -15,8 +14,7 @@ def main():
     nsize: i32
     scalar = 10
     nsize = size(a)
-    # parallel
-    for i in range(nsize):
+    for i in range(nsize): # type: parallel
         a[i] = 5
         b[i] = 5
     triad(a, b, scalar, c)
