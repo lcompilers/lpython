abedf75d94009ca5818cf66201a295057e4f137a
Ondřej Čertík <ondrej@certik.us>
2021-11-11 09:57:11 -0700

    Better message

--- grammar/asdl_py.py
+++ grammar/asdl_py.py
@@ -355,7 +355,7 @@ def main(argv):
     if len(argv) == 4:
         def_file, out_file, util_import_part = argv[1:]
     elif len(argv) == 1:
-        print("Assuming default values of AST.asdl and ast.py")
+        print("Assuming default values of Python.asdl and python_ast.py")
         here = os.path.dirname(__file__)
         def_file = os.path.join(here, "Python.asdl")
         out_file = os.path.join(here, "..", "python_ast.py")
