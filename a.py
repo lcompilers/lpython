40919213a473e2a18e2b270bc864ce1f096e67b6
Ondřej Čertík <ondrej@certik.us>
2021-11-11 11:41:52 -0700

    Transform

--- a.py
+++ a.py
@@ -1,3 +1,4 @@
+import python_ast
 import ast
 
 input = open("doconcurrentloop_01.py").read()
@@ -6,6 +7,39 @@ print(ast.unparse(a))
 print()
 print(ast.dump(a))
 
-from python_ast import GenericASTVisitor
-v = GenericASTVisitor()
+class Transform(ast.NodeVisitor):
+
+    def generic_visit(self, node):
+        d = {}
+        for field, value in ast.iter_fields(node):
+            print(field, value)
+            if isinstance(value, list):
+                new_list = []
+                for item in value:
+                    if isinstance(item, ast.AST):
+                        new_list.append(self.visit(item))
+                d[field] = new_list
+            elif isinstance(value, ast.AST):
+                d[field] = self.visit(value)
+            elif isinstance(value, (str, int)):
+                d[field] = value
+            elif value is None:
+                d[field] = value
+            else:
+                print(type(value))
+                raise Exception("Unsupported type")
+        class_name = node.__class__.__name__
+        print("V:", class_name)
+        new_ast = getattr(python_ast, class_name)
+        print(ast.dump(node))
+        print(d)
+        new_node = new_ast(**d)
+        return new_node
+
+
+#v = GenericASTVisitor()
+#v.visit(a)
+
+print()
+v = Transform()
 v.visit(a)
