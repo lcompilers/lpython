6602cc81075bd70dee7a6b50361e86dbecf7627c
Ondřej Čertík <ondrej@certik.us>
2021-11-16 21:03:50 -0700

    Add python deserialization

---
+++ src/lfortran/python_serialization.cpp
@@ -0,0 +1,50 @@
+#include <string>
+
+#include <lfortran/config.h>
+#include <lfortran/serialization.h>
+#include <lfortran/parser/parser.h>
+#include <lfortran/parser/parser.tab.hh>
+#include <lfortran/bwriter.h>
+#include <lfortran/string_utils.h>
+
+
+namespace LFortran::Python {
+
+class ASTDeserializationVisitor :
+#ifdef WITH_LFORTRAN_BINARY_MODFILES
+    public BinaryReader,
+#else
+    public TextReader,
+#endif
+    public AST::DeserializationBaseVisitor<ASTDeserializationVisitor>
+{
+public:
+    ASTDeserializationVisitor(Allocator &al, const std::string &s) :
+#ifdef WITH_LFORTRAN_BINARY_MODFILES
+        BinaryReader(s),
+#else
+        TextReader(s),
+#endif
+        DeserializationBaseVisitor(al, true) {}
+
+    bool read_bool() {
+        uint8_t b = read_int8();
+        return (b == 1);
+    }
+
+    char* read_cstring() {
+        std::string s = read_string();
+        LFortran::Str cs;
+        cs.from_str_view(s);
+        char* p = cs.c_str(al);
+        return p;
+    }
+};
+
+AST::ast_t* deserialize_ast(Allocator &al, const std::string &s) {
+    ASTDeserializationVisitor v(al, s);
+    return v.deserialize_node();
+}
+
+
+} // namespace LFortran
---
+++ src/lfortran/python_serialization.h
@@ -0,0 +1,14 @@
+#ifndef LFORTRAN_PYTHON_SERIALIZATION_H
+#define LFORTRAN_PYTHON_SERIALIZATION_H
+
+#include <lfortran/parser/parser_stype.h>
+#include <lfortran/python_ast.h>
+#include <lfortran/asr.h>
+
+namespace LFortran::Python {
+
+    AST::ast_t* deserialize_ast(Allocator &al, const std::string &s);
+
+}
+
+#endif // LFORTRAN_PYTHON_SERIALIZATION_H
