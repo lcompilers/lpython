aaeafcf6cf3be37da2bff3370cb10819d5f04827
Ondřej Čertík <ondrej@certik.us>
2021-11-17 17:29:18 -0700

    Make visitors compile

--- src/lfortran/semantics/python_ast_to_asr.cpp
+++ src/lfortran/semantics/python_ast_to_asr.cpp
@@ -16,17 +16,132 @@
 
 namespace LFortran::Python {
 
-Result<ASR::TranslationUnit_t*> python_ast_to_asr(Allocator &al,
-    Python::AST::ast_t &ast, diag::Diagnostics &diagnostics)
+template <class Derived>
+class CommonVisitor : public AST::BaseVisitor<Derived> {
+public:
+    diag::Diagnostics &diag;
+
+
+    ASR::asr_t *tmp;
+    Allocator &al;
+    SymbolTable *current_scope;
+    ASR::Module_t *current_module = nullptr;
+    Vec<char *> current_module_dependencies;
+
+    CommonVisitor(Allocator &al, SymbolTable *symbol_table,
+            diag::Diagnostics &diagnostics)
+        : diag{diagnostics}, al{al}, current_scope{symbol_table} {
+        current_module_dependencies.reserve(al, 4);
+    }
+
+    ASR::asr_t* resolve_variable(const Location &loc, const std::string &var_name) {
+        SymbolTable *scope = current_scope;
+        ASR::symbol_t *v = scope->resolve_symbol(var_name);
+        if (!v) {
+            diag.semantic_error_label("Variable '" + var_name
+                + "' is not declared", {loc},
+                "'" + var_name + "' is undeclared");
+            throw SemanticAbort();
+        }
+        if( v->type == ASR::symbolType::Variable ) {
+            ASR::Variable_t* v_var = ASR::down_cast<ASR::Variable_t>(v);
+            if( v_var->m_type == nullptr &&
+                v_var->m_intent == ASR::intentType::AssociateBlock ) {
+                return (ASR::asr_t*)(v_var->m_symbolic_value);
+            }
+        }
+        return ASR::make_Var_t(al, loc, v);
+    }
+
+};
+
+class SymbolTableVisitor : public CommonVisitor<SymbolTableVisitor> {
+public:
+    SymbolTable *global_scope;
+    std::map<std::string, std::vector<std::string>> generic_procedures;
+    std::map<std::string, std::map<std::string, std::vector<std::string>>> generic_class_procedures;
+    std::map<std::string, std::vector<std::string>> defined_op_procs;
+    std::map<std::string, std::map<std::string, std::string>> class_procedures;
+    std::vector<std::string> assgn_proc_names;
+    std::string dt_name;
+    bool in_module = false;
+    bool in_submodule = false;
+    bool is_interface = false;
+    std::string interface_name = "";
+    bool is_derived_type = false;
+    Vec<char*> data_member_names;
+    std::vector<std::string> current_procedure_args;
+    ASR::abiType current_procedure_abi_type = ASR::abiType::Source;
+    std::map<SymbolTable*, ASR::accessType> assgn;
+    ASR::symbol_t *current_module_sym;
+    std::vector<std::string> excluded_from_symtab;
+
+
+    SymbolTableVisitor(Allocator &al, SymbolTable *symbol_table,
+        diag::Diagnostics &diagnostics)
+      : CommonVisitor(al, symbol_table, diagnostics), is_derived_type{false} {}
+
+
+    ASR::symbol_t* resolve_symbol(const Location &loc, const std::string &sub_name) {
+        SymbolTable *scope = current_scope;
+        ASR::symbol_t *sub = scope->resolve_symbol(sub_name);
+        if (!sub) {
+            throw SemanticError("Symbol '" + sub_name + "' not declared", loc);
+        }
+        return sub;
+    }
+
+    void visit_Module(const AST::Module_t &x) {
+        if (!current_scope) {
+            current_scope = al.make_new<SymbolTable>(nullptr);
+        }
+        LFORTRAN_ASSERT(current_scope != nullptr);
+        global_scope = current_scope;
+
+        // Create the TU early, so that asr_owner is set, so that
+        // ASRUtils::get_tu_symtab() can be used, which has an assert
+        // for asr_owner.
+        ASR::asr_t *tmp0 = ASR::make_TranslationUnit_t(al, x.base.base.loc,
+            current_scope, nullptr, 0);
+
+        /*
+        for (size_t i=0; i<x.n_items; i++) {
+            AST::astType t = x.m_items[i]->type;
+            if (t != AST::astType::expr && t != AST::astType::stmt) {
+                visit_ast(*x.m_items[i]);
+            }
+        }
+        */
+        global_scope = nullptr;
+        tmp = tmp0;
+    }
+};
+
+Result<ASR::asr_t*> symbol_table_visitor(Allocator &al, AST::Module_t &ast,
+        diag::Diagnostics &diagnostics)
 {
-    Error error;
-    diagnostics.diagnostics.push_back(
-            diag::Diagnostic("start", diag::Level::Error, diag::Stage::Semantic, {})
-    );
-    return error;
+    SymbolTableVisitor v(al, nullptr, diagnostics);
+    try {
+        v.visit_Module(ast);
+    } catch (const SemanticError &e) {
+        Error error;
+        diagnostics.diagnostics.push_back(e.d);
+        return error;
+    } catch (const SemanticAbort &) {
+        Error error;
+        return error;
+    }
+    ASR::asr_t *unit = v.tmp;
+    return unit;
+}
+
+Result<ASR::TranslationUnit_t*> python_ast_to_asr(Allocator &al,
+    AST::ast_t &ast, diag::Diagnostics &diagnostics)
+{
+    AST::Module_t *ast_m = AST::down_cast2<AST::Module_t>(&ast);
 
     ASR::asr_t *unit;
-    auto res = symbol_table_visitor(al, ast, diagnostics);
+    auto res = symbol_table_visitor(al, *ast_m, diagnostics);
     if (res.ok) {
         unit = res.result;
     } else {
@@ -35,6 +150,7 @@ Result<ASR::TranslationUnit_t*> python_ast_to_asr(Allocator &al,
     ASR::TranslationUnit_t *tu = ASR::down_cast2<ASR::TranslationUnit_t>(unit);
     LFORTRAN_ASSERT(asr_verify(*tu));
 
+    /*
     auto res2 = body_visitor(al, ast, diagnostics, unit);
     if (res2.ok) {
         tu = res2.result;
@@ -42,6 +158,8 @@ Result<ASR::TranslationUnit_t*> python_ast_to_asr(Allocator &al,
         return res2.error;
     }
     LFORTRAN_ASSERT(asr_verify(*tu));
+    */
+
 
     return tu;
 }
