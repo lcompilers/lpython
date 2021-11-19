2330d3344ba363b664135fcd8f60abc84442d07f
Ondřej Čertík <ondrej@certik.us>
2021-11-18 20:41:23 -0700

    Move it to common visitor

--- src/lfortran/semantics/python_ast_to_asr.cpp
+++ src/lfortran/semantics/python_ast_to_asr.cpp
@@ -53,65 +53,6 @@ public:
         return ASR::make_Var_t(al, loc, v);
     }
 
-};
-
-class SymbolTableVisitor : public CommonVisitor<SymbolTableVisitor> {
-public:
-    SymbolTable *global_scope;
-    std::map<std::string, std::vector<std::string>> generic_procedures;
-    std::map<std::string, std::map<std::string, std::vector<std::string>>> generic_class_procedures;
-    std::map<std::string, std::vector<std::string>> defined_op_procs;
-    std::map<std::string, std::map<std::string, std::string>> class_procedures;
-    std::vector<std::string> assgn_proc_names;
-    std::string dt_name;
-    bool in_module = false;
-    bool in_submodule = false;
-    bool is_interface = false;
-    std::string interface_name = "";
-    bool is_derived_type = false;
-    Vec<char*> data_member_names;
-    std::vector<std::string> current_procedure_args;
-    ASR::abiType current_procedure_abi_type = ASR::abiType::Source;
-    std::map<SymbolTable*, ASR::accessType> assgn;
-    ASR::symbol_t *current_module_sym;
-    std::vector<std::string> excluded_from_symtab;
-
-
-    SymbolTableVisitor(Allocator &al, SymbolTable *symbol_table,
-        diag::Diagnostics &diagnostics)
-      : CommonVisitor(al, symbol_table, diagnostics), is_derived_type{false} {}
-
-
-    ASR::symbol_t* resolve_symbol(const Location &loc, const std::string &sub_name) {
-        SymbolTable *scope = current_scope;
-        ASR::symbol_t *sub = scope->resolve_symbol(sub_name);
-        if (!sub) {
-            throw SemanticError("Symbol '" + sub_name + "' not declared", loc);
-        }
-        return sub;
-    }
-
-    void visit_Module(const AST::Module_t &x) {
-        if (!current_scope) {
-            current_scope = al.make_new<SymbolTable>(nullptr);
-        }
-        LFORTRAN_ASSERT(current_scope != nullptr);
-        global_scope = current_scope;
-
-        // Create the TU early, so that asr_owner is set, so that
-        // ASRUtils::get_tu_symtab() can be used, which has an assert
-        // for asr_owner.
-        ASR::asr_t *tmp0 = ASR::make_TranslationUnit_t(al, x.base.base.loc,
-            current_scope, nullptr, 0);
-
-        for (size_t i=0; i<x.n_body; i++) {
-            visit_stmt(*x.m_body[i]);
-        }
-
-        global_scope = nullptr;
-        tmp = tmp0;
-    }
-
     ASR::ttype_t * ast_expr_to_asr_type(const Location &loc, const AST::expr_t &annotation) {
         Vec<ASR::dimension_t> dims;
         dims.reserve(al, 4);
@@ -174,6 +115,65 @@ public:
             throw SemanticError("Annotation type not supported", loc);
         }
         return type;
+    }
+
+};
+
+class SymbolTableVisitor : public CommonVisitor<SymbolTableVisitor> {
+public:
+    SymbolTable *global_scope;
+    std::map<std::string, std::vector<std::string>> generic_procedures;
+    std::map<std::string, std::map<std::string, std::vector<std::string>>> generic_class_procedures;
+    std::map<std::string, std::vector<std::string>> defined_op_procs;
+    std::map<std::string, std::map<std::string, std::string>> class_procedures;
+    std::vector<std::string> assgn_proc_names;
+    std::string dt_name;
+    bool in_module = false;
+    bool in_submodule = false;
+    bool is_interface = false;
+    std::string interface_name = "";
+    bool is_derived_type = false;
+    Vec<char*> data_member_names;
+    std::vector<std::string> current_procedure_args;
+    ASR::abiType current_procedure_abi_type = ASR::abiType::Source;
+    std::map<SymbolTable*, ASR::accessType> assgn;
+    ASR::symbol_t *current_module_sym;
+    std::vector<std::string> excluded_from_symtab;
+
+
+    SymbolTableVisitor(Allocator &al, SymbolTable *symbol_table,
+        diag::Diagnostics &diagnostics)
+      : CommonVisitor(al, symbol_table, diagnostics), is_derived_type{false} {}
+
+
+    ASR::symbol_t* resolve_symbol(const Location &loc, const std::string &sub_name) {
+        SymbolTable *scope = current_scope;
+        ASR::symbol_t *sub = scope->resolve_symbol(sub_name);
+        if (!sub) {
+            throw SemanticError("Symbol '" + sub_name + "' not declared", loc);
+        }
+        return sub;
+    }
+
+    void visit_Module(const AST::Module_t &x) {
+        if (!current_scope) {
+            current_scope = al.make_new<SymbolTable>(nullptr);
+        }
+        LFORTRAN_ASSERT(current_scope != nullptr);
+        global_scope = current_scope;
+
+        // Create the TU early, so that asr_owner is set, so that
+        // ASRUtils::get_tu_symtab() can be used, which has an assert
+        // for asr_owner.
+        ASR::asr_t *tmp0 = ASR::make_TranslationUnit_t(al, x.base.base.loc,
+            current_scope, nullptr, 0);
+
+        for (size_t i=0; i<x.n_body; i++) {
+            visit_stmt(*x.m_body[i]);
+        }
+
+        global_scope = nullptr;
+        tmp = tmp0;
     }
 
     void visit_FunctionDef(const AST::FunctionDef_t &x) {
