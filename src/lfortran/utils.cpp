2addec0d962d697d138d365ea3a8a250e69aaeaf
Ondřej Čertík <ondrej@certik.us>
2021-11-26 10:29:02 -0700

    Match upper case too

--- src/lfortran/utils.cpp
+++ src/lfortran/utils.cpp
@@ -39,7 +39,8 @@ std::string get_runtime_library_dir()
     get_executable_path(path, dirname_length);
     std::string dirname = path.substr(0,dirname_length);
     if (   endswith(dirname, "src/bin")
-        || endswith(dirname, "src\\bin")) {
+        || endswith(dirname, "src\\bin")
+        || endswith(dirname, "SRC\\BIN")) {
         // Development version
         return dirname + "/../runtime";
     } else if (endswith(dirname, "src/lfortran/tests") ||
