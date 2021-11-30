1bbbf27528336c75b3b1272def54c95a2b4007e6
Gagandeep Singh <gdp.1807@gmail.com>
2021-11-30 16:55:00 +0530

    Updated test for constant array in body and decl

--- integration_tests/arrays_05.f90
+++ integration_tests/arrays_05.f90
@@ -1,9 +1,9 @@
 program arrays_05
 implicit none
-real, dimension(5) :: numbers
+real, dimension(5) :: numbers = [1.5, 3.2, 4.5, 0.9, 7.2]
 integer :: i
 do i = 1, 5
    numbers(i) = i * 2.0
 end do
-numbers = (/ 1.5, 3.2, 4.5, 0.9, 7.2 /)
+numbers = [1.5, 3.2, 4.5, 0.9, 7.2]
 end program
