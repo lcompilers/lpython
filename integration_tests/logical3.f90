ef2f87c56b8e932f8e95a8b6345c2195afda67f2
Ondřej Čertík <ondrej@certik.us>
2021-12-01 17:35:15 -0800

    Add a test for .xor.

--- integration_tests/logical3.f90
+++ integration_tests/logical3.f90
@@ -21,6 +21,13 @@ else
     error stop
 end if
 
+if (a .xor. b) then
+    print *, "Line xor - Condition is true"
+else
+    print *, "Line xor - Condition is false"
+    error stop
+end if
+
 ! changing values
 a = .false.
 b = .true.
@@ -45,4 +52,30 @@ if (b .eqv. a) then
 else
     print *, "Line 5 - Condition is false"
 end if
+
+! changing values
+a = .true.
+b = .true.
+
+if (a .and. b) then
+    print *, "Line 6 - Condition is true"
+else
+    print *, "Line 6 - Condition is false"
+    error stop
+end if
+
+if (a .or. b) then
+    print *, "Line 7 - Condition is true"
+else
+    print *, "Line 7 - Condition is false"
+    error stop
+end if
+
+if (a .xor. b) then
+    print *, "Line 8 xor - Condition is true"
+    error stop
+else
+    print *, "Line 8 xor - Condition is false"
+end if
+
 end program logical3
