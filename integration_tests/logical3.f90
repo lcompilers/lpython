4bca415827352e18263eaa51050c429d618fee7a
Ondřej Čertík <ondrej@certik.us>
2021-12-01 17:24:29 -0800

    Add error stop into the test

--- integration_tests/logical3.f90
+++ integration_tests/logical3.f90
@@ -11,6 +11,7 @@ program logical3
 
        if (a .and. b) then
           print *, "Line 1 - Condition is true"
+          error stop
        else
           print *, "Line 1 - Condition is false"
        end if
@@ -19,6 +20,7 @@ program logical3
           print *, "Line 2 - Condition is true"
        else
           print *, "Line 2 - Condition is false"
+          error stop
        end if
        
        ! changing values
@@ -29,16 +31,19 @@ program logical3
           print *, "Line 3 - Condition is true"
        else
           print *, "Line 3 - Condition is false"
+          error stop
        end if
        
        if (b .neqv. a) then
           print *, "Line 4 - Condition is true"
        else
           print *, "Line 4 - Condition is false"
+          error stop
        end if
        
        if (b .eqv. a) then
           print *, "Line 5 - Condition is true"
+          error stop
        else
           print *, "Line 5 - Condition is false"
        end if
