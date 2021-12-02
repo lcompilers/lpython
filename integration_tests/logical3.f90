c6969601bc9766bb18d4facc295b5d76b018bc1c
Ondřej Čertík <ondrej@certik.us>
2021-12-01 17:31:33 -0800

    Fix whitespace

--- integration_tests/logical3.f90
+++ integration_tests/logical3.f90
@@ -1,51 +1,48 @@
 program logical3
-    ! this program checks logical operators
-    implicit none
-    
-       ! variable declaration
-       logical :: a, b
-       
-       ! assigning values
-       a = .true.
-       b = .false.
+! This program checks logical operators
+implicit none
+logical :: a, b
 
-       if (a .and. b) then
-          print *, "Line 1 - Condition is true"
-          error stop
-       else
-          print *, "Line 1 - Condition is false"
-       end if
-       
-       if (a .or. b) then
-          print *, "Line 2 - Condition is true"
-       else
-          print *, "Line 2 - Condition is false"
-          error stop
-       end if
-       
-       ! changing values
-       a = .false.
-       b = .true.
-       
-       if (.not.(a .and. b)) then
-          print *, "Line 3 - Condition is true"
-       else
-          print *, "Line 3 - Condition is false"
-          error stop
-       end if
-       
-       if (b .neqv. a) then
-          print *, "Line 4 - Condition is true"
-       else
-          print *, "Line 4 - Condition is false"
-          error stop
-       end if
-       
-       if (b .eqv. a) then
-          print *, "Line 5 - Condition is true"
-          error stop
-       else
-          print *, "Line 5 - Condition is false"
-       end if
-       
+! assigning values
+a = .true.
+b = .false.
+
+if (a .and. b) then
+    print *, "Line 1 - Condition is true"
+    error stop
+else
+    print *, "Line 1 - Condition is false"
+end if
+
+if (a .or. b) then
+    print *, "Line 2 - Condition is true"
+else
+    print *, "Line 2 - Condition is false"
+    error stop
+end if
+
+! changing values
+a = .false.
+b = .true.
+
+if (.not.(a .and. b)) then
+    print *, "Line 3 - Condition is true"
+else
+    print *, "Line 3 - Condition is false"
+    error stop
+end if
+
+if (b .neqv. a) then
+    print *, "Line 4 - Condition is true"
+else
+    print *, "Line 4 - Condition is false"
+    error stop
+end if
+
+if (b .eqv. a) then
+    print *, "Line 5 - Condition is true"
+    error stop
+else
+    print *, "Line 5 - Condition is false"
+end if
 end program logical3
