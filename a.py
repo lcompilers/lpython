da6cdae83930b2bbf044153305214a3a390f7c7e
Ondřej Čertík <ondrej@certik.us>
2022-01-12 10:45:19 -0700

    Fix bool handling and add complex

--- a.py
+++ a.py
@@ -18,12 +18,15 @@ class Transform(ast.NodeVisitor):
     def visit_Constant(self, node):
         if isinstance(node.value, str):
             return python_ast.ConstantStr(node.value, node.kind)
+        elif isinstance(node.value, bool):
+            return python_ast.ConstantBool(node.value, node.kind)
         elif isinstance(node.value, int):
             return python_ast.ConstantInt(node.value, node.kind)
         elif isinstance(node.value, float):
             return python_ast.ConstantFloat(node.value, node.kind)
-        elif isinstance(node.value, bool):
-            return python_ast.ConstantBool(node.value, node.kind)
+        elif isinstance(node.value, complex):
+            return python_ast.ConstantComplex(node.value.real,
+                        node.value.imag, node.kind)
         else:
             print(type(node.value))
             raise Exception("Unsupported Constant type")
