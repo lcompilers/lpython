36a0da3b2b60c156c8a10ec2cd0e7bdc4f485879
Ondřej Čertík <ondrej@certik.us>
2022-02-09 15:21:40 -0700

    Implement sys.exit()

--- src/runtime/sys.py
+++ src/runtime/sys.py
@@ -1,6 +1,8 @@
+from ltypes import i32
+
 def exit(error_code: i32):
     """
     Exits the program with an error code `error_code`.
     """
 
-    print("Not implemented yet.")
+    quit(error_code)
