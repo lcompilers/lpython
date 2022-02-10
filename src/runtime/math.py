7bc90a4e91d2dc67c689247a340992179ea8ea5c
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-02-10 18:03:11 +0530

    add more functions to math

--- src/runtime/math.py
+++ src/runtime/math.py
@@ -1,3 +1,5 @@
+from ltypes import i32
+
 def factorial(x: i32) -> i32:
     """
     Computes the factorial of `x`.
@@ -7,7 +9,50 @@ def factorial(x: i32) -> i32:
         return 0
     result: i32
     result = 1
-    while x > 0:
-        result = result * x
-        x -= 1
+    i: i32
+    for i in range(1, x+1):
+        result *= i
     return result
+
+
+def comb(n: i32, k: i32) -> i32:
+    """
+    Computes the result of `nCk`, i.e, the number of ways to choose `k`
+    items from `n` items without repetition and without order.
+    """
+
+    if n < k or n < 0:
+        return 0
+    return factorial(n)//(factorial(k)*factorial(n-k))
+
+
+def perm(n: i32, k: i32) -> i32:
+    """
+    Computes the result of `nPk`, i.e, the number of ways to choose `k` items
+    from `n` items without repetition and with order.
+    """
+
+    if n < k or n < 0:
+        return 0
+    return factorial(n)//factorial(n-k)
+
+
+def isqrt(n: i32) -> i32:
+    """
+    Computes the integer square root of the nonnegative integer `n`.
+    """
+    if n < 0:
+        # TODO: raise error
+        return -1
+    low: i32
+    mid: i32
+    high: i32
+    low = 0
+    high = n+1
+    while low + 1 < high:
+        mid = (low + high)//2
+        if mid*mid <= n:
+            low = mid
+        else:
+            high = mid
+    return low
