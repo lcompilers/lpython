55c4842747ed6bca003e2fa9c4f82c39875704e2
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-02-11 21:46:38 +0530

    add tests

--- integration_tests/test_math.py
+++ integration_tests/test_math.py
@@ -1,5 +1,7 @@
-from math import factorial, isqrt, perm, comb, degrees, radians
+from math import (factorial, isqrt, perm, comb, degrees, radians, exp, pow,
+                  ldexp, fabs)
 from ltypes import i32, f64
+
 
 def test_factorial_1():
     i: i32
@@ -36,9 +38,39 @@ def test_radians():
     print(i)
 
 
+def test_exp():
+    i: f64
+    i = exp(2.34)
+    print(i)
+
+
+def test_pow():
+    i: f64
+    i = pow(2.4, 4.3)
+    print(i)
+
+
+def test_ldexp():
+    i: f64
+    i = ldexp(23.3, 2)
+    print(i)
+
+
+def test_fabs():
+    i: f64
+    j: f64
+    i = fabs(10.3)
+    j = fabs(-10.3)
+    print(i, j)
+
+
 test_factorial_1()
 test_comb()
 test_isqrt()
 test_perm()
 test_degrees()
 test_radians()
+test_exp()
+test_pow()
+test_fabs()
+test_ldexp()
