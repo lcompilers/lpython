5075c2496f1e0fdb2dc3112913ba12db7601e030
Naman Gera <namangera15@gmail.com>
2022-02-13 16:41:13 +0530

    Use `list` type annotation in tests

--- tests/list1.py
+++ tests/list1.py
@@ -1,11 +1,12 @@
 def test_List():
-    a: i32
-    b: i32
-
+    a: list[i32]
     a = [1, 2, 3]
     a = [-3, -2, -1]
-    a = ["a", "b", "c"]
-    a = [[1, 2, 3], [4, 5, 6]]
-    # a = [-2, -1, 0.45] -> semantic error
 
-    b = a[2]
+    b: list[str]
+    b = ["a", "b", "c"]
+
+    c: i32
+    c = [[1, 2, 3], [4, 5, 6]]
+    # a = [-2, -1, 0.45] -> semantic error
+    c = a[2]
