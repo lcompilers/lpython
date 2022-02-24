fe56bec279c31891bdab041012aa159b44c94a86
Naman Gera <namangera15@gmail.com>
2022-02-24 16:27:42 +0530

    Add test for `str` function

--- integration_tests/test_builtin_str.py
+++ integration_tests/test_builtin_str.py
@@ -1,6 +1,8 @@
 def test_str_int():
     s: str
-    s = str_int(356)
+    s = str(356)
     assert s == "356"
-    s = str_int(-567)
+    s = str(-567)
     assert s == "-567"
+    assert str(4) == "4"
+    assert str(-5) == "-5"
