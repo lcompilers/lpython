6418b71c21122e6258d0e65a58361dd208460661
Naman Gera <namangera15@gmail.com>
2022-02-26 23:29:37 +0530

    Write the test for `float`

---
+++ integration_tests/test_builtin_float.py
@@ -0,0 +1,10 @@
+from ltypes import i32
+
+def test_float():
+    i: i32
+    i = 34
+    assert float(i) == 34.0
+    i = -4235
+    assert float(i) == -4235.0
+    assert float(34) == 34.0
+    assert float(-4235) == -4235.0
