91ba961a3f27a141599581026bf8f446ac6f004c
Naman Gera <namangera15@gmail.com>
2022-03-07 04:28:10 +0530

    Write the test for `random.randint()`

--- integration_tests/test_random.py
+++ integration_tests/test_random.py
@@ -1,4 +1,4 @@
-from ltypes import f64
+from ltypes import i32, f64
 import random
 
 
@@ -7,6 +7,19 @@ def test_random():
     r = random.random()
     print(r)
     assert r >= 0.0 and r < 1.0
+    r = random.random()
+    print(r)
+    assert r >= 0.0 and r < 1.0
 
+def test_randint():
+    ri1: i32
+    ri2: i32
+    ri1 = random.randint(0, 10) # [0, 10]
+    print(ri1)
+    assert ri1 >= 0 and ri1 <= 10
+    ri2 = random.randint(-50, 76) # [-50, 76]
+    print(ri2)
+    assert ri2 >= -50 and ri2 <= 76
 
 test_random()
+test_randint()
