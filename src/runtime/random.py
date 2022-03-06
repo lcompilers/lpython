2ee64d0a7ab65608f0977dbb2990b2cab8cdfda8
Naman Gera <namangera15@gmail.com>
2022-03-07 04:43:12 +0530

    Add docstrings

--- src/runtime/random.py
+++ src/runtime/random.py
@@ -1,6 +1,9 @@
 from ltypes import i32, f64, ccall
 
 def random() -> f64:
+    """
+    Returns a random floating point number in the range [0.0, 1.0)
+    """
     return _lfortran_random_float()
 
 @ccall
@@ -8,6 +11,9 @@ def _lfortran_random_float() -> f64:
     pass
 
 def randint(lower: i32, upper: i32) -> i32:
+    """
+    Return a random integer N such that `lower <= N <= upper`.
+    """
     return _lfortran_random_int(lower, upper)
 
 @ccall
