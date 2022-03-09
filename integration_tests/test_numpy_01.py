8362b6e519d1323db6353fefb3ec6c9d97127210
Ondřej Čertík <ondrej@certik.us>
2022-03-09 10:53:50 -0700

    Add a test for numpy

---
+++ integration_tests/test_numpy_01.py
@@ -0,0 +1,20 @@
+from ltypes import f32
+from numpy import empty, float32
+
+def test_local_arrays():
+    a: f32[16]
+    a = empty(16)
+    i: i32
+    for i in range(16):
+        a[i] = i+0.5
+    eps: f64
+    eps = 1e-12
+    assert abs(a[0] - 0.5) < eps
+    assert abs(a[1] - 1.5) < eps
+    assert abs(a[10] - 10.5) < eps
+    assert abs(a[15] - 15.5) < eps
+
+def check():
+    test_local_arrays()
+
+check()
