6472b030a47dc900d9d4e89ce2ff0326ba3ccc24
Ondřej Čertík <ondrej@certik.us>
2022-03-09 11:07:44 -0700

    Initial implementation of sum

--- src/runtime/numpy.py
+++ src/runtime/numpy.py
@@ -1,4 +1,35 @@
-float32: i32 = 1
+from ltypes import overload
+from numpy_builtin import size
 
-def empty(n: i64, dtype: i32):
-    pass
+@overload
+def sum(a: i32[:]) -> i32:
+    s: i32
+    i: i32
+    s = 0
+    for i in range(size(a)):
+        s += a[i]
+    return s
+
+@overload
+def sum(a: i32[:,:]) -> i32:
+    s: i32
+    i: i32
+    j: i32
+    s = 0
+    for i in range(size(a,0)):
+        for j in range(size(a,1)):
+            s += a[i,j]
+    return s
+
+@overload
+def sum(a: i32[:,:,:]) -> i32:
+    s: i32
+    i: i32
+    j: i32
+    k: i32
+    s = 0
+    for i in range(size(a,0)):
+        for j in range(size(a,1)):
+            for k in range(size(a,2)):
+                s += a[i,j,k]
+    return s
