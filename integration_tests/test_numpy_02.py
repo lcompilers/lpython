7838f758cc11422b6150e7e0ce7187dadc9b1ccc
Ondřej Čertík <ondrej@certik.us>
2022-03-13 18:23:58 -0600

    Add a test for arange()

--- integration_tests/test_numpy_02.py
+++ integration_tests/test_numpy_02.py
@@ -13,7 +13,13 @@ def zeros(n: i32) -> f64[n]:
         A[i] = 0.0
     return A
 
-
+def arange(n: i32) -> f64[n]:
+    A: f64[n]
+    A = empty(n)
+    i: i32
+    for i in range(n):
+        A[i] = 1.0 * i
+    return A
 
 def test_zeros():
     a: f64[4]
@@ -25,7 +31,18 @@ def test_zeros():
     assert abs(a[2] - 0.0) < eps
     assert abs(a[3] - 0.0) < eps
 
+def test_arange():
+    a: f64[4]
+    a = arange(4)
+    eps: f64
+    eps = 1e-12
+    assert abs(a[0] - 0.0) < eps
+    assert abs(a[1] - 1.0) < eps
+    assert abs(a[2] - 2.0) < eps
+    assert abs(a[3] - 3.0) < eps
+
 def check():
     test_zeros()
+    test_arange()
 
 check()
