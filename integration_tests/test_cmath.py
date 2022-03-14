e7124ea353262b05147f37f4aaef0ce0b623b50a
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-03-14 15:45:47 +0530

    add tests for trigonometric functions

--- integration_tests/test_cmath.py
+++ integration_tests/test_cmath.py
@@ -1,4 +1,4 @@
-from cmath import exp, log, sqrt
+from cmath import exp, log, sqrt, acos, asin, atan, cos, sin, tan
 from ltypes import c64
 
 def test_power_logarithmic():
@@ -9,4 +9,18 @@ def test_power_logarithmic():
     y = log(x)
     y = sqrt(x)
 
+
+def test_trigonometric():
+    x: c64
+    y: c64
+    x = complex(3, 3)
+    y = acos(x)
+    y = asin(x)
+    y = atan(x)
+    y = cos(x)
+    y = sin(x)
+    y = tan(x)
+
+
 test_power_logarithmic()
+test_trigonometric()
