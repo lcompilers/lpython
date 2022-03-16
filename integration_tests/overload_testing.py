b78c6b55bc75f5418b4dd0782f4c5e0876200114
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-03-16 17:39:13 +0530

    add tests

---
+++ integration_tests/overload_testing.py
@@ -0,0 +1,23 @@
+from ltypes import i32, overload
+
+@overload
+def foo(a: i32, b: i32) -> i32:
+    return a*b
+
+@overload
+def foo(a: i32) -> i32:
+    return a**2
+
+@overload
+def foo(a: str) -> str:
+    return "lpython-" + a
+
+@overload
+def test(a: i32) -> i32:
+    return a + 10
+
+@overload
+def test(a: bool) -> i32:
+    if a:
+        return 10
+    return -10
--- integration_tests/test_generics_01.py
+++ integration_tests/test_generics_01.py
@@ -1,33 +1,39 @@
+from overload_testing import foo, test
 from ltypes import overload, i32
 
 @overload
-def foo(a: i32, b: i32) -> i32:
-    return a*b
+def foo1(a: i32, b: i32) -> i32:
+    return a-b
 
 @overload
-def foo(a: i32) -> i32:
-    return a**2
+def foo1(a: i32) -> i32:
+    return a**4
 
 @overload
-def foo(a: str) -> str:
-    return "lpython-" + a
+def foo1(a: str) -> str:
+    return "lpython-is-fun-" + a
 
 @overload
-def test(a: i32) -> i32:
-    return a + 10
+def test1(a: i32) -> i32:
+    return a + 20
 
 @overload
-def test(a: bool) -> i32:
+def test1(a: bool) -> i32:
     if a:
-        return 10
-    return -10
+        return 20
+    return -20
 
 
 def check():
     assert foo(2) == 4
+    assert foo1(2) == 16
     assert foo(2, 10) == 20
+    assert foo1(2, 10) == -8
     assert foo("hello") == "lpython-hello"
+    assert foo1("hello") == "lpython-is-fun-hello"
     assert test(10) == 20
+    assert test1(10) == 30
     assert test(False) == -test(True) and test(True) == 10
+    assert test1(False) == -test1(True) and test1(True) == 20
 
 check()
