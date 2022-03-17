5bb5c191a7636d1b5344f6760d50d8cb47a641db
Naman Gera <namangera15@gmail.com>
2022-03-18 02:26:45 +0530

    Update tests

--- integration_tests/test_builtin_bool.py
+++ integration_tests/test_builtin_bool.py
@@ -1,4 +1,4 @@
-from ltypes import i32, f64
+from ltypes import i32, i64, f32, f64
 
 def test_bool():
     a: i32
@@ -9,6 +9,10 @@ def test_bool():
     assert bool(-1)
     assert not bool(0)
 
+    a2: i64
+    a2 = 34
+    assert bool(a2)
+
     f: f64
     f = 0.0
     assert not bool(f)
@@ -16,5 +20,25 @@ def test_bool():
     assert bool(f)
     assert bool(56.7868658)
     assert not bool(0.0)
+
+    f2: f32
+    f2 = -235.6
+    assert bool(f2)
+
+    s: str
+    s = ""
+    assert not bool(s)
+    s = "str"
+    assert bool(s)
+    assert not bool('')
+    assert bool("str")
+
+    b: bool
+    b = True
+    assert bool(b)
+    b = False
+    assert not bool(b)
+    assert bool(True)
+    assert not bool(False)
 
 test_bool()
