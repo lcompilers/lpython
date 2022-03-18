4cb034b60580370d44d0fbd2e2076b01c6739e92
Naman Gera <namangera15@gmail.com>
2022-03-18 18:17:14 +0530

    Overload `abs` builtin function

--- integration_tests/test_builtin_abs.py
+++ integration_tests/test_builtin_abs.py
@@ -1,4 +1,4 @@
-from ltypes import f64
+from ltypes import f32, f64, i32
 
 def test_abs():
     x: f64
@@ -9,5 +9,19 @@ def test_abs():
     assert abs(5.5) == 5.5
     assert abs(-5.5) == 5.5
 
+    x2: f32
+    x2 = -5.5
+    assert abs(x2) == 5.5
+
+    i: i32
+    i = -5
+    assert abs(i) == 5
+    assert abs(-1) == 1
+
+    b: bool
+    b = True
+    assert abs(b) == 1
+    b = False
+    assert abs(b) == 0
 
 test_abs()
--- src/runtime/lpython_builtin.py
+++ src/runtime/lpython_builtin.py
@@ -28,8 +28,10 @@ def chr(i: i32) -> str:
 #        exit(1)
 
 
-# This is an implementation for f64.
-# TODO: implement abs() as a generic procedure, and implement for all types
+#: abs() as a generic procedure.
+#: supported types for argument:
+#  i32, f32, f64, bool, c32, c64
+@overload
 def abs(x: f64) -> f64:
     """
     Return the absolute value of `x`.
@@ -38,6 +40,35 @@ def abs(x: f64) -> f64:
         return x
     else:
         return -x
+
+@overload
+def abs(x: f32) -> f32:
+    if x >= 0.0:
+        return x
+    else:
+        return -x
+
+@overload
+def abs(x: i32) -> i64:
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+@overload
+def abs(b: bool) -> i32:
+    if b:
+        return 1
+    else:
+        return 0
+
+@overload
+def abs(c: c32) -> f32:
+    pass
+
+@overload
+def abs(c: c64) -> f64:
+    pass
 
 
 def str(x: i32) -> str:
