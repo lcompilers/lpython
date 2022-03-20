794a3a9be471a3eac05691195f7bf6ab94963738
Ondřej Čertík <ondrej@certik.us>
2022-03-20 12:44:10 -0700

    Add tests for i8, i16, i32 and i64

---
+++ integration_tests/test_types_01.py
@@ -0,0 +1,26 @@
+from ltypes import i8, i16, i32, i64
+
+def test_i8():
+    i: i8
+    i = 5
+    assert i == 5
+
+def test_i16():
+    i: i16
+    i = 5
+    assert i == 5
+
+def test_i32():
+    i: i32
+    i = 5
+    assert i == 5
+
+def test_i64():
+    i: i64
+    i = 5
+    assert i == 5
+
+test_i8()
+test_i16()
+test_i32()
+test_i64()
