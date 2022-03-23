3819ae7894d7c84d942398ab61ae71b9c00b88b1
Ondřej Čertík <ondrej@certik.us>
2022-03-23 10:06:50 -0600

    AST->ASR: do not mark bind(c) fns as intrinsic

--- src/lpython/semantics/python_ast_to_asr.cpp
+++ src/lpython/semantics/python_ast_to_asr.cpp
@@ -154,7 +154,9 @@ ASR::Module_t* load_module(Allocator &al, SymbolTable *symtab,
         for (auto &item : mod2->m_symtab->scope) {
             if (ASR::is_a<ASR::Subroutine_t>(*item.second)) {
                 ASR::Subroutine_t *s = ASR::down_cast<ASR::Subroutine_t>(item.second);
-                s->m_abi = ASR::abiType::Intrinsic;
+                if (s->m_abi == ASR::abiType::Source) {
+                    s->m_abi = ASR::abiType::Intrinsic;
+                }
                 if (s->n_body == 0) {
                     std::string name = s->m_name;
                     if (name == "ubound" || name == "lbound") {
@@ -164,7 +166,9 @@ ASR::Module_t* load_module(Allocator &al, SymbolTable *symtab,
             }
             if (ASR::is_a<ASR::Function_t>(*item.second)) {
                 ASR::Function_t *s = ASR::down_cast<ASR::Function_t>(item.second);
-                s->m_abi = ASR::abiType::Intrinsic;
+                if (s->m_abi == ASR::abiType::Source) {
+                    s->m_abi = ASR::abiType::Intrinsic;
+                }
                 if (s->n_body == 0) {
                     std::string name = s->m_name;
                     if (name == "ubound" || name == "lbound") {
