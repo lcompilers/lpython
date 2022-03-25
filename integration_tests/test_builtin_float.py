f4a4a8e1bc10bd0a7b45145fd4febb6c653a1622
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-03-25 18:10:40 +0530

    Remove the TODO and uncomment and update the tests

--- integration_tests/test_builtin_float.py
+++ integration_tests/test_builtin_float.py
@@ -1,11 +1,10 @@
 from ltypes import i32
 
 def test_float():
-    # TODO: the commented tests should also work
     i: i32
     i = 34
     assert float(34) == 34.0
-    # assert float(i) == 34.0
+    assert float(i) == 34.0
     i = -4235
     assert float(-4235) == -4235.0
     assert float(34) == 34.0
