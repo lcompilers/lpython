c5d5f679447c63eaebb2c6f286cbda5db7d376aa
Ondřej Čertík <ondrej@certik.us>
2022-03-29 09:36:30 -0600

    Update the code to use the new API

--- integration_tests/test_os.py
+++ integration_tests/test_os.py
@@ -1,12 +1,12 @@
 from ltypes import i64
-from os import (open, read, close, O_RDONLY)
+from os import (open, read, close)
 
 def test():
     path: str 
     path = "integration_tests/test_os.py"
     fd: i64
     n: i64
-    fd = open(path, O_RDONLY)
+    fd = open(path, "r")
     n = 100
     print(read(fd, n))
     close(fd) 
--- src/runtime/os.py
+++ src/runtime/os.py
@@ -1,19 +1,13 @@
 from ltypes import i32, i64, ccall
 
-O_RDONLY: i32 # = 0 FIXME: Assign the value 0 to O_RDONLY
-# O_WRONLY: i32 = 1
-# O_RDWR  : i32 = 2
-# O_CREAT : i32 = 64
-# O_APPEND: i32 = 1024
-
-def open(path: str, flag: i32) -> i64:
+def open(path: str, flag: str) -> i64:
     """
     Returns the file descriptor for the newly opened file
     """
     return _lpython_open(path, flag)
 
 @ccall
-def _lpython_open(path: str, flag: i32) -> i64:
+def _lpython_open(path: str, flag: str) -> i64:
     pass
 
 def read(fd: i64, n: i64) -> str:
