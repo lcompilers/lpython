cd765fa0690aebd16c44c0ff527c13c4a4f3b483
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-04-01 18:38:28 +0530

    Add and update tests

---
+++ tests/tokens/symbols1.py
@@ -0,0 +1,48 @@
+from typing import overload
+
+i = {1: [(1, 2), (3, 4)]}
+
+i is i
+j = 1
+j in i
+
+def test():
+    print(":"); print(";")
+
+i = 1 + 2 - 3 * 4 / 5 % 6
+
+i = True < (False > True)
+
+i = not(False == False) != True
+i = 3 <= 4 and 4 >= 3
+i = 3 <= 4 or 4 >= 3
+
+i = ~i
+i = 1 ^ 2
+i = 2 << 3
+i = 3 >> 4
+i = 4 ** 5
+i = 5 // 6
+i = 6 | 7
+i = 7 & 8
+
+i |= 1
+i &= 2
+i ^= 3
+i //= 4
+i **= 5
+i <<= 6
+i >>= 7
+i += 8
+i -= 9
+i *= 10
+i /= 11
+i %= 12
+
+@overload
+def test(x):
+    print("Decorators")
+
+class t:
+    def __init__(self, x):
+        self.x = x
