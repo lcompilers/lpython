3eaf49047fcc94e91dbf008f943c5d99d31a5d56
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-04-01 14:36:33 +0530

    Add and update tests

---
+++ tests/tokens/docstring1.py
@@ -0,0 +1,18 @@
+def test1():
+    """A multi-line
+    docstring.
+    """
+
+def test2():
+    """
+    A multi-line
+    docstring.
+    """
+
+def test2():
+    """ A single-line docstring."""
+
+"""
+A multi-line
+docstring.
+"""
