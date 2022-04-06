fa3cf5ebbf2d49280927abfa14e71e6e100843c5
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-04-06 17:18:33 +0530

    Add a test for mod

--- integration_tests/expr_05.py
+++ integration_tests/expr_05.py
@@ -3,6 +3,10 @@ from ltypes import i32, i64
 def test_multiply(a: i32, b: i32) -> i32:
     return a*b
 
+def test_mod(a: i32, b: i32) -> i32:
+    return a%b
+
+
 def main0():
     a: i32
     b: i32
@@ -13,5 +17,16 @@ def main0():
     i = 1
     i += 1
     assert i == 2
+    a = 2
+    b = 5
+    assert test_mod(a, b) == 2
+    assert test_mod(23, 3) == 2
+    a = 123282374
+    b = 32771
+    assert test_mod(a, b) == 30643
+    a = -123282374
+    b = 32771
+    assert test_mod(a, b) == 2128
+
 
 main0()
