d9d41cd2760dc19a82615ec015241be021bf9c6a
Abdelrahman Khaled <abdokh950@gmail.com>
2022-04-12 00:10:35 +0200

    Corrcting code format again

--- src/lpython/semantics/python_comptime_eval.h
+++ src/lpython/semantics/python_comptime_eval.h
@@ -655,26 +655,25 @@ struct PythonIntrinsicProcedures {
         }
     }
 
-
-    static ASR::expr_t* eval_max(Allocator &al , const Location &loc , Vec<ASR::expr_t*>&args) {
+    static ASR::expr_t *eval_max(Allocator &al, const Location &loc, Vec<ASR::expr_t *> &args) {
         LFORTRAN_ASSERT(ASRUtils::all_args_evaluated(args));
-        bool semantic_error_flag = args.size() !=0;
+        bool semantic_error_flag = args.size() != 0;
         std::string msg = "max() takes many arguments to comparing";
-        ASR::expr_t* first_element = args[0];
-        ASR::ttype_t* first_element_type = ASRUtils::expr_type(first_element);
+        ASR::expr_t *first_element = args[0];
+        ASR::ttype_t *first_element_type = ASRUtils::expr_type(first_element);
         semantic_error_flag &= ASRUtils::is_integer(*first_element_type)
-                                || ASRUtils::is_real(*first_element_type)
-                                || ASRUtils::is_character(*first_element_type);
-        int32_t biggest_ind  = 0;
+                               || ASRUtils::is_real(*first_element_type)
+                               || ASRUtils::is_character(*first_element_type);
+        int32_t biggest_ind = 0;
         if (semantic_error_flag) {
             if (ASRUtils::is_integer(*first_element_type)) {
                 int32_t biggest = 0;
                 for (int i = 0; i < args.size() && semantic_error_flag; i++) {
-                    ASR::expr_t* current_arg = args[i];
-                    ASR::ttype_t* current_arg_type = ASRUtils::expr_type(current_arg);
+                    ASR::expr_t *current_arg = args[i];
+                    ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
                     semantic_error_flag &= current_arg_type->type == first_element_type->type;
                     if (!semantic_error_flag) {
-                        msg = "type of arg in index ["+std::to_string(i)="] is not comparable";
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
                         break;
                     }
                     int32_t current_val = ASR::down_cast<ASR::ConstantInteger_t>(current_arg)->m_n;
@@ -682,7 +681,7 @@ struct PythonIntrinsicProcedures {
                         biggest = current_val;
                         biggest_ind = 0;
                     } else {
-                        if (current_val > biggest ) {
+                        if (current_val > biggest) {
                             biggest = current_val;
                             biggest_ind = i;
                         }
@@ -691,23 +690,22 @@ struct PythonIntrinsicProcedures {
                 if (semantic_error_flag) {
                     return args[biggest_ind];
                 }
-            }
-            else if (ASRUtils::is_real(*first_element_type)) {
+            } else if (ASRUtils::is_real(*first_element_type)) {
                 double_t biggest = 0;
                 for (int i = 0; i < args.size() && semantic_error_flag; i++) {
-                    ASR::expr_t* current_arg = args[i];
-                    ASR::ttype_t* current_arg_type = ASRUtils::expr_type(current_arg);
+                    ASR::expr_t *current_arg = args[i];
+                    ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
                     semantic_error_flag &= current_arg_type->type == first_element_type->type;
                     if (!semantic_error_flag) {
-                        msg = "type of arg in index ["+std::to_string(i)="] is not comparable";
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
                         break;
                     }
                     double_t current_val = ASR::down_cast<ASR::ConstantReal_t>(current_arg)->m_r;
                     if (i == 0) {
-                        biggest = current_val ;
-                        biggest_ind = 0 ;
+                        biggest = current_val;
+                        biggest_ind = 0;
                     } else {
-                        if (current_val - biggest > 1e-6 ) {
+                        if (current_val - biggest > 1e-6) {
                             biggest = current_val;
                             biggest_ind = i;
                         }
@@ -718,27 +716,28 @@ struct PythonIntrinsicProcedures {
                 }
             }
         }
-        throw SemanticError(msg , loc);
+        throw SemanticError(msg, loc);
     }
-    static ASR::expr_t* eval_min(Allocator &al , const Location &loc , Vec<ASR::expr_t*>&args) {
+
+    static ASR::expr_t *eval_min(Allocator &al, const Location &loc, Vec<ASR::expr_t *> &args) {
         LFORTRAN_ASSERT(ASRUtils::all_args_evaluated(args));
-        bool semantic_error_flag = args.size() !=0;
+        bool semantic_error_flag = args.size() != 0;
         std::string msg = "min() takes many arguments to comparing";
         ASR::expr_t *first_element = args[0];
         ASR::ttype_t *first_element_type = ASRUtils::expr_type(first_element);
         semantic_error_flag &= ASRUtils::is_integer(*first_element_type)
                                || ASRUtils::is_real(*first_element_type)
                                || ASRUtils::is_character(*first_element_type);
-        int32_t smallest_ind  = 0;
-        if(semantic_error_flag) {
+        int32_t smallest_ind = 0;
+        if (semantic_error_flag) {
             if (ASRUtils::is_integer(*first_element_type)) {
                 int32_t smallest = 0;
-                for (int i =0; i < args.size() && semantic_error_flag; i++) {
-                    ASR::expr_t* current_arg = args[i];
-                    ASR::ttype_t* current_arg_type = ASRUtils::expr_type(current_arg);
+                for (int i = 0; i < args.size() && semantic_error_flag; i++) {
+                    ASR::expr_t *current_arg = args[i];
+                    ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
                     semantic_error_flag &= current_arg_type->type == first_element_type->type;
                     if (!semantic_error_flag) {
-                        msg = "type of arg in index ["+std::to_string(i)="] is not comparable";
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
                         break;
                     }
                     int32_t current_val = ASR::down_cast<ASR::ConstantInteger_t>(current_arg)->m_n;
@@ -755,15 +754,14 @@ struct PythonIntrinsicProcedures {
                 if (semantic_error_flag) {
                     return args[smallest_ind];
                 }
-            }
-            else if (ASRUtils::is_real(*first_element_type)) {
+            } else if (ASRUtils::is_real(*first_element_type)) {
                 double_t smallest = 0;
-                for (int i =0; i < args.size() && semantic_error_flag; i++) {
+                for (int i = 0; i < args.size() && semantic_error_flag; i++) {
                     ASR::expr_t *current_arg = args[i];
                     ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
                     semantic_error_flag &= current_arg_type->type == first_element_type->type;
                     if (!semantic_error_flag) {
-                        msg = "type of arg in index ["+std::to_string(i)="] is not comparable";
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
                         break;
                     }
                     double_t current_val = ASR::down_cast<ASR::ConstantReal_t>(current_arg)->m_r;
@@ -771,7 +769,7 @@ struct PythonIntrinsicProcedures {
                         smallest = current_val;
                         smallest_ind = 0;
                     } else {
-                        if (smallest -current_val >1e-6 ) {
+                        if (smallest - current_val > 1e-6) {
                             smallest = current_val;
                             smallest_ind = i;
                         }
@@ -782,8 +780,10 @@ struct PythonIntrinsicProcedures {
                 }
             }
         }
-        throw SemanticError(msg , loc);
+        throw SemanticError(msg, loc);
+
     }
+
 
 
 }; // ComptimeEval
