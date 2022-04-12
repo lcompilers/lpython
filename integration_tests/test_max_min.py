90ed029087729dcad5d3fcbc1581aaf91ede31ea
Abdelrahman Khaled <abdokh950@gmail.com>
2022-04-12 05:53:42 +0200

    Add `max()` , `min()` (#335)
    Co-authored-by: Thirumalai Shaktivel <74826228+Thirumalai-Shaktivel@users.noreply.github.com>
    Co-authored-by: Naman Gera <namangera15@gmail.com>

---
+++ integration_tests/test_max_min.py
@@ -0,0 +1,41 @@
+from ltypes import i32 ,f32 ,f64
+
+def test_max_int():
+    a: i32 = 1
+    b: i32 = 2
+    c: i32 = 3
+    assert max(a, b) == b
+    assert max(a, b, c) == c
+    assert max(1, 2, 3) == 3
+    assert max(1, 6) == 6
+
+def test_max_float():
+    d: f64 = 23.233
+    e: f64 = 23.2233
+    f: f64 = 21.23
+    assert max(d, e, f) == d
+    assert max(e, f) == e
+
+def test_min_int():
+    a: i32 = 1
+    b: i32 = 2
+    c: i32 = 3
+    assert min(a, b) == a
+    assert min(a, b, c) == a
+    assert min(1, 2, 3) == 1
+    assert min(1, 6) == 1
+
+def test_min_float():
+    d: f64 = 23.233
+    e: f64 = 23.2233
+    f: f64 = 21.23
+    assert min(d, e, f) == f
+    assert min(e, f) == f
+
+def check():
+    test_max_int()
+    test_max_float()
+    test_min_int()
+    test_min_float()
+
+check()
--- src/lpython/semantics/python_comptime_eval.h
+++ src/lpython/semantics/python_comptime_eval.h
@@ -48,6 +48,8 @@ struct PythonIntrinsicProcedures {
             {"divmod", {m_builtin, &eval_divmod}},
             {"_lpython_floordiv", {m_builtin, &eval__lpython_floordiv}},
             {"_mod", {m_builtin, &eval__mod}},
+            {"max" , {m_builtin , &eval_max}},
+            {"min" , {m_builtin , &eval_min}},
             {"_bitwise_or", {m_builtin, &eval__bitwise_or}},
             {"_bitwise_and", {m_builtin, &eval__bitwise_and}},
             {"_bitwise_xor", {m_builtin, &eval__bitwise_xor}},
@@ -102,7 +104,6 @@ struct PythonIntrinsicProcedures {
                 loc);
         }
     }
-
 
     static ASR::expr_t *eval_abs(Allocator &al, const Location &loc,
             Vec<ASR::expr_t*> &args
@@ -705,6 +706,137 @@ struct PythonIntrinsicProcedures {
                 ASRUtils::type_to_str(arg1_type) + "' and '" + ASRUtils::type_to_str(arg2_type) + "'", loc);
         }
     }
+
+    static ASR::expr_t *eval_max(Allocator &al, const Location &loc, Vec<ASR::expr_t *> &args) {
+        LFORTRAN_ASSERT(ASRUtils::all_args_evaluated(args));
+        bool semantic_error_flag = args.size() != 0;
+        std::string msg = "max() takes many arguments to comparing";
+        ASR::expr_t *first_element = args[0];
+        ASR::ttype_t *first_element_type = ASRUtils::expr_type(first_element);
+        semantic_error_flag &= ASRUtils::is_integer(*first_element_type)
+                               || ASRUtils::is_real(*first_element_type)
+                               || ASRUtils::is_character(*first_element_type);
+        int32_t biggest_ind = 0;
+        if (semantic_error_flag) {
+            if (ASRUtils::is_integer(*first_element_type)) {
+                int32_t biggest = 0;
+                for (int i = 0; i < args.size() && semantic_error_flag; i++) {
+                    ASR::expr_t *current_arg = args[i];
+                    ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
+                    semantic_error_flag &= current_arg_type->type == first_element_type->type;
+                    if (!semantic_error_flag) {
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
+                        break;
+                    }
+                    int32_t current_val = ASR::down_cast<ASR::ConstantInteger_t>(current_arg)->m_n;
+                    if (i == 0) {
+                        biggest = current_val;
+                        biggest_ind = 0;
+                    } else {
+                        if (current_val > biggest) {
+                            biggest = current_val;
+                            biggest_ind = i;
+                        }
+                    }
+                }
+                if (semantic_error_flag) {
+                    return args[biggest_ind];
+                }
+            } else if (ASRUtils::is_real(*first_element_type)) {
+                double_t biggest = 0;
+                for (int i = 0; i < args.size() && semantic_error_flag; i++) {
+                    ASR::expr_t *current_arg = args[i];
+                    ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
+                    semantic_error_flag &= current_arg_type->type == first_element_type->type;
+                    if (!semantic_error_flag) {
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
+                        break;
+                    }
+                    double_t current_val = ASR::down_cast<ASR::ConstantReal_t>(current_arg)->m_r;
+                    if (i == 0) {
+                        biggest = current_val;
+                        biggest_ind = 0;
+                    } else {
+                        if (current_val - biggest > 1e-6) {
+                            biggest = current_val;
+                            biggest_ind = i;
+                        }
+                    }
+                }
+                if (semantic_error_flag) {
+                    return args[biggest_ind];
+                }
+            }
+        }
+        throw SemanticError(msg, loc);
+    }
+
+    static ASR::expr_t *eval_min(Allocator &al, const Location &loc, Vec<ASR::expr_t *> &args) {
+        LFORTRAN_ASSERT(ASRUtils::all_args_evaluated(args));
+        bool semantic_error_flag = args.size() != 0;
+        std::string msg = "min() takes many arguments to comparing";
+        ASR::expr_t *first_element = args[0];
+        ASR::ttype_t *first_element_type = ASRUtils::expr_type(first_element);
+        semantic_error_flag &= ASRUtils::is_integer(*first_element_type)
+                               || ASRUtils::is_real(*first_element_type)
+                               || ASRUtils::is_character(*first_element_type);
+        int32_t smallest_ind = 0;
+        if (semantic_error_flag) {
+            if (ASRUtils::is_integer(*first_element_type)) {
+                int32_t smallest = 0;
+                for (int i = 0; i < args.size() && semantic_error_flag; i++) {
+                    ASR::expr_t *current_arg = args[i];
+                    ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
+                    semantic_error_flag &= current_arg_type->type == first_element_type->type;
+                    if (!semantic_error_flag) {
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
+                        break;
+                    }
+                    int32_t current_val = ASR::down_cast<ASR::ConstantInteger_t>(current_arg)->m_n;
+                    if (i == 0) {
+                        smallest = current_val;
+                        smallest_ind = 0;
+                    } else {
+                        if (current_val < smallest) {
+                            smallest = current_val;
+                            smallest_ind = i;
+                        }
+                    }
+                }
+                if (semantic_error_flag) {
+                    return args[smallest_ind];
+                }
+            } else if (ASRUtils::is_real(*first_element_type)) {
+                double_t smallest = 0;
+                for (int i = 0; i < args.size() && semantic_error_flag; i++) {
+                    ASR::expr_t *current_arg = args[i];
+                    ASR::ttype_t *current_arg_type = ASRUtils::expr_type(current_arg);
+                    semantic_error_flag &= current_arg_type->type == first_element_type->type;
+                    if (!semantic_error_flag) {
+                        msg = "type of arg in index [" + std::to_string(i) = "] is not comparable";
+                        break;
+                    }
+                    double_t current_val = ASR::down_cast<ASR::ConstantReal_t>(current_arg)->m_r;
+                    if (i == 0) {
+                        smallest = current_val;
+                        smallest_ind = 0;
+                    } else {
+                        if (smallest - current_val > 1e-6) {
+                            smallest = current_val;
+                            smallest_ind = i;
+                        }
+                    }
+                }
+                if (semantic_error_flag) {
+                    return args[smallest_ind];
+                }
+            }
+        }
+        throw SemanticError(msg, loc);
+
+    }
+
+
 
 }; // ComptimeEval
 
