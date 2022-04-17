b9db997efc7adc747eaf5932e73d0d937d38a893
Abdelrahman Khaled <abdokh950@gmail.com>
2022-04-17 17:31:13 +0200

    Add graph

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,16 +1,27 @@
-#!/usr/bin/env python
-
 import argparse
-from mimetypes import init
-from pprint import pprint
+from audioop import ratecv
+from concurrent.futures import process
+from logging import captureWarnings
 import subprocess
 import os
+from traceback import print_tb
 import toml
 import re
 from timeit import default_timer as clock
 import ast
+import shlex
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-# when you add option to implemented_benchmarks you should also add class to get values from cpython and python.
+# when you add option to implemented_benchmarks you must also add class to get values from cpython and python.
 implemented_benchmarks = [
     "parser"
 ]
@@ -24,6 +35,8 @@ class Parser:
             parsing_value = parsing_value.replace("parsing:", '')
             parsing_value = parsing_value.replace('ms', '')
             parsing_value = float(parsing_value)
+            del lpython_run
+
         except:
             parsing_value = None
 
@@ -33,25 +46,28 @@ class Parser:
     def get_cpython_result(cls, file_path):
         input = open(file_path).read()
         t1 = clock()
-        print(t1)
         a = ast.parse(input, type_comments=True)
         t2 = clock()
         return float(t2 - t1) * 1000
 
 
 class Graph:
-    def __init__(self, plots) -> None:
+
+    def __init__(self, plots):
         report_file = open(os.path.join('benchmarks', 'report_file.dat'), 'w')
+
         for plot in plots:
             report_file.write(f"{plot[0]},{plot[1]},{plot[2]}\n")
+        
 
     def show(self):
-        termgraph_command = subprocess.run(['termgraph', os.path.join('benchmarks', 'report_file.dat')],
-                                           capture_output=True)
-        print(termgraph_command.stdout.decode('utf-8'))
+        termgraph_command = subprocess.run("termgraph %s --space-between" %(os.path.join('benchmarks', 'report_file.dat')), shell=True ,capture_output=True)
+        return termgraph_command.stdout.decode('utf-8')
+
 
 
 if __name__ == '__main__':
+    os.environ["PATH"] = os.path.join(os.getcwd(), "src", "bin") + os.pathsep + os.environ["PATH"]
     parser = argparse.ArgumentParser(description="Lpython benchmark")
     parser.add_argument("-n", "--numerical", action="store_true", help="show results as nnumerical values")
     parser.add_argument("-p", "--plots", action="store_true", help="show results as graph of plots")
@@ -63,7 +79,6 @@ if __name__ == '__main__':
     if compare_list == None:
         compare_list = implemented_benchmarks[:]
 
-    os.environ["PATH"] = os.path.join(os.getcwd(), "src", "bin") + os.pathsep + os.environ["PATH"]
     files = toml.load("./benchmarks/benchmarks.toml")
     comparsing_map: dict = {}
     for option in implemented_benchmarks:
@@ -78,14 +93,14 @@ if __name__ == '__main__':
         if compare_option == []:
             continue
         if compare_option[0] == 'parser':
+
             compare_result = []
             for filename in compare_option[1]:
-                filename = os.path.join("benchmarks", filename)
+                filename = os.path.join('benchmarks', filename)
                 cpython = Parser.get_cpython_result(filename)
                 lpython = Parser.get_lpython_result(filename)
                 if cpython != None and lpython != None:
                     compare_result.append((filename, lpython, cpython))
-            pprint(compare_result)
             graph = Graph(compare_result)
-            graph.show()
-
+            print(graph.show())
+    
