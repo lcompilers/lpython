e007b2be970affef2761e4ae7b88e57028a52d1c
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-04-21 19:27:59 +0530

    add and update tests

--- tests/set1.py
+++ tests/set1.py
@@ -3,6 +3,9 @@ def test_Set():
     a = {1, 2, 3}
     a = {2, 3, 4, 5, 5}
 
+    a.add(9)
+    a.remove(4)
+
     b: set[str]
     b = {"a", "b", "c"}
     # a = {-1, -2, "c"} -> semantic error for now
