657bfd8f9ffd1c204d4ba2bd675d31af08ff0727
Ondřej Čertík <ondrej@certik.us>
2022-04-27 11:57:44 -0600

    Update ASR from LFortran

--- src/libasr/asr_verify.cpp
+++ src/libasr/asr_verify.cpp
@@ -151,6 +151,29 @@ public:
         current_symtab = parent_symtab;
     }
 
+    void visit_AssociateBlock(const AssociateBlock_t& x) {
+        SymbolTable *parent_symtab = current_symtab;
+        current_symtab = x.m_symtab;
+        require(x.m_symtab != nullptr,
+            "The AssociateBlock::m_symtab cannot be nullptr");
+        require(x.m_symtab->parent == parent_symtab,
+            "The AssociateBlock::m_symtab->parent is not the right parent");
+        require(id_symtab_map.find(x.m_symtab->counter) == id_symtab_map.end(),
+            "AssociateBlock::m_symtab->counter must be unique");
+        require(x.m_symtab->asr_owner == (ASR::asr_t*)&x,
+            "The X::m_symtab::asr_owner must point to X");
+        require(ASRUtils::symbol_symtab(down_cast<symbol_t>(current_symtab->asr_owner)) == current_symtab,
+            "The asr_owner invariant failed");
+        id_symtab_map[x.m_symtab->counter] = x.m_symtab;
+        for (auto &a : x.m_symtab->scope) {
+            this->visit_symbol(*a.second);
+        }
+        for (size_t i=0; i<x.n_body; i++) {
+            visit_stmt(*x.m_body[i]);
+        }
+        current_symtab = parent_symtab;
+    }
+
     void visit_Module(const Module_t &x) {
         SymbolTable *parent_symtab = current_symtab;
         current_symtab = x.m_symtab;
