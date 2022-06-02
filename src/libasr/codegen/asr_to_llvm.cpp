3e201bf3847405439c4c759d8c7b91e57acf9b68
Naman Gera <namangera15@gmail.com>
2022-06-02 13:29:06 +0530

    LLVM: Implement `ComplexUnaryMinus` visitor

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -3163,6 +3163,39 @@ public:
         tmp = builder->CreateFSub(zero, tmp);
     }
 
+    void visit_ComplexUnaryMinus(const ASR::ComplexUnaryMinus_t &x) {
+        this->visit_expr_wrapper(x.m_arg, true);
+        llvm::Value *c = tmp;
+        double re = 0.0;
+        double im = 0.0;
+        llvm::Value *re2, *im2;
+        llvm::Type *type;
+        int a_kind = down_cast<ASR::Complex_t>(x.m_type)->m_kind;
+        std::string f_name;
+        switch (a_kind) {
+            case 4: {
+                re2 = llvm::ConstantFP::get(context, llvm::APFloat((float)re));
+                im2 = llvm::ConstantFP::get(context, llvm::APFloat((float)im));
+                type = complex_type_4;
+                f_name = "_lfortran_complex_sub_32";
+                break;
+            }
+            case 8: {
+                re2 = llvm::ConstantFP::get(context, llvm::APFloat(re));
+                im2 = llvm::ConstantFP::get(context, llvm::APFloat(im));
+                type = complex_type_8;
+                f_name = "_lfortran_complex_sub_64";
+                break;
+            }
+            default: {
+                throw CodeGenError("kind type is not supported");
+            }
+        }
+        tmp = complex_from_floats(re2, im2, type);
+        llvm::Value *zero_c = tmp;
+        tmp = lfortran_complex_bin_op(zero_c, c, f_name, type);
+    }
+
     void visit_IntegerConstant(const ASR::IntegerConstant_t &x) {
         int64_t val = x.m_n;
         int a_kind = ((ASR::Integer_t*)(&(x.m_type->base)))->m_kind;
