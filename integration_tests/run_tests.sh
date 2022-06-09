a771542b20202ca5fac51599b7dce8a9d0312dd9
Ondřej Čertík <ondrej@certik.us>
2022-06-09 14:47:17 -0500

    run_tests: Implement parallel building and running

--- integration_tests/run_tests.sh
+++ integration_tests/run_tests.sh
@@ -2,15 +2,18 @@
 
 set -ex
 
+# Append "-j4" or "-j" to run in parallel
+jn=$1
+
 export PATH="$(pwd)/../src/bin:$PATH"
 
 cmake .
-ctest --output-on-failure
+ctest $jn --output-on-failure
 
 cmake -DLPYTHON_BACKEND=llvm .
-make
-ctest --output-on-failure
+make $jn
+ctest $jn --output-on-failure
 
 cmake -DLPYTHON_BACKEND=c .
-make
-ctest --output-on-failure
+make $jn
+ctest $jn --output-on-failure
