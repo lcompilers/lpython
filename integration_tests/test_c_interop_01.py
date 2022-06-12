343c6413739ec4bbb6fe3b1d9181c117a031a61a
Ondřej Čertík <ondrej@certik.us>
2022-06-11 22:19:47 -0600

    Comment out _lfortran_random_number for now

--- integration_tests/test_c_interop_01.py
+++ integration_tests/test_c_interop_01.py
@@ -17,9 +17,9 @@ def _lfortran_bgt32(i: i32, j: i32) -> i32:
 def _lfortran_bgt64(i: i64, j: i64) -> i32:
     pass
 
-@ccall
-def _lfortran_random_number(n: i64, v: f64[:]):
-    pass
+#@ccall
+#def _lfortran_random_number(n: i64, v: f64[:]):
+#    pass
 
 def test_c_callbacks():
     pi: f64
