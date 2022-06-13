f998ef8a7cb3d157115dce05661b557f0473f465
Gagandeep Singh <gdp.1807@gmail.com>
2022-06-13 13:14:14 +0530

    1. Use pointer call to pass the pointer to p_c_pointer 2. print pointers to see if the addresses really match

--- integration_tests/test_c_interop_03.py
+++ integration_tests/test_c_interop_03.py
@@ -8,7 +8,8 @@ def test_c_callbacks():
     xi32: i32
     xi32 = 3
     p: CPtr
-    p_c_pointer(xi32, p)
+    p_c_pointer(pointer(xi32), p)
+    print(pointer(xi32), p)
     assert f_pi32_i32(p) == 4
 
 test_c_callbacks()
