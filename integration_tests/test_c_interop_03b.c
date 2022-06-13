1a2f39f2f302b4e7f306b3e9e45827c34c3db583
Ondřej Čertík <ondrej@certik.us>
2022-06-13 08:11:05 +0300

    Add tests for other pointer argument types

--- integration_tests/test_c_interop_03b.c
+++ integration_tests/test_c_interop_03b.c
@@ -3,3 +3,15 @@
 int32_t f_pi32_i32(int32_t *x) {
     return *x+1;
 }
+
+int64_t f_pi64_i32(int64_t *x) {
+    return *x+1;
+}
+
+float   f_pf32_i32(float *x) {
+    return *x+1;
+}
+
+double  f_pf64_i32(double *x) {
+    return *x+1;
+}
--- integration_tests/test_c_interop_03b.h
+++ integration_tests/test_c_interop_03b.h
@@ -5,6 +5,9 @@
 
 
 int32_t f_pi32_i32(int32_t *x);
+int64_t f_pi64_i32(int64_t *x);
+float   f_pf32_i32(float *x);
+double  f_pf64_i32(double *x);
 
 
 #endif // TEST_C_INTEROP_03B
