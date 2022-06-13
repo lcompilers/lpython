f11cf2a251e3aec47477eba6e4d44e3887ed595d
Ondřej Čertík <ondrej@certik.us>
2022-06-13 03:17:33 +0300

    Use CPtr for the C interop
    Disable the C backend test for now as it has a few bugs to fix, only do LLVM.

--- integration_tests/test_c_interop_03.py
+++ integration_tests/test_c_interop_03.py
@@ -1,14 +1,16 @@
-from ltypes import ccall, f32, f64, i32, i64, CPtr, pointer, c_pp
+from ltypes import ccall, f32, f64, i32, i64, CPtr, pointer, Pointer, p_c_pointer
 
 @ccall
-def f_pi32_i32(x: Pointer[i32]) -> i32:
+def f_pi32_i32(x: CPtr) -> i32:
     pass
 
 def test_c_callbacks():
     xi32: i32
     xi32 = 3
     pxi32: Pointer[i32]
-#    pxi32 = pointer(xi32)
-#    assert f_pi32_i32(pxi32) == 4
+    pxi32 = pointer(xi32)
+    p: CPtr
+#    p_c_pointer(p, pxi32)
+#    assert f_pi32_i32(p) == 4
 
 test_c_callbacks()
