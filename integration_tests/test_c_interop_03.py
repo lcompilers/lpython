140f5a5c6b0d15189cb6d29ad4988d4b90ff3ac7
Ondřej Čertík <ondrej@certik.us>
2022-06-13 03:19:59 +0300

    Further simplify

--- integration_tests/test_c_interop_03.py
+++ integration_tests/test_c_interop_03.py
@@ -7,10 +7,8 @@ def f_pi32_i32(x: CPtr) -> i32:
 def test_c_callbacks():
     xi32: i32
     xi32 = 3
-    pxi32: Pointer[i32]
-    pxi32 = pointer(xi32)
     p: CPtr
-#    p_c_pointer(p, pxi32)
+#    p_c_pointer(p, xi32)
 #    assert f_pi32_i32(p) == 4
 
 test_c_callbacks()
