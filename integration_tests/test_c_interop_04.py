a6a3101f2f8af0860272c15c2b0be45564a642f2
Ondřej Čertík <ondrej@certik.us>
2022-06-14 15:18:35 +0300

    Implement cpython support in test_c_interop_04

--- integration_tests/test_c_interop_04.py
+++ integration_tests/test_c_interop_04.py
@@ -1,4 +1,6 @@
-from ltypes import ccall, f32, f64, i32, i64, CPtr, pointer, Pointer, p_c_pointer
+from ltypes import (ccall, f32, f64, i32, i64, CPtr, pointer, Pointer,
+        p_c_pointer, empty_c_void_p)
+from numpy import empty, int32
 
 @ccall
 def sum_pi32_i32(x: CPtr) -> i32:
@@ -6,6 +8,7 @@ def sum_pi32_i32(x: CPtr) -> i32:
 
 def test_c_callbacks():
     xi32: i32[4]
+    xi32 = empty(4, dtype=int32)
     sumi32: i32
     xi32[0] = 3
     xi32[1] = 4
@@ -13,6 +16,7 @@ def test_c_callbacks():
     xi32[3] = 6
     print(xi32[0], xi32[1], xi32[2], xi32[3])
     p: CPtr
+    p = empty_c_void_p()
     p_c_pointer(pointer(xi32), p)
     print(pointer(xi32), p)
     sumi32 =  sum_pi32_i32(p)
