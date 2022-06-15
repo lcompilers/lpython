9025f9c162fed9d348113b202bbb2aefdd898818
Ondřej Čertík <ondrej@certik.us>
2022-06-15 20:16:35 +0300

    Add assert tests

--- integration_tests/structs_01.py
+++ integration_tests/structs_01.py
@@ -13,8 +13,15 @@ def g():
     x: A
     x = A(3, 3.3)
     f(x)
+    # TODO: the above constructor does not initialize `A` in LPython yet, so
+    # the below does not work:
+    #assert x.x == 3
+    #assert x.y == 3.3
+
     x.x = 5
     x.y = 5.5
     f(x)
+    assert x.x == 5
+    assert x.y == 5.5
 
 g()
