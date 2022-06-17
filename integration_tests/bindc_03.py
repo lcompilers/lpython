a9a026f3a468b3569de3b0134086065ba9f4f50f
Gagandeep Singh <gdp.1807@gmail.com>
2022-06-17 19:54:03 +0530

    Added a test for getting pointers to array elements

---
+++ integration_tests/bindc_03.py
@@ -0,0 +1,33 @@
+from ltypes import c_p_pointer, CPtr, pointer, i16, i32, Pointer, ccall, p_c_pointer
+
+@ccall
+def g(a: CPtr, value: i32) -> None:
+    pass
+
+@ccall
+def get_array(size: i32) -> CPtr:
+    pass
+
+@ccallable
+def f(q_void: CPtr) -> None:
+    i: i32
+    el: i32
+    q: Pointer[i32[:]]
+    c_p_pointer(q_void, q)
+    for i in range(10):
+        q2: CPtr
+        p_c_pointer(pointer(q[i]), q2)
+        g(q2, i * i)
+        # TODO: Use q[i] directly in the assert.
+        el = q[i]
+        print(el)
+        assert el == i * i
+
+def run():
+    a: CPtr
+    size: i32
+    size = 10
+    a = get_array(size)
+    f(a)
+
+run()
---
+++ integration_tests/bindc_03b.h
@@ -0,0 +1,10 @@
+#ifndef BINDC_03B
+#define BINDC_03B
+
+#include <stdint.h>
+
+void g(int32_t* x, int32_t value);
+
+void* get_array(int32_t size);
+
+#endif // BINDC_03B
