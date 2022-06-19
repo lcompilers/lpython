d89c01b3be45e732edbb1888bd9a7980e0129311
Ondřej Čertík <ondrej@certik.us>
2022-06-19 11:26:41 +0300

    Add tests

--- integration_tests/test_c_interop_03.py
+++ integration_tests/test_c_interop_03.py
@@ -29,23 +29,27 @@ def test_c_callbacks():
     p_c_pointer(pointer(xi32, i32), p)
     print(pointer(xi32, i32), p)
     assert f_pi32_i32(p) == 4
+    assert f_pi32_i32(f_pvoid_pvoid(p)) == 4
 
     xi64: i64
     xi64 = 3
     p_c_pointer(pointer(xi64, i64), p)
     print(pointer(xi64, i64), p)
     assert f_pi64_i32(p) == 4
+    assert f_pi64_i32(f_pvoid_pvoid(p)) == 4
 
     xf32: f32
     xf32 = 3.3
     p_c_pointer(pointer(xf32, f32), p)
     print(pointer(xf32, f32), p)
     assert abs(f_pf32_i32(p)-4.3) < 1e-6
+    assert abs(f_pf32_i32(f_pvoid_pvoid(p))-4.3) < 1e-6
 
     xf64: f64
     xf64 = 3.3
     p_c_pointer(pointer(xf64, f64), p)
     print(pointer(xf64, f64), p)
     assert abs(f_pf64_i32(p)-4.3) < 1e-12
+    assert abs(f_pf64_i32(f_pvoid_pvoid(p))-4.3) < 1e-12
 
 test_c_callbacks()
