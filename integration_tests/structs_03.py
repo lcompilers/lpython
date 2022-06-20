d42ec60e7212ceaec8889a9367352e5bdd6cfce1
Gagandeep Singh <gdp.1807@gmail.com>
2022-06-20 16:29:41 +0530

    Initialise pointers at declaration

--- integration_tests/structs_03.py
+++ integration_tests/structs_03.py
@@ -10,11 +10,12 @@ def f(pa: Pointer[A]):
     print(pa.y)
 
 def g():
-    x: A = A(3, 3.5)
-    px: Pointer[A]
-    px = pointer(x)
-    px.x = 5
-    px.y = 5.5
-    f(px)
+    x: A = A(3, 3.25)
+    xp: Pointer[A] = pointer(x)
+    assert xp.x == 3
+    assert xp.y == 3.25
+    xp.x = 5
+    xp.y = 5.5
+    f(xp)
 
 g()
