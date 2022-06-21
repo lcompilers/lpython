96753e05e625de09e1c40d032337fd4540b07c71
Ondřej Čertík <ondrej@certik.us>
2022-06-21 14:53:55 +0300

    Add a test for strings with \n

--- tests/parser/string1.py
+++ tests/parser/string1.py
@@ -2,6 +2,8 @@ f"Hello, {first_name} {last_name}. You are {age} years old."
 
 print(f"{__file__} executed in {elapsed} seconds.")
 
+"\nsometext\nanotherline\n"
+
 # TODO: Support format specifiers
 
 # print(f"{__file__} executed in {elapsed:0.2f} seconds.")
