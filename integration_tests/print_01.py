22527d5659e62693d363004be95a1842743314c5
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-06-25 20:13:17 +0530

    Add a test

--- integration_tests/print_01.py
+++ integration_tests/print_01.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+def f():
+    print("Hello World!")
+    x: str
+    x = ","
+    print("a", "b", sep=x)
+    print("a", "b", sep=":")
+    print("LCompilers", "LPython")
+f()
