e72de071a918309432461b474cd0cf74e5ffafa9
Gagandeep Singh <gdp.1807@gmail.com>
2022-06-30 20:28:07 +0530

    Remove else after return for proper testing of inlining

--- integration_tests/func_inline_01.py
+++ integration_tests/func_inline_01.py
@@ -3,14 +3,14 @@ from ltypes import i64
 def fib(n: i64) -> i64:
     if n < 2:
         return n
-    else:
-        return fib(n - 1) + fib(n - 2)
+    return fib(n - 1) + fib(n - 2)
 
 def main():
     ans: i64
     x: i64
-    x = 8
+    x = 40
     ans = fib(x)
     print(ans)
+    assert ans == 102334155
 
 main()
