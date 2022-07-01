cc477abcb8d2232ebcad5730b48e828b54bc2a90
Madhav2310 <madhav.mittal.mat19@itbhu.ac.in>
2022-07-02 03:05:02 +0530

    error & reference files added

---
+++ tests/errors/test_factorial.py
@@ -0,0 +1,6 @@
+from math import factorial
+from ltypes import f64
+def f():
+    x: f64
+    x = 6.3
+    print(factorial(x))
--- tests/errors/test_pow2.py
+++ tests/errors/test_pow2.py
@@ -2,4 +2,3 @@ def main():
     print(pow(2, 2))
     print(2 ** 2)
 
-main()
