cadfa883b6d72c6f0227599eaa975810e667e2f1
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-01 19:48:54 +0530

    Initialise dataclasses before using them

--- integration_tests/structs_05.py
+++ integration_tests/structs_05.py
@@ -6,17 +6,27 @@ class A:
     x: i32
 
 def g():
+    # TODO: Replace y: A[2] with y: A[2] = [None, None]
+    # TODO: And enable cpython in integration_tests.
     y: A[2]
-    eps: f64 = 1e-12
-    y[0].x = 1
-    y[0].y = 1.1
-    y[1].x = 2
-    y[1].y = 2.2
+    y[0] = A(1.1, 1)
+    y[1] = A(2.2, 2)
     print(y[0].x, y[0].y)
     assert y[0].x == 1
-    assert abs(y[0].y - 1.1) < eps
+    assert y[0].y == 1.1
     print(y[1].x, y[1].y)
     assert y[1].x == 2
-    assert abs(y[1].y - 2.2) < eps
+    assert y[1].y == 2.2
+    eps: f64 = 1e-12
+    y[0].x = 2
+    y[0].y = 1.2
+    y[1].x = 3
+    y[1].y = 2.3
+    print(y[0].x, y[0].y)
+    assert y[0].x == 2
+    assert abs(y[0].y - 1.2) < eps
+    print(y[1].x, y[1].y)
+    assert y[1].x == 3
+    assert abs(y[1].y - 2.3) < eps
 
 g()
