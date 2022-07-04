3b406ceb4a100920af84e98705cfec0d904a1bbc
Madhav2310 <madhav.mittal.mat19@itbhu.ac.in>
2022-07-04 09:07:55 +0530

    fixes

--- src/runtime/decimal.py
+++ src/runtime/decimal.py
@@ -1,19 +1,45 @@
 from ltypes import i8, i16, i32, f32, f64, ccall
 
-@overload
-def Decimal(x: i64) -> i64:
-    return x
 
-@overload
-def Decimal(x: i32) -> i32:
-    return x
+@dataclass
+class Decimal:
+    value: str
 
-@overload
-def Decimal(x: f32) -> f64:
-    return float(x)
-@overload
-def Decimal(x: f64) -> f64:
-    return float(x)
+    @overload
+    def __init__(self, value_: i64) -> i64:
+        return self.value
+
+    @overload
+    def __init__(self, value_: i32) -> i64:
+        return self.value
+
+    @overload
+    def __init__(self, value_: f32) -> f64:
+        result: f64
+        result = self.value
+        return result
+
+    @overload
+    def __init__(self, value_: f64) -> f64:
+        return float(self.value)
+
+
+# @overload
+# def Decimal(x: i64) -> i64:
+#     return x
+
+# @overload
+# def Decimal(x: i32) -> i32:
+#     return x
+
+# @overload
+# def Decimal(x: f32) -> f64:
+#     result:f64
+#     result = x
+#     return result
+# @overload
+# def Decimal(x: f64) -> f64:
+#     return float(x)
 
 # The following to be implemented once the above code works.
 
