3508846243c6a706397a7c73df5ad3715f2f37f2
Madhav2310 <madhav.mittal.mat19@itbhu.ac.in>
2022-07-08 01:43:35 +0530

    test expanded

--- integration_tests/test_decimal.py
+++ integration_tests/test_decimal.py
@@ -1,17 +1,23 @@
 from decimal import (Decimal)
-from ltypes import i32, f32, f64, i64
+from ltypes import i64, f32, f64, i64
 
 eps: f64
 eps = 1e-12
 
 def test_Decimal_1():
-    i: i32
-    i = Decimal(4)
+    i: i64
+    a: i64
+    a = 4
+    i = Decimal(a)
     assert i == 4
+    b: i64
+    b = 2
     j: i64
-    j = Decimal(2)
+    j = Decimal(b)
     k: f64
-    k = Decimal(0.5)
+    c:f64
+    c = 0.5
+    k = Decimal(c)
     assert abs(float(j ** k) - 1.414213562373095048801688724) < eps
 
 def check():
