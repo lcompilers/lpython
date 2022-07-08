030b917c9a61f5133bd37cbb77db8b7430ed5432
Madhav2310 <madhav.mittal.mat19@itbhu.ac.in>
2022-07-08 23:44:27 +0530

    Type changed

--- integration_tests/test_decimal.py
+++ integration_tests/test_decimal.py
@@ -5,20 +5,21 @@ eps: f64
 eps = 1e-12
 
 def test_Decimal_1():
-    i: i64
+    i: Decimal
     a: i64
     a = 4
     i = Decimal(a)
     assert i == 4
     b: i64
     b = 2
-    j: i64
+    j: Decimal
     j = Decimal(b)
-    k: f64
-    c:f64
+    k: Decimal
+    c: f64
     c = 0.5
     k = Decimal(c)
     assert abs(float(j ** k) - 1.414213562373095048801688724) < eps
+    assert i**j == Decimal(16)
 
 def check():
     test_Decimal_1()
--- src/runtime/decimal.py
+++ src/runtime/decimal.py
@@ -22,31 +22,3 @@ class Decimal:
     @overload
     def __init__(self, value_: f64) -> f64:
         return float(self.value)
-
-
-# @overload
-# def Decimal(x: i64) -> i64:
-#     return x
-
-# @overload
-# def Decimal(x: i32) -> i32:
-#     return x
-
-# @overload
-# def Decimal(x: f32) -> f64:
-#     result:f64
-#     result = x
-#     return result
-# @overload
-# def Decimal(x: f64) -> f64:
-#     return float(x)
-
-# The following to be implemented once the above code works.
-
-# @overload
-# def Decimal(x: str) -> f64:
-#     return float(x)
-
-# @overload
-# def Decimal(x: str) -> i64:
-#     return int(x)
