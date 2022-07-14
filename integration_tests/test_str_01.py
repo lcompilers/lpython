48ed9220abd3cc9df15f7369db7d5ddd0ee541be
redbopo <redbopo.lan@gmail.com>
2022-07-15 05:00:41 +0800

    Add test_str_01.py

--- integration_tests/test_str_01.py
+++ integration_tests/test_str_01.py
@@ -49,6 +49,23 @@ def test_constant_str_subscript():
     assert "abc"[2] == "c"
     assert "abc"[:2] == "ab"
 
+def test_str_slice_step():
+    s: str
+    start: i32
+    end: i32
+    step: i32
+    s = "abcdefghijk"
+    start = 1
+    end = 4
+    step = 1
+    assert s[3:12:3] == "dgj"
+    assert s[10:3:-2] == "kige"
+    assert s[::] == "abcdefghijk"
+    assert s[1:4:] == "bcd"
+    assert s[:4:5] == "a"
+    assert s[start:end:step] == "bcd"
+    assert s[start:2*end-3:step] == "bcde"
+
 def check():
     f()
     test_str_concat()
@@ -56,5 +73,6 @@ def check():
     test_str_slice()
     test_str_repeat()
     test_constant_str_subscript()
+    test_str_slice_step()
 
 check()
