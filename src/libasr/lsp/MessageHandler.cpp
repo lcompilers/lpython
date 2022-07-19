de3cb7d913f8b235c2529e172fa56ddd9b6b1c53
Ondřej Čertík <ondrej@certik.us>
2022-07-19 13:24:20 -0600

    Check if there is a compilation error

--- src/libasr/lsp/MessageHandler.cpp
+++ src/libasr/lsp/MessageHandler.cpp
@@ -5,6 +5,7 @@ namespace LFortran::LPython {
     std::vector<lsp_locations> get_SymbolLists(const std::string &infile,
         const std::string &runtime_library_dir,
         LFortran::CompilerOptions &compiler_options) {
+            std::vector<lsp_locations> symbol_lists;
             Allocator al(4*1024);
             LFortran::diag::Diagnostics diagnostics;
             LFortran::LocationManager lm;
@@ -13,10 +14,11 @@ namespace LFortran::LPython {
             lm.init_simple(input);
             LFortran::Result<LFortran::LPython::AST::ast_t*> r1 = LFortran::parse_python_file(
                 al, runtime_library_dir, infile, diagnostics, compiler_options.new_parser);
+            if (!r1.ok) return symbol_lists;
             LFortran::LPython::AST::ast_t* ast = r1.result;
             LFortran::Result<LFortran::ASR::TranslationUnit_t*> x = LFortran::LPython::python_ast_to_asr(al, *ast, diagnostics, true,
             compiler_options.disable_main, compiler_options.symtab_only, infile);
-            std::vector<lsp_locations> symbol_lists;
+            if (!x.ok) return symbol_lists;
             lsp_locations loc;
             for (auto &a : x.result->m_global_scope->get_scope()) {
                 std::string symbol_name = a.first;
