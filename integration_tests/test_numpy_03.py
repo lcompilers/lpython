6b7d6faf531a0c18d09b45440b2553f85fdc5144
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-21 19:42:20 +0530

    Tested reshape for multiple scenarios

--- integration_tests/test_numpy_03.py
+++ integration_tests/test_numpy_03.py
@@ -1,20 +1,15 @@
 from ltypes import f64, i32
 from numpy import reshape, empty
 
-def test_nd_to_1d():
-    a: f64[16, 16]
-    a = empty((16, 16))
+def test_nd_to_1d(a: f64[:, :]):
     i: i32
     j: i32
     k: i32
+    l: i32
     eps: f64
     eps = 1e-12
 
-    for i in range(16):
-        for j in range(16):
-            a[i, j] = i + j + 0.5
-
-    b: f64[256]
+    b: f64[256] = empty(256)
     newshape: i32[1] = empty(1, dtype=int)
     newshape[0] = 256
     b = reshape(a, newshape)
@@ -23,4 +18,80 @@ def test_nd_to_1d():
         j = k - i*16
         assert abs(b[k] - i - j - 0.5) <= eps
 
-test_nd_to_1d()
+    c: f64[16, 16, 16] = empty((16, 16, 16))
+    c = empty((16, 16, 16))
+    for i in range(16):
+        for j in range(16):
+            for k in range(16):
+                c[i, j, k] = i + j + k + 0.5
+
+    d: f64[4096] = empty(4096)
+    newshape1: i32[1] = empty(1, dtype=int)
+    newshape1[0] = 4096
+    d = reshape(c, newshape1)
+    for l in range(4096):
+        i = int(l/256)
+        j = (l - i*256)//16
+        k = (l - i*256 - j*16)
+        assert abs(d[l] - i - j - k - 0.5) <= eps
+
+def test_1d_to_nd(d: f64[:]):
+    i: i32
+    j: i32
+    k: i32
+    l: i32
+    eps: f64
+    eps = 1e-12
+
+    b: f64[256] = empty(256)
+    for k in range(256):
+        i = k//16
+        j = k - i*16
+        b[k] = i + j + 0.5
+
+    a: f64[16, 16]
+    a = empty((16, 16))
+    newshape: i32[2] = empty(2, dtype=int)
+    newshape[0] = 16
+    newshape[1] = 16
+    a = reshape(b, newshape)
+    for i in range(16):
+        for j in range(16):
+            assert abs(a[i, j] - i - j - 0.5) <= eps
+
+    c: f64[16, 16, 16]
+    c = empty((16, 16, 16))
+    newshape1: i32[3] = empty(3, dtype=int)
+    newshape1[0] = 16
+    newshape1[1] = 16
+    newshape1[2] = 16
+    c = reshape(d, newshape1)
+    for i in range(16):
+        for j in range(16):
+            for k in range(16):
+                assert abs(c[i, j, k] - i - j - k - 0.5) <= eps
+
+def test_reshape_with_argument():
+    i: i32
+    j: i32
+    k: i32
+    l: i32
+
+    a: f64[16, 16]
+    a = empty((16, 16))
+    for i in range(16):
+        for j in range(16):
+            a[i, j] = i + j + 0.5
+
+    test_nd_to_1d(a)
+
+    d: f64[4096] = empty(4096)
+    for l in range(4096):
+        i = int(l/256)
+        j = (l - i*256)//16
+        k = (l - i*256 - j*16)
+        d[l] = i + j + k + 0.5
+
+    test_1d_to_nd(d)
+
+test_reshape_with_argument()
