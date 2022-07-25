4a09b3f0489e2a0ea5929855a9d515d84dabc61a
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-25 20:17:05 +0530

    Added vectorize cos

--- src/runtime/lpython_intrinsic_numpy.py
+++ src/runtime/lpython_intrinsic_numpy.py
@@ -7,3 +7,11 @@ def _lfortran_dsin(x: f64) -> f64:
 @vectorize
 def sin(x: f64) -> f64:
     return _lfortran_dsin(x)
+
+@ccall
+def _lfortran_dcos(x: f64) -> f64:
+    pass
+
+@vectorize
+def cos(x: f64) -> f64:
+    return _lfortran_dcos(x)
