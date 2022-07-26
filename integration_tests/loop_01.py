c8f071cac59e04d47c9f2c5950612fd949d4897c
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-07-26 19:23:40 +0530

    Add a test

--- integration_tests/loop_01.py
+++ integration_tests/loop_01.py
@@ -5,4 +5,13 @@ def main0():
     for c in s:
         print(c)
 
+def test_issue_770():
+    i: i64
+    res: i64
+    res = 0
+    for i in range(10):
+        res += i
+    assert res == 45
+
 main0()
+test_issue_770()
