52c44ccdee264d17c04abbc31486c8fa3ed01361
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-27 12:01:43 +0530

    Updated elemental_01.py to use f32 types

--- integration_tests/elemental_01.py
+++ integration_tests/elemental_01.py
@@ -1,10 +1,10 @@
-from ltypes import i32, f64
+from ltypes import i32, f64, f32
 from numpy import empty, sin, cos, reshape
 
-def verify1d(array: f64[:], result: f64[:], size: i32):
+def verify1d(array: f32[:], result: f32[:], size: i32):
     i: i32
-    eps: f64
-    eps = 1e-12
+    eps: f32
+    eps = 1e-6
 
     for i in range(size):
         assert abs(sin(sin(array[i])) - result[i]) <= eps
@@ -36,8 +36,8 @@ def elemental_sin():
     j: i32
     k: i32
 
-    array1d: f64[256] = empty(256)
-    sin1d: f64[256] = empty(256)
+    array1d: f32[256] = empty(256)
+    sin1d: f32[256] = empty(256)
 
     for i in range(256):
         array1d[i] = float(i)
@@ -78,12 +78,12 @@ def elemental_trig_identity():
     j: i32
     k: i32
     l: i32
-    eps: f64
-    eps = 1e-12
+    eps: f32
+    eps = 1e-6
 
-    arraynd: f64[64, 32, 8, 4] = empty((64, 32, 8, 4))
-    observed: f64[64, 32, 8, 4] = empty((64, 32, 8, 4))
-    observed1d: f64[65536] = empty(65536)
+    arraynd: f32[64, 32, 8, 4] = empty((64, 32, 8, 4))
+    observed: f32[64, 32, 8, 4] = empty((64, 32, 8, 4))
+    observed1d: f32[65536] = empty(65536)
 
     for i in range(64):
         for j in range(32):
