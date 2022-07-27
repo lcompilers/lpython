53a41e3cbd9d71008703333e982b4bd0cad93e2c
Naman Gera <namangera15@gmail.com>
2022-07-27 23:54:37 +0530

    Add more robust tests

--- integration_tests/test_tuples.py
+++ integration_tests/test_tuples.py
@@ -2,17 +2,25 @@ from ltypes import i32, f64
 
 def f():
     t1: tuple[i32, f64, str, bool]
+    t2: tuple[i32, f64, str, bool]
     t1 = (1, 2.0, "3", True)
-    assert t1[0] == 1
-    assert t1[2] == "3"
-    assert t1[3]
+    t2 = (2, 3.0, "3", False)
+    assert t2[0] - t1[0] == 1
+    assert abs(t2[1] - t1[1] - 1.0) <= 1e-12
+    assert t1[2] == t2[2]
+    assert t1[3] or t2[3]
 
-    t2: tuple[i32, i32]
-    t2 = (1, 2)
+    t3: tuple[i32, i32]
+    t4: tuple[f64, f64]
+    t3 = (1, 2)
+    t4 = (1.0, 2.0)
     a: i32
     b: i32
-    (a, b) = t2
-    assert a == 1
-    assert b == 2
+    af: f64
+    bf: f64
+    (a, b) = t3
+    (af, bf) = t4
+    assert af == a
+    assert bf == b
 
 f()
