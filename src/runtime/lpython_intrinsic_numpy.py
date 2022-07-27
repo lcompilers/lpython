abcdc153089bfd29593a046e99c0175924b2d2b9
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-27 12:01:10 +0530

    Overloaded sin, cos for f32 types

--- src/runtime/lpython_intrinsic_numpy.py
+++ src/runtime/lpython_intrinsic_numpy.py
@@ -1,17 +1,37 @@
-from ltypes import f64, ccall, vectorize
+from ltypes import f64, f32, ccall, vectorize, overload
 
 @ccall
 def _lfortran_dsin(x: f64) -> f64:
     pass
 
+@overload
 @vectorize
 def sin(x: f64) -> f64:
     return _lfortran_dsin(x)
 
 @ccall
+def _lfortran_ssin(x: f32) -> f32:
+    pass
+
+@overload
+@vectorize
+def sin(x: f32) -> f32:
+    return _lfortran_ssin(x)
+
+@ccall
 def _lfortran_dcos(x: f64) -> f64:
     pass
 
+@overload
 @vectorize
 def cos(x: f64) -> f64:
     return _lfortran_dcos(x)
+
+@ccall
+def _lfortran_scos(x: f32) -> f32:
+    pass
+
+@overload
+@vectorize
+def cos(x: f32) -> f32:
+    return _lfortran_scos(x)
