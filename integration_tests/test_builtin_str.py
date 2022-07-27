60e916b719f73a3ab524d63a6ce5c5958f3f7344
Abdelrahman Khaled <abdokh950@gmail.com>
2022-07-27 20:46:06 +0200

    Add tests, update references

--- integration_tests/test_builtin_str.py
+++ integration_tests/test_builtin_str.py
@@ -10,6 +10,8 @@ def test_str_int():
     assert str("1234") == "1234"
     assert str(False) == "False"
     assert str(True) == "True"
+    assert str("just a str") == "just a str"
+    assert str(12.1234) == "12.1234"
 
 def str_conv_for_variables():
     x: i32
@@ -21,6 +23,21 @@ def str_conv_for_variables():
     assert "-12" == str(x)
     x = -121212
     assert "-121212" == str(x)
+    xx: f32
+    xx = 12.322234
+    assert str(xx) == "12.322234"
+    yy : f64
+    yy = 12.322234
+    assert str(yy) == "12.322234"
+    bool_t :bool
+    bool_t = True 
+    assert str(bool_t) == "True"
+    bool_t = False
+    assert str(bool_t) == "False"
+    str_t :str 
+    str_t = "just a str"
+    assert str(str_t) == str_t
+
 
 test_str_int()
 str_conv_for_variables()
