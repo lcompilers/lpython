7f31cd758683508484f4c75cf7a1ceedacd20071
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-28 18:47:36 +0530

    Updated test_list_01 for ListConstant

--- integration_tests/test_list_01.py
+++ integration_tests/test_list_01.py
@@ -1,13 +1,13 @@
 from ltypes import i32
 
 def test_list_i32():
-    a: list[i32]
+    a: list[i32] = [0, 1, 2, 3, 4]
     i: i32
 
     for i in range(10):
-        a.append(i)
+        a.append(i + 5)
 
-    for i in range(i):
+    for i in range(15):
         assert a[i] == i
 
 test_list_i32()
