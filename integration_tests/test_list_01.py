f7fe3ff9fea7ff154d81f55709c89c3777808fb6
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-29 22:40:08 +0530

    Test for f64 typed lists

--- integration_tests/test_list_01.py
+++ integration_tests/test_list_01.py
@@ -1,13 +1,16 @@
-from ltypes import i32
+from ltypes import f64, i32
 
-def test_list_i32():
+def test_list():
     a: list[i32] = [0, 1, 2, 3, 4]
+    f: list[f64] = [1.0, 2.0, 3.0, 4.0, 5.0]
     i: i32
 
     for i in range(10):
         a.append(i + 5)
+        f.append(float(i + 6))
+
 
     for i in range(15):
-        assert a[i] == i
+        assert (f[i] - a[i]) == 1.0
 
-test_list_i32()
+test_list()
