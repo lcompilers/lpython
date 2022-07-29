0d1e5b9a3fdf38fa9fd03ffa1687a7f040ed28c8
Gagandeep Singh <gdp.1807@gmail.com>
2022-07-29 22:08:27 +0530

    Made test_list_01 robust and natural

--- integration_tests/test_list_01.py
+++ integration_tests/test_list_01.py
@@ -1,12 +1,13 @@
 from ltypes import i32
 
 def test_list_i32():
-    a: list[i32] = [1]
-    a.append(2)
-    a.append(3)
-    a.append(4)
-    a.append(5)
-    print(a[1])
-    assert a[1] == 2 or a[1] == 3
+    a: list[i32] = [0, 1, 2, 3, 4]
+    i: i32
+
+    for i in range(10):
+        a.append(i + 5)
+
+    for i in range(15):
+        assert a[i] == i
 
 test_list_i32()
