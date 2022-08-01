8b2b5d35c56f8ed6f5016563c5b50c61c7085c81
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-08-02 00:05:58 +0530

    Add tests and Update refs.

---
+++ integration_tests/test_numpy_04.py
@@ -0,0 +1,22 @@
+from ltypes import i32, f64
+from numpy import array
+
+def test_array_01():
+    x: f64[3] = array([1.0, 2.0, 3.0])
+    eps: f64 = 1e-16
+    assert abs(x[1] - 1.0) < eps
+    assert abs(x[2] - 2.0) < eps
+    assert abs(x[3] - 3.0) < eps
+
+def test_array_02():
+    x: i32[3] = array([1, 2, 3])
+    eps: f64 = 1e-16
+    assert abs(x[1] - 1) < eps
+    assert abs(x[2] - 2) < eps
+    assert abs(x[3] - 3) < eps
+
+def check():
+    test_array_01()
+    test_array_02()
+
+check()
