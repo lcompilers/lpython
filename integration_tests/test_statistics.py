928408b944d396030019b2d8719206fa9395d073
Madhav2310 <madhav.mittal.mat19@itbhu.ac.in>
2022-08-03 09:36:23 +0530

    fmean, Median, Median_low, median_high, mean overload functions implemented

--- integration_tests/test_statistics.py
+++ integration_tests/test_statistics.py
@@ -1,4 +1,4 @@
-from statistics import (mean, geometric_mean, harmonic_mean)
+from statistics import (mean, geometric_mean, harmonic_mean, fmean, median, median_low, median_high)
 from ltypes import i32, f64, i64
 
 eps: f64
@@ -17,6 +17,26 @@ def test_mean():
     k = mean(c)
     assert abs(k - 5.4) < eps
 
+    d: list[i32]
+    d = [1, 3, 11]
+    l: f64
+    l = mean(d)
+    assert abs(l - 5) < eps
+
+def test_fmean():
+    a: list[i32]
+    a = [9, 4, 10]
+    j: f64
+    j = fmean(a)
+    assert abs(j - 7.666666666666667) < eps
+
+    b: list[f64]
+    b = [-1.9, -13.8, -6, 4.2, 5.9, 9.1]
+    k: f64
+    k = fmean(b)
+    assert abs(k + 0.41666666666666674) < eps
+
+
 def test_geometric_mean():
     c: list[i32]
     c = [1,2,3]
@@ -31,10 +51,71 @@ def test_harmonic_mean():
     k = harmonic_mean(c)
     assert abs(k - 4.740458015267175) < eps
 
+def test_median():
+    a: list[i32]
+    a = [9, 4, 10]
+    j: f64
+    j = median(a)
+    assert abs(j - 9) < eps
+
+    b: list[i32]
+    b = [9, 4, 1, 10]
+    k: f64
+    k = median(b)
+    assert abs(k - 6.5) < eps
+
+    c: list[f64]
+    c = [-1.2, 3.4, 9.7, 6.0, 5.1]
+    l: f64
+    l = median(c)
+    assert abs(l - 5.1) < eps
+
+def test_median_low():
+    a: list[i32]
+    a = [9, 4, 10]
+    j: f64
+    j = median_low(a)
+    assert abs(j - 9) < eps
+
+    b: list[i32]
+    b = [9, 4, 1, 10]
+    k: f64
+    k = median_low(b)
+    assert abs(k - 4) < eps
+
+    c: list[f64]
+    c = [-1.2, 3.4, 6.0, 5.1]
+    l: f64
+    l = median_low(c)
+    assert abs(l - 3.4) < eps
+
+def test_median_high():
+    a: list[i32]
+    a = [9, 4, 10]
+    j: f64
+    j = median_high(a)
+    assert abs(j - 9) < eps
+
+    b: list[i32]
+    b = [9, 4, 1, 10]
+    k: f64
+    k = median_high(b)
+    assert abs(k - 9) < eps
+
+    c: list[f64]
+    c = [-1.2, 3.4, 6.0, 5.1]
+    l: f64
+    l = median_high(c)
+    assert abs(l - 5.1) < eps
+
 
 def check():
     test_mean()
     test_geometric_mean()
     test_harmonic_mean()
+    test_fmean()
+    test_median()
+    test_median_low()
+    test_median_high()
 
 check()
--- src/runtime/statistics.py
+++ src/runtime/statistics.py
@@ -1,7 +1,11 @@
 from ltypes import i32, f64
 
+
 @overload
 def mean(x: list[i32]) -> f64:
+    """
+    Returns the arithmetic mean of a data sequence of numbers
+    """
     k: i32 = len(x)
     if k == 0:
         return 0.0
@@ -17,6 +21,9 @@ def mean(x: list[i32]) -> f64:
 
 @overload
 def mean(x: list[f64]) -> f64:
+    """
+    Returns the arithmetic mean of a data sequence of numbers
+    """
     k: i32 = len(x)
     if k == 0:
         return 0.0
@@ -30,7 +37,68 @@ def mean(x: list[f64]) -> f64:
     return ans
 
 @overload
+def mean(x: list[i32]) -> i32:
+    """
+    Returns the arithmetic mean of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0
+    sum: i32
+    sum = 0
+
+    i: i32
+    for i in range(k):
+        sum += x[i]
+
+    if sum%k == 0:
+        ans: i32
+        ans = sum/k
+        return ans
+
+
+@overload
+def fmean(x: list[i32]) -> f64:
+    """
+    Returns the floating type arithmetic mean of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    sum: f64
+    sum = 0.0
+    i: i32
+
+    for i in range(k):
+        sum += x[i]
+    ans: f64
+    ans = sum/k
+    return ans
+
+
+@overload
+def fmean(x: list[f64]) -> f64:
+    """
+    Returns the floating type arithmetic mean of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    sum: f64
+    sum = 0.0
+    i: i32
+    for i in range(k):
+        sum += x[i]
+    ans: f64
+    ans = sum/k
+    return ans
+
+
+@overload
 def geometric_mean(x: list[i32]) -> f64:
+    """
+    Returns the geometric mean of a data sequence of numbers
+    """
     k: i32 = len(x)
     if k == 0:
         return 0.0
@@ -47,6 +115,9 @@ def geometric_mean(x: list[i32]) -> f64:
 
 @overload
 def harmonic_mean(x: list[i32]) -> f64:
+    """
+    Returns the harmonic mean of a data sequence of numbers
+    """
     k: i32 = len(x)
     if k == 0:
         return 0.0
@@ -61,4 +132,188 @@ def harmonic_mean(x: list[i32]) -> f64:
         sum += 1 / x[i]
     ans: f64
     ans = k/sum
+    return ans
+
+@overload
+def median(x: list[i32]) -> f64:
+    """
+    Returns the median of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    i: i32
+    j: i32
+    for i in range(k):
+        for j in range(i+1,k):
+            if x[i]>x[j]:
+                x[i],x[j]=x[j],x[i]
+    ans: f64
+    if k%2==1:
+        medianidx: i32
+        medianidx = k/2
+        ans = float(x[medianidx])
+    else:
+        lowmedidx: i32
+        highmedidx: i32
+        lowmedidx = (k/2)-1
+        highmedidx = k/2
+        ans = float((x[lowmedidx] + x[highmedidx])/2)
+    return ans
+
+@overload
+def median(x: list[f64]) -> f64:
+    """
+    Returns the median of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    i: i32
+    j: i32
+    for i in range(k):
+        for j in range(i+1,k):
+            if x[i]>x[j]:
+                x[i],x[j]=x[j],x[i]
+    ans: f64
+    if k%2==1:
+        medianidx: i32
+        medianidx = k/2
+        ans = x[medianidx]
+    else:
+        lowmedidx: i32
+        highmedidx: i32
+        lowmedidx = (k/2)-1
+        highmedidx = k/2
+        ans = (x[lowmedidx] + x[highmedidx])/2
+    return ans
+
+@overload
+def median(x: list[i32]) -> i32:
+    """
+    Returns the median of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0
+    i: i32
+    j: i32
+    for i in range(k):
+        for j in range(i+1,k):
+            if x[i]>x[j]:
+                x[i],x[j]=x[j],x[i]
+    ans: i32
+    if k%2==1:
+        medianidx: i32
+        medianidx = k/2
+        ans = x[medianidx]
+        return ans
+    else:
+        lowmedidx: i32
+        highmedidx: i32
+        lowmedidx = (k/2)-1
+        highmedidx = k/2
+        if (x[lowmedidx] + x[highmedidx])%2 ==0:
+            ans = (x[lowmedidx] + x[highmedidx])/2
+            return ans
+
+@overload
+def median_low(x: list[i32]) -> i32:
+    """
+    Returns the lowmedian of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0
+    i: i32
+    j: i32
+    for i in range(k):
+        for j in range(i+1,k):
+            if x[i]>x[j]:
+                x[i],x[j]=x[j],x[i]
+    ans: i32
+    if k%2==1:
+        medianidx: i32
+        medianidx = k/2
+        ans = x[medianidx]
+    else:
+        lowmedidx: i32
+        lowmedidx = (k/2)-1
+        ans = x[lowmedidx]
+    return ans
+
+@overload
+def median_low(x: list[f64]) -> f64:
+    """
+    Returns the low median of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    i: i32
+    j: i32
+    for i in range(k):
+        for j in range(i+1,k):
+            if x[i]>x[j]:
+                x[i],x[j]=x[j],x[i]
+    ans: f64
+    if k%2==1:
+        medianidx: i32
+        medianidx = k/2
+        ans = x[medianidx]
+    else:
+        lowmedidx: i32
+        lowmedidx = (k/2)-1
+        ans = x[lowmedidx]
+    return ans
+
+
+@overload
+def median_high(x: list[i32]) -> i32:
+    """
+    Returns the high median of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0
+    i: i32
+    j: i32
+    for i in range(k):
+        for j in range(i+1,k):
+            if x[i]>x[j]:
+                x[i],x[j]=x[j],x[i]
+    ans: i32
+    if k%2==1:
+        medianidx: i32
+        medianidx = k/2
+        ans = x[medianidx]
+    else:
+        highmedidx: i32
+        highmedidx = (k/2)
+        ans = x[highmedidx]
+    return ans
+
+@overload
+def median_high(x: list[f64]) -> f64:
+    """
+    Returns the high median of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    i: i32
+    j: i32
+    for i in range(k):
+        for j in range(i+1,k):
+            if x[i]>x[j]:
+                x[i],x[j]=x[j],x[i]
+    ans: f64
+    if k%2==1:
+        medianidx: i32
+        medianidx = k/2
+        ans = x[medianidx]
+    else:
+        highmedidx: i32
+        highmedidx = (k/2)
+        ans = x[highmedidx]
     return ans
