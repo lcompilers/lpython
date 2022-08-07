a6f546c2e5fc937e7dff3ba07ec8aaed3f732ea3
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-08-07 19:55:39 +0530

    Add tests

--- integration_tests/test_list_01.py
+++ integration_tests/test_list_01.py
@@ -1,6 +1,6 @@
 from ltypes import f64, i32
 
-def test_list():
+def test_list_01():
     a: list[i32] = [0, 1, 2, 3, 4]
     f: list[f64] = [1.0, 2.0, 3.0, 4.0, 5.0]
     i: i32
@@ -18,4 +18,23 @@ def test_list():
     for i in range(15):
         assert (f[i] - a[i]) == (i + 1.0)
 
-test_list()
+def test_list_02():
+    x: list[i32] = [1, 2]
+    y: list[i32] = []
+
+    i: i32
+    for i in range(3, 50):
+        x.append(i + 29)
+
+    for i in x:
+        y.append(i)
+
+    j: i32
+    for j in range(len(y)):
+        assert x[j] == y[j]
+
+def tests():
+    test_list_01()
+    test_list_02()
+
+tests()
