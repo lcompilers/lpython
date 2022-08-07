19150c1d0f847d29c1114f5ae6eb486bde7db15e
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-08-07 17:21:06 +0530

    Add a test

--- integration_tests/test_list_03.py
+++ integration_tests/test_list_03.py
@@ -1,6 +1,6 @@
 from ltypes import i32
 
-def test_list(n: i32) -> i32:
+def test_list_01(n: i32) -> i32:
     a: list[i32] = []
     i: i32
     for i in range(n):
@@ -10,7 +10,20 @@ def test_list(n: i32) -> i32:
         sum += a[i]
     return sum
 
+def test_list_02(n: i32) -> i32:
+    x: list[i32] = [50, 1]
+
+    i: i32
+    for i in range(n):
+        x.insert(1, i)
+
+    sum: i32 = 0
+    for i in range(n):
+        sum += x[i]
+    return sum
+
 def verify():
-    assert test_list(11) == 55
+    assert test_list_01(11) == 55
+    assert test_list_02(50) == 1275
 
 verify()
