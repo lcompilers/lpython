a7935f453834bc52dae997338bb0b3ee9de0e83e
Akshānsh <53227127+akshanshbhatt@users.noreply.github.com>
2022-08-09 19:43:44 +0530

    Add tests and update refs.

---
+++ tests/parser/unicode.py
@@ -0,0 +1,12 @@
+def test_frompyfunc_name(self):
+    # name conversion was failing for python 3 strings
+    # resulting in the default '?' name. Also test utf-8
+    # encoding using non-ascii name.
+    def cassé(x):
+        return x
+
+ℙƴtℌøἤ = 1
+print(ℙƴtℌøἤ)
+
+if 诶 != 2:
+    pass
