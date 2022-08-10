1c13a4f7404cd3a4efacdca2a93715eb1e566d45
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-08-10 14:02:08 +0530

    Add tests
    Co-Authored-By: Madhav <madhav.mittal.mat19@itbhu.ac.in>

--- integration_tests/test_statistics.py
+++ integration_tests/test_statistics.py
@@ -1,4 +1,5 @@
-from statistics import (mean, geometric_mean, harmonic_mean, fmean)
+from statistics import (mean, fmean, geometric_mean, harmonic_mean,
+                        variance, stdev)
 from ltypes import i32, f64, i64
 
 eps: f64
@@ -51,10 +52,41 @@ def test_harmonic_mean():
     k = harmonic_mean(c)
     assert abs(k - 4.740458015267175) < eps
 
+
+def test_variance():
+    a: list[i32]
+    a = [1, 2, 5, 4, 8, 9, 12]
+    j: f64
+    j = variance(a)
+    assert abs(j - 15.80952380952381) < eps
+
+    b: list[f64]
+    b = [2.74, 1.23, 2.63, 2.22, 3.0, 1.98]
+    k: f64
+    k = variance(b)
+    assert abs(k - 0.40924) < eps
+
+
+def test_stdev():
+    a: list[i32]
+    a = [1, 2, 3, 4, 5]
+    j: f64
+    j = stdev(a)
+    assert abs(j - 1.5811388300841898) < eps
+
+    b: list[f64]
+    b = [1.23, 1.45, 2.1, 2.2, 1.9]
+    k: f64
+    k = stdev(b)
+    assert abs(k - 0.41967844833872525) < eps
+
+
 def check():
     test_mean()
     test_geometric_mean()
     test_harmonic_mean()
     test_fmean()
+    test_variance()
+    test_stdev()
 
 check()
