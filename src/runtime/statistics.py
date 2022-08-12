2874bf9a039f65be24429850c07bc05fbde8bec9
Madhav2310 <madhav.mittal.mat19@itbhu.ac.in>
2022-08-12 13:10:16 +0530

    gmean, Hmean expanded for other datatypes

--- src/runtime/statistics.py
+++ src/runtime/statistics.py
@@ -102,7 +102,7 @@ def fmean(x: list[f32]) -> f64:
     """
     return mean(x)
 
-
+@overload
 def geometric_mean(x: list[i32]) -> f64:
     """
     Returns the geometric mean of a data sequence of numbers
@@ -115,11 +115,50 @@ def geometric_mean(x: list[i32]) -> f64:
     i: i32
 
     for i in range(k):
+        if(x[i]<=0):
+            raise Exception("geometric mean requires a non-empty dataset containing positive numbers")
         product *= float(x[i])
 
     return product**(1/k)
 
+@overload
+def geometric_mean(x: list[i64]) -> f64:
+    """
+    Returns the geometric mean of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    product: f64
+    product = 1.0
+    i: i32
 
+    for i in range(k):
+        if(x[i]<=0):
+            raise Exception("geometric mean requires a non-empty dataset containing positive numbers")
+        product *= float(x[i])
+
+    return product**(1/k)
+
+@overload
+def geometric_mean(x: list[f64]) -> f64:
+    """
+    Returns the geometric mean of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    product: f64
+    product = 1.0
+    i: i32
+
+    for i in range(k):
+        if(x[i]<=0.0):
+            raise Exception("geometric mean requires a non-empty dataset containing positive numbers")
+        product *= x[i]
+
+    return product**(1/k)
+@overload
 def harmonic_mean(x: list[i32]) -> f64:
     """
     Returns the harmonic mean of a data sequence of numbers
@@ -134,6 +173,48 @@ def harmonic_mean(x: list[i32]) -> f64:
     for i in range(k):
         if x[i] == 0:
             return 0.0
+        if(x[i]<0.0):
+            raise Exception("Harmonic mean does not support negative values")
+        sum += 1 / x[i]
+
+    return float(k/sum)
+@overload
+def harmonic_mean(x: list[i64]) -> f64:
+    """
+    Returns the harmonic mean of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    sum: f64
+    sum = 0.0
+    i: i32
+
+    for i in range(k):
+        if x[i] == 0:
+            return 0.0
+        if(x[i]<0):
+            raise Exception("Harmonic mean does not support negative values")
+        sum += 1 / x[i]
+    return k/sum
+
+@overload
+def harmonic_mean(x: list[f64]) -> f64:
+    """
+    Returns the harmonic mean of a data sequence of numbers
+    """
+    k: i32 = len(x)
+    if k == 0:
+        return 0.0
+    sum: f64
+    sum = 0.0
+    i: i32
+
+    for i in range(k):
+        if x[i] == 0.0:
+            return 0.0
+        if(x[i]<0.0):
+            raise Exception("Harmonic mean does not support negative values")
         sum += 1 / x[i]
 
     return k/sum
@@ -188,3 +269,4 @@ def stdev(x: list[i32]) -> f64:
     Returns the standard deviation of a data sequence of numbers
     """
     return variance(x)**0.5
+
