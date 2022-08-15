fdcc78c40df02b14ef218d0c6ba807bc5d26d849
Akshānsh <53227127+akshanshbhatt@users.noreply.github.com>
2022-08-15 22:14:28 +0530

    Add tests and update refs.

--- tests/parser/type_comment1.py
+++ tests/parser/type_comment1.py
@@ -3,3 +3,17 @@ import pytest  # type: ignore
 def ndarray_func(x):
     # type: (np.ndarray) -> np.ndarray
     return x
+
+@decorator1 # type: ignore
+
+# Comment
+
+@decorator2
+
+# Comment
+
+@decorator3 # type: ignore
+
+def test(x):
+    # type: (np.ndarray) -> np.ndarray
+    return x
