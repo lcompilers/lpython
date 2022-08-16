b082768bb7e07ae194cba68698f3492082a9975f
Abdelrahman Khaled <abdokh950@gmail.com>
2022-08-16 23:03:06 +0200

    Add tests for `str.lower()`

--- integration_tests/test_str_attributes.py
+++ integration_tests/test_str_attributes.py
@@ -7,5 +7,12 @@ def capitalize():
     s = " tom and jerry"
     assert s.capitalize() == s
 
-capitalize()
 
+def lower():
+    s: str
+    s = "AaaaAABBbbbbBB!@12223BN"
+    assert s.lower() == "aaaaaabbbbbbbb!@12223bn"
+    assert "DDd12Vv" .lower() == "ddd12vv"
+
+capitalize()
+lower()
