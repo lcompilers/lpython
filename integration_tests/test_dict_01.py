8a0806514aed5dde15ee9965e0182e9947fc62a6
Gagandeep Singh <gdp.1807@gmail.com>
2022-08-22 19:27:42 +0530

    Initialise with empty dict

--- integration_tests/test_dict_01.py
+++ integration_tests/test_dict_01.py
@@ -1,9 +1,11 @@
 from ltypes import i32, f64
 
 def test_dict():
-    rollnumber2cpi: dict[i32, f64] = {0: 1.1}
+    rollnumber2cpi: dict[i32, f64] = {}
     i: i32
     size: i32 = 1000
+
+    rollnumber2cpi[0] = 1.1
 
     for i in range(1000, 1000 + size):
         rollnumber2cpi[i] = float(i/100.0 + 5.0)
