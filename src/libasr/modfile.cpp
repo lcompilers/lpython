39020a631a9ac710b2440e7689e36612d34d8c94
Gagandeep Singh <gdp.1807@gmail.com>
2022-08-22 15:42:04 +0530

    Added load_pycfile for loading LPython's ASR from a pyc file

--- src/libasr/modfile.cpp
+++ src/libasr/modfile.cpp
@@ -57,6 +57,34 @@ std::string save_modfile(const ASR::TranslationUnit_t &m) {
     return b.get_str();
 }
 
+std::string save_pycfile(const ASR::TranslationUnit_t &m) {
+#ifdef WITH_LFORTRAN_BINARY_MODFILES
+    BinaryWriter b;
+#else
+    TextWriter b;
+#endif
+    // Header
+    b.write_string(lfortran_modfile_type_string);
+    b.write_string(LFORTRAN_VERSION);
+
+    // AST section: Original module source code:
+    // Currently empty.
+    // Note: in the future we can save here:
+    // * A path to the original source code
+    // * Hash of the orig source code
+    // * AST binary export of it (this AST only changes if the hash changes)
+
+    // ASR section:
+
+    // Export ASR:
+    // Currently empty.
+
+    // Full ASR:
+    b.write_string(serialize(m));
+
+    return b.get_str();
+}
+
 ASR::TranslationUnit_t* load_modfile(Allocator &al, const std::string &s,
         bool load_symtab_id, SymbolTable &symtab) {
 #ifdef WITH_LFORTRAN_BINARY_MODFILES
@@ -74,6 +102,29 @@ ASR::TranslationUnit_t* load_modfile(Allocator &al, const std::string &s,
     }
     std::string asr_binary = b.read_string();
     ASR::asr_t *asr = deserialize_asr(al, asr_binary, load_symtab_id, symtab);
+
+    ASR::TranslationUnit_t *tu = ASR::down_cast2<ASR::TranslationUnit_t>(asr);
+    return tu;
+}
+
+ASR::TranslationUnit_t* load_pycfile(Allocator &al, const std::string &s,
+        bool load_symtab_id) {
+#ifdef WITH_LFORTRAN_BINARY_MODFILES
+    BinaryReader b(s);
+#else
+    TextReader b(s);
+#endif
+
+    std::string file_type = b.read_string();
+    if (file_type != lfortran_modfile_type_string) {
+        throw LCompilersException("LFortran Modfile format not recognized");
+    }
+    std::string version = b.read_string();
+    if (version != LFORTRAN_VERSION) {
+        throw LCompilersException("Incompatible format: LFortran Modfile was generated using version '" + version + "', but current LFortran version is '" + LFORTRAN_VERSION + "'");
+    }
+    std::string asr_binary = b.read_string();
+    ASR::asr_t *asr = deserialize_asr(al, asr_binary, load_symtab_id);
 
     ASR::TranslationUnit_t *tu = ASR::down_cast2<ASR::TranslationUnit_t>(asr);
     return tu;
