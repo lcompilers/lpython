322bc65b1cb40b3a347bbce29097c8b979bdb5c8
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-08-25 11:42:39 +0530

    Add and update tests

--- integration_tests/expr_01.py
+++ integration_tests/expr_01.py
@@ -1,7 +1,15 @@
+from ltypes import inline, i32
+
+@inline
+def add(x: i32, y: i32) -> i32:
+    return x + y
+
 def main0():
-    x: i32
+    x: i32; y: i32
     x = (2+3)*5
+    y = add(x, 2)*2
     assert x == 25
+    assert y == 54
 
 main0()
 
