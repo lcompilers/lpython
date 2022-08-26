7edf8eba032a03e6097eda169adce3a245bcb256
Lubis <luthfanlubis@gmail.com>
2022-08-27 03:20:45 +0900

    Temporary test modification

--- integration_tests/restrictions_01.py
+++ integration_tests/restrictions_01.py
@@ -6,12 +6,15 @@ T = TypeVar('T')
 def plus(x: T, y: T) -> T:
     pass
 
+def add_string(x: str, y: str) -> str:
+    return x + y
+
 def f(x: T, y: T) -> T:
     return plus(x,y)
 
 # print(f(1,2,plus=int.__add__))
-print(f(1,2))
+# print(f(1,2))
+print(f("a","b",plus=add_string))
 '''
-print(f("a","b"))
 print(f("c","d"))
 '''
