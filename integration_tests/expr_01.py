92c71c4367f9bb8d020ddd7c76fc0cd62f93d59a
Ron Nuriel <rnuriel@gsitechnology.com>
2022-08-29 15:30:45 +0300

    Added test for & op
    Signed-off-by: Ron Nuriel <rnuriel@gsitechnology.com>

--- integration_tests/expr_01.py
+++ integration_tests/expr_01.py
@@ -4,12 +4,22 @@ from ltypes import inline, i32
 def add(x: i32, y: i32) -> i32:
     return x + y
 
+@inline
+def and_op(x: i32, y: i32) -> i32:
+    return x & y
+
 def main0():
-    x: i32; y: i32
+    x: i32
+    y: i32
+    z: i32
     x = (2+3)*5
     y = add(x, 2)*2
     assert x == 25
     assert y == 54
+
+    z = and_op(x, y)
+    assert z == 16
+
 
 main0()
 
