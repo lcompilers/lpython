c04a64fb20f9cc5e8514a7365bd11359d0fef58b
Gagandeep Singh <gdp.1807@gmail.com>
2022-09-01 20:10:16 +0530

    Handle positional arguments and keyword arguments simultaneously

--- src/lpython/semantics/python_ast_to_asr.cpp
+++ src/lpython/semantics/python_ast_to_asr.cpp
@@ -656,6 +656,88 @@ public:
         }
     }
 
+    bool is_arg_already_present(Vec<ASR::call_arg_t>& call_args_vec,
+        ASR::Function_t* orig_func, std::string arg_name,
+        const Location& call_loc) {
+        int64_t arg_position = -1;
+        for( size_t i = 0; i < orig_func->n_args; i++ ) {
+            ASR::Var_t* arg_Var = ASR::down_cast<ASR::Var_t>(orig_func->m_args[i]);
+            std::string original_arg_name = ASRUtils::symbol_name(arg_Var->m_v);
+            if( original_arg_name == arg_name ) {
+                arg_position = i;
+                break ;
+            }
+        }
+        if( arg_position == -1 ) {
+            throw SemanticError("Function " + std::string(orig_func->m_name) +
+                                " doesn't have an argument named " + arg_name,
+                                call_loc);
+        }
+        if( call_args_vec[arg_position].m_value != nullptr ) {
+            return true;
+        }
+        return false;
+    }
+
+    int64_t find_argument_position_from_name(ASR::Function_t* orig_func, std::string arg_name,
+        const Location& call_loc) {
+        int64_t arg_position = -1;
+        for( size_t i = 0; i < orig_func->n_args; i++ ) {
+            ASR::Var_t* arg_Var = ASR::down_cast<ASR::Var_t>(orig_func->m_args[i]);
+            std::string original_arg_name = ASRUtils::symbol_name(arg_Var->m_v);
+            if( original_arg_name == arg_name ) {
+                return i;
+            }
+        }
+        if( arg_position == -1 ) {
+            throw SemanticError("Function " + std::string(orig_func->m_name) +
+                                " doesn't have an argument named " + arg_name,
+                                call_loc);
+        }
+        return -1;
+    }
+
+    void visit_expr_list(AST::expr_t** pos_args, size_t n_pos_args,
+                         AST::keyword_t* kwargs, size_t n_kwargs,
+                         Vec<ASR::call_arg_t>& call_args_vec,
+                         ASR::Function_t* orig_func, const Location& call_loc) {
+        LFORTRAN_ASSERT(call_args_vec.reserve_called);
+
+        // Fill the whole call_args_vec with nullptr
+        // This is for error handling later on.
+        for( size_t i = 0; i < n_pos_args + n_kwargs; i++ ) {
+            ASR::call_arg_t call_arg;
+            Location loc;
+            loc.first = loc.last = 1;
+            call_arg.m_value = nullptr;
+            call_arg.loc = loc;
+            call_args_vec.push_back(al, call_arg);
+        }
+
+        // Now handle positional arguments in the following loop
+        for( size_t i = 0; i < n_pos_args; i++ ) {
+            this->visit_expr(*pos_args[i]);
+            ASR::expr_t* expr = ASRUtils::EXPR(tmp);
+            call_args_vec.p[i].loc = expr->base.loc;
+            call_args_vec.p[i].m_value = expr;
+        }
+
+        // Now handle keyword arguments in the following loop
+        for( size_t i = 0; i < n_kwargs; i++ ) {
+            this->visit_expr(*kwargs[i].m_value);
+            ASR::expr_t* expr = ASRUtils::EXPR(tmp);
+            std::string arg_name = std::string(kwargs[i].m_arg);
+            if( is_arg_already_present(call_args_vec, orig_func, arg_name, call_loc) ) {
+                throw SemanticError(std::string(orig_func->m_name) + "() got multiple values for argument '"
+                                    + arg_name + "'",
+                                    call_loc);
+            }
+            size_t arg_pos = find_argument_position_from_name(orig_func, arg_name, call_loc);
+            call_args_vec.p[arg_pos].loc = expr->base.loc;
+            call_args_vec.p[arg_pos].m_value = expr;
+        }
+    }
+
     void visit_expr_list(Vec<ASR::call_arg_t>& exprs, size_t n,
                          Vec<ASR::expr_t*>& exprs_vec) {
         LFORTRAN_ASSERT(exprs_vec.reserve_called);
@@ -4454,12 +4536,18 @@ public:
     void visit_Call(const AST::Call_t &x) {
         std::string call_name;
         Vec<ASR::call_arg_t> args;
-        args.reserve(al, x.n_args);
-        visit_expr_list(x.m_args, x.n_args, args);
         if (AST::is_a<AST::Name_t>(*x.m_func)) {
             AST::Name_t *n = AST::down_cast<AST::Name_t>(x.m_func);
             call_name = n->m_id;
+            ASR::symbol_t *s = current_scope->resolve_symbol(call_name);
+            LFORTRAN_ASSERT(ASR::is_a<ASR::Function_t>(*s));
+            ASR::Function_t* func = ASR::down_cast<ASR::Function_t>(s);
+            args.reserve(al, x.n_args + x.n_keywords);
+            visit_expr_list(x.m_args, x.n_args, x.m_keywords, x.n_keywords, args, func, x.base.base.loc);
         } else if (AST::is_a<AST::Attribute_t>(*x.m_func)) {
+            Vec<ASR::call_arg_t> args;
+            args.reserve(al, x.n_args);
+            visit_expr_list(x.m_args, x.n_args, args);
             AST::Attribute_t *at = AST::down_cast<AST::Attribute_t>(x.m_func);
             if (AST::is_a<AST::Name_t>(*at->m_value)) {
                 AST::Name_t *n = AST::down_cast<AST::Name_t>(at->m_value);
