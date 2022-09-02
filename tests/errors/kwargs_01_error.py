2f3064cd50448eee9383cbf9573d9841b2552590
Gagandeep Singh <gdp.1807@gmail.com>
2022-09-02 13:44:50 +0530

    Added tests for errors while passing keyword arguments to functions

---
+++ tests/errors/kwargs_01_error.py
@@ -0,0 +1,13 @@
+from ltypes import i32, i64, f32, f64, c64, overload
+
+def func01(a: i32, b: i64) -> i64:
+    return int(a) + b
+
+def test_arg_passing():
+    arg1: i32;
+
+    arg1 = 1
+
+    func01(arg1, a=int(8))
+
+test_arg_passing()
