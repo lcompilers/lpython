4f038eab79176e483793ebf65facfcd4113f6293
Gagandeep Singh <gdp.1807@gmail.com>
2022-09-12 15:04:12 +0530

    Added tests to verify that the errors are raised correctly

---
+++ tests/errors/test_dict12.py
@@ -0,0 +1,4 @@
+def test_dict():
+    print({0.0: 1.1}) # constant dict with floating point value as key
+
+test_dict()
---
+++ tests/errors/test_dict13.py
@@ -0,0 +1,4 @@
+def test_dict():
+    print({complex(0.0, 1.0): 1.1}) # constant dict with complex value as key
+
+test_dict()
