5a62b112ec3eb242e8ea3fb03fa186cdd4c53b35
Lubis <luthfanlubis@gmail.com>
2022-09-27 21:39:08 +0900

    Updated tests on generics array

--- integration_tests/generics_array_02.py
+++ integration_tests/generics_array_02.py
@@ -1,4 +1,4 @@
-from ltypes import TypeVar, restriction, i32
+from ltypes import TypeVar, restriction, i32, f32
 from numpy import empty
 
 n: i32
@@ -9,12 +9,24 @@ T = TypeVar('T')
 def add(x: T, y: T) -> T:
     pass
 
-def g(n: i32, a: T[n], b: T[n]) -> T[n]:
-    return add(a,b)
+def g(n: i32, a: T[n], b: T[n]):
+  r: T[n]
+  r = empty(n)
+  i: i32
+  for i in range(n):
+    r[i] = add(a[i], b[i])
+  print(r[0])
 
 def main():
-    a: i32[1] = empty(1)
-    b: i32[1] = empty(1)
-    g(1, a, b)
+    a_int: i32[1] = empty(1)
+    a_int[0] = 400
+    b_int: i32[1] = empty(1)
+    b_int[0] = 20
+    g(1, a_int, b_int)
+    a_float: f32[1] = empty(1)
+    a_float[0] = 400.0
+    b_float: f32[1] = empty(1)
+    b_float[0] = 20.0
+    g(1, a_float ,b_float)
 
 main()
--- integration_tests/generics_array_03.py
+++ integration_tests/generics_array_03.py
@@ -1,22 +1,36 @@
-from ltypes import TypeVar, restriction, i32
+from ltypes import TypeVar, restriction, i32, f32
 from numpy import empty
 
+n: i32
+n = TypeVar("n")
+m: i32
+m = TypeVar("m")
 T = TypeVar('T')
 
 @restriction
 def add(x: T, y: T) -> T:
     pass
-
-def g(a: T[:], b: T[:]) -> T:
-    r: T[size(a)]
+    
+def g(n: i32, m: i32, a: T[n,m], b: T[n,m]) -> T[n,m]:
+    r: T[n,m]
+    r = empty([n,m])
     i: i32
-    for i in range(size(a)):
-        r[i] = add(a[i], b[i])
-    return r
-
+    j: i32
+    for i in range(n):
+      for j in range(m):
+        r[i,j] = add(a[i,j],b[i,j])
+    print(r[0,0])
+    
 def main():
-    a: i32[1] = empty(1)
-    b: i32[1] = empty(1)
-    g(a, b)
+    a_int: i32[1,1] = empty([1,1])
+    a_int[0,0] = 400
+    b_int: i32[1,1] = empty([1,1])
+    b_int[0,0] = 20
+    g(1, 1, a_int, b_int)
+    a_float: i32[1,1] = empty([1,1])
+    a_float[0,0] = 400.0
+    b_float: i32[1,1] = empty([1,1])
+    b_float[0,0] = 20.0
+    g(1, 1, a_float, b_float)
 
 main()
