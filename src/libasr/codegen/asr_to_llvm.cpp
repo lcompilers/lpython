7e5a05ef7d8a618cb648c37eb277c46e83855438
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-10-20 21:38:13 +0530

    Add  `print_error` function to use  `_lcompilers_print_error` in the llvm_utils

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -4584,10 +4584,10 @@ public:
                 char* s = ASR::down_cast<ASR::StringConstant_t>(x.m_msg)->m_s;
                 llvm::Value *fmt_ptr = builder->CreateGlobalStringPtr("AssertionError: %s\n");
                 llvm::Value *fmt_ptr2 = builder->CreateGlobalStringPtr(s);
-                printf(context, *module, *builder, {fmt_ptr, fmt_ptr2});
+                print_error(context, *module, *builder, {fmt_ptr, fmt_ptr2});
             } else {
                 llvm::Value *fmt_ptr = builder->CreateGlobalStringPtr("AssertionError\n");
-                printf(context, *module, *builder, {fmt_ptr});
+                print_error(context, *module, *builder, {fmt_ptr});
             }
             int exit_code_int = 1;
             llvm::Value *exit_code = llvm::ConstantInt::get(context,
@@ -5359,7 +5359,7 @@ public:
 
     void visit_Stop(const ASR::Stop_t &x) {
         llvm::Value *fmt_ptr = builder->CreateGlobalStringPtr("STOP\n");
-        printf(context, *module, *builder, {fmt_ptr});
+        print_error(context, *module, *builder, {fmt_ptr});
         llvm::Value *exit_code;
         if (x.m_code && ASRUtils::expr_type(x.m_code)->type == ASR::ttypeType::Integer) {
             this->visit_expr(*x.m_code);
@@ -5374,7 +5374,7 @@ public:
 
     void visit_ErrorStop(const ASR::ErrorStop_t & /* x */) {
         llvm::Value *fmt_ptr = builder->CreateGlobalStringPtr("ERROR STOP\n");
-        printf(context, *module, *builder, {fmt_ptr});
+        print_error(context, *module, *builder, {fmt_ptr});
         int exit_code_int = 1;
         llvm::Value *exit_code = llvm::ConstantInt::get(context,
                 llvm::APInt(32, exit_code_int));
