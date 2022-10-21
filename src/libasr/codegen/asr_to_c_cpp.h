163efa5329d686464166c5c990087bb08497dd48
Gagandeep Singh <gdp.1807@gmail.com>
2022-10-21 16:07:02 +0530

    fix warnings

--- src/libasr/codegen/asr_to_c_cpp.h
+++ src/libasr/codegen/asr_to_c_cpp.h
@@ -640,7 +640,6 @@ R"(#include <stdio.h>
         self().visit_expr(*x.m_value);
         std::string value = src;
         ASR::ttype_t* value_type = ASRUtils::expr_type(x.m_value);
-        ASR::ttype_t* target_type = ASRUtils::expr_type(x.m_target);
         if( ASR::is_a<ASR::Struct_t>(*value_type) &&
             ASR::is_a<ASR::Var_t>(*x.m_value) ) {
             value = "*" + value;
