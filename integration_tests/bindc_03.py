1cf69daf1a6024753a0eeb55b965e0c0d4d5cc96
Gagandeep Singh <gdp.1807@gmail.com>
2022-10-21 10:47:21 +0530

    Update bindc_03.py,
    1. Use bool argument in ccall functions
    2. Use CPtr argument in python functions
    3. Use CPtr member in dataclass

--- integration_tests/bindc_03.py
+++ integration_tests/bindc_03.py
@@ -1,8 +1,15 @@
-from ltypes import c_p_pointer, CPtr, pointer, i16, i32, Pointer, ccall, p_c_pointer
+from ltypes import c_p_pointer, CPtr, pointer, i32, Pointer, ccall, p_c_pointer, dataclass
+
+@dataclass
+class ArrayWrapped:
+    array: CPtr
 
 @ccall
-def g(a: CPtr, value: i32) -> None:
+def g(a: CPtr, value: i32, offset_value: bool) -> None:
     pass
+
+def gpy(a: CPtr, value: i32, offset_value: bool) -> None:
+    g(a, value, offset_value)
 
 @ccall
 def get_array(size: i32) -> CPtr:
@@ -17,17 +24,19 @@ def f(q_void: CPtr) -> None:
     for i in range(10):
         q2: CPtr
         p_c_pointer(pointer(q[i]), q2)
-        g(q2, i * i)
+        gpy(q2, i * i, bool(i%2))
         # TODO: Use q[i] directly in the assert.
         el = q[i]
         print(el)
-        assert el == i * i
+        assert el == i * i + i%2
 
 def run():
     a: CPtr
+    array_wrapped: ArrayWrapped = ArrayWrapped(a)
     size: i32
     size = 10
     a = get_array(size)
-    f(a)
+    array_wrapped.array = a
+    f(array_wrapped.array)
 
 run()
