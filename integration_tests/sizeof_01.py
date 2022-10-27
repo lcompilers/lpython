2c584fcd5b03cb650ef6c59b1075fc55f98fd504
Gagandeep Singh <gdp.1807@gmail.com>
2022-10-27 10:02:00 +0530

    Added ``sizeof`` support for ``i16`` (#1233)

--- integration_tests/sizeof_01.py
+++ integration_tests/sizeof_01.py
@@ -1,4 +1,4 @@
-from ltypes import sizeof, i64, i32, f32, f64, c32, c64, ccall, CPtr
+from ltypes import sizeof, i64, i32, f32, f64, c32, c64, i16, ccall, CPtr
 
 @ccall
 def cmalloc(bytes: i64) -> CPtr:
@@ -17,12 +17,14 @@ def sum_carray(arr: CPtr, size: i32) -> i64:
     pass
 
 def test_sizeof():
+    xi16: i16 = 0
     xi: i32 = 0
     yi: i64 = 0
     xf: f32 = 0.0
     yf: f64 = 0.0
     xz: c32 = complex(0, 0)
     yz: c64 = complex(0, 0)
+    assert sizeof(xi16) == sizeof(i16)
     assert sizeof(xi) == sizeof(i32)
     assert sizeof(yi) == sizeof(i64)
     assert sizeof(xf) == sizeof(f32)
