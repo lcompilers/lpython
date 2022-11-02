1ef1683626c689ac06caf1315a07d78cf3b0d8f0
Gagandeep Singh <gdp.1807@gmail.com>
2022-11-02 11:03:28 +0530

    done

--- integration_tests/const_03.py
+++ integration_tests/const_03.py
@@ -1,4 +1,4 @@
-from ltypes import i64, f64, i32, Const
+from ltypes import i64, f64, i32, Const, ccall
 
 CONST_1: Const[f64] = 32.0
 CONST_2: Const[f64] = CONST_1 * 2.0
@@ -10,10 +10,16 @@ CONST_12: Const[i32] = CONST_11
 def print_value(value: Const[f64]):
     print(value)
 
+@ccall
+def print_value_c(value: Const[f64]):
+    pass
+
 def test_global_consts():
     print_value(CONST_1)
     print_value(CONST_2)
     print(CONST_12)
+    print_value_c(CONST_1)
+    print_value_c(CONST_2)
     assert CONST_1 == 32
     assert CONST_2 == 64
     assert abs(CONST_3 - 96.0) < 1e-12
---
+++ integration_tests/const_03b.c
@@ -0,0 +1,6 @@
+#include "const_03b.h"
+#include <stdio.h>
+
+void print_value_c(const double value) {
+    printf("print_value_c: %lf\n", value);
+}
