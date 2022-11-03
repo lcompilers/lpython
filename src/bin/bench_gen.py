ce2edf8d2aec19498fedfd0c647e6ecfb235569b
Ubaid <shaikhubaid769@gmail.com>
2022-11-03 13:39:56 +0530

    Bench: Add benchmark generator script

---
+++ src/bin/bench_gen.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+N = 10000
+
+A_functions = ""
+calls = ""
+for i in range(N):
+    func_A = f"""
+def A{i}(x: i32) -> i32:
+    y: i32
+    z: i32
+    y = {i}
+    z = 5
+    x = x + y * z
+    return x
+"""
+    A_functions += func_A
+    calls += f"    y = A{i}(y)\n"
+
+source = f"""\
+from ltypes import i32
+
+{A_functions}
+
+def Driver(test_val: i32) -> i32:
+    y: i32
+    y = test_val
+{calls}
+    return y
+
+def Main0():
+    print(Driver(5))
+
+Main0()
+"""
+
+print(source)
