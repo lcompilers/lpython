3565447e8b810bd3dff7777cfabb0e40987583f0
Ondřej Čertík <ondrej@certik.us>
2022-11-04 12:51:47 -0600

    Make the test pass in CPython

--- integration_tests/to_bytes_01.py
+++ integration_tests/to_bytes_01.py
@@ -1,12 +1,12 @@
 def main0():
     x: i32
-    x = 100
+    x = 8
     s: str
     s = (7).to_bytes(5,'big')
     print(s)
     assert str(s) == r"b'\x00\x00\x00\x00\x07'"
-    #s = x.to_bytes(3,'big')
-    #print(s)
-    #assert s == "(TODO: 100)"
+    s = x.to_bytes(4,'big')
+    print(s)
+    assert str(s) == "(TODO: 8)" or str(s) == r"b'\x00\x00\x00\x08'"
 
 main0()
