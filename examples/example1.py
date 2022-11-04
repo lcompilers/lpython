3a4e288cd255e28350e80998ad11e0507998964d
Ondřej Čertík <ondrej@certik.us>
2022-11-04 12:46:26 -0600

    Make it a test

--- examples/example1.py
+++ examples/example1.py
@@ -1,7 +1,12 @@
-def main0():	
-	x: i32
-	x = 100
-	print((7).to_bytes(5,'big'))
-	#print(x.to_bytes())
-	
+def main0():
+    x: i32
+    x = 100
+    s: str
+    s = (7).to_bytes(5,'big')
+    print(s)
+    assert s == "b'\x00\x00\x00\x00\x07'"
+    s = x.to_bytes()
+    print(s)
+    assert s == "(TODO: 100)"
+
 main0()
