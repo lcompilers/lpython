03fbb067d00d4bb105cf4c62e3042e69cff5bb69
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-11-04 21:47:15 +0530

    WASM_X86: Implement Visit if-else

--- src/libasr/codegen/wasm_to_x86.cpp
+++ src/libasr/codegen/wasm_to_x86.cpp
@@ -16,6 +16,7 @@ class X86Visitor : public WASMDecoder<X86Visitor>,
    public:
     X86Assembler &m_a;
     uint32_t cur_func_idx;
+    std::vector<uint32_t> unique_id;
 
     X86Visitor(X86Assembler &m_a, Allocator &al,
                diag::Diagnostics &diagonostics, Vec<uint8_t> &code)
@@ -66,6 +67,27 @@ class X86Visitor : public WASMDecoder<X86Visitor>,
             // push eax value onto stack
             m_a.asm_push_r32(X86Reg::eax);
         }
+    }
+
+    void visit_EmtpyBlockType() {}
+
+    void visit_If() {
+        unique_id.push_back(offset);
+        m_a.asm_pop_r32(X86Reg::eax);
+        m_a.asm_cmp_r32_imm8(LFortran::X86Reg::eax, 1);
+        m_a.asm_je_label(".then_" + std::to_string(unique_id.back()));
+        m_a.asm_jmp_label(".else_" + std::to_string(unique_id.back()));
+        m_a.add_label(".then_" + std::to_string(unique_id.back()));
+        {
+            decode_instructions();
+        }
+        m_a.add_label(".endif_" + std::to_string(unique_id.back()));
+        unique_id.pop_back();
+    }
+
+    void visit_Else() {
+        m_a.asm_jmp_label(".endif_" + std::to_string(unique_id.back()));
+        m_a.add_label(".else_" + std::to_string(unique_id.back()));
     }
 
     void visit_LocalGet(uint32_t localidx) {
