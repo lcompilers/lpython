46e2df01086c515dbd396343cb16e8a76fcfbd8e
Ubaid <shaikhubaid769@gmail.com>
2022-11-04 21:56:21 +0530

    TEST: WASM_X86: Add another test for if-else

--- integration_tests/if_01.py
+++ integration_tests/if_01.py
@@ -22,7 +22,38 @@ def Test_if_01():
     else:
         print(0)
 
+def Test_if_02():
+    if True:
+        print(1)
+        if True:
+            print(2)
+            if False:
+                print(3)
+            elif True:
+                print(4)
+            else:
+                print(5)
+        else:
+            print(6)
+
+    if True:
+        print(7)
+        if False:
+            print(8)
+            if False:
+                print(9)
+            else:
+                print(10)
+        else:
+            if True:
+                print(11)
+            else:
+                print(12)
+            print(13)
+    print(14)
+
 def Verify():
     Test_if_01()
+    Test_if_02()
 
 Verify()
