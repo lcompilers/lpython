0f1a06478481d71c05238ec02ed33362bb48679e
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-11-08 10:55:38 +0530

    [WASM_x86] Convert the offset into strings and store it to use as a unique_id

--- src/libasr/codegen/wasm_to_x86.cpp
+++ src/libasr/codegen/wasm_to_x86.cpp
@@ -16,7 +16,7 @@ class X86Visitor : public WASMDecoder<X86Visitor>,
    public:
     X86Assembler &m_a;
     uint32_t cur_func_idx;
-    std::vector<uint32_t> unique_id;
+    std::vector<std::string> unique_id;
 
     X86Visitor(X86Assembler &m_a, Allocator &al,
                diag::Diagnostics &diagonostics, Vec<uint8_t> &code)
@@ -72,22 +72,22 @@ class X86Visitor : public WASMDecoder<X86Visitor>,
     void visit_EmtpyBlockType() {}
 
     void visit_If() {
-        unique_id.push_back(offset);
+        unique_id.push_back(std::to_string(offset));
         m_a.asm_pop_r32(X86Reg::eax);
         m_a.asm_cmp_r32_imm8(LFortran::X86Reg::eax, 1);
-        m_a.asm_je_label(".then_" + std::to_string(unique_id.back()));
-        m_a.asm_jmp_label(".else_" + std::to_string(unique_id.back()));
-        m_a.add_label(".then_" + std::to_string(unique_id.back()));
+        m_a.asm_je_label(".then_" + unique_id.back());
+        m_a.asm_jmp_label(".else_" + unique_id.back());
+        m_a.add_label(".then_" + unique_id.back());
         {
             decode_instructions();
         }
-        m_a.add_label(".endif_" + std::to_string(unique_id.back()));
+        m_a.add_label(".endif_" + unique_id.back());
         unique_id.pop_back();
     }
 
     void visit_Else() {
-        m_a.asm_jmp_label(".endif_" + std::to_string(unique_id.back()));
-        m_a.add_label(".else_" + std::to_string(unique_id.back()));
+        m_a.asm_jmp_label(".endif_" + unique_id.back());
+        m_a.add_label(".else_" + unique_id.back());
     }
 
     void visit_LocalGet(uint32_t localidx) {
