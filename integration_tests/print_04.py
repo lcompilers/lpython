946b1e79fde6d5770fc6104c6efe189b6cee9e5a
Gagandeep Singh <gdp.1807@gmail.com>
2022-12-12 11:35:52 +0530

    upd

--- integration_tests/print_04.py
+++ integration_tests/print_04.py
@@ -1,10 +1,10 @@
 from ltypes import i8, i16, i32, i64
 
-u: i64 = i64(-18)
+u: i64 = i64(-9223372036854775808)
 print(u)
-x: i32 = i32(-18)
+x: i32 = i32(-2147483648)
 print(x)
-y: i16 = i16(-18)
+y: i16 = i16(-32768)
 print(y)
-z: i8 = i8(-18)
+z: i8 = i8(-128)
 print(z)
