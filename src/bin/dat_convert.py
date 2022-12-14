d09f7e6a800826f3a54a734955f17636b6991c34
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-12-14 14:29:50 +0530

    Use command line argument to get the filename in dat_convert

--- src/bin/dat_convert.py
+++ src/bin/dat_convert.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
 from struct import unpack
-import sys
-import re
+from sys import argv
+from re import sub
 
 lines = ""
-
-with open('lines.dat', "rb") as f:
+with open(argv[1], "rb") as f:
     lines = f.read()
 
 list = []
-
 for i in range(0, len(lines), 24):
-    list.append(re.sub('[(),]', '', str(unpack("3Q", lines[i:i+24]))))
+    list.append(sub('[(),]', '', str(unpack("3Q", lines[i:i+24]))))
 
-with open("lines_dat.txt", "w") as f:
+with open(argv[1] + ".txt", "w") as f:
     j = 0
     for i in list:
-        # print(str.encode(i))
         f.write(i+'\n')
