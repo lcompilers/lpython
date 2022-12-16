90c6c1e0355533bf5d87ac5cba88148e515dbb2c
Ubaid <shaikhubaid769@gmail.com>
2022-12-16 19:33:55 +0530

    X86Assembler: Add TODO comment in asm_mov_r64_label()

--- src/libasr/codegen/x86_assembler.h
+++ src/libasr/codegen/x86_assembler.h
@@ -711,6 +711,7 @@ public:
         X86Reg r32 = X86Reg(r64 & 7);
         m_code.push_back(m_al, rex(1, 0, 0, r64 >> 3));
         m_code.push_back(m_al, 0xb8 + r32);
+        // TODO: reference_symbol().value should return 64-bit value
         uint64_t imm64 = reference_symbol(label).value;
         push_back_uint64(m_code, m_al, imm64);
         EMIT("mov " + r2s(r64) + ", " + label);
