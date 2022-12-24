4e84b631c0f93ac5aa32c14a0a00b967aa316a44
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-12-24 09:09:42 +0530

    Add tests

---
+++ integration_tests/test_global_decl.py
@@ -0,0 +1,17 @@
+from ltypes import i32
+from numpy import empty, int32
+
+# issue-1368
+
+SIZE: i32 = i32(3)
+
+def main() -> None:
+    xs: i32[SIZE] = empty(SIZE, dtype=int32)
+    i: i32
+    for i in range(SIZE):
+        xs[i] = i+1
+
+    for i in range(SIZE):
+        assert xs[i] == i+1
+
+main()
