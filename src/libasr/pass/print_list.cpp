0fb001f13d574b8f87c5f6ebefd8274afac91c15
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-01-16 16:43:13 +0530

    Add printing tuple support

--- src/libasr/pass/print_list.cpp
+++ src/libasr/pass/print_list.cpp
@@ -163,14 +163,17 @@ class PrintListVisitor
             loop_head.m_increment =
                 ASRUtils::EXPR(ASR::make_IntegerConstant_t(
                     al, loc, 1, int_type));
-
-            if (!ASR::is_a<ASR::List_t>(*listC->m_type)) {
-                loop_body.reserve(al, 2);
-                loop_body.push_back(al, print_item);
-            } else {
+            if (ASR::is_a<ASR::List_t>(*listC->m_type)){
                 print_list_helper(list_item, nullptr, empty_str, loc);
                 loop_body.from_pointer_n_copy(al, print_pass_result_tmp.p, print_pass_result_tmp.size());
                 print_pass_result_tmp.n = 0;
+            } else if (ASR::is_a<ASR::Tuple_t>(*listC->m_type)) {
+                print_tuple_helper(list_item, nullptr, empty_str, loc);
+                loop_body.from_pointer_n_copy(al, print_pass_result_tmp.p, print_pass_result_tmp.size());
+                print_pass_result_tmp.n = 0;
+            } else {
+                loop_body.reserve(al, 2);
+                loop_body.push_back(al, print_item);
             }
             loop_body.push_back(al, if_cond);
         }
@@ -183,6 +186,99 @@ class PrintListVisitor
             print_pass_result_tmp.push_back(al, loop);
             print_pass_result_tmp.push_back(al, print_close_bracket);
         }
+    }
+
+    void print_tuple_helper(ASR::expr_t *tup_expr, ASR::expr_t *sep_expr,
+                ASR::expr_t *end_expr, const Location &loc) {
+        ASR::Tuple_t *tup =
+                ASR::down_cast<ASR::Tuple_t>(ASRUtils::expr_type(tup_expr));
+        ASR::ttype_t *int_type = ASRUtils::TYPE(
+                ASR::make_Integer_t(al, loc, 4, nullptr, 0));
+        ASR::ttype_t *str_type_len_0 = ASRUtils::TYPE(ASR::make_Character_t(
+            al, loc, 1, 0, nullptr, nullptr, 0));
+        ASR::ttype_t *str_type_len_1 = ASRUtils::TYPE(ASR::make_Character_t(
+            al, loc, 1, 1, nullptr, nullptr, 0));
+        ASR::ttype_t *str_type_len_2 = ASRUtils::TYPE(ASR::make_Character_t(
+            al, loc, 1, 2, nullptr, nullptr, 0));
+        ASR::expr_t *comma_space =
+            ASRUtils::EXPR(ASR::make_StringConstant_t(
+                al, loc, s2c(al, ", "), str_type_len_2));
+        ASR::expr_t *single_quote =
+            ASRUtils::EXPR(ASR::make_StringConstant_t(
+                al, loc, s2c(al, "'"), str_type_len_1));
+        ASR::expr_t *empty_str = ASRUtils::EXPR(ASR::make_StringConstant_t(
+            al, loc, s2c(al, ""), str_type_len_0));
+        ASR::expr_t *open_bracket =
+            ASRUtils::EXPR(ASR::make_StringConstant_t(
+                al, loc, s2c(al, "("), str_type_len_1));
+        ASR::expr_t *close_bracket =
+            ASRUtils::EXPR(ASR::make_StringConstant_t(
+                al, loc, s2c(al, ")"), str_type_len_1));
+
+        std::string tup_iter_var_name;
+        ASR::expr_t *tup_iter_var, *tup_item;
+        Vec<ASR::expr_t *> v1, v3, v4;
+        v1.reserve(al, 1);
+        v3.reserve(al, 1);
+        v4.reserve(al, 1);
+        v1.push_back(al, open_bracket);
+        v3.push_back(al, close_bracket);
+        v4.push_back(al, comma_space);
+
+        Vec<ASR::stmt_t*> tmp_vec;
+        tmp_vec.reserve(al, 3);
+        ASR::stmt_t *print_open_bracket = ASRUtils::STMT(
+            ASR::make_Print_t(al, loc, nullptr, v1.p, v1.size(),
+                                nullptr, empty_str));
+        ASR::stmt_t *print_comma_space = ASRUtils::STMT(
+            ASR::make_Print_t(al, loc, nullptr, v4.p, v4.size(),
+                                empty_str, empty_str));
+        ASR::stmt_t *print_close_bracket = ASRUtils::STMT(
+            ASR::make_Print_t(al, loc, nullptr, v3.p, v3.size(),
+                                sep_expr, end_expr));
+
+        tmp_vec.push_back(al, print_open_bracket);
+        for (size_t i=0; i<tup->n_type; i++) {
+            tup_iter_var = ASRUtils::EXPR(
+                ASR::make_IntegerConstant_t(al, loc, i, int_type));
+            tup_item = ASRUtils::EXPR(ASR::make_TupleItem_t(al, loc, tup_expr,
+                                    tup_iter_var, tup->m_type[i], nullptr));
+            if (ASR::is_a<ASR::List_t>(*tup->m_type[i])) {
+                print_pass_result_tmp.n = 0;
+                print_list_helper(tup_item, nullptr, empty_str, loc);
+                for (size_t j=0; j<print_pass_result_tmp.n; j++) {
+                    tmp_vec.push_back(al, print_pass_result_tmp[j]);
+                }
+                print_pass_result_tmp.n = 0;
+            } else if (ASR::is_a<ASR::Tuple_t>(*tup->m_type[i])) {
+                print_pass_result_tmp.n = 0;
+                print_tuple_helper(tup_item, nullptr, empty_str, loc);
+                for (size_t j=0; j<print_pass_result_tmp.n; j++) {
+                    tmp_vec.push_back(al, print_pass_result_tmp[j]);
+                }
+                print_pass_result_tmp.n = 0;
+            } else {
+                Vec<ASR::expr_t *> v2;
+                if (ASR::is_a<ASR::Character_t>(*tup->m_type[i])) {
+                    v2.reserve(al, 3);
+                    v2.push_back(al, single_quote);
+                    v2.push_back(al, tup_item);
+                    v2.push_back(al, single_quote);
+                } else {
+                    v2.reserve(al, 1);
+                    v2.push_back(al, tup_item);
+                }
+                ASR::stmt_t *print_item = ASRUtils::STMT(
+                    ASR::make_Print_t(al, loc, nullptr, v2.p, v2.size(),
+                                empty_str, empty_str));
+                tmp_vec.push_back(al, print_item);
+            }
+            if (i != tup->n_type - 1) {
+                tmp_vec.push_back(al, print_comma_space);
+            }
+        }
+        tmp_vec.push_back(al, print_close_bracket);
+        print_pass_result_tmp.from_pointer_n_copy(al, tmp_vec.p, tmp_vec.size());
     }
 
     void visit_Print(const ASR::Print_t &x) {
