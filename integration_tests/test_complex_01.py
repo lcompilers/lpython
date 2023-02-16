d22dc948a4019e4fd67b957e7b458ef9bb7b620a
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-02-17 04:50:23 +0530

    TEST: Update test_complex_01 to work with WAT

--- integration_tests/test_complex_01.py
+++ integration_tests/test_complex_01.py
@@ -9,35 +9,35 @@ def test_real_imag():
     eps = 1e-12
     a = x.real
     b = x.imag
-    assert abs(a - 2.0) < eps
-    assert abs(b - 3.0) < eps
+    assert abs(a - 2.0) <= eps
+    assert abs(b - 3.0) <= eps
 
 def test_complex():
     x: c64
     x = complex(4.5, 6.7)
     eps: f64
     eps = 1e-12
-    assert abs(x.real - 4.5) < eps
-    assert abs(x.imag - 6.7) < eps
+    assert abs(x.real - 4.5) <= eps
+    assert abs(x.imag - 6.7) <= eps
 
     x = complex(-4, 2)
-    assert abs(x.real - (-4.0)) < eps
-    assert abs(x.imag - 2.0) < eps
+    assert abs(x.real - (-4.0)) <= eps
+    assert abs(x.imag - 2.0) <= eps
 
     x = complex(4, 7.89)
-    assert abs(x.real - 4.0) < eps
-    assert abs(x.imag - 7.89) < eps
+    assert abs(x.real - 4.0) <= eps
+    assert abs(x.imag - 7.89) <= eps
 
     x = complex(5.6, 0)
-    assert abs(x.real - 5.6) < eps
-    assert abs(x.imag - 0.0) < eps
+    assert abs(x.real - 5.6) <= eps
+    assert abs(x.imag - 0.0) <= eps
 
     a: f64
     a = 534.6
     x = complex(a, -a) # (f64, f64)
 
-    assert abs(x.real - 534.60000000000002274) < eps
-    assert abs(x.imag - (-534.60000000000002274)) < eps
+    assert abs(x.real - 534.60000000000002274) <= eps
+    assert abs(x.imag - (-534.60000000000002274)) <= eps
 
     a2: f32
     a2 = -f32(423.5430806348152437)
@@ -46,7 +46,7 @@ def test_complex():
     x2: c32
     x2 = c32(complex(a2, a3)) # (f32, f32)
 
-    assert f64(abs(x2.imag - f32(34.5))) < eps
+    assert f64(abs(x2.imag - f32(34.5))) <= eps
 
     i1: i32
     i1 = -5
@@ -65,21 +65,21 @@ def test_complex_unary_minus():
     c = c32(complex(3, 4.5))
     _c: c32
     _c = -c
-    assert abs(f64(_c.real) - (-3.0)) < 1e-12
-    assert abs(f64(_c.imag) - (-4.5)) < 1e-12
+    assert abs(f64(_c.real) - (-3.0)) <= 1e-12
+    assert abs(f64(_c.imag) - (-4.5)) <= 1e-12
     _c = c32(complex(5, -78))
     _c = -_c
-    assert abs(f64(_c.real) - (-5.0)) < 1e-12
-    assert abs(f64(_c.imag) - 78.0) < 1e-12
+    assert abs(f64(_c.real) - (-5.0)) <= 1e-12
+    assert abs(f64(_c.imag) - 78.0) <= 1e-12
     c2: c64
     c2 = complex(-4.5, -7.8)
     c2 = -c2
-    assert abs(c2.real - 4.5) < 1e-12
-    assert abs(c2.imag - 7.8) < 1e-12
+    assert abs(c2.real - 4.5) <= 1e-12
+    assert abs(c2.imag - 7.8) <= 1e-12
     c2 = c64(3) + 4j
     c2 = -c2
-    assert abs(c2.real - (-3.0)) < 1e-12
-    assert abs(c2.imag - (-4.0)) < 1e-12
+    assert abs(c2.real - (-3.0)) <= 1e-12
+    assert abs(c2.imag - (-4.0)) <= 1e-12
 
 def test_complex_not():
     c: c32
