e48516c05faff076330a02ad71adef71bf87add6
Manali Kundu <90084877+Manali0806@users.noreply.github.com>
2023-03-25 10:35:58 +0530

    Fix undeclared variable error in array_01.py Add static type to variable 'foo' in array_01.py

--- integration_tests/array_01.py
+++ integration_tests/array_01.py
@@ -4,7 +4,7 @@ from numpy import empty
 def main0():
     Nx: i32 = 600; Ny: i32 = 450
     arr: i32[450, 600] = empty([Ny, Nx])
-    i: i32
+    i: i32 = 42
     j: i32
     for i in range(Ny):
         for j in range(Nx):
