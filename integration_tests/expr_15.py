fa7fee09f57fdef7f92baf4aee3bbe4c5f91851a
faze-geek <bhat.1@iitj.ac.in>
2023-03-25 20:31:14 +0530

    Fix % for i16 inputs

--- integration_tests/expr_15.py
+++ integration_tests/expr_15.py
@@ -13,7 +13,21 @@ def test_issue_1586():
     b4 = i8(123)
     assert b4//a4 == i8(12)
 
+def test_issue_1619():
+    a: i16
+    b: i16
+    a = i16(10)
+    b = i16(12345)
+    assert b%a == i16(5)
+
+    c: i8
+    d: i8
+    c = i8(10)
+    d = i8(123)
+    assert d%c == i8(3)
+
 def check():
     test_issue_1586()
+    test_issue_1619()
 
 check()
