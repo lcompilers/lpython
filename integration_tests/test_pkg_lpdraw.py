88854688672c4320f07e5aa5936379c74ad3c338
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-03-29 00:37:22 +0530

    TEST: Update package to work with wasm

--- integration_tests/test_pkg_lpdraw.py
+++ integration_tests/test_pkg_lpdraw.py
@@ -1,11 +1,18 @@
 from lpdraw import Line, Circle, Clear, Display, DisplayTerminal
 
+from lpython import i32
+from numpy import empty, int32
+
 def main():
-    Clear()
-    Line(2, 4, 99, 11)
-    Line(0, 39, 49, 0)
-    Circle(52, 20, 6.0)
-    Display()
-    DisplayTerminal()
+    Width: i32 = 100 # x-axis limits [0, 99]
+    Height: i32 = 40 # y-axis limits [0, 39]
+    Screen: i32[40, 100] = empty((40, 100), dtype=int32)
+
+    Clear(Height, Width, Screen)
+    Line(Height, Width, Screen, 2, 4, 99, 11)
+    Line(Height, Width, Screen, 0, 39, 49, 0)
+    Circle(Height, Width, Screen, 52, 20, 6.0)
+    Display(Height, Width, Screen)
+    DisplayTerminal(Height, Width, Screen)
 
 main()
