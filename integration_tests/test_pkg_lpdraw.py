17833f6efe20dd416e6bb015762486bb889a2885
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-03-30 10:24:35 +0530

    TEST: Add assertion for robustness

--- integration_tests/test_pkg_lpdraw.py
+++ integration_tests/test_pkg_lpdraw.py
@@ -1,7 +1,20 @@
 from lpdraw import Line, Circle, Clear, Display, DisplayTerminal
 
-from lpython import i32
+from lpython import i32, TypeVar
 from numpy import empty, int32
+
+H = TypeVar("H")
+W = TypeVar("W")
+
+def test_screen(H: i32, W: i32, Screen: i32[H, W]):
+    i: i32
+    j: i32
+    cnt: i32 = 0
+    for i in range(H):
+        for j in range(W):
+            cnt += (Screen[i, j] - 256)
+
+    assert cnt == -979375
 
 def main():
     Width: i32 = 100 # x-axis limits [0, 99]
@@ -14,5 +27,7 @@ def main():
     Circle(Height, Width, Screen, 52, 20, 6.0)
     Display(Height, Width, Screen)
     DisplayTerminal(Height, Width, Screen)
+
+    test_screen(Height, Width, Screen)
 
 main()
