3e47fbc1164be32c13f8ec261cde908a47266b4c
Harsh Singh Jadon <intelligent24harsh@gmail.com>
2023-03-31 22:40:10 +0530

    Support for Global Dictionaries

---
+++ integration_tests/global_syms_05.py
@@ -0,0 +1,27 @@
+from lpython import i32, str
+
+# GLOBAL Dictionary
+x: dict[i32, i32]
+x = {0 : 0,  1: 1000, 2: 2000, 3 : 3000}
+
+x[4] = 4000
+assert len(x) == 5
+x[5] = 5000
+assert x[2] == 2000
+i: i32
+for i in range(len(x)):
+    assert x[i] == i * 1000
+
+# Copy of Dictionary
+tmp: dict[i32, i32]
+tmp = x
+tmp[6] = 6000
+assert len(tmp) == 7
+assert tmp[6] == 6000
+assert tmp[1] == 1000
+
+# Dictionary with (KEY, VALUE) as String
+y : dict[str, str]
+y = {"fruit": "apple", "animal": "lion"}
+assert y["animal"] == "lion"
+assert y["fruit"] == "apple"
