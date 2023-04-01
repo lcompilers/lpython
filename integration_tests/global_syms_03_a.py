7befb71bb0168ffb01afa90a7aff22ffe422d6c5
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-04-01 12:36:47 +0530

    Add tests and register in CMakeLists

--- integration_tests/global_syms_03_a.py
+++ integration_tests/global_syms_03_a.py
@@ -1,7 +1,13 @@
-from lpython import i32
+from lpython import i32, f64
+
+print("Imported from global_syms_03_a")
 
 l_1: list[str] = ['Monday', 'Tuesday', 'Wednesday']
+l_1.append('Thursday')
 
 def populate_lists() -> list[i32]:
     return [10, -20]
 l_2: list[i32] = populate_lists()
+
+l_3: list[f64]
+l_3 = [1.0, 2.0, 3.0]
