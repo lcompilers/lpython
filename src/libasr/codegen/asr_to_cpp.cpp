b60c3e0100cbde1f0f143faeb386024966255fc0
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-04-04 16:12:45 +0530

    Use IntrinsicFunction pass in ASR->CPP

--- src/libasr/codegen/asr_to_cpp.cpp
+++ src/libasr/codegen/asr_to_cpp.cpp
@@ -10,6 +10,7 @@
 #include <libasr/asr_utils.h>
 #include <libasr/string_utils.h>
 #include <libasr/pass/unused_functions.h>
+#include <libasr/pass/intrinsic_function.h>
 
 
 namespace LCompilers {
@@ -745,6 +746,7 @@ Result<std::string> asr_to_cpp(Allocator &al, ASR::TranslationUnit_t &asr,
     LCompilers::PassOptions pass_options;
     pass_options.always_run = true;
     pass_unused_functions(al, asr, pass_options);
+    pass_replace_intrinsic_function(al, asr, pass_options);
     ASRToCPPVisitor v(diagnostics, co, default_lower_bound);
     try {
         v.visit_asr((ASR::asr_t &)asr);
