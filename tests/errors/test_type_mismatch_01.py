8808708bb5f6f54383738736c40c8ac7af04b2cb
Sarthak Gupta <62467938+gptsarthak@users.noreply.github.com>
2023-04-10 22:36:44 +0530

    Update test_type_mismatch_01.py
    Co-authored-by: Gagandeep Singh <gdp.1807@gmail.com>

--- tests/errors/test_type_mismatch_01.py
+++ tests/errors/test_type_mismatch_01.py
@@ -1,14 +1,6 @@
-from lpython import i32, i64, f64
+from lpython import i32, f64
 
 def test():
     a : i32 = 10
     b : f64 = 20.0
-    a *= b
     a /= b
-
-    e : i64 = i64(15)
-    f : bool = True
-    e *= f
-    e /= f
-
-test()
