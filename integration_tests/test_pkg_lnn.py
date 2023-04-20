a507c7fd52a4a83ba62ed770ffe104b82137a294
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-04-20 10:10:41 +0530

    TEST: Add assert and finalize test_pkg_lnn.py

--- integration_tests/test_pkg_lnn.py
+++ integration_tests/test_pkg_lnn.py
@@ -1,17 +1,20 @@
-from lnn.perceptron import init_perceptron, print_perceptron, normalize_input_vectors
-
+from lnn.perceptron import init_perceptron, print_perceptron, normalize_input_vectors, Perceptron, train_dataset
 from lpython import i32, f64
 
-
-
 def main0():
-    print("hi")
-    # p: Perceptron
-    # init_perceptron(p, 5, 0.05, 100000, 90.0)
-    # print_perceptron(p)
-    input_vectors: list[list[f64]] = [[-15.0, -10.0], [-10.0, 10.0], [15.0, -10.0], [10.0, 10.0]]
+    p: Perceptron
+    init_perceptron(p, 2, 0.05, 10000, 90.0)
+    print_perceptron(p)
+    print("=================================")
+
+    input_vectors: list[list[f64]] = [[-1.0, -1.0], [-1.0, 1.0], [1.0, -1.0], [1.0, 1.0]]
     outputs: list[i32] = [1, 1, 1, -1]
 
     normalize_input_vectors(input_vectors)
-    print(input_vectors)
+    train_dataset(p, input_vectors, outputs)
+    print_perceptron(p)
+
+    assert p.cur_accuracy > 50.0
+    assert p.epochs_cnt > 1
+
 main0()
