6b4bf077d5dc313f9c622645978f8c38e405c690
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-04-20 12:49:31 +0530

    TEST: Add another test

--- integration_tests/test_pkg_lnn.py
+++ integration_tests/test_pkg_lnn.py
@@ -67,4 +67,23 @@ def main0():
 
     plot_graph(p, input_vectors, outputs)
 
+def main1():
+    p: Perceptron = Perceptron(0, [0.0], 0.0, 0, 0.0, 0.0, 0)
+    init_perceptron(p, 2, 0.05, 10000, 90.0)
+    print_perceptron(p)
+    print("=================================")
+
+    input_vectors: list[list[f64]] = [[-1.0, -1.0], [-1.0, 1.0], [1.0, -1.0], [1.0, 1.0], [1.5, 1.0]]
+    outputs: list[i32] = [1, 1, -1, 1, -1]
+
+    normalize_input_vectors(input_vectors)
+    train_dataset(p, input_vectors, outputs)
+    print_perceptron(p)
+
+    assert p.cur_accuracy > 50.0
+    assert p.epochs_cnt > 1
+
+    plot_graph(p, input_vectors, outputs)
+
 main0()
+main1()
