cbd49a4e094d02a38abd9e7354bb213d4f1fa6bb
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-04-23 12:52:57 +0530

    TEST: Return integer and add assert()

--- integration_tests/test_import/sys.py
+++ integration_tests/test_import/sys.py
@@ -1,2 +1,5 @@
-def hi_from_user_sys():
+from lpython import i32
+
+def hi_from_user_sys() -> i32:
     print("hi from user sys!")
+    return -5
--- integration_tests/test_import_05.py
+++ integration_tests/test_import_05.py
@@ -1,3 +1,3 @@
 from test_import.sys import hi_from_user_sys
 
-hi_from_user_sys()
+assert hi_from_user_sys() == -5
