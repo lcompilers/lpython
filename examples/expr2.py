05e6c56ae5e302b23d94ea6f9214ce1f16b31bd0
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-04-25 10:47:47 +0530

    tmp

--- examples/expr2.py
+++ examples/expr2.py
@@ -1,10 +1,15 @@
+from lpython import i32, dataclass
+
+@dataclass
+class A:
+    x: list[i32]
+
 def main0():
-    x: i32
-    x = (2+3)*5
-    print(x)
+    a: A = A([1, 2, 3])
+    a.x.append(4)
+    print(a.x)
+    p: str
+    p = "abc"
+    p.upper()
 
 main0()
-
-# Not implemented yet in LPython:
-#if __name__ == "__main__":
-#    main()
