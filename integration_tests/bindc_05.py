2fd2ba2a62502a22f0d5141dd259b2af0d156b39
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-04-26 19:25:32 +0530

    Enable tests

--- integration_tests/bindc_05.py
+++ integration_tests/bindc_05.py
@@ -4,11 +4,11 @@ from lpython import CPtr, empty_c_void_p, i32, f32, dataclass, Pointer, ccall, p
 class Void:
     data: CPtr
 
-@ccall
+@ccall(header="bindc_05b.h")
 def trunc_custom(value: Pointer[CPtr]) -> CPtr:
     pass
 
-@ccall
+@ccall(header="bindc_05b.h")
 def print_value(value: CPtr):
     pass
 
--- integration_tests/bindc_05b.h
+++ integration_tests/bindc_05b.h
@@ -1,2 +1,7 @@
+#ifndef BINDC_05BH
+#define BINDC_05BH
+
 void* trunc_custom(void** value);
 void print_value(float* value);
+
+#endif // BINDC_05BH
