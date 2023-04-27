dac940b2e70c976372e28388123620c27e950ffb
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-04-28 04:39:01 +0530

    ASR: Support modules in pickle_json()

--- src/lpython/pickle.h
+++ src/lpython/pickle.h
@@ -13,15 +13,15 @@ namespace LCompilers::LPython {
             bool show_intrinsic_modules=false);
     std::string pickle(ASR::TranslationUnit_t &asr, bool colors=false,
             bool indent=false, bool show_intrinsic_modules=false);
-    
+
     // Print the tree structure
 	std::string pickle_tree_python(AST::ast_t &ast, bool colors=true);
     std::string pickle_tree(ASR::asr_t &asr, bool colors, bool show_intrinsic_modules);
     std::string pickle_tree(ASR::TranslationUnit_t &asr, bool colors, bool show_intrinsic_modules);
 
     std::string pickle_json(AST::ast_t &ast, LocationManager &lm);
-    std::string pickle_json(ASR::asr_t &asr, LocationManager &lm);
-    std::string pickle_json(ASR::TranslationUnit_t &asr, LocationManager &lm);
+    std::string pickle_json(ASR::asr_t &asr, LocationManager &lm, bool show_intrinsic_modules);
+    std::string pickle_json(ASR::TranslationUnit_t &asr, LocationManager &lm, bool show_intrinsic_modules);
 
 }
 
