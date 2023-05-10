a5eda0ccd8e17fdea3d69622629513934c66529f
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-05-10 10:01:39 +0530

    Add CPython test and register in CMakeLists

---
+++ integration_tests/test_jit_01.py
@@ -0,0 +1,16 @@
+from numpy import array
+from lpython import i32, f64, jit
+
+@jit
+def fast_sum(n: i32, x: f64[:]) -> f64:
+    s: f64 = 0.0
+    i: i32
+    for i in range(n):
+        s += x[i]
+    return s
+
+def test():
+    x: f64[3] = array([1.0, 2.0, 3.0])
+    assert fast_sum(3, x) == 6.0
+
+test()
