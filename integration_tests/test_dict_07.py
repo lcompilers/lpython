f403c41e6089ca27fdb27e66282a70ebf1b800ad
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-05-19 09:42:32 +0530

    Enable and update tests

--- integration_tests/test_dict_07.py
+++ integration_tests/test_dict_07.py
@@ -1,13 +1,16 @@
+from lpython import i32
+
+
 def fill_smalltocapital() -> dict[str, str]:
-    return {'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D','e': 'E', 
-            'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I','j': 'J', 
-            'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N','o': 'O', 
-            'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S','t': 'T', 
-            'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X','y': 'Y', 
+    return {'a': 'A', 'b': 'B', 'c': 'C', 'd': 'D','e': 'E',
+            'f': 'F', 'g': 'G', 'h': 'H', 'i': 'I','j': 'J',
+            'k': 'K', 'l': 'L', 'm': 'M', 'n': 'N','o': 'O',
+            'p': 'P', 'q': 'Q', 'r': 'R', 's': 'S','t': 'T',
+            'u': 'U', 'v': 'V', 'w': 'W', 'x': 'X','y': 'Y',
             'z': 'Z'}
 
 def test_dict():
-    i : i32
+    i: i32
     smalltocaps: dict[str, str]
     smalltocaps = fill_smalltocapital()
 
