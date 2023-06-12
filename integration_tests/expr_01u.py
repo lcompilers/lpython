18625f62e261a0c3f18a4885434cd7baa1dd50a8
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-06-12 11:47:29 +0530

    Add tests

--- integration_tests/expr_01u.py
+++ integration_tests/expr_01u.py
@@ -1,4 +1,4 @@
-from lpython import inline, u32
+from lpython import inline, u32, u64, i64
 
 @inline
 def uadd(x: u32, y: u32) -> u32:
@@ -19,6 +19,14 @@ def main1():
 
     z = uand_op(x, y)
     assert z == u32(16)
+
+    # test issue 1867
+    cycles_count: u64
+    end_cycle: i64 = i64(100)
+    start_cycle: i64 = i64(4)
+    num_iters: i64 = i64(4)
+    cycles_count = u64((end_cycle - start_cycle) / num_iters)
+    assert cycles_count == u64(24)
 
 
 main1()
