381165951b992ee0b140375c6348590b405c21a2
Ondřej Čertík <ondrej@certik.us>
2023-06-19 19:19:29 -0600

    run_tests: Show the default backends

--- integration_tests/run_tests.py
+++ integration_tests/run_tests.py
@@ -41,7 +41,7 @@ def get_args():
     parser.add_argument("-j", "-n", "--no_of_threads", type=int,
                 help="Parallel testing on given number of threads")
     parser.add_argument("-b", "--backends", nargs="*", default=["llvm", "cpython"],
-                type=str, help="Test the requested backends (%s)" % \
+                type=str, help="Test the requested backends (%s), default: llvm, cpython" % \
                         ", ".join(SUPPORTED_BACKENDS))
     parser.add_argument("-nf", "--no_fast", action='store_true',
                 help="Disable --fast testing of integration tests")
