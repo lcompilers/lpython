73e24ba637e7135a2b98efe620f2c0019e36ca28
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2023-06-23 13:34:22 +0530

    Add a test

--- integration_tests/lpython_decorator_02.py
+++ integration_tests/lpython_decorator_02.py
@@ -1,19 +1,34 @@
 from numpy import array
-from lpython import i32, f64, lpython, TypeVar
+from lpython import i32, i64, f64, lpython, TypeVar
 
 n = TypeVar("n")
 
 @lpython
-def multiply(n: i32, x: f64[:]) -> f64[n]:
+def multiply_01(n: i32, x: f64[:]) -> f64[n]:
     i: i32
     for i in range(n):
         x[i] *= 5.0
     return x
 
-def test():
-    size: i32 = 5
-    x: f64[5] = array([11.0, 12.0, 13.0, 14.0, 15.0])
-    y: f64[5] = multiply(size, x)
+@lpython
+def multiply_02(n: i32, x: i64[:], y: i64[:]) -> i64[n]:
+    z: i64[n]; i: i32
+    for i in range(n):
+        z[i] = x[i] * y[i]
+    return z
+
+
+def test_01():
+    size = 5
+    x = array([11.0, 12.0, 13.0, 14.0, 15.0])
+    y = multiply_01(size, x)
     assert y[2] == 65.
 
-test()
+    size = 3
+    x = array([11, 12, 13])
+    y = array([14, 15, 16])
+    z = multiply_02(size, x, y)
+    for i in range(size):
+        assert z[i] == x[i] * y[i]
+
+test_01()
