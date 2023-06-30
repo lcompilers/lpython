6e0059f5b60219ac1a3d36310d23c9203f536d91
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-06-30 11:57:07 +0530

    Fix wrong test

--- integration_tests/structs_10.py
+++ integration_tests/structs_10.py
@@ -3,16 +3,16 @@ from numpy import empty, float64
 
 @dataclass
 class Mat:
-    mat: f64[2, 2]
+    mat: f64[2, 2] = empty((2, 2), dtype=float64)
 
 @dataclass
 class Vec:
-    vec: f64[2]
+    vec: f64[2] = empty(2, dtype=float64)
 
 @dataclass
 class MatVec:
-    mat: Mat = Mat([f64(0.0), f64(0.0)])
-    vec: Vec = Vec([f64(0.0), f64(0.0)])
+    mat: Mat = Mat()
+    vec: Vec = Vec()
 
 def rotate(mat_vec: MatVec) -> f64[2]:
     rotated_vec: f64[2] = empty(2, dtype=float64)
