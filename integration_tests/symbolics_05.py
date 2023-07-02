315e020555ede176f9a25c56d5d2c39236d10688
anutosh491 <andersonbhat491@gmail.com>
2023-07-02 13:23:02 +0530

    Refactored test file

--- integration_tests/symbolics_05.py
+++ integration_tests/symbolics_05.py
@@ -5,10 +5,10 @@ def test_operations():
     x: S = Symbol('x')
     y: S = Symbol('y')
     z: S = Symbol('z')
-    
-    # test expand
     a: S = (x + y)**S(2)
     b: S = (x + y + z)**S(3)
+
+    # test expand
     assert(a.expand() == S(2)*x*y + x**S(2) + y**S(2))
     assert(expand(b) == S(3)*x*y**S(2) + S(3)*x*z**S(2) + S(3)*x**S(2)*y + S(3)*x**S(2)*z +\
     S(3)*y*z**S(2) + S(3)*y**S(2)*z + S(6)*x*y*z + x**S(3) + y**S(3) + z**S(3))
@@ -16,11 +16,9 @@ def test_operations():
     print(expand(b))
 
     # test diff
-    c: S = (x + y)**S(2)
-    d: S = (x + y + z)**S(3)
-    assert(c.diff(x) == S(2)*(x + y))
-    assert(diff(d, x) == S(3)*(x + y + z)**S(2))
-    print(c.diff(x))
-    print(diff(d, x))
+    assert(a.diff(x) == S(2)*(x + y))
+    assert(diff(b, x) == S(3)*(x + y + z)**S(2))
+    print(a.diff(x))
+    print(diff(b, x))
 
 test_operations()
