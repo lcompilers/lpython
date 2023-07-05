836ac01a2d00d1c05d40e45b837de84c6cfd8551
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-07-05 23:39:11 +0530

    TEST: Cast for unsigned int with neg value

--- integration_tests/cast_02.py
+++ integration_tests/cast_02.py
@@ -34,9 +34,46 @@ def test_02():
     print(w)
     assert w == u32(11)
 
+def test_03():
+    x : u32 = u32(-10)
+    print(x)
+    assert x == u32(4294967286)
+
+    y: u16 = u16(x)
+    print(y)
+    assert y == u16(65526)
+
+    z: u64 = u64(y)
+    print(z)
+    assert z == u64(65526)
+
+    w: u8 = u8(z)
+    print(w)
+    assert w == u8(246)
+
+def test_04():
+    x : u64 = u64(-11)
+    print(x)
+    # TODO: We are unable to store the following u64 in AST/R
+    # assert x == u64(18446744073709551605)
+
+    y: u8 = u8(x)
+    print(y)
+    assert y == u8(245)
+
+    z: u16 = u16(y)
+    print(z)
+    assert z == u16(245)
+
+    w: u32 = u32(z)
+    print(w)
+    assert w == u32(245)
+
 
 def main0():
     test_01()
     test_02()
+    test_03()
+    test_04()
 
 main0()
