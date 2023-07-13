b63e46ee26e5088b2c8e68593298a7936e4f0c57
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-07-13 12:17:34 +0530

    Add and update references

--- run_tests.py
+++ run_tests.py
@@ -34,7 +34,8 @@ def single_test(test, verbose, no_llvm, skip_run_with_dbg, update_reference,
     pass_ = test.get("pass", None)
     optimization_passes = ["flip_sign", "div_to_mul", "fma", "sign_from_value",
                            "inline_function_calls", "loop_unroll",
-                           "dead_code_removal", "loop_vectorise", "print_list_tuple"]
+                           "dead_code_removal", "loop_vectorise", "print_list_tuple",
+                           "class_constructor"]
 
     if pass_ and (pass_ not in ["do_loops", "global_stmts"] and
                   pass_ not in optimization_passes):
