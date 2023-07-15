67c1c30688378ddf6892e758967ddf63743c682c
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-07-15 18:58:59 +0530

    Do not allow redeclaration in global scope

--- src/lpython/semantics/python_ast_to_asr.cpp
+++ src/lpython/semantics/python_ast_to_asr.cpp
@@ -4921,17 +4921,13 @@ public:
 
         if (current_scope->get_scope().find(var_name) !=
                 current_scope->get_scope().end()) {
-            if (current_scope->parent != nullptr) {
-                // Re-declaring a global scope variable is allowed,
-                // otherwise raise an error
-                ASR::symbol_t *orig_decl = current_scope->get_symbol(var_name);
-                throw SemanticError(diag::Diagnostic(
-                    "Symbol is already declared in the same scope",
-                    diag::Level::Error, diag::Stage::Semantic, {
-                        diag::Label("original declaration", {orig_decl->base.loc}, false),
-                        diag::Label("redeclaration", {x.base.base.loc}),
-                    }));
-            }
+            ASR::symbol_t *orig_decl = current_scope->get_symbol(var_name);
+            throw SemanticError(diag::Diagnostic(
+                "Symbol is already declared in the same scope",
+                diag::Level::Error, diag::Stage::Semantic, {
+                    diag::Label("original declaration", {orig_decl->base.loc}, false),
+                    diag::Label("redeclaration", {x.base.base.loc}),
+                }));
         }
         ASR::expr_t *init_expr = nullptr;
         visit_AnnAssignUtil(x, var_name, init_expr);
