1b036e95bba257cab9b154c1bdfa005558b5c81c
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2023-07-27 11:57:53 +0530

    Add an issue test

---
+++ integration_tests/structs_35.py
@@ -0,0 +1,34 @@
+from lpython import (i8, i32, i64, f32, f64,
+                     dataclass
+                     )
+from numpy import (empty,
+                   int8,
+                   )
+
+# test issue 2131
+
+r : i8 = i8(240)
+
+@dataclass
+class Foo:
+    a : i8[4] = empty(4, dtype=int8)
+    dim : i32 = 4
+
+def trinary_majority(x : Foo, y : Foo, z : Foo) -> Foo:
+    foo : Foo = Foo()
+
+    assert foo.dim == x.dim == y.dim == z.dim
+
+    return foo
+
+
+t1 : Foo = Foo()
+t1.a = empty(4, dtype=int8)
+
+t2 : Foo = Foo()
+t2.a = empty(4, dtype=int8)
+
+t3 : Foo = Foo()
+t3.a = empty(4, dtype=int8)
+
+r1 : Foo = trinary_majority(t1, t2, t3)
