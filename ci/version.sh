5c52b5a36470602c7fcf9b44fcab4ead904ccf4f
OmarMesqq <omarmsqt@gmail.com>
2023-09-01 20:34:53 -0300

    refactor: :recycle: makes errors visible and adds default tag if there are erros in git describe

--- ci/version.sh
+++ ci/version.sh
@@ -9,10 +9,20 @@
 # Each tag starts with "v", so we strip the "v", and the final version becomes:
 #
 #     0.6.0-37-g3878937f-dirty
+#
 
-set -ex
+git_describe_output=$(git describe --tags --dirty 2>&1)
+exit_code=$?
 
-version=$(git describe --tags --dirty 2>/dev/null || echo "0.0.0-0")
-version="${version:1}"
-echo "$version" > version
-
+set -ex 
+if [ $exit_code -ne 0 ]; then
+  echo "There were errors while getting the tags. Defaulting to 0.0.0-0"
+  version="0.0.0-0"
+  echo $version > version
+  exit 1
+else
+  echo "Tags captured successfully."
+  version="${git_describe_output:1}"
+  echo $version > version
+  exit 0
+fi
