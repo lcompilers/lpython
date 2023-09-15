825575a001e5dda60872b0ea73efc10470f7b3b8
Khushi Agrawal <khushiagrawal411@gmail.com>
2023-09-16 01:26:16 +0530

    [small] tidy up warnings (#2323)

--- src/libasr/runtime/lfortran_intrinsics.c
+++ src/libasr/runtime/lfortran_intrinsics.c
@@ -1991,7 +1991,13 @@ LFORTRAN_API void _lfortran_read_int64(int64_t *p, int32_t unit_num)
 {
     if (unit_num == -1) {
         // Read from stdin
-        scanf("%lld", p);
+        scanf(
+#ifdef HAVE_LFORTRAN_MACHO
+            "%lld"
+#else
+            "%ld"
+#endif
+            , p);
         return;
     }
 
@@ -2005,7 +2011,13 @@ LFORTRAN_API void _lfortran_read_int64(int64_t *p, int32_t unit_num)
     if (unit_file_bin) {
         fread(p, sizeof(*p), 1, filep);
     } else {
-        fscanf(filep, "%lld", p);
+        fscanf(filep,
+#ifdef HAVE_LFORTRAN_MACHO
+            "%lld"
+#else
+            "%ld"
+#endif
+            , p);
     }
 }
 
