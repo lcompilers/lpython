b50013baaeb880a08cd5d45034c7379b8aa746e3
anutosh491 <andersonbhat491@gmail.com>
2023-10-08 13:59:14 +0530

    Added tests

--- integration_tests/symbolics_02.py
+++ integration_tests/symbolics_02.py
@@ -1,9 +1,11 @@
-from sympy import Symbol
+from sympy import Symbol, pi
 from lpython import S
 
 def test_symbolic_operations():
     x: S = Symbol('x')
     y: S = Symbol('y')
+    p1: S = pi
+    p2: S = pi
     
     # Addition
     z: S = x + y
@@ -36,5 +38,20 @@ def test_symbolic_operations():
     c: S = a + b
     assert(c == S(0))
     print(c)
+
+    # Comparison
+    b1: bool = p1 == p2
+    print(b1)
+    assert(b1 == True)
+    b2: bool = p1 == pi
+    print(b2)
+    assert(b2 == True)
+    b3: bool = p1 != x
+    print(b3)
+    assert(b3 == True)
+    b4: bool = pi != Symbol("x")
+    print(b4)
+    assert(b4 == True)
+
 
 test_symbolic_operations()
