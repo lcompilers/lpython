24ec97641ed412aa32529190f26f47337dc71164
Prince Roshan <princekrroshan01@gmail.com>
2023-10-08 17:32:07 +0530

    Update src/lpython/semantics/python_ast_to_asr.cpp
    Co-authored-by: Ondřej Čertík <ondrej@certik.us>

--- src/lpython/semantics/python_ast_to_asr.cpp
+++ src/lpython/semantics/python_ast_to_asr.cpp
@@ -6651,7 +6651,7 @@ public:
             /*
                 String Validation Methods i.e all "is" based functions are handled here
             */
-            std::vector<std::string> validation_methods{"lower", "upper", "decimal", "ascii","space"};  // Database of validation methods supported
+            std::vector<std::string> validation_methods{"lower", "upper", "decimal", "ascii", "space"};  // Database of validation methods supported
             std::string method_name = attr_name.substr(2);
 
             if(std::find(validation_methods.begin(),validation_methods.end(), method_name) == validation_methods.end()) {
