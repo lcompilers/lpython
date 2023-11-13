0ea42ec0b2f349e6d8790addbad4060a63357798
anutosh491 <andersonbhat491@gmail.com>
2023-11-13 11:27:10 +0530

    Added explicit test for Out[S]

--- integration_tests/symbolics_13.py
+++ integration_tests/symbolics_13.py
@@ -1,12 +1,22 @@
 from lpython import S
 from sympy import pi, Symbol
 
-def func() -> S:
+def func1() -> S:
     return pi
 
-def test_func():
-    z: S = func()
+def test_func1():
+    z: S = func1()
     print(z)
     assert z == pi
 
-test_func()
+def func2(r: Out[S]) -> None:
+    r = pi
+
+def test_func2():
+    z: S
+    func2(z)
+    print(z)
+    assert z == pi
+
+test_func1()
+test_func2()
