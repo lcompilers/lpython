033e941b80783e66c5579e7254da8f5fa7d9766c
anutosh491 <andersonbhat491@gmail.com>
2024-02-04 16:07:26 +0530

    Maded suitable changes in symbolics_15.py

--- integration_tests/symbolics_15.py
+++ integration_tests/symbolics_15.py
@@ -33,13 +33,12 @@ def mmrv(r: Out[list[CPtr]]) -> None:
     basic_new_stack(x)
     basic_const_pi(x)
 
-    # l1: list[S]
+    # l1: list[S] = [x]
+    _l1: list[CPtr] = [x]
     l1: list[CPtr] = []
 
-    # l1 = [x]
     i: i32 = 0
-    Len: i32 = 1
-    for i in range(Len):
+    for i in range(len(_l1)):
         tmp: CPtr = basic_new_heap()
         l1.append(tmp)
         basic_assign(l1[0], x)
