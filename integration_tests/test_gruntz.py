59599c4a7e20bf7877707cb49ed45c14c8626983
anutosh491 <andersonbhat491@gmail.com>
2024-02-08 17:22:02 +0530

    Added tests

--- integration_tests/test_gruntz.py
+++ integration_tests/test_gruntz.py
@@ -2,16 +2,28 @@ from lpython import S
 from sympy import Symbol
 
 def mmrv(e: S, x: S) -> list[S]:
-    l: list[S] = []
     if not e.has(x):
-        return l
+        list0: list[S] = []
+        return list0
+    elif e == x:
+        list1: list[S] = [x]
+        return list1
     else:
         raise
 
-def test_mrv1():
+def test_mrv():
+    # Case 1
     x: S = Symbol("x")
     y: S = Symbol("y")
-    ans: list[S] = mmrv(y, x)
-    assert len(ans) == 0
+    ans1: list[S] = mmrv(y, x)
+    print(ans1)
+    assert len(ans1) == 0
 
-test_mrv1()
+    # Case 2
+    ans2: list[S] = mmrv(x, x)
+    ele1: S = ans2[0]
+    print(ele1)
+    assert ele1 == x
+    assert len(ans2) == 1
+
+test_mrv()
