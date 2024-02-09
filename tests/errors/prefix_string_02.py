9a65faacf9d48e8a718373ea723c991d29bcec53
Vaibhav Pathak <mrv@pop-os.localdomain>
2024-02-09 19:59:49 +0530

    Add test for prefix strings

---
+++ tests/errors/prefix_string_02.py
@@ -0,0 +1,9 @@
+from lpython import i32
+
+# fix difference between lpython and cpython in prefix string grammar
+# Prefix should be attached to the quote with any whitespace.
+
+def main():
+	print(r "Hello World")
+
+main()
