c3b43d9f4696f3d2b155659eb6d03028372166d5
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-03-03 17:27:18 +0530

    updated references

--- integration_tests/test_str_attributes.py
+++ integration_tests/test_str_attributes.py
@@ -366,7 +366,37 @@ def is_space():
     s = ""
     assert s.isspace() == False
 
+def is_alnum():
+    a: str = "helloworld"
+    b: str = "hj kl"
+    c: str = "a12(){}A"
+    d: str = " "
+    e: str = ""
+    f: str = "ab23"
+    g: str = "ab2%3"
+    res: bool = a.isalnum()
+    res2: bool = b.isalnum()
+    res3: bool = c.isalnum()
+    res4: bool = d.isalnum()
+    res5: bool = e.isalnum()
+    res6: bool = f.isalnum()
+    res7: bool = g.isalnum()
 
+    assert res == True
+    assert res2 == False
+    assert res3 == False
+    assert res4 == False
+    assert res5 == False
+    assert res6 == True
+    assert res7 == False
+
+    assert "helloworld".isalnum() == True
+    assert "hj kl".isalnum() == False
+    assert "a12(){}A".isalnum() == False
+    assert " ".isalnum() == False
+    assert "".isalnum() == False
+    assert "ab23".isalnum() == True
+    assert "ab2%3".isalnum() == False
 
 def check():
     capitalize()
@@ -384,6 +414,7 @@ def check():
     is_decimal()
     is_ascii()
     is_alpha()
+    is_alnum()
     is_title()
     is_space()
 
