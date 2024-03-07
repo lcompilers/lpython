2fb304a7d8c3ce6612048f185188c5427c21edda
Saurabh Kumar <151380951+kmr-srbh@users.noreply.github.com>
2024-03-07 13:16:45 +0530

    Update type checking logic for `Const dict`
    Co-authored-by: Thirumalai Shaktivel <74826228+Thirumalai-Shaktivel@users.noreply.github.com>

--- src/libasr/codegen/asr_to_llvm.cpp
+++ src/libasr/codegen/asr_to_llvm.cpp
@@ -1541,9 +1541,7 @@ public:
     }
 
     void visit_DictItem(const ASR::DictItem_t& x) {
-        ASR::Dict_t *dict_type = ASR::is_a<ASR::Const_t>(*ASRUtils::expr_type(x.m_a))
-                                     ? ASR::down_cast<ASR::Dict_t>(ASRUtils::type_get_past_const(ASRUtils::expr_type(x.m_a)))
-                                     : ASR::down_cast<ASR::Dict_t>(ASRUtils::expr_type(x.m_a));
+        ASR::Dict_t *dict_type = ASR::down_cast<ASR::Dict_t>(ASRUtils::type_get_past_const(ASRUtils::expr_type(x.m_a)));
 
         int64_t ptr_loads_copy = ptr_loads;
         ptr_loads = 0;
