fdf6cb380e876e3bdbd2e96a7acb72f9425aa8ce
Shaikh Ubaid <shaikhubaid769@gmail.com>
2024-03-10 16:22:24 +0530

    Add print statements in test

--- integration_tests/test_const_list.py
+++ integration_tests/test_const_list.py
@@ -4,10 +4,14 @@ from lpython import i32, list, str, Const
 def test_const_list():
     CONST_INTEGER_LIST: Const[list[i32]] = [1, 2, 3, 4, 5, 1]
 
+    print(CONST_INTEGER_LIST.count(1))
+    print(CONST_INTEGER_LIST.index(1))
     assert CONST_INTEGER_LIST.count(1) == 2
     assert CONST_INTEGER_LIST.index(1) == 0
 
     CONST_STRING_LIST: Const[list[str]] = ["ALPHA", "BETA", "RELEASE"]
+    print(CONST_STRING_LIST.count("ALPHA"))
+    print(CONST_STRING_LIST.index("RELEASE"))
     assert CONST_STRING_LIST.count("ALPHA") == 1
     assert CONST_STRING_LIST.index("RELEASE") == 2
 
