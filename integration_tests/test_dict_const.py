99172277ea737ebc96c0707a50c1a81f5da76ea3
Saurabh Kumar <developer.saurabh@outlook.com>
2024-03-10 13:07:02 +0530

    Update error test references

--- integration_tests/test_dict_const.py
+++ integration_tests/test_dict_const.py
@@ -2,17 +2,17 @@ from lpython import i32, f64, str, dict, Const
 
 
 def test_dict_const():
-    d_int: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
+    CONST_DICTIONARY_INTEGR: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
 
-    assert d_int.get("a") == 1
-    assert d_int.keys() == ["c", "a", "b"]
-    assert d_int.values() == [3, 1, 2]
+    assert CONST_DICTIONARY_INTEGR.get("a") == 1
+    assert CONST_DICTIONARY_INTEGR.keys() == ["c", "a", "b"]
+    assert CONST_DICTIONARY_INTEGR.values() == [3, 1, 2]
 
-    d_float: Const[dict[str, f64]] = {"a": 1.0, "b": 2.0, "c": 3.0}
+    CONST_DICTIONARY_FLOAT: Const[dict[str, f64]] = {"a": 1.0, "b": 2.0, "c": 3.0}
     
-    assert d_float.get("a") == 1.0
-    assert d_float.keys() == ["c", "a", "b"]
-    assert d_float.values() == [3.0, 1.0, 2.0]
+    assert CONST_DICTIONARY_FLOAT.get("a") == 1.0
+    assert CONST_DICTIONARY_FLOAT.keys() == ["c", "a", "b"]
+    assert CONST_DICTIONARY_FLOAT.values() == [3.0, 1.0, 2.0]
 
 
 test_dict_const()
--- tests/errors/test_dict_const.py
+++ tests/errors/test_dict_const.py
@@ -2,6 +2,8 @@ from lpython import i32, f64, str, dict, Const
 
 
 def test_dict_const():
-    d: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
-    print(d.pop("a"))
-    i: i32 = d.pop("a")
+    CONST_DICTIONARY: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
+    print(CONST_DICTIONARY.pop("a"))
+    i: i32 = CONST_DICTIONARY.pop("a")
+
+test_dict_const()
