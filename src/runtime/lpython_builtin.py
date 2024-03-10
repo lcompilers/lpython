e48738e1226fde4714ccf697b9e84740df368d2d
Saurabh Kumar <developer.saurabh@outlook.com>
2024-03-10 08:05:29 +0530

    Remove redundant list

--- src/runtime/lpython_builtin.py
+++ src/runtime/lpython_builtin.py
@@ -1019,32 +1019,6 @@ def _lpython_str_isascii(s: str) -> bool:
 def _lpython_str_isspace(s: str) -> bool:
     # A Unicode character is considered a 'whitespace' if it has has a bidirectional
     # type 'WS', 'B' or 'S'; or the category 'Zs'.
-    WHITESPACE: list[str] = [
-        " ",        # SPACE
-        "\n",       # LINE FEED (LF)
-        "\r",       # CARRIAGE RETURN (CR)
-        "\t",       # CHARACTER TABULATION (HT)
-        "\v",       # VERTICAL TAB (VT)
-        "\f",       # FORM FEED (FF)
-        "\u00A0",   # NO-BREAK SPACE
-        "\u1680",   # OGHAM SPACE MARK
-        "\u2000",   # EN QUAD
-        "\u2001",   # EM QUAD
-        "\u2002",   # EN SPACE
-        "\u2003",   # EM SPACE
-        "\u2004",   # THREE-PER-EM SPACE
-        "\u2005",   # FOUR-PER-EM SPACE
-        "\u2006",   # SIX-PER-EM SPACE
-        "\u2007",   # FIGURE SPACE
-        "\u2008",   # PUNCTUATION SPACE
-        "\u2009",   # THIN SPACE
-        "\u200A",   # HAIR SPACE
-        "\u2028",   # LINE SEPARATOR
-        "\u2029",   # PARAGRAPH SEPARATOR
-        "\u202F",   # NARROW NO-BREAK SPACE
-        "\u205F",   # MEDIUM MATHEMATICAL SPACE
-        "\u3000",   # IDEOGRAPHIC SPACE
-    ]
     if len(s) == 0:
         return False
     
