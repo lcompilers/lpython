03d5998f7a5e0c65007ce3cd0d9b108018748f24
Saurabh Kumar <developer.saurabh@outlook.com>
2024-03-10 15:11:08 +0530

    Tests: Update test references

--- integration_tests/test_const_dict.py
+++ integration_tests/test_const_dict.py
@@ -1,7 +1,7 @@
 from lpython import i32, f64, str, dict, Const
 
 
-def test_dict_const():
+def test_const_dict():
     CONST_DICTIONARY_INTEGR: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
 
     assert CONST_DICTIONARY_INTEGR.get("a") == 1
@@ -9,10 +9,10 @@ def test_dict_const():
     assert CONST_DICTIONARY_INTEGR.values() == [3, 1, 2]
 
     CONST_DICTIONARY_FLOAT: Const[dict[str, f64]] = {"a": 1.0, "b": 2.0, "c": 3.0}
-    
+
     assert CONST_DICTIONARY_FLOAT.get("a") == 1.0
     assert CONST_DICTIONARY_FLOAT.keys() == ["c", "a", "b"]
     assert CONST_DICTIONARY_FLOAT.values() == [3.0, 1.0, 2.0]
 
 
-test_dict_const()
+test_const_dict()
--- tests/errors/test_const_dict.py
+++ tests/errors/test_const_dict.py
@@ -1,9 +1,9 @@
 from lpython import i32, f64, str, dict, Const
 
 
-def test_dict_const():
+def test_const_dict():
     CONST_DICTIONARY: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
     print(CONST_DICTIONARY.pop("a"))
-    i: i32 = CONST_DICTIONARY.pop("a")
 
-test_dict_const()
+
+test_const_dict()
