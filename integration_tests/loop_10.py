d8a8b19191b23700713b448ab12376fd49a2fbc2
advik <advikkabra25@gmail.com>
2024-04-04 01:14:30 +0530

    Add print statements in tests

--- integration_tests/loop_10.py
+++ integration_tests/loop_10.py
@@ -22,6 +22,7 @@ def no_break_for():
     for i in range(2):
         j += 1
     else:
+        print(j)
         assert j == 2
         return
     assert False
@@ -35,68 +36,83 @@ def break_in_if_for():
             break
     else:
         assert False
+    print(j)
     assert j == 2
 
 def nested_loop_for_for():
     i: i32
     j: i32
+    m: i32 = 0
     for i in range(2):
         for j in range(10, 20):
             break
     else:
-        return
-    assert False
+        m = 10
+    print(m)
+    assert m == 10
 
 def nested_loop_for_while():
     i: i32
     j: i32 = 10
+    m: i32 = 0
     for i in range(2):
         while j < 20:
             break
     else:
-        return
-    assert False
+        m = 10
+    print(m)
+    assert m == 10
 
 def nested_loop_while_for():
     i: i32 = 0
     j: i32
+    m: i32 = 0
     while i < 2:
         i += 1
         for j in range(10, 20):
             break
     else:
+        print(i)
         assert i == 2
-        return
-    assert False
+        m = 10
+    print(m)
+    assert m == 10
 
 def nested_loop_while_while():
     i: i32 = 0
     j: i32 = 10
+    m: i32 = 0
     while i < 2:
         i += 1
         while j < 20:
             break
     else:
+        print(i)
         assert i == 2
-        return
-    assert False
+        m = 10
+    print(m)
+    assert m == 10
 
 def nested_loop_else():
     i: i32
     j: i32
     l: i32 = 0
+    m: i32 = 0
     for i in range(2):
         l += 1
         m: i32 = 0
         for j in range(10, 12):
             m += 1
         else:
+            print(m)
             assert m == 2
             l += 10
     else:
+        print(l)
         assert l == 22
-        return
-    assert False
+        m = 10
+    print(m)
+    assert m == 10
 
 
 with_break_for()
