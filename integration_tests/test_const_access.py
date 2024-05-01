ed8662d9e87dedf1bcdbd1d791c93fdb51f600c5
Saurabh Kumar <developer.saurabh@outlook.com>
2024-05-01 19:33:20 +0530

    Tests: Add test to CMakeLists and update error references

--- integration_tests/test_const_access.py
+++ integration_tests/test_const_access.py
@@ -1,14 +1,9 @@
-from lpython import i32, str, list, dict, Const
+from lpython import i32, Const
 
+CONST_LIST: Const[list[i32]] = [1, 2, 3, 4, 5]
+CONST_DICTIONARY: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
 
-def test_const_access():
-    CONST_LIST: Const[list[i32]] = [1, 2, 3, 4, 5]
-    CONST_DICTIONARY: Const[dict[str, i32]] = {"a": 1, "b": 2, "c": 3}
+assert CONST_LIST[0] == 1
+assert CONST_LIST[-2] == 4
 
-    assert CONST_LIST[0] == 1
-    assert CONST_LIST[-2] == 4
-
-    assert CONST_DICTIONARY["a"] == 1
-
-
-test_const_access()
+assert CONST_DICTIONARY["a"] == 1
