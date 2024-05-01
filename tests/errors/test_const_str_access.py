c0407b545f5033eb54440cb946257ae8c77b1f84
Saurabh Kumar <developer.saurabh@outlook.com>
2024-05-01 21:45:15 +0530

    Tests: Remove error references

--- tests/errors/test_const_str_access.py
+++
@@ -1,8 +0,0 @@
-from lpython import str, Const
-
-
-def test_const_str_access():
-    CONST_STRING: Const[str] = "Hello, LPython!"
-
-
-test_const_str_access()
--- tests/errors/test_const_tuple_access.py
+++
@@ -1,9 +0,0 @@
-from lpython import str, tuple, Const
-
-
-def test_const_tuple_access():
-    CONST_TUPLE: Const[tuple[str, str, str]] = ("hello", "LPython", "!")
-
-
-test_const_tuple_access()
-
