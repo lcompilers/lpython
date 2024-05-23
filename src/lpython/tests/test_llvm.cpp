f40c47fb5cfbe3b25b25fc27a3b6decd5b3c4d2e
Vipul Cariappa <vipulcariappa@gmail.com>
2024-05-23 15:45:07 +0530

    Fix ASR verify pass error while using Interactive

--- src/lpython/tests/test_llvm.cpp
+++ src/lpython/tests/test_llvm.cpp
@@ -785,6 +785,23 @@ TEST_CASE("PythonCompiler i64 declaration") {
     CHECK(r.result.i64 == 14);
 }
 
+TEST_CASE("PythonCompiler 2") {
+    CompilerOptions cu;
+    cu.po.disable_main = true;
+    cu.emit_debug_line_column = false;
+    cu.generate_object_code = false;
+    cu.interactive = true;
+    cu.po.runtime_library_dir = LCompilers::LPython::get_runtime_library_dir();
+    PythonCompiler e(cu);
+    LCompilers::Result<PythonCompiler::EvalResult>
+    r = e.evaluate2("i: i32 = 3 % 1");
+    CHECK(r.ok);
+    CHECK(r.result.type == PythonCompiler::EvalResult::none);
+    r = e.evaluate2("i");
+    CHECK(r.ok);
+    CHECK(r.result.type == PythonCompiler::EvalResult::none); // TODO: change to integer4 and check the value once printing top level expressions is implemented
+}
+
 TEST_CASE("PythonCompiler u32 expressions") {
     CompilerOptions cu;
     cu.po.disable_main = true;
