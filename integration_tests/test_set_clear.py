ac29d79e493c36d3698ccd16bad503be6e95fbee
advik <advikkabra25@gmail.com>
2024-06-25 17:57:42 +0530

    Update valid tests

--- integration_tests/test_set_clear.py
+++ integration_tests/test_set_clear.py
@@ -2,18 +2,18 @@ def test_clear():
     a: set[i32] = {1, 2}
 
     a.clear()
-    a.add(3)
+    # a.add(3)
 
-    assert len(a) == 1
+    assert len(a) == 0
     # a.remove(3)
     # assert len(a) == 0
 
     b: set[str] = {'a', 'b'}
 
     b.clear()
-    b.add('c')
+    # b.add('c')
 
-    assert len(b) == 1
+    assert len(b) == 0
     # b.remove('c')
     # assert len(b) == 0
 
