0f16696637eac048ef7c23ac0c7f879add4cd39e
advik <advikkabra25@gmail.com>
2024-06-26 00:02:59 +0530

    Fix bugs and refactor code

--- integration_tests/test_set_clear.py
+++ integration_tests/test_set_clear.py
@@ -2,20 +2,20 @@ def test_clear():
     a: set[i32] = {1, 2}
 
     a.clear()
-    # a.add(3)
+    a.add(3)
 
+    assert len(a) == 1
+    a.remove(3)
     assert len(a) == 0
-    # a.remove(3)
-    # assert len(a) == 0
 
     b: set[str] = {'a', 'b'}
 
     b.clear()
-    # b.add('c')
+    b.add('c')
 
+    assert len(b) == 1
+    b.remove('c')
     assert len(b) == 0
-    # b.remove('c')
-    # assert len(b) == 0
 
 
 test_clear()
