ffb11345ae6cc148e1971d83b54b94cdc22529fb
advik <advikkabra25@gmail.com>
2024-07-01 17:14:54 +0530

    Add tests

---
+++ integration_tests/test_attributes.py
@@ -0,0 +1,12 @@
+def test_attributes() -> None:
+    a: i32 = 10
+    assert a.bit_length() == 4
+
+    b: str = 'abc'
+    assert b.upper() == 'ABC'
+
+    c: list[i32] = [10, 20, 30]
+    assert c[0].bit_length() == 4
+    assert c.pop() == 30
+
+test_attributes()
