eebf8d63119ac6003dae5c1254b8c592e44ec339
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-07-25 12:55:50 +0200

    more improvements

--- build1.sh
+++ build1.sh
@@ -3,20 +3,36 @@
 set -e
 set -x
 
-mkdir -p ./build
-cd build
+SRCDIR=`pwd`
+CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH_LPYTHON;$CONDA_PREFIX"
 
-cmake .. \
+# if we are in a pixi project, use the pixi "host" environment for the dependencies
+if [[ ! -z $PIXI_PROJECT_ROOT ]]; then
+   CMAKE_PREFIX_PATH=$PIXI_PROJECT_ROOT/.pixi/envs/host/
+   OUTOFTREE=false
+
+   if [[ ! -f src/libasr/asr.h ]]; then
+       pixi run build_0
+   fi
+fi
+
+# if the outoftree env var is set to true, create a build directory
+if [[ "$OUTOFTREE" == "true" ]]; then
+    mkdir -p build
+    cd build
+fi
+
+
+cmake $SRCDIR \
     -GNinja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
     -DLPYTHON_BUILD_ALL=yes \
     -DWITH_STACKTRACE=yes \
     -DWITH_RUNTIME_STACKTRACE=yes \
-    -DWITH_INTRINSIC_MODULES=yes \
     -DWITH_LSP=no \
     -DWITH_LFORTRAN_BINARY_MODFILES=no \
-    -DCMAKE_PREFIX_PATH="$PIXI_PROJECT_ROOT/.pixi/envs/host/" \
-    -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
+    -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/inst
 
-cmake --build . --target install
+cmake --build . -j --target install
