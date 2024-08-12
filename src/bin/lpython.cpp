839f6b9240c1ac05ed056f9bb0e7cb7848565a58
Vipul Cariappa <vipulcariappa@gmail.com>
2024-08-12 19:41:34 +0530

    fix related to previous commit

--- src/bin/lpython.cpp
+++ src/bin/lpython.cpp
@@ -1535,7 +1535,7 @@ int link_executable(const std::vector<std::string> &infiles,
                 cmd += " -L$CONDA_PREFIX/lib -Wl,-rpath -Wl,$CONDA_PREFIX/lib -lsymengine";
             }
 
-            if (compiler_options.enable_cpython) {
+            if (compiler_options.po.enable_cpython) {
                 std::string py_version = "3.10";
                 std::string py_flags = R"(-I $CONDA_PREFIX/include/python)" + py_version + R"( -L$CONDA_PREFIX/lib -Wl,-rpath -Wl,$CONDA_PREFIX/lib -lpython)" + py_version + R"()";
                 if (compiler_options.link_numpy) {
@@ -1594,7 +1594,7 @@ int link_executable(const std::vector<std::string> &infiles,
         if (compiler_options.enable_symengine) {
             cmd += " -L$CONDA_PREFIX/lib -Wl,-rpath -Wl,$CONDA_PREFIX/lib -lsymengine";
         }
-        if (compiler_options.enable_cpython) {
+        if (compiler_options.po.enable_cpython) {
             std::string py_version = "3.10";
             std::string py_flags = R"(-I $CONDA_PREFIX/include/python)" + py_version + R"( -L$CONDA_PREFIX/lib -Wl,-rpath -Wl,$CONDA_PREFIX/lib -lpython)" + py_version + R"()";
             if (compiler_options.link_numpy) {
