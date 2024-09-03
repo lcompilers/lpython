728fb21919f54d269dfbf13df6004f99d7f2c14c
Ayush Agarwal <136371195+Ayush-Devs@users.noreply.github.com>
2024-09-04 04:54:00 +0530

    better argument names for StringContains (#2806)

--- src/libasr/codegen/asr_to_c_cpp.h
+++ src/libasr/codegen/asr_to_c_cpp.h
@@ -1246,9 +1246,9 @@ PyMODINIT_FUNC PyInit_lpython_module_)" + fn_name + R"((void) {
 
     void visit_StringContains(const ASR::StringContains_t &x) {
         CHECK_FAST_C_CPP(compiler_options, x)
-        self().visit_expr(*x.m_left);
+        self().visit_expr(*x.m_substr);
         std::string substr = src;
-        self().visit_expr(*x.m_right);
+        self().visit_expr(*x.m_str);
         std::string str = src;
         src = "_lfortran_str_contains(" + str + ", " + substr + ")";
     }
