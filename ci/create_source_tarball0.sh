ade48591e676297958bb397a1f234bf6a0306f73
Ubaid Shaikh <shaikhubaid769@gmail.com>
2025-03-30 09:12:31 +0530

    CI: Support lfortran submodule

--- ci/create_source_tarball0.sh
+++ ci/create_source_tarball0.sh
@@ -9,6 +9,7 @@ cmake -E copy_directory src $dest/src
 cmake -E copy_directory share $dest/share
 cmake -E copy_directory cmake $dest/cmake
 cmake -E copy_directory examples $dest/examples
+cmake -E copy_directory lfortran/src/libasr $dest/lfortran/src/libasr
 
 # Copy Files:
 cmake -E copy CMakeLists.txt README.md LICENSE lp_version $dest
